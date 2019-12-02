f6340b60abad1add4fbfc81f1c4067bd6fb4fc85
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-02 20:32:52 +0200

    Aidinov Alexii

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+const wall = document.body.querySelector('.wall');
+const wallLeftPos = wall.clientLeft + wall.offsetLeft;
+const wallTopPos = wall.clientTop + wall.offsetTop;
+const spider = document.body.querySelector('.spider');
+const spiderStyles = getComputedStyle(spider);
+const spiderSize = parseFloat(spiderStyles.width);
+
+const moveSpider = (click) => {
+  const coordinates = click.target;
+
+  if (!coordinates) {
+    return;
+  }
+
+  const gorSpiderMove = click.clientX - wallLeftPos - spiderSize / 2;
+  const vertSpiderMove = click.clientY - wallTopPos - spiderSize / 2;
+
+  spider.style.left = `${gorSpiderMove}px`;
+  spider.style.top = `${vertSpiderMove}px`;
+
+  if (click.clientX < wallLeftPos + spiderSize) {
+    spider.style.left = `0`;
+  }
+
+  if (click.clientX > wallLeftPos + wall.clientWidth - spiderSize) {
+    spider.style.left = `${wall.clientWidth - spiderSize}px`;
+  }
+
+  if (click.clientY < wallTopPos + spiderSize) {
+    spider.style.top = `0`;
+  }
+
+  if (click.clientY > wallTopPos + wall.clientHeight - spiderSize) {
+    spider.style.top = `${wall.clientHeight - spiderSize}px`;
+  }
+};
+
+wall.addEventListener('click', moveSpider);
