1c196e9fbe0e8c91a9ca812c14e70b099c590598
ksukhaldun <kseniya.khaldun@gmail.com>
2019-12-02 18:47:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,37 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+function moveSpider(wall) {
+  const spider = wall.querySelector('.spider');
+
+  wall.addEventListener('click', (e) => {
+    const wallPosition = wall.getBoundingClientRect();
+    const posX = wallPosition.x + wall.clientLeft;
+    const posY = wallPosition.y + wall.clientTop;
+
+    let x = e.clientX - posX - spider.offsetWidth / 2;
+    let y = e.clientY - posY - spider.offsetHeight / 2;
+
+    const maxX = wall.clientWidth - spider.offsetWidth;
+    const maxY = wall.clientHeight - spider.offsetHeight;
+
+    if (x < 0) {
+      x = 0;
+    }
+
+    if (x > maxX) {
+      x = maxX;
+    }
+
+    if (y < 0) {
+      y = 0;
+    }
+
+    if (y > maxY) {
+      y = maxY;
+    }
+
+    spider.style.left = x + 'px';
+    spider.style.top = y + 'px';
+  });
+}
+moveSpider(document.querySelector('.wall'));
