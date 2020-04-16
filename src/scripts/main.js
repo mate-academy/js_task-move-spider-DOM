366d3ec555a079ce1c9bbe73d3da97cb3a5a4f96
Tata2222 <tataolcha@gmail.com>
2020-04-16 17:15:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,43 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+const gameField = document.querySelector('.wall');
+
+function initGame(wall) {
+  const spider = wall.querySelector('.spider');
+  let maxX = wall.clientWidth - spider.offsetWidth;
+  let maxY = wall.clientHeight - spider.offsetHeight;
+
+  spider.addEventListener('load', () => {
+    maxX = wall.clientWidth - spider.offsetWidth;
+    maxY = wall.clientHeight - spider.offsetHeight;
+  });
+
+  wall.addEventListener('click', e => {
+    const wallPosition = wall.getBoundingClientRect();
+    const wallX = wallPosition.left + wall.clientLeft;
+    const wallY = wallPosition.top + wall.clientTop;
+
+    let x = event.clientX - wallX - spider.offsetWidth / 2;
+    let y = event.clientY - wallY - spider.offsetHeight / 2;
+
+    if (x < 0) {
+      x = 0;
+    }
+
+    if (x > maxX) {
+      x = maxX;
+    }
+
+    if (y < 0) {
+      y = 0;
+    }
+
+    if (y > maxY) {
+      y = maxY;
+    }
+    spider.style.top = `${y}px`;
+    spider.style.left = `${x}px`;
+  });
+}
+
+initGame(gameField);
