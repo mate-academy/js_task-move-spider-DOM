23bdcffda17b483230fc497b1b52769de134e119
Vitalii Korol <vitaliikorol@github.com>
2020-04-16 16:06:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+function moveSpider(wall) {
+  const spider = wall.querySelector('.spider');
+
+  wall.addEventListener('click', (mouse) => {
+    const wallPosition = wall.getBoundingClientRect();
+    // console.log(wallPosition);
+    const wallY = wallPosition.top + wall.clientTop;
+    const wallX = wallPosition.left + wall.clientLeft;
+    const y = mouse.clientY - wallY - spider.offsetWidth / 2;
+    const x = mouse.clientX - wallX - spider.offsetHeight / 2;
+    const maxY = wall.clientWidth - spider.offsetWidth;
+    const maxX = wall.clientHeight - spider.offsetHeight;
+
+    spider.style.top = y < 0 ? 0 : y > maxY ? `${maxY}px` : `${y}px`;
+    spider.style.left = x < 0 ? 0 : x > maxX ? `${maxX}px` : `${x}px`;
+  });
+};
+
+moveSpider(
+  document.querySelector('.wall')
+);
