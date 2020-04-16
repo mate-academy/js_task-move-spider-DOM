c1a167272420c2e1244d2886de4c39a67641caeb
Andrey Faychak <faychak333@gmail.com>
2020-04-16 15:37:27 +0300

    'solution'

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', moveSpider);
+
+function moveSpider() {
+  const wallCoords = event.target.getBoundingClientRect();
+  const spiderCoords = {
+    top: event.clientY - wallCoords.top
+      - wall.clientTop - spider.clientHeight / 2,
+    left: event.clientX - wallCoords.left
+      - wall.clientLeft - spider.clientWidth / 2,
+  };
+
+  if (spiderCoords.top < 0) {
+    spiderCoords.top = 0;
+  }
+
+  if (spiderCoords.left < 0) {
+    spiderCoords.left = 0;
+  }
+
+  if (spiderCoords.left + spider.clientWidth > wall.clientWidth) {
+    spiderCoords.left = wall.clientWidth - spider.clientWidth;
+  }
+
+  if (spiderCoords.top + spider.clientHeight > wall.clientHeight) {
+    spiderCoords.top = wall.clientHeight - spider.clientHeight;
+  }
+
+  spider.style.left = spiderCoords.left + 'px';
+  spider.style.top = spiderCoords.top + 'px';
+};
