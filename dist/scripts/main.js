7616faf6509dc5977e130960e4317a413fbb2aed
crash2bash <sachagurov@gmail.com>
2020-04-16 19:51:47 +0300

    solurion

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,5 +1,41 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', e => {
+  e.preventDefault();
+
+  moveSpider(e);
 });
+
+function moveSpider(evt) {
+  const wallPosition = wall.getBoundingClientRect();
+  const wallX = wallPosition.left + wall.clientLeft;
+  const wallY = wallPosition.top + wall.clientTop;
+
+  let x = evt.clientX - wallX - spider.offsetWidth / 2;
+  let y = evt.clientY - wallY - spider.offsetHeight / 2;
+
+  const maxX = wall.clientWidth - spider.offsetWidth;
+  const maxY = wall.clientHeight - spider.offsetHeight;
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (x > maxX) {
+    x = maxX;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (y > maxY) {
+    y = maxY;
+  }
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
+}
