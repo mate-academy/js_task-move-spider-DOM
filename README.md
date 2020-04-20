c210cee0a7547701bcb75e410738c0542420968d
Igor Kulish <igormagnetic@gmail.com>
2020-04-20 22:17:58 +0300

    sol

--- README.md
+++ README.md
@@ -28,4 +28,4 @@ click coordinates.
 
 **Result**
 
-- [UPDATE DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+- [UPDATE DEMO LINK](https://IgorKulish.github.io/deploy/)
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,5 +1,39 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+function initSpider(wall) {
+  const spider = wall.querySelector('.spider');
+
+  wall.addEventListener('click', (e) => {
+    const wallPosition = wall.getBoundingClientRect();
+    const wallX = wallPosition.x + wall.clientLeft;
+    const wallY = wallPosition.y + wall.clientTop;
+
+    let x = e.clientX - wallX - spider.offsetHeight / 2;
+    let y = e.clientY - wallY - spider.offsetWidth / 2;
+    const maxX = wall.clientHeight - spider.offsetHeight;
+    const maxY = wall.clientWidth - spider.offsetWidth;
+
+    if (x < 0) {
+      x = 0;
+    }
+
+    if (x > maxX) {
+      x = maxX;
+    }
+
+    if (y < 0) {
+      y = 0;
+    }
+
+    if (y > maxY) {
+      y = maxY;
+    }
+
+    spider.style.top = `${y}px`;
+    spider.style.left = `${x}px`;
+  });
+};
+
+initSpider(
+  document.querySelector('.wall')
+);
