c89a0b8498e9510fa92016dd80f252556eaf240a
euxos <eugen.ostrozhinsky@gmail.com>
2020-04-21 12:00:28 +0300

    solution

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -8,27 +8,27 @@ function spiderMove(area) {
       return;
     }
 
-    let x = e.offsetX - spider.offsetWidth / 2;
-    let y = e.offsetY - spider.offsetHeight / 2;
+    let a = e.offsetX - spider.offsetWidth / 2;
+    let b = e.offsetY - spider.offsetHeight / 2;
 
-    if (x < 0) {
-      x = 0;
+    if (a < 0) {
+      a = 0;
     }
 
-    if (y < 0) {
-      y = 0;
+    if (b < 0) {
+      b = 0;
     }
 
-    if (x >= area.clientWidth - spider.offsetWidth) {
-      x = area.clientWidth - spider.offsetWidth;
+    if (a >= area.clientWidth - spider.offsetWidth) {
+      a = area.clientWidth - spider.offsetWidth;
     }
 
-    if (y >= area.clientHeight - spider.offsetHeight) {
-      y = area.clientHeight - spider.offsetHeight;
+    if (b >= area.clientHeight - spider.offsetHeight) {
+      b = area.clientHeight - spider.offsetHeight;
     }
 
-    spider.style.left = `${x}px`;
-    spider.style.top = `${y}px`;
+    spider.style.left = `${a}px`;
+    spider.style.top = `${b}px`;
   });
 }
 
