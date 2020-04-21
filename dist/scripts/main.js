0833ea52783a48de390d2bc709dd28aa378d9b0c
Vlad <oneviddi@gmail.com>
2020-04-21 17:12:41 +0300

    solution

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,5 +1,37 @@
+/* eslint-disable no-useless-return */
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target !== wall && e.target !== spider) {
+    return;
+  }
+
+  const wallPosition = wall.getBoundingClientRect();
+  const wallX = wallPosition.x + wall.clientLeft;
+  const wallY = wallPosition.y + wall.clientTop;
+
+  let x = e.clientX - wallX - spider.offsetWidth / 2;
+  let y = e.clientY - wallY - spider.offsetHeight / 2;
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (x > wall.clientWidth - spider.offsetWidth) {
+    x = wall.clientWidth - spider.offsetWidth;
+  }
+
+  if (y > wall.clientHeight - spider.offsetHeight) {
+    y = wall.clientHeight - spider.offsetHeight;
+  }
+
+  spider.style.top = y + 'px';
+  spider.style.left = x + 'px';
 });
