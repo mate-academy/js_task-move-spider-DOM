0beb8152ec05261cd2f2511118e55e7981d5220d
Liliia <chuiko1112@gmail.com>
2020-06-16 18:21:37 +0300

    solution

--- README.md
+++ README.md
@@ -28,4 +28,4 @@ click coordinates.
 
 **Result**
 
-- [UPDATE DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+- [UPDATE DEMO LINK](https://liliya-dev.github.io/js_task-move-spider-DOM/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.closest('.wall') === null) {
+    return;
+  }
+
+  const wall = document.querySelector('.wall');
+  const border = parseInt(window.getComputedStyle(wall, null).borderWidth) * 2;
+  const spider = document.querySelector('.spider');
+  const position = wall.getBoundingClientRect();
+  let x = e.clientX - position.x - e.target.clientLeft - spider.offsetWidth / 2;
+  let y = e.clientY - position.y - e.target.clientTop - spider.offsetHeight / 2;
+  const maxX = wall.offsetWidth - spider.offsetWidth - border;
+  const maxY = wall.offsetHeight - spider.offsetHeight - border;
+
+  if (x > maxX) {
+    x = maxX;
+  } else if (x < 0) {
+    x = 0;
+  }
+
+  if (y > maxY) {
+    y = maxY;
+  } else if (y < 0) {
+    y = 0;
+  }
+
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
 });
