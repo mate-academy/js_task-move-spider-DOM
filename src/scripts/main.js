4dd3e1acae14519abb175bd7df7ea8a9e67630a7
Denys <gryshko_denys@gmail.com>
2020-06-18 12:28:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const spiderPosition = spider.getBoundingClientRect();
+const spiderX = spiderPosition.x;
+const spiderY = spiderPosition.y;
+const spiderWidth = spiderPosition.width;
+const spiderHeight = spiderPosition.height;
+
 document.addEventListener('click', e => {
-  // write code here
+  const target = e.target.closest('div');
+
+  if (target) {
+    const differenceX = e.clientX - spiderX - spiderWidth / 2;
+    const differenceY = e.clientY - spiderY - spiderHeight / 2;
+
+    differenceX >= 0
+      ? spider.style.left = `${differenceX}px`
+      : spider.style.right = `-${differenceX}px`;
+
+    differenceY >= 0
+      ? spider.style.top = `${differenceY}px`
+      : spider.style.bottom = `-${differenceY}px`;
+  }
 });
