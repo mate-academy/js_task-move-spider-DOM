2f62de2581ee14b5bd51d135daf71ebe4c2107cc
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-18 09:17:42 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+  const position = wall.getBoundingClientRect();
+
+  const maxX = wall.clientWidth - spider.offsetWidth;
+  const maxY = wall.clientHeight - spider.offsetHeight;
+
+  let x = e.clientX - (position.x + wall.clientLeft) - spider.offsetWidth / 2;
+  let y = e.clientY - (position.y + wall.clientTop) - spider.offsetWidth / 2;
+
+  if (!e.target.closest('.wall')) {
+    return;
+  };
+
+  x = x < 0 ? 0 : x;
+  x = x > maxX ? maxX : x;
+
+  y = y < 0 ? 0 : y;
+  y = y > maxY ? maxY : y;
+
+  spider.style.transform = `translate(${x}px, ${y}px)`;
 });
