fcd4fcc49aef8cbc93cfa9d5f0d69ae2cb3ffdc3
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-07-17 18:10:46 +0300

    Review fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,9 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  const spider = document.querySelector('.spider');
-  const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
 
-  if (e.target !== wall) {
-    return;
-  }
-
+wall.addEventListener('click', (e) => {
   const wallCoords = wall.getBoundingClientRect();
   const wallX = wallCoords.x + wall.clientLeft;
   const wallY = wallCoords.y + wall.clientLeft;
@@ -16,21 +12,11 @@ document.addEventListener('click', (e) => {
   let x = e.clientX - wallX - spider.offsetWidth / 2;
   let y = e.clientY - wallY - spider.offsetWidth / 2;
 
-  if (x < 0) {
-    x = 0;
-  }
+  x = Math.max(x, 0);
+  x = Math.min(x, maxX);
 
-  if (y < 0) {
-    y = 0;
-  }
-
-  if (x > maxX) {
-    x = maxX;
-  }
-
-  if (y > maxY) {
-    y = maxY;
-  }
+  y = Math.max(y, 0);
+  y = Math.min(y, maxY);
 
   spider.style.top = `${y}px`;
   spider.style.left = `${x}px`;
