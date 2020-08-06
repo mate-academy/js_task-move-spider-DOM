b7d2615f7796476fcce85e6f4525930db7ebde17
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-08-06 23:19:04 +0300

    Fixed Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,62 +3,45 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-document.addEventListener('click', e => {
+wall.addEventListener('click', e => {
   // write code here
-  const item = e.target.closest('.wall');
-  let left = 0;
-  let top = 0;
-
-  if (!item) {
-    return;
-  }
-
-  const wallEdge = checkEdgesWall(e);
-
-  if (wallEdge.xPosition === 0) {
-    left = e.clientX - spider.x - (spider.offsetWidth / 2);
-  } else {
-    left = wallEdge.xPosition - spider.x;
-  }
-
-  if (wallEdge.yPosition === 0) {
-    top = e.clientY - spider.y - (spider.offsetHeight / 2);
-  } else {
-    top = wallEdge.yPosition - spider.y;
-  }
+  const move = checkEdgesWall(e);
 
   if (spider.style.left === '') {
     spider.style.left = `${0}px`;
     spider.style.top = `${0}px`;
   }
 
-  spider.style.left = `${parseFloat(spider.style.left) + left}px`;
-  spider.style.top = `${parseFloat(spider.style.top) + top}px`;
+  spider.style.left = `${parseFloat(spider.style.left) + move.xPosition}px`;
+  spider.style.top = `${parseFloat(spider.style.top) + move.yPosition}px`;
 });
 
 function checkEdgesWall(event) {
-  const edgeLeft = wall.offsetLeft + wall.clientLeft;
-  const edgeTop = wall.offsetTop + wall.clientTop;
-  const edgeRight = edgeLeft - spider.offsetWidth + wall.clientWidth;
-  const edgeDown = edgeTop - spider.offsetHeight + wall.clientHeight;
-
   let positionX = 0;
   let positionY = 0;
 
-  if (event.clientX <= edgeLeft + (spider.offsetWidth / 2)) {
-    positionX = edgeLeft;
+  if (event.clientX <= wall.offsetLeft + spider.offsetWidth) {
+    positionX = wall.offsetLeft + wall.clientLeft - spider.x;
+  } else {
+    if (event.clientX >= wall.offsetLeft - spider.offsetWidth
+      + wall.offsetWidth) {
+      positionX = wall.offsetLeft + wall.offsetWidth
+        - wall.clientLeft - spider.offsetWidth - spider.x;
+    } else {
+      positionX = event.clientX - spider.x - (spider.offsetWidth / 2);
+    }
   }
 
-  if (event.clientX >= edgeRight + (spider.offsetWidth / 2)) {
-    positionX = edgeRight;
-  }
-
-  if (event.clientY <= edgeTop + (spider.offsetHeight / 2)) {
-    positionY = edgeTop;
-  }
-
-  if (event.clientY >= edgeDown + (spider.offsetHeight / 2)) {
-    positionY = edgeDown;
+  if (event.clientY <= wall.offsetTop + spider.offsetHeight) {
+    positionY = wall.offsetTop + wall.clientTop - spider.y;
+  } else {
+    if (event.clientY >= wall.offsetTop - spider.offsetHeight
+      + wall.offsetHeight) {
+      positionY = wall.offsetTop + wall.offsetHeight
+      - wall.clientTop - spider.offsetHeight - spider.y;
+    } else {
+      positionY = event.clientY - spider.y - (spider.offsetHeight / 2);
+    }
   }
 
   return {
