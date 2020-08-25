4a4bd6c0cc22f4840120a73e1abdcb2133102301
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-25 16:32:23 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,22 @@
 'use strict';
 
-document.querySelector('.wall').addEventListener('click', e => {
-  document.querySelector('.spider').style.cssText
-  = `top: ${e.offsetY}px; left: ${e.offsetX}px`;
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallSize = parseInt(window.getComputedStyle(wall).width);
+const spiderSize = parseInt(window.getComputedStyle(spider).width);
+const border = wallSize - (spiderSize / 2);
+
+wall.addEventListener('click', e => {
+  const x = e.offsetX > border
+    ? border
+    : e.offsetX < spiderSize / 2
+      ? spiderSize / 2 : e.offsetX;
+  const y = e.offsetY > border
+    ? border
+    : e.offsetY < spiderSize / 2
+      ? spiderSize / 2 : e.offsetY;
+
+  spider.style.cssText = `top: ${y}px; left: ${x}px;
+  transform: translate(-50%, -50%);
+  pointer-events: none;`;
 });
