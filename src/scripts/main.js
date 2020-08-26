5cec2cf14cc4800a19a570f3c7d4a1a2be75169b
Mikle25 <mikle2593@gmail.com>
2020-08-26 13:20:28 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target.closest('.wall')) {
+    const spiderX = e.offsetX - spider.offsetWidth / 2;
+    const spiderY = e.offsetY - spider.offsetHeight / 2;
+
+    spider.style.left = positionSpider(spiderX) + 'px';
+    spider.style.top = positionSpider(spiderY) + 'px';
+  }
+
+  function positionSpider(SpiderCoords) {
+    if (SpiderCoords < 0) {
+      return 0;
+    } else if (SpiderCoords > (wall.clientHeight - spider.offsetHeight)) {
+      return wall.clientHeight - spider.offsetHeight;
+    }
+
+    return SpiderCoords;
+  }
 });
