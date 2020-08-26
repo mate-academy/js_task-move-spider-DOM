bf9fc6897725215460f7b22e7b074996457c0839
Anton <a.v.sorokin95@gmail.com>
2020-08-26 16:52:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (event) => {
+  const top = event.pageY - (window.innerHeight / 2) + 175;
+  const left = event.pageX - (window.innerWidth / 2) + 175;
+
+  spider.style.top = (top < 0) ? '0px' : (top > 350) ? '350px' : `${top}px`;
+  spider.style.left = left < 0 ? '0px' : (left > 350) ? '350px' : `${left}px`;
 });
