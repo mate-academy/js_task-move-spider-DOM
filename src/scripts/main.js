fabc947d31cc95736423bc24a7e5715a9159bd3f
Elena-brv <leena.brv@gmail.com>
2020-08-26 14:12:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,43 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  if (!e.target.classList.contains('wall')) {
+    return;
+  }
+
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const spiderStyle = window.getComputedStyle(spider);
+  const spiderWidth = parseInt(spiderStyle.width);
+  const spiderHeight = parseInt(spiderStyle.height);
+
+  const wallStyle = window.getComputedStyle(wall);
+  const wallWidth = parseInt(wallStyle.width);
+  const wallHeight = parseInt(wallStyle.height);
+
+  let transformX;
+  let transformY;
+
+  if (e.offsetX > (wallWidth - spiderWidth)) {
+    transformX = wallWidth - spiderWidth;
+  } else if (e.offsetX < spiderWidth / 2) {
+    transformX = 0;
+  } else {
+    transformX = e.offsetX - spiderWidth / 2;
+  }
+
+  if (e.offsetY > (wallHeight - spiderHeight)) {
+    transformY = wallHeight - spiderHeight;
+  } else if (e.offsetY < spiderHeight / 2) {
+    transformY = 0;
+  } else {
+    transformY = e.offsetY - spiderHeight / 2;
+  }
+
+  spider.style.transform = `
+    translate(
+      ${transformX}px,
+      ${transformY}px
+  `;
 });
