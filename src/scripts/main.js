3690671f2139b0e3f5ad207c07cd0f35518a7e41
DmJSCourse <dimonua@gmail.com>
2020-08-26 16:36:05 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', event => {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+
+  const point = {
+    x: event.offsetX - (spider.width / 2),
+    y: event.offsetY - (spider.width / 2),
+  };
+
+  if (wall.clientWidth - event.offsetX < spider.width) {
+    point.x = wall.clientWidth - spider.width;
+  }
+
+  if (spider.width > event.offsetX) {
+    point.x = 0;
+  }
+
+  if (wall.clientHeight - event.offsetY < spider.height) {
+    point.y = wall.clientHeight - spider.height;
+  }
+
+  if (spider.height > event.offsetY) {
+    point.y = 0;
+  }
+
+  spider.style.left = `${point.x}px`;
+  spider.style.top = `${point.y}px`;
 });
