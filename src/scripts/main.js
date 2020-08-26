4a7536e311ce22187ce9816b69bb9f9daf1d9b1d
sobchuksasha <sobchuksasha1995@gmail.com>
2020-08-27 01:08:18 +0300

    version1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+document.addEventListener('click', (event) => {
+  if (event.target !== wall) {
+    return;
+  }
+
+  let spiderLeft = event.offsetX - spider.offsetWidth / 2;
+  let spiderRight = event.offsetY - spider.offsetHeight / 2;
+
+  function isCorrect(direction, spiderSize, wallSize) {
+    let x = direction;
+
+    if (x < 0) {
+      x = 0;
+    } else if (x > wallSize - spiderSize) {
+      x = wallSize - spiderSize;
+    };
+
+    return x;
+  }
+
+  spiderLeft = isCorrect(spiderLeft, spider.offsetWidth, wall.clientWidth);
+  spiderRight = isCorrect(spiderRight, spider.offsetHeight, wall.clientHeight);
+
+  spider.style.left = `${spiderLeft}px`;
+  spider.style.top = `${spiderRight}px`;
 });
