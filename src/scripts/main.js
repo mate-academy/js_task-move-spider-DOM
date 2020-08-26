3e47cd76920d86c8ea42da506630d74528fcc29e
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-26 11:29:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,44 @@
+/* eslint-disable no-undef */
+// they sad "getComputedStyle is not undefined"
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const spiderWidth = parseFloat(getComputedStyle(spider).width);
+const spiderHeight = parseFloat(getComputedStyle(spider).height);
+
+const wall = document.querySelector('.wall');
+
+spider.style.transitionTimingFunction = 'linear';
+
+wall.addEventListener('mousemove', event => {
+  spider.style.left = `${getRightX(event.offsetX)}px`;
+  spider.style.top = `${getRightY(event.offsetY)}px`;
 });
+
+function getRightX(eventX) {
+  let x = eventX;
+
+  if (x < 0 + spiderWidth / 2) {
+    x = spiderWidth / 2;
+  }
+
+  if (x > wall.clientWidth - spiderWidth) {
+    x = wall.clientWidth - spiderWidth / 2;
+  }
+
+  return x - spiderWidth / 2;
+}
+
+function getRightY(eventY) {
+  let y = eventY;
+
+  if (y < 0 + spiderHeight / 2) {
+    y = spiderHeight / 2;
+  }
+
+  if (y > wall.clientHeight - spiderHeight) {
+    y = wall.clientHeight - spiderHeight / 2;
+  }
+
+  return y - spiderHeight / 2;
+}
