3242554385fd9ca92d058515967fba0ef7eb4126
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-26 15:49:29 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,18 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 const wallSize = parseInt(window.getComputedStyle(wall).width);
 const spiderSize = parseInt(window.getComputedStyle(spider).width);
-const border = wallSize - (spiderSize / 2);
+const maxCoord = wallSize - (spiderSize / 2);
 
 wall.addEventListener('click', e => {
-  const x = e.offsetX > border
-    ? border
-    : e.offsetX < spiderSize / 2
-      ? spiderSize / 2 : e.offsetX;
-  const y = e.offsetY > border
-    ? border
-    : e.offsetY < spiderSize / 2
-      ? spiderSize / 2 : e.offsetY;
+  function getDistance(coord) {
+    return coord > maxCoord
+    ? maxCoord
+    : coord < spiderSize / 2
+      ? spiderSize / 2 : coord;
+  }
+
+  const x = getDistance(e.offsetX);
+  const y = getDistance(e.offsetY);
 
   spider.style.cssText = `top: ${y}px; left: ${x}px;
   transform: translate(-50%, -50%);
