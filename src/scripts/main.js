ac4668b183ab76512ea241eae510a37eac0049cc
unknown <pavlo.com.5@gmail.com>
2020-08-26 19:00:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const spiderSize = window.getComputedStyle(spider).width.slice(0, -2);
+const wall = document.querySelector('.wall');
+const wallSize = window.getComputedStyle(wall).width.slice(0, -2);
+
+document.addEventListener('click', event => {
+  if (event.target !== wall) {
+    return;
+  }
+
+  const maxValue = wallSize - spiderSize;
+
+  if (event.offsetX >= maxValue) {
+    spider.style.left = `${maxValue}px`;
+  } else if (event.offsetX <= spiderSize) {
+    spider.style.left = `0px`;
+  } else {
+    spider.style.left = `${event.offsetX - spiderSize / 2}px`;
+  }
+
+  if (event.offsetY >= maxValue) {
+    spider.style.top = `${maxValue}px`;
+  } else if (event.offsetY <= spiderSize) {
+    spider.style.top = `0px`;
+  } else {
+    spider.style.top = `${event.offsetY - spiderSize / 2}px`;
+  }
 });
