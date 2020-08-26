3bdd7eff3ea94e9178c7ae288e1fe914c6002293
JuliaNastenko <juli.nastenko@gmail.com>
2020-08-26 17:50:45 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
-document.addEventListener('click', e => {
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderSize = spider.offsetWidth;
+const wallSize = wall.clientWidth;
+
+document.addEventListener('click', (event) => {
   // write code here
+  const item = event.target.closest('.wall');
+
+  if (!item || !document.contains(item)) {
+    return;
+  };
+
+  const coordY = event.offsetY - spider.offsetHeight / 2;
+  const coordX = event.offsetX - spider.offsetWidth / 2;
+
+  function getCoords(coords) {
+    if (coords > wallSize - spiderSize) {
+      return wallSize - spiderSize;
+    }
+
+    if (coords < 0) {
+      return 0;
+    }
+
+    return coords;
+  };
+
+  spider.style.cssText = `
+    top: ${getCoords(coordY)}px;
+    left: ${getCoords(coordX)}px;
+  `;
 });
