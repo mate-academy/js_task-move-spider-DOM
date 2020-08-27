9ef836ab59408852cad4fcce4139d8ae1c4ffe16
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-08-27 16:51:05 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  };
+
+  let positionX = e.offsetX - spider.offsetWidth / 2;
+  let positionY = e.offsetY - spider.offsetHeight / 2;
+
+  function correctionPosition(coord, spiderSize, wallSize) {
+    let resultCoord = coord;
+
+    if (resultCoord < 0) {
+      resultCoord = 0;
+    } else if (resultCoord > wallSize - spiderSize) {
+      resultCoord = wallSize - spiderSize;
+    };
+
+    return resultCoord;
+  }
+
+  positionX = correctionPosition(
+    positionX,
+    spider.offsetWidth,
+    wall.clientWidth
+  );
+
+  positionY = correctionPosition(
+    positionY,
+    spider.offsetHeight,
+    wall.clientHeight
+  );
+
+  spider.style.top = `${positionY}px`;
+  spider.style.left = `${positionX}px`;
 });
