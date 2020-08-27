5f8c4d982ced05e29e735bc024e4d5015ca3a079
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-27 08:45:56 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallCoords = wall.getBoundingClientRect();
+
+  if (e.target.children === wall.children) {
+    const X = e.clientX - wallCoords.x - 10 - (spider.offsetWidth / 2);
+    const Y = e.clientY - wallCoords.y - 10 - (spider.offsetWidth / 2);
+
+    X < 0
+      ? spider.style.left = '0px'
+      : X > 350
+        ? spider.style.left = '350px'
+        : spider.style.left = X + 'px';
+
+    Y < 0
+      ? spider.style.top = '0px'
+      : Y > 350
+        ? spider.style.top = '350px'
+        : spider.style.top = Y + 'px';
+  }
 });
