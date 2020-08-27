c5584a7bce8f125446afe74e52d13c224c71979f
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-27 09:04:22 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,21 +4,23 @@ document.addEventListener('click', e => {
   const spider = document.querySelector('.spider');
   const wall = document.querySelector('.wall');
   const wallCoords = wall.getBoundingClientRect();
+  const maxX = wall.clientWidth - spider.offsetWidth;
+  const maxY = wall.clientHeight - spider.offsetHeight;
 
-  if (e.target.children === wall.children) {
+  if (e.target === wall) {
     const X = e.clientX - wallCoords.x - 10 - (spider.offsetWidth / 2);
-    const Y = e.clientY - wallCoords.y - 10 - (spider.offsetWidth / 2);
+    const Y = e.clientY - wallCoords.y - 10 - (spider.offsetHeight / 2);
 
     X < 0
       ? spider.style.left = '0px'
-      : X > 350
-        ? spider.style.left = '350px'
+      : X > maxX
+        ? spider.style.left = maxX + 'px'
         : spider.style.left = X + 'px';
 
     Y < 0
       ? spider.style.top = '0px'
-      : Y > 350
-        ? spider.style.top = '350px'
+      : Y > maxY
+        ? spider.style.top = maxY + 'px'
         : spider.style.top = Y + 'px';
   }
 });
