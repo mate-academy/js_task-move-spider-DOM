0a6ff80a4a079fcd252906b0ea0c9dae8683f783
Kostya Sabada <kostyasabada@gmail.com>
2020-08-27 12:34:34 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+document.addEventListener('click', (event) => {
+  if (event.target !== wall) {
+    return;
+  }
+
+  let spiderTop = event.offsetY - spider.clientHeight / 2;
+  let spiderLeft = event.offsetX - spider.clientWidth / 2;
+
+  if (event.offsetY < spider.clientHeight / 2) {
+    spiderTop = 0;
+  } else if (event.offsetY > (wall.clientHeight - spider.clientHeight)) {
+    spiderTop = wall.clientHeight - spider.clientHeight;
+  }
+
+  if (event.offsetX < spider.clientWidth / 2) {
+    spiderLeft = 0;
+  } else if (event.offsetX > wall.clientWidth - spider.clientWidth) {
+    spiderLeft = wall.clientWidth - spider.clientWidth;
+  }
+
+  spider.style.top = spiderTop + `px`;
+  spider.style.left = spiderLeft + `px`;
 });
