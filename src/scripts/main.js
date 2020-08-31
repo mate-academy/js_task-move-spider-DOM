2386d1dab8dd62881218ed074edaed16e75b83ae
kshvetsova <katshvetsova@gmail.com>
2020-08-31 15:28:02 +0300

    Solution fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,11 +15,17 @@ document.addEventListener('click', (event) => {
 
   if (x < 0) {
     x = 0;
-  } else if (y < 0) {
+  }
+
+  if (y < 0) {
     y = 0;
-  } else if (x > maxX) {
+  }
+
+  if (x > maxX) {
     x = maxX;
-  } else if (y > maxY) {
+  }
+
+  if (y > maxY) {
     y = maxY;
   }
   spider.style.top = y + 'px';
