e82d91b480b346b0c3883c61298aa2bbd77a3351
unknown <pavlo.com.5@gmail.com>
2020-08-31 11:27:14 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
-const spiderSize = window.getComputedStyle(spider).width.slice(0, -2);
 const wall = document.querySelector('.wall');
-const wallSize = window.getComputedStyle(wall).width.slice(0, -2);
 
 document.addEventListener('click', event => {
   if (event.target !== wall) {
     return;
   }
 
+  const spiderSize = window.getComputedStyle(spider).width.slice(0, -2);
+  const wallSize = window.getComputedStyle(wall).width.slice(0, -2);
   const maxValue = wallSize - spiderSize;
 
   if (event.offsetX >= maxValue + spiderSize / 2) {
