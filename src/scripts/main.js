e4639d88939b0f71b888b3aa855cd46cae3aa61a
Alex Arabov <arabov@ukr.net>
2020-10-03 10:31:23 +0300

    Fixed after first click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,38 @@
 'use strict';
 
-document.addEventListener('click', el => {
-  const wall = document.querySelector('.wall');
-  const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
 
-  wall.addEventListener('click', e => {
-    const posInWall = e.target.getBoundingClientRect();
+wall.addEventListener('click', e => {
+  const posInWall = e.target.getBoundingClientRect();
 
-    const posX = e.clientX;
-    const posY = e.clientY;
+  const posX = e.clientX;
+  const posY = e.clientY;
 
-    const wallCL = wall.clientLeft;
-    const wallCT = wall.clientTop;
-    const spiderCW = spider.clientWidth;
-    const spiderCH = spider.clientHeight;
+  const wallCL = wall.clientLeft;
+  const wallCT = wall.clientTop;
+  const spiderCW = spider.clientWidth;
+  const spiderCH = spider.clientHeight;
 
-    let spiderPosLeft = posX - posInWall.left - wallCL - spiderCW / 2;
-    let spiderPosTop = posY - posInWall.top - wallCT - spiderCH / 2;
+  let spiderPosLeft = posX - posInWall.left - wallCL - spiderCW / 2;
+  let spiderPosTop = posY - posInWall.top - wallCT - spiderCH / 2;
 
-    if (spiderPosLeft < 0) {
-      spiderPosLeft = 0;
-    }
+  if (spiderPosLeft < 0) {
+    spiderPosLeft = 0;
+  }
 
-    if (spiderPosTop < 0) {
-      spiderPosTop = 0;
-    }
+  if (spiderPosTop < 0) {
+    spiderPosTop = 0;
+  }
 
-    if (spiderPosLeft + spider.clientWidth > wall.clientWidth) {
-      spiderPosLeft = wall.clientWidth - spider.clientWidth;
-    }
+  if (spiderPosLeft + spider.clientWidth > wall.clientWidth) {
+    spiderPosLeft = wall.clientWidth - spider.clientWidth;
+  }
 
-    if (spiderPosTop + spider.clientHeight > wall.clientHeight) {
-      spiderPosTop = wall.clientHeight - spider.clientHeight;
-    }
+  if (spiderPosTop + spider.clientHeight > wall.clientHeight) {
+    spiderPosTop = wall.clientHeight - spider.clientHeight;
+  }
 
-    spider.style.left = `${spiderPosLeft}px`;
-    spider.style.top = `${spiderPosTop}px`;
-  });
+  spider.style.left = `${spiderPosLeft}px`;
+  spider.style.top = `${spiderPosTop}px`;
 });
