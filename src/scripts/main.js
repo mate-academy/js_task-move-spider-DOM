a33fc3360cda4f0d5a91e5ea527efcff853d6855
Artem Lavreniuk <artemlavrenyuk@gmail.com>
2020-10-15 09:45:49 +0300

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,43 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  const spider = document.querySelector('.spider');
-  const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const horizontalWallBorder = (wall.offsetWidth - wall.clientWidth) / 2;
+const verticalWallBorder = (wall.offsetHeight - wall.clientHeight) / 2;
+const rightMaxPosition = wall.clientWidth - spider.clientWidth;
+const bottomMaxPosition = wall.clientHeight - spider.clientHeight;
 
-  const horizontalWallBorder = (wall.offsetWidth - wall.clientWidth) / 2;
-  const verticalWallBorder = (wall.offsetHeight - wall.clientHeight) / 2;
+document.addEventListener('click', e => {
+  if (e.target !== wall) {
+    return;
+  }
 
   let xPosition = e.clientX
   - wall.getBoundingClientRect().left
-  - horizontalWallBorder;
+  - horizontalWallBorder
+  - spider.clientWidth / 2;
 
   let yPosition = e.clientY
   - wall.getBoundingClientRect().top
-  - verticalWallBorder;
+  - verticalWallBorder
+  - spider.clientHeight / 2;
 
   if (xPosition < 0) {
     xPosition = 0;
+  }
+
+  if (xPosition > rightMaxPosition) {
+    xPosition = rightMaxPosition;
   }
 
   if (yPosition < 0) {
     yPosition = 0;
   }
 
-  if (xPosition > wall.clientWidth - spider.clientWidth) {
-    xPosition = wall.clientWidth - spider.clientWidth;
+  if (yPosition > bottomMaxPosition) {
+    yPosition = bottomMaxPosition;
   }
 
-  if (yPosition > wall.clientHeight - spider.clientHeight) {
-    yPosition = wall.clientHeight - spider.clientHeight;
-  }
-
-  if (e.target === wall) {
-    spider.style.left = `${xPosition}px`;
-    spider.style.top = `${yPosition}px`;
-  }
+  spider.style.left = `${xPosition}px`;
+  spider.style.top = `${yPosition}px`;
 });
