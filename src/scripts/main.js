1a5fcd894294ec3f335585391ed39f7ef8d21cd3
Anastasia Chervona <av.chervona@gmail.com>
2020-10-16 15:04:25 +0300

    Bugs on small screen fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,27 +2,28 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-const wallGapVertical = wall.offsetTop + wall.clientTop;
-const wallGapHorizontal = wall.offsetLeft + wall.clientLeft;
 
 wall.addEventListener('click', event => {
-  let x = event.clientX - wallGapHorizontal - spider.clientWidth / 2;
-  let y = event.clientY - wallGapVertical - spider.clientHeight / 2;
+  const wallGapVertical = wall.offsetTop + wall.clientTop;
+  const wallGapHorizontal = wall.offsetLeft + wall.clientLeft;
+
+  let x = event.clientX - wallGapHorizontal - spider.offsetWidth / 2;
+  let y = event.clientY - wallGapVertical - spider.offsetHeight / 2;
 
   if (x < 0) {
     x = 0;
   }
 
-  if (x > wall.clientWidth - spider.clientWidth) {
-    x = wall.clientWidth - spider.clientWidth;
+  if (x > wall.clientWidth - spider.offsetWidth) {
+    x = wall.clientWidth - spider.offsetWidth;
   }
 
   if (y < 0) {
     y = 0;
   }
 
-  if (y > wall.clientHeight - spider.clientHeight) {
-    y = wall.clientHeight - spider.clientHeight;
+  if (y > wall.clientHeight - spider.offsetHeight) {
+    y = wall.clientHeight - spider.offsetHeight;
   }
 
   spider.style.left = `${x}px`;
