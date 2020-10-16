a78f27bd2c2c17a67b2aeb9db474e5fd9b70fc78
andlap24 <andrew.laptev24@gmail.com>
2020-10-16 18:31:50 +0300

    fix left pos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,18 +9,18 @@ const spiderHeight = spider.offsetHeight;
 
 document.addEventListener('click', e => {
   if (wall.contains(e.target)) {
-    if (e.offsetY > (wallHeight - spiderHeight / 2)
-      && e.offsetX > (wallWidth - spiderWidth / 2)) {
+    if (e.offsetY > (wallHeight - spiderHeight)
+      && e.offsetX > (wallWidth - spiderWidth)) {
       spider.style.left = `${e.offsetX - spiderWidth}px`;
       spider.style.top = `${e.offsetY - spiderHeight}px`;
     }
 
-    if (e.offsetY > (wallHeight - spiderHeight / 2)) {
+    if (e.offsetY > (wallHeight - spiderHeight)) {
       spider.style.left = `${e.offsetX - spiderWidth / 2}px`;
       spider.style.top = `${e.offsetY - spiderHeight}px`;
     }
 
-    if (e.offsetX > (wallWidth - spiderWidth / 2)) {
+    if (e.offsetX > (wallWidth - spiderWidth)) {
       spider.style.left = `${e.offsetX - spiderWidth}px`;
       spider.style.top = `${e.offsetY - spiderHeight / 2}px`;
     } else {
