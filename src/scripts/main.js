97eef7762ad25c9c9ba43aa75d4553039a7924d8
Yuriy Putsan <putsan57@gmail.com>
2020-10-17 18:06:12 +0300

    resurrected the spider)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  function checkPosition(coordinate) {
+    const halfSpiderSize = spider.clientWidth / 2;
+    const wallSize = e.target.clientWidth;
+
+    if (coordinate < halfSpiderSize) {
+      return halfSpiderSize;
+    } else if (coordinate > wallSize - halfSpiderSize) {
+      return wallSize - halfSpiderSize;
+    }
+
+    return coordinate;
+  };
+
+  const spider = document.querySelector('.spider');
+  const topPosition = checkPosition(e.offsetY);
+  const leftPosition = checkPosition(e.offsetX);
+
+  spider.style.transform = 'translate(-50%, -50%)';
+  spider.style.top = `${topPosition}px`;
+  spider.style.left = `${leftPosition}px`;
 });
