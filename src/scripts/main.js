fd030aae112fdf9b28cd2ed13d51578621a01b44
Eugene <evolokhin@gmail.com>
2020-10-19 17:50:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,36 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const wallHeight = document.querySelector('.wall').clientHeight;
+  const wallWidth = document.querySelector('.wall').clientWidth;
+
+  const spider = document.querySelector('.spider');
+  const spiderHeigth = spider.clientHeight;
+  const spiderWidth = spider.clientWidth;
+
+  if (!e.target.classList.contains('wall')) {
+    return;
+  }
+
+  let coordsX = `${e.offsetX - spiderWidth / 2}px`;
+  let coordsY = `${e.offsetY - spiderHeigth / 2}px`;
+
+  if (e.offsetY < spiderHeigth / 2) {
+    coordsY = 0;
+  }
+
+  if (e.offsetY > (wallHeight - spiderHeigth)) {
+    coordsY = `${wallHeight - spiderHeigth}px`;
+  }
+
+  if (e.offsetX < spiderWidth / 2) {
+    coordsX = 0;
+  }
+
+  if (e.offsetX > (wallWidth - spiderWidth)) {
+    coordsX = `${wallWidth - spiderWidth}px`;
+  }
+
+  spider.style.left = coordsX;
+  spider.style.top = coordsY;
 });
