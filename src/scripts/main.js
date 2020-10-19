f791f0f3afb47b93e15eccf43273e9801768c5b5
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-19 16:19:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', event => {
+  if (event.target !== wall) {
+    return;
+  }
+
+  let eventX = event.offsetX - spider.offsetWidth / 2;
+  let eventY = event.offsetY - spider.offsetHeight / 2;
+
+  const breakPointStart = 0;
+  const breakPointX = wall.clientWidth - spider.offsetWidth;
+  const breakPointY = wall.clientHeight - spider.offsetHeight;
+
+  if (eventX < breakPointStart) {
+    eventX = breakPointStart;
+  }
+
+  if (eventY < breakPointStart) {
+    eventY = breakPointStart;
+  }
+
+  if (eventX > breakPointX) {
+    eventX = breakPointX;
+  }
+
+  if (eventY > breakPointY) {
+    eventY = breakPointY;
+  }
+
+  spider.style.top = `${eventY}px`;
+  spider.style.left = `${eventX}px`;
 });
