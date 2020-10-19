6aa549a9ab86d405e8a3cb31ed520b556a65bd09
andlap24 <andrew.laptev24@gmail.com>
2020-10-19 11:23:54 +0300

    fix spider position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,17 +11,17 @@ const spiderHeight = spider.offsetHeight;
 document.addEventListener('click', e => {
   if (wall.contains(e.target)) {
     if (e.offsetX > (wallWidth - spiderWidth)) {
-      spider.style.left = `${e.offsetX - spiderWidth}px`;
+      spider.style.left = `${wall.clientWidth - spiderWidth}px`;
     } else if (e.offsetX < spiderWidth) {
-      spider.style.left = `${e.offsetX / 2}px`;
+      spider.style.left = `${0}px`;
     } else {
       spider.style.left = `${e.offsetX - spiderWidth / 2}px`;
     }
 
     if (e.offsetY > (wallHeight - spiderHeight)) {
-      spider.style.top = `${e.offsetY - spiderHeight}px`;
+      spider.style.top = `${wall.clientHeight - spiderHeight}px`;
     } else if (e.offsetY < spiderHeight) {
-      spider.style.top = `${e.offsetY}px`;
+      spider.style.top = `${0}px`;
     } else {
       spider.style.top = `${e.offsetY - spiderHeight / 2}px`;
     }
