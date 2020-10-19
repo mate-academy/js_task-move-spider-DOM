95a402142d371cd75abdd3e20065a02056f04f51
andlap24 <andrew.laptev24@gmail.com>
2020-10-19 18:01:23 +0300

    add function moveSpider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,22 +8,32 @@ const spider = document.querySelector('.spider');
 const spiderWidth = spider.offsetWidth;
 const spiderHeight = spider.offsetHeight;
 
-document.addEventListener('click', e => {
-  if (wall.contains(e.target)) {
-    if (e.offsetX > (wallWidth - spiderWidth)) {
-      spider.style.left = `${wall.clientWidth - spiderWidth}px`;
-    } else if (e.offsetX < spiderWidth) {
-      spider.style.left = `${0}px`;
-    } else {
-      spider.style.left = `${e.offsetX - spiderWidth / 2}px`;
-    }
-
-    if (e.offsetY > (wallHeight - spiderHeight)) {
-      spider.style.top = `${wall.clientHeight - spiderHeight}px`;
-    } else if (e.offsetY < spiderHeight) {
-      spider.style.top = `${0}px`;
-    } else {
-      spider.style.top = `${e.offsetY - spiderHeight / 2}px`;
-    }
+document.addEventListener('click', event => {
+  if (!wall.contains(event.target)) {
+    return;
   }
+
+  moveSpider(event.offsetX, event.offsetY);
 });
+
+function moveSpider(x, y) {
+  if (x > (wallWidth - spiderWidth)) {
+    spider.style.left = `${wall.clientWidth - spiderWidth}px`;
+  } else if (x < spiderWidth) {
+    spider.style.left = `${0}px`;
+  } else {
+    spider.style.left = moveAlong(x);
+  }
+
+  if (y > (wallHeight - spiderHeight)) {
+    spider.style.top = `${wall.clientHeight - spiderHeight}px`;
+  } else if (y < spiderHeight) {
+    spider.style.top = `${0}px`;
+  } else {
+    spider.style.top = moveAlong(y);
+  }
+}
+
+function moveAlong(coordinate) {
+  return `${coordinate - spiderHeight / 2}px`;
+}
