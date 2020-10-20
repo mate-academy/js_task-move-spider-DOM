8b033a38476226b94f6307f8f830a43478f46449
Yurii <yurastt1@gmail.com>
2020-10-20 19:25:29 +0300

    fixed corners

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,23 @@ const wallPositionTop = wall.offsetTop + wall.clientTop;
 const wallPositionLeft = wall.offsetLeft + wall.clientLeft;
 
 wall.addEventListener('click', e => {
-  const topIndentation = e.clientY - wallPositionTop - spider.height / 2;
-  const leftIndentation = e.clientX - wallPositionLeft - spider.width / 2;
+  let topIndentation = e.clientY - wallPositionTop - spider.height / 2;
+  let leftIndentation = e.clientX - wallPositionLeft - spider.width / 2;
+
+  if (topIndentation < 0) {
+    topIndentation = 0;
+  } else if (topIndentation > wall.clientHeight - spider.width) {
+    topIndentation = wall.clientHeight - spider.width;
+  }
+
+  if (leftIndentation < 0) {
+    leftIndentation = 0;
+  } else if (leftIndentation > wall.clientHeight - spider.height) {
+    leftIndentation = wall.clientHeight - spider.height;
+  }
 
   spider.style.cssText = `
-  top: ${topIndentation < 0 ? 0 : topIndentation}px;
-  left: ${leftIndentation < 0 ? 0 : leftIndentation}px;
+  top: ${topIndentation}px;
+  left: ${leftIndentation}px;
   `;
 });
