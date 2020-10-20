852b4228c86694380bb36f0fa0ce9e1a32325482
Liza <elizabethhoncharenko@gmail.com>
2020-10-20 14:49:22 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,31 @@
 'use strict';
 
-const spiderItem = document.querySelector('.spider');
-const wallItem = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
 
-wallItem.addEventListener('click', event => {
-  let coordX = event.offsetX - spiderItem.offsetWidth / 2;
-  let coordY = event.offsetY - spiderItem.offsetHeight / 2;
+wall.addEventListener('click', event => {
+  let coordX = event.offsetX - spider.offsetWidth / 2;
+  let coordY = event.offsetY - spider.offsetHeight / 2;
+
+  const maxCoordX = wall.clientWidth - spider.offsetWidth;
+  const minCoordY = wall.clientHeight - spider.offsetHeight;
 
   if (coordX < 0) {
     coordX = 0;
   }
 
-  if (coordX > wallItem.clientWidth - spiderItem.offsetWidth) {
-    coordX = wallItem.clientWidth - spiderItem.offsetWidth;
+  if (coordX > maxCoordX) {
+    coordX = maxCoordX;
   }
 
   if (coordY < 0) {
     coordY = 0;
   }
 
-  if (coordY > wallItem.clientHeight - spiderItem.offsetHeight) {
-    coordY = wallItem.clientHeight - spiderItem.offsetHeight;
+  if (coordY > minCoordY) {
+    coordY = minCoordY;
   }
 
-  spiderItem.style.left = coordX + 'px';
-  spiderItem.style.top = coordY + 'px';
+  spider.style.left = coordX + 'px';
+  spider.style.top = coordY + 'px';
 });
