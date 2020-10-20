61ea8895d78bb538b05ab22b2418ca0f660e0ea5
Yurii <yurastt1@gmail.com>
2020-10-20 18:29:20 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallPositionTop = wall.offsetTop + wall.clientTop;
+const wallPositionLeft = wall.offsetLeft + wall.clientLeft;
+
+wall.addEventListener('click', e => {
+  const topIndentation = e.clientY - wallPositionTop - spider.height / 2;
+  const leftIndentation = e.clientX - wallPositionLeft - spider.width / 2;
+
+  spider.style.cssText = `
+  top: ${topIndentation < 0 ? 0 : topIndentation}px;
+  left: ${leftIndentation < 0 ? 0 : leftIndentation}px;
+  `;
 });
