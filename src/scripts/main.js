cbef0083fc752b670a2d13cd07748bcf4ec19ab9
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-10-20 14:29:31 +0300

    move spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const spiderSize = spider.offsetWidth;
+const wall = document.querySelector('.wall');
+
+document.addEventListener('click', event => {
+  const wallTop = wall.offsetTop + wall.clientTop;
+  const wallLeft = wall.offsetLeft + wall.clientLeft;
+
+  const yPosition = event.clientY - wallTop - spiderSize / 2;
+  const xPosition = event.clientX - wallLeft - spiderSize / 2;
+
+  spider.style.top = spiderCoords(yPosition) + 'px';
+  spider.style.left = spiderCoords(xPosition) + 'px';
 });
+
+function spiderCoords(coords) {
+  let position = coords;
+
+  if (position < 0) {
+    position = 0;
+  }
+
+  if (position > wall.clientWidth - spiderSize) {
+    position = wall.clientWidth - spiderSize;
+  }
+
+  return position;
+}
