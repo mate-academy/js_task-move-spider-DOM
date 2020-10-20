2b6a415e783cc888123bc53d5a1e13c65fc0a4e3
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-10-20 15:42:07 +0300

    fixed function return

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,17 +6,18 @@ const spider = document.querySelector('.spider');
 wall.addEventListener('click', event => {
   const spiderHeight = spider.offsetHeight;
   const wallHeigth = wall.clientHeight;
+
   const leftWall = wall.clientLeft + wall.offsetLeft;
   const topWall = wall.clientTop + wall.offsetTop;
+
   const positionX = event.clientX - leftWall - spider.offsetWidth / 2;
   const positionY = event.clientY - topWall - spider.offsetWidth / 2;
-  
 
   function correctPosition(coord, spiderSize, wallSize) {
     let resultCorrection = coord;
 
     if (resultCorrection < 0) {
-      resultCorrection = 0;
+      return 0;
     } else if (resultCorrection > wallSize - spiderSize) {
       resultCorrection = wallSize - spiderSize;
     }
