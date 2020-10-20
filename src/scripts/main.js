df91e0a94599929c664e0492805c704a5f23ef9d
Marina <m.gerasymova29@gmail.com>
2020-10-20 12:58:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,44 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderWidth = spider.offsetHeight;
+const spiderHeight = spider.offsetWidth;
+
+const heightDifference = wall.offsetTop + wall.clientTop;
+const widthDifference = wall.offsetLeft + wall.clientLeft;
+
+document.addEventListener('click', event => {
+  const spiderLeft = event.clientX - widthDifference - spiderWidth / 2;
+  const spiderTop = event.clientY - heightDifference - spiderHeight / 2;
+
+  spider.style.left = `${validateCoords(spiderLeft, 'left')}px`;
+  spider.style.top = `${validateCoords(spiderTop, 'top')}px`;
 });
+
+function validateCoords(position, direction) {
+  let coord = position;
+  let spiderSize;
+  let sizeProperty;
+
+  if (position < 0) {
+    coord = 0;
+  }
+
+  if (direction === 'left') {
+    spiderSize = spiderWidth;
+    sizeProperty = 'clientWidth';
+  } else {
+    spiderSize = spiderHeight;
+    sizeProperty = 'clientHeight';
+  }
+
+  const gap = coord + spiderSize;
+
+  if (gap > wall[sizeProperty]) {
+    coord -= gap - wall[sizeProperty];
+  }
+
+  return coord;
+}
