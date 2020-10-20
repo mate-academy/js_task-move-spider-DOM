f21b4e03850492699e846166378e36b20b39af7f
Lancaelot <Lancaelot@gmail.com>
2020-10-20 13:45:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', moveSpider);
+
+function moveSpider(event) {
+  const leftEdge = wall.offsetLeft;
+  const topEdge = wall.offsetTop;
+
+  const spiderMaxCoordX = wall.clientWidth - spider.clientWidth;
+  const spiderMinCoordX = 0;
+  const spiderMaxCoordY = wall.clientHeight - spider.clientHeight;
+  const spiderMinCoordY = 0;
+
+  let coordX = event.clientX - leftEdge - spider.clientWidth / 2;
+  let coordY = event.clientY - topEdge - spider.clientHeight / 2;
+
+  if (coordX < spiderMinCoordX) {
+    coordX = spiderMinCoordX;
+  }
+
+  if (coordX > spiderMaxCoordX) {
+    coordX = spiderMaxCoordX;
+  }
+
+  if (coordY < spiderMinCoordY) {
+    coordY = spiderMinCoordY;
+  }
+
+  if (coordY > spiderMaxCoordY) {
+    coordX = spiderMaxCoordY;
+  }
+
+  spider.style.left = `${coordX}px`;
+  spider.style.top = `${coordY}px`;
+}
