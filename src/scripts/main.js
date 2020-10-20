22e3a6f4bd5db8c97d53000215d14c9c10dfb882
OnlyRaydek <bayramsaid00@gmail.com>
2020-10-20 19:09:28 +0300

    add condition for edges

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,25 @@ theWall.addEventListener('click', (event) => {
   const leftWallEdge = theWall.offsetLeft + theWall.clientLeft;
   const topWallEdge = theWall.offsetTop + theWall.clientTop;
 
-  const xPosition = event.clientX - leftWallEdge - (theSpider.offsetWidth / 2);
+  let xPosition = event.clientX - leftWallEdge - (theSpider.offsetWidth / 2);
 
-  const yPosition = event.clientY - topWallEdge - (theSpider.offsetHeight / 2);
+  let yPosition = event.clientY - topWallEdge - (theSpider.offsetHeight / 2);
+
+  if (xPosition < 0) {
+    xPosition = 0;
+  }
+
+  if (xPosition > theWall.clientWidth - theSpider.offsetWidth) {
+    xPosition = theWall.clientWidth - theSpider.offsetWidth;
+  }
+
+  if (yPosition < 0) {
+    yPosition = 0;
+  }
+
+  if (yPosition > theWall.clientHeight - theSpider.offsetHeight) {
+    yPosition = theWall.clientHeight - theSpider.offsetHeight;
+  }
 
   theSpider.style.left = xPosition + 'px';
   theSpider.style.top = yPosition + 'px';
