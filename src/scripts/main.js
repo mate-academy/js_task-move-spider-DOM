e015a10a933471a7ce99b4b4008becd723e86354
martakupka <marta-k2008@ukr.net>
2020-10-20 17:23:43 +0300

    Fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,29 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  const wall = document.querySelector('.wall');
-  const spider = document.querySelector('.spider');
-  const wallWidth = wall.clientWidth;
-  const wallHeight = wall.clientHeight;
-  const spiderWidth = spider.clientWidth;
-  const spiderHeight = spider.clientHeight;
-
-  const clickCoordX = Math.ceil(
-    e.clientX - wall.getBoundingClientRect().left - wall.clientLeft
-  );
-  const clickCoordY = Math.ceil(
-    e.clientY - wall.getBoundingClientRect().top - wall.clientTop
-  );
-
-  let movePositionX = clickCoordX - spiderWidth / 2;
-  let movePositionY = clickCoordY - spiderHeight / 2;
-
   if (e.target.className !== 'wall') {
     return;
   }
 
-  if (clickCoordX <= spiderWidth / 2) {
-    movePositionX = 0;
-  }
+  const spider = document.querySelector('.spider');
+  const wallSize = document.querySelector('.wall').clientWidth;
+  const spiderSize = spider.clientWidth;
 
-  if (clickCoordY <= spiderHeight / 2) {
-    movePositionY = 0;
-  }
+  const coordinateX = e.offsetX - spiderSize / 2;
+  const coordinateY = e.offsetY - spiderSize / 2;
 
-  if (clickCoordX >= wallWidth - spiderWidth / 2) {
-    movePositionX = wallWidth - spiderWidth;
-  }
-
-  if (clickCoordY >= wallHeight - spiderHeight / 2) {
-    movePositionY = wallHeight - spiderHeight;
-  }
-
-  spider.style.left = movePositionX + 'px';
-  spider.style.top = movePositionY + 'px';
+  spider.style.left = getMovePosition(coordinateX, wallSize, spiderSize) + 'px';
+  spider.style.top = getMovePosition(coordinateY, wallSize, spiderSize) + 'px';
 });
+
+function getMovePosition(coordinate, wallSize, spiderSize) {
+  if (coordinate < 0) {
+    return 0;
+  }
+
+  if (coordinate > wallSize - spiderSize) {
+    return wallSize - spiderSize;
+  }
+
+  return coordinate;
+}
