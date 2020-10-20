5d38d7e0c498f80cf52384ff422106689a1ccc56
Ivan Beletskiy <beletsky02@gmail.com>
2020-10-20 12:35:49 +0300

    make function to calc coords

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,46 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
+const spiderHeight = spider.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+const wall = document.querySelector('.wall');
+const wallHeight = wall.clientHeight;
+const wallWidth = wall.clientWidth;
 
 document.addEventListener('click', e => {
-  const wall = e.target.closest('.wall');
-
-  if (!wall) {
-    return;
-  }
-
   if (e.target === spider) {
     spider.style.left
-      = `${Math.random() * (wall.clientWidth - spider.offsetWidth)}px`;
+      = `${Math.random() * (wallWidth - spider.offsetWidth)}px`;
 
     spider.style.top
-      = `${Math.random() * (wall.clientHeight - spider.offsetHeight)}px`;
+      = `${Math.random() * (wallHeight - spider.offsetHeight)}px`;
 
     return;
   }
 
-  spider.style.left = `${e.offsetX - spider.offsetWidth / 2}px`;
-  spider.style.top = `${e.offsetY - spider.offsetHeight / 2}px`;
-
-  if (e.offsetX <= spider.offsetWidth / 2) {
-    spider.style.left = '0px';
+  if (e.target !== wall) {
+    return;
   }
 
-  if (e.offsetY <= spider.offsetHeight / 2) {
-    spider.style.top = '0px';
-  }
+  const cursorX = e.offsetX - spiderWidth / 2;
+  const cursorY = e.offsetY - spiderHeight / 2;
 
-  if (e.offsetX >= wall.clientWidth - spider.offsetWidth / 2) {
-    spider.style.left = `${wall.clientWidth - spider.offsetWidth}px`;
-  }
-
-  if (e.offsetY >= wall.clientHeight - spider.offsetHeight / 2) {
-    spider.style.top = `${wall.clientHeight - spider.offsetHeight}px`;
-  }
+  spider.style.left = `${calculateCoords(cursorX, 'x')}px`;
+  spider.style.top = `${calculateCoords(cursorY, 'y')}px`;
 });
+
+function calculateCoords(coords, dimension) {
+  if (coords < 0) {
+    return '0';
+  }
+
+  if (dimension === 'x' && coords > wallWidth - spiderWidth) {
+    return String(wallWidth - spiderWidth);
+  }
+
+  if (dimension === 'y' && coords > wallHeight - spiderHeight) {
+    return String(wallHeight - spiderHeight);
+  }
+
+  return coords;
+}
