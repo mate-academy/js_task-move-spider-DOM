83f87199df63af33f9aa2c7f04b45d6e2de42c90
milla-romankova <milla-romankova@gmail.com>
2020-10-20 15:58:45 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,27 +4,32 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 document.addEventListener('click', event => {
-  if (!event.target.closest('.wall')) {
+  if (!event.target.closest('.wall') || event.target === spider) {
     return;
   }
 
-  moveSomeObject(event, spider);
+  moveSomeObject(event, spider, wall);
 });
 
-function moveSomeObject(event, someObject) {
-  if (event.offsetX <= someObject.offsetWidth / 2) {
+function moveSomeObject(event, someObject, area) {
+  const maxRight = area.clientWidth - someObject.offsetWidth;
+  const halfObjectWidth = someObject.offsetWidth / 2;
+  const maxDown = area.clientHeight - someObject.offsetHeight;
+  const halfObjectHeight = someObject.offsetHeight / 2;
+
+  if (event.offsetX <= halfObjectWidth) {
     someObject.style.left = '0px';
-  } else if (event.offsetX >= wall.clientWidth - someObject.offsetWidth / 2) {
-    someObject.style.left = `${wall.clientWidth - someObject.offsetWidth}px`;
+  } else if (event.offsetX >= maxRight + halfObjectWidth) {
+    someObject.style.left = `${maxRight}px`;
   } else {
-    someObject.style.left = `${event.offsetX - someObject.offsetWidth / 2}px`;
+    someObject.style.left = `${event.offsetX - halfObjectWidth}px`;
   }
 
-  if (event.offsetY <= someObject.offsetHeight / 2) {
+  if (event.offsetY <= halfObjectHeight) {
     someObject.style.top = '0px';
-  } else if (event.offsetY >= wall.clientHeight - someObject.offsetHeight / 2) {
-    someObject.style.top = `${wall.clientHeight - someObject.offsetHeight}px`;
+  } else if (event.offsetY >= maxDown + halfObjectHeight) {
+    someObject.style.top = `${maxDown}px`;
   } else {
-    someObject.style.top = `${event.offsetY - someObject.offsetHeight / 2}px`;
+    someObject.style.top = `${event.offsetY - halfObjectHeight}px`;
   }
 }
