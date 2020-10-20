8fea152468c02317f35a73c20d2c90ce5e22fa36
KirillLutsenko <mnrovski68@gmail.com>
2020-10-20 12:42:31 +0300

    add a function with a condition check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,31 +2,33 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+const wallWidth = wall.offsetWidth;
+const wallHeight = wall.offsetHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
 
-document.addEventListener('click', e => {
-  if (e.target !== wall) {
+document.addEventListener('click', event => {
+  if (!wall.contains(event.target)) {
     return;
   }
 
-  let x = e.offsetX - spider.offsetWidth / 2;
-  let y = e.offsetY - spider.offsetWidth / 2;
-
-  if (x < 0) {
-    x = 0;
-  }
-
-  if (x > wall.clientWidth - spider.offsetWidth) {
-    x = wall.clientWidth - spider.offsetWidth;
-  }
-
-  if (y < 0) {
-    y = 0;
-  }
-
-  if (y > wall.clientHeight - spider.offsetHeight) {
-    y = wall.clientHeight - spider.offsetHeight;
-  }
-
-  spider.style.left = x + 'px';
-  spider.style.top = y + 'px';
+  spiderWay(event.offsetX, event.offsetY);
 });
+
+function spiderWay(x, y) {
+  if (x > (wallWidth - spiderWidth)) {
+    spider.style.left = `${wall.clientWidth - spiderWidth}px`;
+  } else if (x < spiderWidth) {
+    spider.style.left = `${0}px`;
+  } else {
+    spider.style.left = `${x - spiderHeight / 2}px`;
+  }
+
+  if (y > (wallHeight - spiderHeight)) {
+    spider.style.top = `${wall.clientHeight - spiderHeight}px`;
+  } else if (y < spiderHeight) {
+    spider.style.top = `${0}px`;
+  } else {
+    spider.style.top = `${y - spiderHeight / 2}px`;
+  };
+};
