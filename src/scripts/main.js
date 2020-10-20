d1b7a44d64d33b8b4051164ad2ce56e9f4160da2
Evgeniy <more.inspirationn@gmail.com>
2020-10-20 12:20:11 +0300

    add spaces between vars

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,10 @@
 document.addEventListener('click', e => {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
+
   const spiderWidth = spider.offsetWidth;
   const spiderHeight = spider.offsetHeight;
+
   const wallWidth = wall.clientWidth;
   const wallHeight = wall.clientHeight;
 
