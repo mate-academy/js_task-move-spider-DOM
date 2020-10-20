7dc3c742b9858eeda92f3f74ed544868c987d453
Yuriy Putsan <putsan57@gmail.com>
2020-10-20 14:00:16 +0300

    more refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,31 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
+const halfSpiderSize = spider.clientWidth / 2;
 
 document.addEventListener('click', e => {
   if (e.target.className !== 'wall') {
     return;
   }
 
-  const halfSpiderSize = spider.clientWidth / 2;
   const wallSize = e.target.clientWidth;
 
-  function checkPosition(coordinate) {
-    if (coordinate < halfSpiderSize) {
-      return halfSpiderSize;
-    }
-
-    if (coordinate > wallSize - halfSpiderSize) {
-      return wallSize - halfSpiderSize;
-    }
-
-    return coordinate;
-  };
-
-  const topPosition = checkPosition(e.offsetY);
-  const leftPosition = checkPosition(e.offsetX);
+  const topPosition = checkPosition(e.offsetY, wallSize);
+  const leftPosition = checkPosition(e.offsetX, wallSize);
 
   spider.style.transform = 'translate(-50%, -50%)';
   spider.style.top = `${topPosition}px`;
   spider.style.left = `${leftPosition}px`;
 });
+
+function checkPosition(coordinate, placeSize) {
+  if (coordinate < halfSpiderSize) {
+    return halfSpiderSize;
+  }
+
+  if (coordinate > placeSize - halfSpiderSize) {
+    return placeSize - halfSpiderSize;
+  }
+
+  return coordinate;
+};
