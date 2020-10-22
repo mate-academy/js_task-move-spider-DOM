38f8055fc099266a90d5e084e64d84dcdd65dfaa
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-23 00:57:33 +0300

    fixed variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,38 @@
 'use strict';
 
-const wall = document.getElementsByClassName('wall')[0];
-const spider = document.getElementsByClassName('spider')[0];
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const borderWidth = parseFloat(
+  window.getComputedStyle(wall).getPropertyValue('border-width'));
 
 wall.addEventListener('click', event => {
   event.preventDefault();
 
   const { left, top, width, height } = wall.getBoundingClientRect();
-
-  const borderWidth = parseFloat(
-    window.getComputedStyle(wall).getPropertyValue('border-width'));
-  const spiderWidth = spider.getBoundingClientRect().width;
-  const spiderHeight = spider.getBoundingClientRect().height;
+  const { width: spiderWidth, height: spiderHeight }
+    = spider.getBoundingClientRect();
 
   let clickX = event.clientX - left;
   let clickY = event.clientY - top;
+  const virtualBorderLeft = borderWidth + spiderWidth / 2;
+  const virtualBorderRight = width - borderWidth - spiderWidth / 2;
+  const virtualBorderTop = borderWidth + spiderHeight / 2;
+  const virtualBorderBottom = height - borderWidth - spiderHeight / 2;
 
-  if (clickX < borderWidth + spiderWidth / 2) {
-    clickX = borderWidth + spiderWidth / 2;
+  if (clickX < virtualBorderLeft) {
+    clickX = virtualBorderLeft;
   }
 
-  if (clickX > width - borderWidth - spiderWidth / 2) {
-    clickX = width - borderWidth - spiderWidth / 2;
+  if (clickX > virtualBorderRight) {
+    clickX = virtualBorderRight;
   }
 
-  if (clickY < borderWidth + spiderHeight / 2) {
-    clickY = borderWidth + spiderHeight / 2;
+  if (clickY < virtualBorderTop) {
+    clickY = virtualBorderTop;
   }
 
-  if (clickY > height - borderWidth - spiderHeight / 2) {
-    clickY = height - borderWidth - spiderHeight / 2;
+  if (clickY > virtualBorderBottom) {
+    clickY = virtualBorderBottom;
   }
 
   spider.style.left = (clickX - borderWidth - spiderWidth / 2) + 'px';
