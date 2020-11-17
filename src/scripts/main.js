dc1af7ef09d8a800ea4267e2dfbda78bd3c582dd
Dmitrii <dmitrii.zinchuk@dstarlab.com>
2020-11-17 14:10:12 +0200

    align

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', e => {
+  if (e.target !== spider) {
+    spider.style.top = e.offsetY - (spider.clientHeight / 2) + 'px';
+    spider.style.left = e.offsetX - (spider.clientWidth / 2) + 'px';
+  }
 });
