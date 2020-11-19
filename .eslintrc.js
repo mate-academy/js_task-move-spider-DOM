7f79ad10516365c2c6393c0e4cf2a2254f86dd16
Taras Shchadylo <tarasdidg@gmail.com>
2020-11-19 15:02:16 +0200

    Solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
+  "rules": {
+    "max-len": ["error", {
+      "code": 200,
+    }]
+  }
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,44 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+const wall = document.querySelector('.wall');
+const spider = wall.querySelector('.spider');
+
+const rect = wall.getBoundingClientRect();
+
+const spiderWidth = spider.clientWidth / 2;
+const spiderHeight = spider.clientHeight / 2;
+
+const wallMinX = rect.left + wall.clientLeft;
+const wallMinY = rect.top + wall.clientTop;
+
+const wallMaxY = wall.clientHeight - spider.clientHeight;
+const wallMaxX = wall.clientWidth - spider.clientWidth;
+
+wall.addEventListener('click', getClickPosition, false);
+
+function getClickPosition(e) {
+  if (e.target !== wall) {
+    return;
+  }
+
+  let xPos = e.clientX - wallMinX - spiderWidth;
+  let yPos = e.clientY - wallMinY - spiderHeight;
+
+  if (xPos < 0) {
+    xPos = 0;
+  }
+
+  if (yPos < 0) {
+    yPos = 0;
+  }
+
+  if (xPos > wallMaxX) {
+    xPos = wallMaxX;
+  }
+
+  if (yPos > wallMaxY) {
+    yPos = wallMaxY;
+  }
+
+  spider.style.transform = `translate(${xPos}px, ${yPos}px)`;
+}
