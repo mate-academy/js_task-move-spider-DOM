9971a97bd1debd6d9573af1f542d5809e9839242
Dmitrii <dmitrii.zinchuk@dstarlab.com>
2020-11-20 12:38:19 +0200

    fix spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,22 @@ const wall = document.querySelector('.wall');
 
 wall.addEventListener('click', e => {
   if (e.target !== spider) {
-    spider.style.top = e.offsetY - (spider.clientHeight / 2) + 'px';
-    spider.style.left = e.offsetX - (spider.clientWidth / 2) + 'px';
+    const spiderHeight = spider.clientHeight;
+    const spiderWidth = spider.clientWidth;
+
+    const wallHeight = wall.clientHeight;
+    const wallWidth = wall.clientWidth;
+
+    let y = e.offsetY;
+    let x = e.offsetX;
+
+    y = y < spiderHeight ? y = spiderHeight / 2 : y;
+    x = x < spiderWidth ? x = spiderWidth / 2 : x;
+
+    y = y > wallHeight - spiderHeight / 2 ? wallHeight - spiderHeight / 2 : y;
+    x = x > wallWidth - spiderWidth / 2 ? wallWidth - spiderWidth / 2 : x;
+
+    spider.style.top = y - (spiderHeight / 2) + 'px';
+    spider.style.left = x - (spiderWidth / 2) + 'px';
   }
 });
