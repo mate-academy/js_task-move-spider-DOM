791056ee62c0b7da6428fe780e94d01848284210
vovador <vovadorko@gmail.com>
2021-01-06 18:04:33 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.classList.contains('wall')) {
+    const spider = document.querySelector('.spider');
+    let verticalPosition = (e.offsetY - spider.clientHeight / 2)
+     > 0 ? e.offsetY - spider.clientHeight / 2 : 0;
+    let horizontalPosition = (e.offsetX - spider.clientWidth / 2)
+    > 0 ? e.offsetX - spider.clientWidth / 2 : 0;
+
+    if (horizontalPosition >= e.target.clientWidth - spider.clientWidth) {
+      horizontalPosition = e.target.clientWidth - spider.clientWidth;
+    }
+
+    if (verticalPosition >= e.target.clientHeight - spider.clientHeight) {
+      verticalPosition = e.target.clientHeight - spider.clientHeight;
+    }
+    spider.style.top = `${verticalPosition}px`;
+    spider.style.left = `${horizontalPosition}px`;
+  }
 });
