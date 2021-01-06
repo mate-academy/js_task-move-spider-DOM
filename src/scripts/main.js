a3b7c5202530a98c83e684ea92206c3c6e4549fe
Your Name <mogolgogol.90@gmail.com>
2021-01-06 17:49:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,46 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const wallBorder = wall.clientTop;
+
+const spyderWidth = parseInt(spider.clientWidth);
+const spyderHeight = parseInt(spider.clientHeight);
+const wallWidth = parseInt(wall.clientWidth);
+const wallHeight = parseInt(wall.clientHeight);
+
+const distanceFromTop = wall.offsetTop;
+const distanceFromLeft = wall.offsetLeft;
+
+const translateCenter = spyderWidth / 2 + wallBorder;
+
 document.addEventListener('click', e => {
-  // write code here
+  const item = e.target;
+
+  if (!item.classList.contains('wall')) {
+    return;
+  }
+
+  const mouseInsideWallX = e.clientX - distanceFromLeft;
+  const mouseInsideWallY = e.clientY - distanceFromTop;
+
+  spider.style.top = `${mouseInsideWallY - translateCenter}px`;
+  spider.style.left = `${mouseInsideWallX - translateCenter}px`;
+
+  if (mouseInsideWallX < spyderWidth / 2 + wallBorder) {
+    spider.style.left = `0px`;
+  }
+
+  if (mouseInsideWallX >= wallWidth - spyderWidth / 2 + wallBorder) {
+    spider.style.left = `${wallWidth - spyderWidth}px`;
+  }
+
+  if (mouseInsideWallY < spyderHeight / 2 + wallBorder) {
+    spider.style.top = `0px`;
+  }
+
+  if (mouseInsideWallY >= wallHeight - spyderHeight / 2 + wallBorder) {
+    spider.style.top = `${wallHeight - spyderHeight}px`;
+  }
 });
