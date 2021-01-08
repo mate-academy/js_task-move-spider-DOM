840b1b7760462059dfce74d6506628da3e0d9328
PaulT <ptimofejev@gmail.com>
2021-01-08 20:16:23 +0200

    Move spider solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderHeight = spider.clientHeight;
+const spiderWidth = spider.clientWidth;
+
+spider.addEventListener('click', ev => {});
+
+wall.addEventListener('click', ev => {
+  const offsetY = (ev.target === spider)
+    ? spider.offsetTop + ev.offsetY
+    : ev.offsetY;
+
+  const offsetX = (ev.target === spider)
+    ? spider.offsetLeft + ev.offsetX
+    : ev.offsetX;
+
+  const spiderTop = Math.min(
+    Math.max(offsetY, spiderHeight / 2),
+    wall.clientHeight - spiderHeight / 2
+  ) - spiderHeight / 2;
+
+  const spiderLeft = Math.min(
+    Math.max(offsetX, spiderWidth / 2),
+    wall.clientWidth - spiderWidth / 2
+  ) - spiderWidth / 2;
+
+  spider.style.top = `${spiderTop}px`;
+  spider.style.left = `${spiderLeft}px`;
 });
