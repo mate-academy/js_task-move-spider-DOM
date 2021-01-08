c1474411329ab0eed564ce1512e9e2f196878c6c
troospieler <troospieler1990@gmail.com>
2021-01-08 15:56:35 +0200

    improved solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,29 +9,21 @@ document.addEventListener('click', e => {
   const x = e.offsetX;
   const y = e.offsetY;
 
+  function calculation(number) {
+    if (number < spiderSize / 2) {
+      return spiderSize / 2 - number;
+    }
+
+    if ((number - wallSize + spiderSize / 2) > 0) {
+      return (number - wallSize + spiderSize / 2) * -1;
+    }
+
+    return 0;
+  };
+
   if (e.target === wall) {
-    let topAdder = 0;
-    let leftAdder = 0;
-    let rightSubtracter = 0;
-    let bottomSubtracter = 0;
-
-    if (x < spiderSize / 2) {
-      leftAdder = spiderSize / 2 - x;
-    }
-
-    if ((x - wallSize + spiderSize / 2) > 0) {
-      rightSubtracter = x - wallSize + spiderSize / 2;
-    }
-
-    if (y < spiderSize / 2) {
-      topAdder = spiderSize / 2 - y;
-    }
-
-    if ((y - wallSize + spiderSize / 2) > 0) {
-      bottomSubtracter = y - wallSize + spiderSize / 2;
-    }
-    spider.style.left = `${x - spiderSize / 2 + leftAdder - rightSubtracter}px`;
-    spider.style.top = `${y - spiderSize / 2 + topAdder - bottomSubtracter}px`;
+    spider.style.left = `${x - spiderSize / 2 + calculation(x)}px`;
+    spider.style.top = `${y - spiderSize / 2 + calculation(y)}px`;
   }
 
   if (e.target === spider) {
