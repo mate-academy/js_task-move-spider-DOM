30b5c2540893f0b85a59f13165989d0bff161864
KirillLutsenko <mnrovski68@gmail.com>
2021-01-08 15:14:26 +0200

    move the spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+const spider = document.querySelector('.spider');
+const spiderWidth = spider.offsetWidth;
+
+const spiderMovementHandler = (e) => {
+  const wall = e.target.closest('.wall');
+  const positionX = e.offsetX - spiderWidth / 2;
+  const positionY = e.offsetY - spiderWidth / 2;
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+
+  if (!wall.contains(e.target)) {
+    return;
+  }
+
+  function calculateSpiderPosition(coords, wallSide) {
+    if (coords < 0) {
+      return 0;
+    } else if (coords > (wallSide - spiderWidth)) {
+      return wallSide - spiderWidth;
+    }
+
+    return coords;
+  }
+
+  spider.style.left = `${calculateSpiderPosition(positionX, wallWidth)}px`;
+  spider.style.top = `${calculateSpiderPosition(positionY, wallHeight)}px`;
+};
+
+document.addEventListener('click', spiderMovementHandler);
