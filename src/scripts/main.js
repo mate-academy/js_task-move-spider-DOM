70d707d5f892a0013221093e77d6ab2b832c9b9a
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-01-08 20:43:32 +0200

    Created function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,27 +4,25 @@ document.addEventListener('click', e => {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
 
+  function setCoord(coord, outerSize, innerSize) {
+    const moveCoord = coord - innerSize / 2;
+
+    if (moveCoord < 0) {
+      return 0;
+    }
+
+    if (moveCoord > (outerSize - innerSize)) {
+      return outerSize - innerSize;
+    }
+
+    return moveCoord;
+  }
+
   if (e.target === wall) {
-    let topCoord = e.offsetY - spider.height / 2;
-    let leftCoord = e.offsetX - spider.width / 2;
+    spider.style.top
+    = `${setCoord(e.offsetY, wall.clientHeight, spider.height)}px`;
 
-    if (topCoord < 0) {
-      topCoord = 0;
-    }
-
-    if (topCoord > (wall.clientHeight - spider.height)) {
-      topCoord = wall.clientHeight - spider.height;
-    }
-
-    if (leftCoord < 0) {
-      leftCoord = 0;
-    }
-
-    if (leftCoord > (wall.clientWidth - spider.width)) {
-      leftCoord = wall.clientWidth - spider.width;
-    }
-
-    spider.style.top = `${topCoord}px`;
-    spider.style.left = `${leftCoord}px`;
+    spider.style.left
+    = `${setCoord(e.offsetX, wall.clientWidth, spider.width)}px`;
   }
 });
