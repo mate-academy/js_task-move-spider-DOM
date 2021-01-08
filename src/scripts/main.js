679a74e94ed9ff949b282361cf9fdc8b7689ab0c
Vitalii Siedykh <siedykh.v@gmail.com>
2021-01-08 14:37:57 +0200

    soluti

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+  const spiderSize = spider.clientWidth;
+  const wallSize = document.querySelector('.wall').clientWidth;
+
+  const xCor = e.offsetX - spiderSize / 2;
+  const yCor = e.offsetY - spiderSize / 2;
+
+  spider.style.left = getMovePosition(xCor, wallSize, spiderSize) + 'px';
+  spider.style.top = getMovePosition(yCor, wallSize, spiderSize) + 'px';
 });
+
+function getMovePosition(coordinate, wallSize, spiderSize) {
+  if (coordinate < 0) {
+    return 0;
+  }
+
+  if (coordinate > wallSize - spiderSize) {
+    return wallSize - spiderSize;
+  }
+
+  return coordinate;
+}
