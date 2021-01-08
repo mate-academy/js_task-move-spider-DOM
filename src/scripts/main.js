24b4029ac5b27965a969dd445fbdd2e672cd33fc
Lora Levitska <larakad94@gmail.com>
2021-01-08 17:16:09 +0200

    code was optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,19 +9,24 @@ area.addEventListener('click', e => {
   const spiderH = spider.clientHeight;
   const Y = e.clientY - areaCoords.top - area.clientTop - spiderH / 2;
   const X = e.clientX - areaCoords.left - area.clientLeft - spiderW / 2;
-  let myTop = Y < 0 ? 0 : Y;
-  let myLeft = X < 0 ? 0 : X;
 
-  if (Y + spiderH > area.clientHeight) {
-    myTop = area.clientHeight - spiderH;
-  }
-
-  if (X + spiderW > area.clientWidth) {
-    myLeft = area.clientWidth - spiderW;
-  }
+  const myTop = definition(Y, spiderH, area.clientHeight);
+  const myLeft = definition(X, spiderW, area.clientWidth);
 
   spider.style.cssText = `
   top: ${myTop}px;
   left: ${myLeft}px;
   `;
 });
+
+function definition(coord, direction, method) {
+  let result = coord;
+
+  if (coord + direction > method) {
+    result = method - direction;
+  } else if (coord < 0) {
+    result = 0;
+  }
+
+  return result;
+}
