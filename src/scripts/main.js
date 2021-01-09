d2fff286159a0ec92c8ca8bf748103b731503fa2
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-09 23:01:27 +0200

    made tast spider move

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const border = (wall.offsetHeight - wall.clientHeight) / 2;
+const wallLeft = wall.getBoundingClientRect().left;
+const wallTop = wall.getBoundingClientRect().top;
+const halfSpider = (spider.offsetHeight / 2);
+
+function coord(click, halfSp, wallPosition, wallSize) {
+  const positionTop = (click - halfSp);
+  const positionButtom = (click + halfSp);
+  const bordTop = (wallPosition + border);
+  const bordButtom = (wallPosition + wallSize + border);
+  const positionOut = (click - halfSp - wallPosition - border);
+
+  if (positionTop < bordTop) {
+    return `${positionOut + (bordTop - positionTop)}px`;
+  }
+
+  if (positionButtom > bordButtom) {
+    return `${positionOut - (positionButtom - bordButtom)}px`;
+  }
+
+  return `${positionOut}px`;
+}
+
 document.addEventListener('click', e => {
-  // write code here
+  const item = e.target.closest('.wall');
+
+  if (!document.contains(item)) {
+    return;
+  }
+  spider.style.top = coord(e.clientY, halfSpider, wallTop, wall.clientHeight);
+  spider.style.left = coord(e.clientX, halfSpider, wallLeft, wall.clientWidth);
 });
