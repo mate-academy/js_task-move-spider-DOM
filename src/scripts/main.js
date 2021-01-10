b9590de3afa0c396d91b0fdd61e9c48bf62156fd
Alex <alexmogwaiii@gmail.com>
2021-01-10 14:56:04 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,39 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+
+const getCoord = (coord, direction) => {
+  const wallMaxHeight = wall.clientHeight - spiderHeight;
+  const wallNaxWidth = wall.clientWidth - spiderWidth;
+
+  if (coord < 0) {
+    return 0;
+  }
+
+  if (coord > wallMaxHeight && direction === 'y') {
+    return wallMaxHeight;
+  }
+
+  if (coord > wallNaxWidth && direction === 'x') {
+    return wallNaxWidth;
+  }
+
+  return coord;
+};
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.className === 'wall') {
+    let y = e.offsetY - spiderHeight / 2;
+    let x = e.offsetX - spiderWidth / 2;
+
+    y = getCoord(y, 'y');
+    x = getCoord(x, 'x');
+
+    spider.style.top = `${y}px`;
+    spider.style.left = `${x}px`;
+  }
 });
