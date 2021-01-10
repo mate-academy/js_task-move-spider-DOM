6dcad5c2b81abe36023bdba85fc3749810322265
Andrey Gavriluk <bugabuga.008@gmail.com>
2021-01-10 22:38:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,21 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+  const spiderSize = spider.clientWidth;
+  const wallSize = wall.clientWidth;
+
+  if (e.target === wall) {
+    let leftPos = e.offsetX - spiderSize / 2;
+    let topPos = e.offsetY - spiderSize / 2;
+
+    leftPos = leftPos < 0 ? 0 : leftPos;
+    leftPos = leftPos > wallSize - spiderSize ? wallSize - spiderSize : leftPos;
+    topPos = topPos < 0 ? 0 : topPos;
+    topPos = topPos > wallSize - spiderSize ? wallSize - spiderSize : topPos;
+
+    spider.style.left = `${leftPos}px`;
+    spider.style.top = `${topPos}px`;
+  }
 });
