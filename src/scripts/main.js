32a37909832be0863bb42eca0b3a01c1d6b3f53b
NikitaTudakov <tudakov.nikita@gmail.com>
2021-01-11 18:41:17 +0200

    Some Correct

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,7 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-document.addEventListener('click', e => {
-  if (!e.target.classList.contains('wall')) {
-    return;
-  }
-
+wall.addEventListener('click', e => {
   const x = e.offsetX - (spider.clientWidth / 2);
   const y = e.offsetY - (spider.clientHeight / 2);
   const maxPointX = wall.clientWidth - spider.clientWidth;
