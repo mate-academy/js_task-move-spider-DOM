aaed29e6530cf93789cff04de9418d6c7046d0f2
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-01-11 14:56:18 +0200

    Moved vars and the function outside

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,23 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  const wall = document.querySelector('.wall');
-  const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
 
-  function setCoord(coord, outerSize, innerSize) {
-    const moveCoord = coord - innerSize / 2;
+function setCoord(coord, outerSize, innerSize) {
+  const moveCoord = coord - innerSize / 2;
 
-    if (moveCoord < 0) {
-      return 0;
-    }
-
-    if (moveCoord > (outerSize - innerSize)) {
-      return outerSize - innerSize;
-    }
-
-    return moveCoord;
+  if (moveCoord < 0) {
+    return 0;
   }
 
+  if (moveCoord > (outerSize - innerSize)) {
+    return outerSize - innerSize;
+  }
+
+  return moveCoord;
+}
+
+document.addEventListener('click', e => {
   if (e.target === wall) {
     spider.style.top
     = `${setCoord(e.offsetY, wall.clientHeight, spider.height)}px`;
