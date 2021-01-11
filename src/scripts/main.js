19ae3127390ae1c46529073122ffe4bd632aa98f
chri911 <delightsome@bigmir.net>
2021-01-11 20:50:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-document.addEventListener('click', () => {
+document.addEventListener('click', el => {
   const spider = document.querySelector('.spider');
   const wall = document.querySelector('.wall');
   const borderWidth = getComputedStyle(wall).borderLeftWidth;
@@ -9,24 +9,25 @@ document.addEventListener('click', () => {
   const topSide = wall.getBoundingClientRect().top;
   const bottomSide = wall.getBoundingClientRect().bottom;
 
-  if (event.clientX > leftSide && event.clientX < rightSide
-  && event.clientY > topSide && event.clientY < bottomSide) {
-    if (event.clientX < leftSide + spider.width) {
-      spider.style.left = 0;
-    } else if (event.clientX > rightSide - spider.width) {
-      spider.style.left = wall.clientWidth - spider.clientWidth + 'px';
-    } else {
-      spider.style.left = (event.clientX - leftSide
-        - Number.parseFloat(borderWidth) - spider.width / 2) + 'px';
-    }
+  if (el.target !== wall) {
+    return
+  }
 
-    if (event.clientY < topSide + spider.width) {
-      spider.style.top = 0;
-    } else if (event.clientY > bottomSide - spider.width) {
-      spider.style.top = wall.clientHeight - spider.clientHeight + 'px';
-    } else {
-      spider.style.top = (event.clientY - topSide
-        - Number.parseFloat(borderWidth) - spider.width / 2) + 'px';
-    }
+  if (event.clientX < leftSide + spider.width) {
+    spider.style.left = 0;
+  } else if (event.clientX > rightSide - spider.width) {
+    spider.style.left = wall.clientWidth - spider.clientWidth + 'px';
+  } else {
+    spider.style.left = (event.clientX - leftSide
+      - Number.parseFloat(borderWidth) - spider.width / 2) + 'px';
+  }
+
+  if (event.clientY < topSide + spider.width) {
+    spider.style.top = 0;
+  } else if (event.clientY > bottomSide - spider.width) {
+    spider.style.top = wall.clientHeight - spider.clientHeight + 'px';
+  } else {
+    spider.style.top = (event.clientY - topSide
+      - Number.parseFloat(borderWidth) - spider.width / 2) + 'px';
   }
 });
