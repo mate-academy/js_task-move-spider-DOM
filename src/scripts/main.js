3d50d3e6180a7f60dabacc8fcad8a2d3fa60f0e3
Olesia <olesia.tk@gmail.com>
2021-01-11 16:27:38 +0200

    Fixed with creating function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,38 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  const wall = document.querySelector('.wall');
-  const spider = document.querySelector('.spider');
-
-  const maxTop = wall.clientHeight - spider.clientHeight;
-  const maxLeft = wall.clientWidth - spider.clientWidth;
-
-  if (e.target === wall) {
-    spider.style.left = `${e.clientX - wall.offsetLeft
-      - spider.clientWidth / 2 - wall.clientLeft}px`;
-
-    spider.style.top = `${e.clientY - wall.offsetTop
-      - spider.clientHeight / 2 - wall.clientTop}px`;
-
-    if (parseFloat(spider.style.left) < 0) {
-      spider.style.left = 0;
-    }
-
-    if (parseFloat(spider.style.top) < 0) {
-      spider.style.top = 0;
-    }
-
-    if (parseFloat(spider.style.left) > maxLeft) {
-      spider.style.left = `${maxLeft}px`;
-    }
-
-    if (parseFloat(spider.style.top) > maxTop) {
-      spider.style.top = `${maxTop}px`;
-    }
+  if (e.target !== wall) {
+    return;
   }
+  moveElement(e.clientX, e.clientY, wall, spider);
 });
+
+function moveElement(X, Y, box, element) {
+  const maxTop = box.clientHeight - element.clientHeight;
+  const maxLeft = box.clientWidth - element.clientWidth;
+
+  element.style.left = `${X - box.offsetLeft
+    - element.clientWidth / 2 - box.clientLeft}px`;
+
+  element.style.top = `${Y - box.offsetTop
+    - element.clientHeight / 2 - box.clientTop}px`;
+
+  if (parseFloat(element.style.left) < 0) {
+    element.style.left = 0;
+  }
+
+  if (parseFloat(element.style.top) < 0) {
+    element.style.top = 0;
+  }
+
+  if (parseFloat(element.style.left) > maxLeft) {
+    element.style.left = `${maxLeft}px`;
+  }
+
+  if (parseFloat(element.style.top) > maxTop) {
+    element.style.top = `${maxTop}px`;
+  }
+}
