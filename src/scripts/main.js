fefbb67b412395dcbeaa64eb354cc26e27b813e2
MaximZhuravlov <maksym.zhuravlov1995@gmail.com>
2021-01-11 16:43:53 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,52 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const rectangle = e.target.getBoundingClientRect();
+  const x = e.clientX - rectangle.left
+    - parseInt(getComputedStyle(wall).borderLeftWidth);
+  const y = e.clientY - rectangle.top
+    - parseInt(getComputedStyle(wall).borderTopWidth);
+
+  moveSpider(x, y);
 });
+
+/**
+ * Moves the spider to the specified coordinates.
+ * @param {number} x x coordinate.
+ * @param {number} y y coordinate.
+ */
+function moveSpider(x, y) {
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  let leftOffset = x - spiderWidth / 2;
+  let topOffset = y - spiderHeight / 2;
+
+  if (x < spiderWidth / 2) {
+    leftOffset = 0;
+  }
+
+  if (x > wallWidth - spiderWidth / 2) {
+    leftOffset = wallWidth - spiderWidth;
+  }
+
+  if (y < spiderHeight / 2) {
+    topOffset = 0;
+  }
+
+  if (y > wallHeight - spiderHeight / 2) {
+    topOffset = wallHeight - spiderHeight;
+  }
+
+  spider.style.top = `${topOffset}px`;
+  spider.style.left = `${leftOffset}px`;
+}
