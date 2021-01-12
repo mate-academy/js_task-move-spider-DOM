a9492e52e072617a4727d6443084336578b53b35
Rostyslav Meleshko <meleshkorostyslav@gmail.com>
2021-01-12 20:43:31 +0200

    Solution after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,22 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  const wall = document.querySelector('.wall');
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
 
-  const spider = document.querySelector('.spider');
+wall.addEventListener('click', e => {
+  let spiderCoordsX = (e.clientX - wall.offsetLeft
+    - wall.clientLeft - spider.clientWidth / 2);
+  let spiderCoordsY = (e.clientY - wall.offsetTop
+     - wall.clientTop - spider.clientHeight / 2);
 
-  const borderX = wall.clientLeft;
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
 
-  const borderY = wall.clientTop;
+  spiderCoordsX = spiderCoordsX < 0 ? 0 : spiderCoordsX;
+  spiderCoordsY = spiderCoordsY < 0 ? 0 : spiderCoordsY;
+  spiderCoordsX = spiderCoordsX > maxX ? maxX : spiderCoordsX;
+  spiderCoordsY = spiderCoordsY > maxY ? maxY : spiderCoordsY;
 
-  const X = e.clientX - borderX - wall.offsetLeft;
-
-  const Y = e.clientY - borderY - wall.offsetTop;
-
-  if (X >= -borderX && X <= wall.clientWidth + borderX
-    && Y >= -borderY && Y <= wall.clientHeight + borderY) {
-    if (X < spider.clientWidth / 2) {
-      spider.style.left = '0px';
-    } else if (X > wall.clientWidth - spider.clientWidth / 2) {
-      spider.style.left = `${wall.clientWidth - spider.clientWidth}px`;
-    } else {
-      spider.style.left = `${X - spider.clientWidth / 2}px`;
-    }
-
-    if (Y < spider.clientHeight / 2) {
-      spider.style.top = '0px';
-    } else if (Y > wall.clientHeight - spider.clientHeight / 2) {
-      spider.style.top = `${wall.clientHeight - spider.clientHeight}px`;
-    } else {
-      spider.style.top = `${Y - spider.clientHeight / 2}px`;
-    }
-  }
+  spider.style.left = spiderCoordsX + 'px';
+  spider.style.top = spiderCoordsY + 'px';
 });
