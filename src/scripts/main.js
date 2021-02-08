34b6e48dbcfce04015516fa0ca0f7d93f1a3e9f9
Georgy Dzumenko <erterj05@gmail.com>
2021-02-08 20:55:44 +0200

    After some fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,24 +6,31 @@ const wall = document.querySelector('div');
 
 document.addEventListener('click', e => {
   const item = e.target;
-  const coordsY = e.offsetY - spiderParams.width / 2;
-  const coordsX = e.offsetX - spiderParams.height / 2;
 
-  if (!item.classList.contains('wall')) {
+  if (!item.classList.contains('wall') && !item.classList.contains('spider')) {
     return;
   }
 
-  if (e.offsetX > wall.clientWidth - spiderParams.width) {
+  let coordsY = e.offsetY - spiderParams.width / 2;
+  let coordsX = e.offsetX - spiderParams.height / 2;
+  const isSpider = item.classList.contains('spider');
+
+  if (isSpider) {
+    coordsY += parseFloat(spider.style.top);
+    coordsX += parseFloat(spider.style.left);
+  }
+
+  if (e.offsetX > wall.clientWidth - spiderParams.width && !isSpider) {
     spider.style.left = `${wall.clientWidth - spiderParams.width}px`;
-  } else if (e.offsetX < 0 + spiderParams.width) {
+  } else if (e.offsetX < 0 + spiderParams.width && !isSpider) {
     spider.style.left = `${0}px`;
   } else {
     spider.style.left = `${coordsX}px`;
   }
 
-  if (e.offsetY > wall.clientHeight - spiderParams.height) {
+  if (e.offsetY > wall.clientHeight - spiderParams.height && !isSpider) {
     spider.style.top = `${wall.clientHeight - spiderParams.height}px`;
-  } else if (e.offsetY < 0 + spiderParams.height) {
+  } else if (e.offsetY < 0 + spiderParams.height && !isSpider) {
     spider.style.top = `${0}px`;
   } else {
     spider.style.top = `${coordsY}px`;
