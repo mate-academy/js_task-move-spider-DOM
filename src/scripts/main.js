85234b9f1f7c5e531f766155613e5aaca5343c9b
Artem Fedoseev <afedoseev359@gmail.com>
2021-02-17 13:33:32 +0200

    Substituted if statements for limitCoord function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,16 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-const borderW = parseFloat(getComputedStyle(wall).borderWidth);
-const spiderW = spider.offsetWidth;
-const spiderH = spider.offsetHeight;
-const leftEdge = wall.getBoundingClientRect().left + borderW;
-const topEdge = wall.getBoundingClientRect().top + borderW;
-const maxX = wall.clientWidth - spiderW;
-const maxY = wall.clientHeight - spiderH;
 
 document.addEventListener('click', e => {
+  const borderW = parseFloat(getComputedStyle(wall).borderWidth);
+  const spiderW = spider.offsetWidth;
+  const spiderH = spider.offsetHeight;
+  const leftEdge = wall.getBoundingClientRect().left + borderW;
+  const topEdge = wall.getBoundingClientRect().top + borderW;
+  const maxX = wall.clientWidth - spiderW;
+  const maxY = wall.clientHeight - spiderH;
+
   if (!e.target.classList.contains('wall')) {
     return;
   }
@@ -18,18 +19,13 @@ document.addEventListener('click', e => {
   let spiderX = e.clientX - leftEdge - (spiderW / 2);
   let spiderY = e.clientY - topEdge - (spiderH / 2);
 
-  if (spiderX < 0) {
-    spiderX = 0;
-  } else if (spiderX > maxX) {
-    spiderX = maxX;
-  }
-
-  if (spiderY < 0) {
-    spiderY = 0;
-  } else if (spiderY > maxY) {
-    spiderY = maxY;
-  }
+  spiderX = limitCoord(spiderX, maxX);
+  spiderY = limitCoord(spiderY, maxY);
 
   spider.style.top = `${spiderY}px`;
   spider.style.left = `${spiderX}px`;
 });
+
+function limitCoord(coord, max, min = 0) {
+  return Math.max(coord, min) > min ? Math.min(coord, max) : min;
+}
