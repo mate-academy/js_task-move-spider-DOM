a6a81aacfe6f65c6ced9c164b13b86c6acc84411
chri911 <delightsome@bigmir.net>
2021-03-01 12:44:00 +0200

    fix solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,23 +4,23 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 wall.addEventListener('click', e => {
-  if (event.target === spider) {
+  if (e.target === spider) {
     return;
   }
 
-  if (event.offsetX <= spider.clientWidth / 2) {
+  if (e.offsetX <= spider.clientWidth / 2) {
     spider.style.left = 0;
-  } else if (event.offsetX >= wall.clientWidth - spider.clientWidth) {
+  } else if (e.offsetX >= wall.clientWidth - spider.clientWidth) {
     spider.style.left = wall.clientWidth - spider.clientWidth + 'px';
   } else {
-    spider.style.left = event.offsetX - spider.clientWidth / 2 + 'px';
+    spider.style.left = e.offsetX - spider.clientWidth / 2 + 'px';
   }
 
-  if (event.offsetY <= spider.clientHeight / 2) {
+  if (e.offsetY <= spider.clientHeight / 2) {
     spider.style.top = 0;
-  } else if (event.offsetY >= wall.clientHeight - spider.clientHeight) {
+  } else if (e.offsetY >= wall.clientHeight - spider.clientHeight) {
     spider.style.top = wall.clientHeight - spider.clientHeight + 'px';
   } else {
-    spider.style.top = event.offsetY - spider.clientHeight / 2 + 'px';
+    spider.style.top = e.offsetY - spider.clientHeight / 2 + 'px';
   }
 });
