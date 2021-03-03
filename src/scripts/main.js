44a8ed08a7087b88c517cb21603ae2d097024560
Igor Lisogor <illisogori@gmail.com>
2021-03-03 14:49:13 +0200

    Spider movement fixed.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BODY') {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const borderPaddingWidth = (wall.getBoundingClientRect().width - wall.clientWidth) / 2;
+  const spiderCenterX = spider.width / 2;
+  const spiderCenterY = spider.width / 2;
+  let spiderPositionY = e.clientY - wall.offsetTop - borderPaddingWidth - spiderCenterY;
+  let spiderPositionX = e.clientX - wall.offsetLeft - borderPaddingWidth - spiderCenterX;
+
+  if (e.clientX < wall.offsetLeft + borderPaddingWidth) {
+    spiderPositionX = 0;
+  }
+
+  if (e.clientX + spider.width > wall.clientWidth + wall.offsetLeft) {
+    spiderPositionX = wall.clientWidth - spider.width;
+  }
+
+  if (e.clientY < wall.offsetTop + borderPaddingWidth) {
+    spiderPositionY = 0;
+  }
+
+  if (e.clientY + spider.height > wall.clientHeight + wall.offsetTop) {
+    spiderPositionY = wall.clientHeight - spider.height;
+  }
+
+  spider.style.left = spiderPositionX + 'px';
+  spider.style.top = spiderPositionY + 'px';
 });
