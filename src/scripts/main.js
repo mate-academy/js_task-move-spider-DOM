d91e9956c150af5ff2afb224fc9369ba76d3efde
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-03-11 12:56:39 +0200

    added movings for spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target === spider) {
+    return;
+  }
+
+  let mouseX = e.offsetX;
+  let mouseY = e.offsetY;
+  const wallHeight = wall.clientHeight;
+  const wallWidth = wall.clientWidth;
+  const spiderHeight = spider.clientHeight;
+  const spiderWidth = spider.clientWidth;
+
+  if (mouseX <= spiderWidth / 2) {
+    mouseX = spiderWidth / 2;
+  }
+
+  if (mouseX >= wallWidth - spiderWidth / 2) {
+    mouseX = wallWidth - (spiderWidth / 2);
+  }
+
+  if (mouseY <= spiderHeight / 2) {
+    mouseY = spiderHeight / 2;
+  }
+
+  if (mouseY >= wallHeight - spiderHeight / 2) {
+    mouseY = wallHeight - (spiderHeight / 2);
+  }
+  spider.style.top = `${mouseY - spider.offsetHeight / 2}px`;
+  spider.style.left = `${mouseX - spider.offsetWidth / 2}px`;
 });
