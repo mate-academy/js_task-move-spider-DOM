664979505cedadd4dfc16faba2dd2f0d06353e4a
Oleg Bezriadin <muhachov86@gmail.com>
2021-03-12 15:21:52 +0200

    SpiderSize become an object

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,10 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-const spiderSize = [
-  spider.clientWidth / 2,
-  spider.clientHeight / 2,
-];
+const spiderSize = {
+  thick: (spider.clientWidth / 2),
+  tall: (spider.clientHeight / 2),
+};
 const borderSize = wall.clientLeft;
 
 document.addEventListener('click', e => {
@@ -16,31 +16,33 @@ document.addEventListener('click', e => {
   let spiderX;
   let spiderY;
 
-  const maxOpenWidth = wall.clientWidth - spiderSize[0];
-  const maxOpenHeight = wall.clientWidth - spiderSize[1];
+  const maxOpenWidth = wall.clientWidth - spiderSize.thick;
+  const maxOpenHeight = wall.clientWidth - spiderSize.tall;
 
-  if (e.offsetX - spiderSize[0] > maxOpenWidth - spiderSize[0] - borderSize) {
-    spiderX = (maxOpenWidth - spiderSize[1]) + 'px';
+  if (e.offsetX - spiderSize.thick
+        > maxOpenWidth - spiderSize.thick - borderSize) {
+    spiderX = (maxOpenWidth - spiderSize.tall) + 'px';
   }
 
-  if (e.offsetX - spiderSize[0] <= 0) {
+  if (e.offsetX - spiderSize.thick <= 0) {
     spiderX = 0;
   }
 
-  if (e.offsetY - spiderSize[1] > maxOpenHeight - spiderSize[1] - borderSize) {
-    spiderY = (maxOpenHeight - spiderSize[1]) + 'px';
+  if (e.offsetY - spiderSize.tall
+        > maxOpenHeight - spiderSize.tall - borderSize) {
+    spiderY = (maxOpenHeight - spiderSize.tall) + 'px';
   }
 
-  if (e.offsetY - spiderSize[1] <= 0) {
+  if (e.offsetY - spiderSize.tall <= 0) {
     spiderY = 0;
   }
 
   if (spiderY === undefined) {
-    spiderY = e.offsetY - spiderSize[1] + 'px';
+    spiderY = e.offsetY - spiderSize.tall + 'px';
   }
 
   if (spiderX === undefined) {
-    spiderX = e.offsetX - spiderSize[0] + 'px';
+    spiderX = e.offsetX - spiderSize.thick + 'px';
   }
 
   spider.style.top = spiderY;
