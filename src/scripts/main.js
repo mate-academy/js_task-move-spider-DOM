8417364e36550d26c1d9a8da2faf8183c8738759
Bohdan Prokopov <b.prokopov96@gmail.com>
2021-03-27 20:53:50 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  e.preventDefault();
+
+  if (e.target.tagName !== 'DIV') {
+    return;
+  };
+
+  let coordsLeft = e.clientX - wall.getBoundingClientRect().left
+  - wall.clientLeft - spider.clientWidth / 2;
+  let coordsTop = e.clientY - wall.getBoundingClientRect().top
+  - wall.clientTop - spider.clientHeight / 2;
+
+  if (coordsLeft < 0) {
+    coordsLeft = 0;
+  };
+
+  if (coordsTop < 0) {
+    coordsTop = 0;
+  };
+
+  if (coordsLeft + spider.clientWidth > wall.clientWidth) {
+    coordsLeft = wall.clientWidth - spider.clientWidth;
+  };
+
+  if (coordsTop + spider.clientWidth > wall.clientWidth) {
+    coordsTop = wall.clientWidth - spider.clientWidth;
+  };
+
+  spider.style.top = `${coordsTop}px`;
+  spider.style.left = `${coordsLeft}px`;
 });
