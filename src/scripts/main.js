f3e0edad053307a1678cff5006e77f66b6daafd9
dinexpod <work.toxa@gmail.com>
2021-04-01 21:17:18 +0300

    do code review suggestions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,34 @@
 'use strict';
 
 const wall = document.querySelector('.wall');
-const body = wall.parentElement;
 const spider = wall.querySelector('.spider');
 
-document.addEventListener('click', e => {
-  const wallTop = (body.offsetHeight - wall.offsetHeight) / 2;
-  const wallLeft = (body.offsetWidth - wall.offsetWidth) / 2;
+wall.addEventListener('click', e => {
+  const wallTop = (wall.parentElement.offsetHeight - wall.offsetHeight) / 2;
+  const wallLeft = (wall.parentElement.offsetWidth - wall.offsetWidth) / 2;
   const halfSpider = spider.offsetWidth / 2;
 
-  if (e.clientX >= wallLeft
-    && e.clientX < (wallLeft + wall.offsetWidth)
-    && e.clientY >= wallTop
-    && e.clientY < (wallTop + wall.offsetHeight)
-  ) {
-    const spiderLeft = e.clientX - wall.clientLeft - wallLeft - halfSpider;
-    const spiderTop = e.clientY - wall.clientTop - wallTop - halfSpider;
+  const spiderPlace = {
+    spiderLeft: e.clientX - wall.clientLeft - wallLeft - halfSpider,
+    spiderTop: e.clientY - wall.clientTop - wallTop - halfSpider,
+  };
 
-    if (spiderLeft < 0) {
-      spider.style.left = '0';
-    } else if (wall.clientWidth < spider.offsetWidth + spiderLeft) {
-      spider.style.left = (wall.clientWidth - spider.clientWidth) + 'px';
-    } else {
-      spider.style.left = spiderLeft + 'px';
-    }
-
-    if (spiderTop < 0) {
-      spider.style.top = '0';
-    } else if (wall.clientHeight < spider.offsetHeight + spiderTop) {
-      spider.style.top = (wall.clientHeight - spider.clientHeight) + 'px';
-    } else {
-      spider.style.top = spiderTop + 'px';
-    }
+  if (spiderPlace.spiderLeft < 0) {
+    spiderPlace.spiderLeft = 0;
   }
+
+  if (wall.clientWidth < spider.offsetWidth + spiderPlace.spiderLeft) {
+    spiderPlace.spiderLeft = (wall.clientWidth - spider.clientWidth);
+  }
+
+  if (spiderPlace.spiderTop < 0) {
+    spiderPlace.spiderTop = 0;
+  }
+
+  if (wall.clientHeight < spider.offsetHeight + spiderPlace.spiderTop) {
+    spiderPlace.spiderTop = (wall.clientHeight - spider.clientHeight);
+  }
+
+  spider.style.top = spiderPlace.spiderTop + 'px';
+  spider.style.left = spiderPlace.spiderLeft + 'px';
 });
