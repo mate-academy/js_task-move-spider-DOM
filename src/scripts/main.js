7d2a4690350d2818ecc725cba3dc6d9380678fd2
Maksym Matuliak <maksymmatuliak@gmail.com>
2021-04-05 19:58:10 +0300

    added listener only on wall

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,11 @@
 const spider = document.getElementsByClassName('spider')[0];
 const wall = document.getElementsByClassName('wall')[0];
 
-document.addEventListener('click', e => {
+wall.addEventListener('click', e => {
   const startOfWallX = wall.getBoundingClientRect().x + wall.clientLeft;
   const startOfWallY = wall.getBoundingClientRect().y + wall.clientTop;
   const endOfWallX = wall.clientWidth + startOfWallX;
   const endOfWallY = wall.clientHeight + startOfWallY;
-
-  if (isOutOfTheWall(e)) {
-    return;
-  }
 
   if (e.clientX < startOfWallX + spider.width / 2) {
     spider.style.left = 0;
@@ -29,10 +25,3 @@ document.addEventListener('click', e => {
     spider.style.top = e.clientY - startOfWallY - spider.height / 2 + 'px';
   }
 });
-
-function isOutOfTheWall(e) {
-  return e.clientX < wall.getClientRects()[0].x
-    || e.clientY < wall.getClientRects()[0].y
-    || e.clientX > wall.getClientRects()[0].x + wall.offsetWidth
-    || e.clientY > wall.getClientRects()[0].y + wall.offsetHeight;
-}
