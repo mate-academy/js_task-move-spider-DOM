e12f7ca87f17085f72f4b28787aa3ea0b48ff4f2
Mariia Kornieva <mariiakornieva@gmail.com>
2021-04-11 20:30:38 +0300

    Improved solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,34 +4,24 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 document.addEventListener('click', e => {
-  const wallLeft = wall.offsetLeft + wall.clientLeft;
-  const wallRight = wall.clientWidth + wall.offsetLeft + wall.clientLeft;
-  const wallTop = wall.offsetTop + wall.clientTop;
-  const wallBottom = wall.clientHeight + wall.offsetTop + wall.clientTop;
-
-  const horizontal = e.clientX >= wallLeft && e.clientX <= wallRight;
-  const vertical = e.clientY >= wallTop && e.clientY <= wallBottom;
-
-  if (horizontal && vertical) {
-    const topOffset = e.clientY - wallTop - (spider.clientHeight / 2);
-    const bottomOffset = wallBottom - e.clientY;
-    const leftOffset = e.clientX - wallLeft - (spider.clientWidth / 2);
-    const rightOffset = wallRight - e.clientX;
-
-    if (topOffset < 0) {
-      spider.style.top = '0px';
-    } else if (bottomOffset < (spider.clientHeight / 2)) {
-      spider.style.top = wall.clientHeight - spider.clientHeight + 'px';
-    } else {
-      spider.style.top = topOffset + 'px';
-    }
-
-    if (leftOffset < 0) {
-      spider.style.left = '0px';
-    } else if (rightOffset < (spider.clientWidth / 2)) {
-      spider.style.left = wall.clientWidth - spider.clientWidth + 'px';
-    } else {
-      spider.style.left = leftOffset + 'px';
-    }
+  if (document.elementFromPoint(e.clientX, e.clientY) !== wall) {
+    return;
   }
+
+  const wallBottom = wall.clientHeight - spider.clientHeight;
+  const wallRight = wall.clientWidth - spider.clientWidth;
+
+  let spiderPosX = e.clientX - wall.clientLeft - wall.offsetLeft
+    - (spider.clientWidth / 2);
+  let spiderPosY = e.clientY - wall.clientTop - wall.offsetTop
+    - (spider.clientHeight / 2);
+
+  spiderPosX = spiderPosX < 0 ? 0 : spiderPosX;
+  spiderPosX = spiderPosX > wallRight ? wallRight : spiderPosX;
+
+  spiderPosY = spiderPosY < 0 ? 0 : spiderPosY;
+  spiderPosY = spiderPosY > wallBottom ? wallBottom : spiderPosY;
+
+  spider.style.top = `${spiderPosY}px`;
+  spider.style.left = `${spiderPosX}px`;
 });
