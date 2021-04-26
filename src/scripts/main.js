3b1928c0d4e19b2bb4f6730864ca07eba85f7b2d
gennady.tabala <gennady.tabala@gmail.com>
2021-04-26 22:14:35 +0300

    Solution 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,21 @@
 'use strict';
 
 document.querySelector('.wall').addEventListener('click', e => {
-  const wallElem = document.querySelector('.wall');
-  const wallClientRect = wallElem.getBoundingClientRect();
-  const wallBorder = Number.parseInt(getComputedStyle(wallElem).borderTopWidth);
-  const wallLeft = wallClientRect.left + wallBorder + window.pageXOffset;
-  const wallTop = wallClientRect.top + wallBorder + window.pageYOffset;
-  const wallRight = wallClientRect.right - wallBorder + window.pageXOffset;
-  const wallBottom = wallClientRect.bottom - wallBorder + window.pageYOffset;
-  const wallHeight = wallBottom - wallTop;
-  const wallWidth = wallRight - wallLeft;
-  const spiderElem = document.querySelector('.spider');
-  const spiderClientRect = spiderElem.getBoundingClientRect();
-  const spiderWidth = spiderClientRect.right - spiderClientRect.left;
-  const spiderHeight = spiderClientRect.bottom - spiderClientRect.top;
-  const coords = {
-    left: e.clientX - (spiderWidth) / 2 + window.pageXOffset,
-    right: e.clientX + (spiderWidth) / 2 + window.pageXOffset,
-    top: e.clientY - (spiderHeight) / 2 + window.pageYOffset,
-    bottom: e.clientY + (spiderHeight) / 2 + window.pageYOffset,
-  };
-  let correctedLeft = coords.left < wallLeft ? 0 : coords.left - wallLeft;
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-  correctedLeft = coords.right > wallRight
-    ? wallWidth - spiderWidth : correctedLeft;
+  const xShift = wall.offsetLeft + wall.clientLeft + spider.offsetWidth / 2;
+  const yShift = wall.offsetTop + wall.clientTop + spider.offsetHeight / 2;
 
-  let correctedTop = coords.top < wallTop ? 0 : coords.top - wallTop;
+  let x = e.clientX - xShift;
+  let y = e.clientY - yShift;
 
-  correctedTop = coords.bottom > wallBottom
-    ? wallHeight - spiderHeight : correctedTop;
-  spiderElem.style.left = `${correctedLeft}px`;
-  spiderElem.style.top = `${correctedTop}px`;
+  x = Math.max(0, x);
+  y = Math.max(0, y);
+
+  x = Math.min(x, wall.clientWidth - spider.offsetWidth);
+  y = Math.min(y, wall.clientHeight - spider.offsetHeight);
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
