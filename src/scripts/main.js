d8093252c175de3df8fc227b6080dc6c015fb5ea
romandubovskyi <rdubowskij@gmail.com>
2021-05-18 12:06:48 +0300

    moved spider on click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = e.target.closest('.wall');
+
+  if (!wall) {
+    return;
+  }
+
+  const wallBorder = Number.parseInt(getComputedStyle(wall).border);
+
+  const wallX = wall.getBoundingClientRect().x;
+  const wallY = wall.getBoundingClientRect().y;
+
+  const targetX = e.clientX - wallX - (spider.offsetWidth / 2) - wallBorder;
+  const targetY = e.clientY - wallY - (spider.offsetHeight / 2) - wallBorder;
+
+  const maxX = wall.offsetWidth - (spider.offsetWidth) - wallBorder * 2;
+  const maxY = wall.offsetHeight - (spider.offsetHeight) - wallBorder * 2;
+
+  spider.style.left = validateBorders(targetX, maxX) + 'px';
+  spider.style.top = validateBorders(targetY, maxY) + 'px';
 });
+
+function validateBorders(coord, maxValue) {
+  return coord < 0 ? 0 : coord > maxValue ? maxValue : coord;
+}
