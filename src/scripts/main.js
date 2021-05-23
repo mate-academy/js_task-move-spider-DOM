b8c4b63487f605b17049f2f88944d95f372b8de8
Serhii Kolesnyk <kolesniks203@gmail.com>
2021-05-23 09:18:35 +0300

    move spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', e => {
+  let x = e.clientX - wall.offsetLeft - wall.clientLeft
+    - spider.offsetWidth / 2;
+  let y = e.clientY - wall.offsetTop - wall.clientTop
+    - spider.offsetHeight / 2;
+  const limit = wall.clientWidth - spider.offsetWidth;
+
+  if (x > limit) {
+    x = limit;
+  }
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (y > limit) {
+    y = limit;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
