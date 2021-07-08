72c153bdd822707452be22ded520ca763d8a4538
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-08 17:43:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,42 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const wallStyle = window.getComputedStyle(wall);
+const spiderStyle = window.getComputedStyle(spider);
+const maxTopPosition
+  = parseFloat(wallStyle.height) - parseFloat(spiderStyle.height);
+const maxLeftPosition
+  = parseFloat(wallStyle.width) - parseFloat(spiderStyle.width);
+const halfOfSpider
+  = parseFloat(wallStyle.borderWidth) + parseFloat(spiderStyle.width) / 2;
+
 document.addEventListener('click', e => {
-  // write code here
+  const clickIntoWall = e.target.closest('.wall');
+
+  if (clickIntoWall) {
+    let calcTop;
+    let calcLeft;
+    const calcX = e.clientX - wall.offsetLeft;
+    const calcY = e.clientY - wall.offsetTop;
+
+    if (calcY > maxTopPosition) {
+      calcTop = 350;
+    } else if (calcY > halfOfSpider) {
+      calcTop = calcY - halfOfSpider;
+    } else {
+      calcTop = 0;
+    }
+
+    if (calcX > maxLeftPosition) {
+      calcLeft = 350;
+    } else if (calcX > halfOfSpider) {
+      calcLeft = calcX - halfOfSpider;
+    } else {
+      calcLeft = 0;
+    }
+
+    spider.style.top = `${calcTop}px`;
+    spider.style.left = `${calcLeft}px`;
+  }
 });
