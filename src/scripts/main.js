62ad60aef2fdd9e6daa316646d1e9f6c66f7e197
David Dohru <tronsmartmy@gmail.com>
2021-07-13 01:52:46 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const wallSize = wall.clientWidth;
+  const spider = document.querySelector('.spider');
+
+  wall.addEventListener('click', (occasion) => {
+    let CoordinateX = occasion.offsetX;
+    let CoordinateY = occasion.offsetY;
+    const halfSpiderWidth = spider.clientWidth / 2;
+
+    if (CoordinateX >= wallSize - halfSpiderWidth) {
+      CoordinateX = wallSize - halfSpiderWidth;
+    } else if (CoordinateX < halfSpiderWidth) {
+      CoordinateX = halfSpiderWidth;
+    }
+
+    if (CoordinateY >= wallSize - halfSpiderWidth) {
+      CoordinateY = wallSize - halfSpiderWidth;
+    } else if (CoordinateY < halfSpiderWidth) {
+      CoordinateY = halfSpiderWidth;
+    }
+    spider.style.left = CoordinateX - halfSpiderWidth + 'px';
+    spider.style.top = CoordinateY - halfSpiderWidth + 'px';
+  });
 });
