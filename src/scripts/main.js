4d4534104aad75a017299babc2890126e7d5b035
Andriy Fesych <andriyhimself@gmail.com>
2021-07-15 23:15:52 +0300

    Solution_0.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,40 +2,24 @@
 
 document.addEventListener('click', e => {
   const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
   const wallStyle = getComputedStyle(wall);
   const wallWidth = parseFloat(wallStyle.width);
   const wallHeight = parseFloat(wallStyle.height);
   const wallBorder = (parseFloat(wallStyle.getPropertyValue('border-width')));
-  const spider = document.querySelector('.spider');
 
-  let coordX = e.clientX - wall.offsetLeft
+  let x = e.clientX - wall.offsetLeft
   - (parseFloat(spider.width) / 2) - wallBorder;
-
-  let coordY = e.clientY - wall.offsetTop
+  let y = e.clientY - wall.offsetTop
   - (parseFloat(spider.height) / 2) - wallBorder;
 
-  if (coordX <= 0) {
-    coordX = 0;
-  }
+  (x <= 0) && (x = 0);
+  (x >= wallWidth - spider.width) && (x = wallWidth - spider.width);
+  (y <= 0) && (y = 0);
+  (y >= wallHeight - spider.height) && (y = wallHeight - spider.height);
 
-  if (coordX >= wallWidth - spider.width) {
-    coordX = wallWidth - spider.width;
-  }
-
-  if (coordY <= 0) {
-    coordY = 0;
-  }
-
-  if (coordY >= wallHeight - spider.height) {
-    coordY = wallHeight - spider.height;
-  }
-
-  if ((e.clientX - wall.offsetLeft >= 0
-    && e.clientX - wall.offsetLeft - wallBorder * 2 <= wallWidth)
-    && (e.clientY - wall.offsetTop >= 0
-      && e.clientY - wall.offsetTop - wallBorder * 2 <= wallHeight)
-  ) {
-    spider.style.left = `${coordX}px`;
-    spider.style.top = `${coordY}px`;
+  if (wall.contains(e.target)) {
+    spider.style.left = `${x}px`;
+    spider.style.top = `${y}px`;
   }
 });
