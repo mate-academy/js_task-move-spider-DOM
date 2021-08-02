7d08f9f6e56bc869143766857ec5b105c92efcc7
Dmytro Prontenko <dprontenko@gmail.com>
2021-08-02 10:44:13 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,35 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallCoords = wall.getBoundingClientRect();
+
+  if (e.target.className === 'wall') {
+    const spiderCoords = {
+      top: e.clientY - wallCoords.top - wall.clientTop
+      - spider.clientHeight / 2,
+      left: e.clientX - wallCoords.left - wall.clientLeft
+      - spider.clientWidth / 2,
+    };
+
+    if (spiderCoords.top < 0) {
+      spiderCoords.top = 0;
+    };
+
+    if (spiderCoords.left < 0) {
+      spiderCoords.left = 0;
+    }
+
+    if (spiderCoords.left + spider.clientWidth > wall.clientWidth) {
+      spiderCoords.left = wall.clientWidth - spider.clientWidth;
+    }
+
+    if (spiderCoords.top + spider.clientHeight > wall.clientHeight) {
+      spiderCoords.top = wall.clientHeight - spider.clientHeight;
+    }
+
+    spider.style.left = spiderCoords.left + 'px';
+    spider.style.top = spiderCoords.top + 'px';
+  }
 });
