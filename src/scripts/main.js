ddb070c3673d85d703aa402f9a57b952918e0c72
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-05 15:49:45 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+
+wall.addEventListener('click', e => {
+  let yCoords = e.offsetY - (spiderHeight / 2);
+  let xCoords = e.offsetX - (spiderHeight / 2);
+
+  const yLimit = wallWidth - spiderHeight;
+  const xLimit = wallHeight - spiderWidth;
+
+  xCoords = Math.max(0, xCoords);
+  xCoords = Math.min(xLimit, xCoords);
+  yCoords = Math.max(0, yCoords);
+  yCoords = Math.min(yLimit, yCoords);
+
+  spider.style.top = `${yCoords}px`;
+  spider.style.left = `${xCoords}px`;
 });
