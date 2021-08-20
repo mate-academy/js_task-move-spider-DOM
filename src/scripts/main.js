66e75adc66bac1a0cb2a80d2c110997459a89261
sergei-trofimov <feast.st@gmail.com>
2021-08-20 14:23:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', e => {
+  const spiderSize = spider.getBoundingClientRect();
+  const wallSize = getComputedStyle(wall);
+  let topPosition = e.offsetY - (spiderSize.height / 2);
+  let leftPosition = e.offsetX - (spiderSize.width / 2);
+
+  if (e.path[0].classList.contains('spider')) {
+    return;
+  }
+
+  if (topPosition < 0) {
+    topPosition = 0;
+  }
+
+  if (e.offsetY + spiderSize.height > parseInt(wallSize.height)) {
+    topPosition = parseInt(wallSize.height) - spiderSize.height;
+  }
+
+  if (leftPosition < 0) {
+    leftPosition = 0;
+  }
+
+  if (e.offsetX + spiderSize.width > parseInt(wallSize.width)) {
+    leftPosition = parseInt(wallSize.width) - spiderSize.width;
+  }
+
+  spider.style.top = `${topPosition}px`;
+  spider.style.left = `${leftPosition}px`;
 });
