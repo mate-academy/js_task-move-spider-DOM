872061a9fbaf24cfa8cd2dfb0da7606032204d84
MaxStets <weawer458@gmail.com>
2021-09-30 19:36:16 +0300

    Solution -- 'move-spider-DOM'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', e => {
+  const wallCoords = wall.getBoundingClientRect();
+  const clickX = e.clientX;
+  const clickY = e.clientY;
+  const spiderSize = {
+    width: spider.clientWidth,
+    height: spider.clientHeight,
+  };
+
+  const coordsSpider = {
+    left: clickX - wallCoords.left - spider.clientWidth / 2,
+    top: clickY - wallCoords.top - spider.clientHeight / 2,
+  };
+
+  if (coordsSpider.top < 0) {
+    coordsSpider.top = 0;
+  }
+
+  if (coordsSpider.left < 0) {
+    coordsSpider.left = 0;
+  }
+
+  if (coordsSpider.left + spiderSize.width > wall.clientWidth) {
+    coordsSpider.left = wall.clientWidth - spiderSize.width;
+  }
+
+  if (coordsSpider.top + spiderSize.height > wall.clientHeight) {
+    coordsSpider.top = wall.clientHeight - spiderSize.height;
+  }
+  
+  spider.style.top = coordsSpider.top + 'px';
+  spider.style.left = coordsSpider.left + 'px';
 });
