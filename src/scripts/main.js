31e5d73fafc3a02f08dd2dd4503fc17c5e92f9ad
bohdan_kochetov <bodja1208@gmail.com>
2021-09-30 18:49:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const div = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const divStyle = getComputedStyle(div);
+const spiderStyle = getComputedStyle(spider);
+
+const max = parseFloat(divStyle.width) - (parseFloat(spiderStyle.width) / 2);
+const min = parseFloat(spiderStyle.width) / 2;
+
+div.addEventListener('click', e => {
+  let x = e.offsetX;
+  let y = e.offsetY;
+
+  if (x > max) {
+    x = max;
+  }
+
+  if (x < min) {
+    x = min;
+  }
+
+  if (y > max) {
+    y = max;
+  }
+
+  if (y < min) {
+    y = min;
+  }
+
+  spider.style.top = `${y - min}px`;
+  spider.style.left = `${x - min}px`;
 });
