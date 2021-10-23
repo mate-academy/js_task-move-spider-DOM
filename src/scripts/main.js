58804981732527face149bc7b2690021c9caf576
Anton Kulchytskyi <antek1809@gmail.com>
2021-10-23 22:58:14 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,39 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const item = document.querySelector('.spider');
+  const itemRect = item.getBoundingClientRect();
+
+  const field = document.querySelector('.wall');
+  const fieldRect = field.getBoundingClientRect();
+
+  const xMinValue = 0;
+  const xMaxValue = field.clientWidth - itemRect.width;
+  const yMinValue = 0;
+  const yMaxValue = field.clientHeight - itemRect.height;
+
+  if (e.clientX > fieldRect.left && e.clientX < fieldRect.right
+    && e.clientX > fieldRect.top && e.clientY < fieldRect.bottom) {
+    let x = e.clientX - fieldRect.left - itemRect.width / 2;
+    let y = e.clientY - fieldRect.top - itemRect.height / 2;
+
+    if (x > xMaxValue) {
+      x = xMaxValue;
+    }
+
+    if (x < xMinValue) {
+      x = xMinValue;
+    }
+
+    if (y < yMinValue) {
+      y = yMinValue;
+    }
+
+    if (y > yMaxValue) {
+      y = yMaxValue;
+    }
+
+    item.style.top = `${y}px`;
+    item.style.left = `${x}px`;
+  }
 });
