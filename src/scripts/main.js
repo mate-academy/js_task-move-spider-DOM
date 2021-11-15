ff8070c4b14bcc424efaf2bfe8dbf7b4b05999d4
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-15 19:11:25 +0200

    Give birth to a spider man that can crawls

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spiderMan = document.querySelector('.spider');
+const manhattanBuilding = document.querySelector('.wall');
+
+const wallWidth = manhattanBuilding.clientWidth;
+const wallHeight = manhattanBuilding.clientHeight;
+
+manhattanBuilding.addEventListener('click', e => {
+  if (e.target === spiderMan) {
+    return;
+  }
+
+  let moveX;
+  let moveY;
+
+  if (e.offsetX - spiderMan.width < 0) {
+    moveX = 0;
+  } else if (e.offsetX + spiderMan.width > wallWidth) {
+    moveX = wallWidth - spiderMan.width;
+  } else {
+    moveX = e.offsetX - spiderMan.width / 2;
+  }
+
+  if (e.offsetY - spiderMan.height < 0) {
+    moveY = 0;
+  } else if (e.offsetY + spiderMan.height > wallHeight) {
+    moveY = wallHeight - spiderMan.height;
+  } else {
+    moveY = e.offsetY - spiderMan.height / 2;
+  }
+
+  spiderMan.style.left = moveX + 'px';
+  spiderMan.style.top = moveY + 'px';
 });
