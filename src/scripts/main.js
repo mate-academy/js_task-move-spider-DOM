73f601321f029067f5b23fa535e1f86b44343cab
Dina Samoilova <s13.dina@gmail.com>
2021-12-04 23:07:23 +0200

    solution after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,33 +14,36 @@ document.addEventListener('click', e => {
   const rectTop = Math.round(rect.top);
   const rectLeft = Math.round(rect.left);
 
-  const item = e.target;
+  const item = e.target.classList.contains('wall');
 
-  if (!item.classList.contains('wall')) {
+  if (!item) {
     return;
   }
 
-  if (e.clientY >= rectTop
-    && e.clientY <= (rectTop + borderWidth + spiderHalfWidth)) {
-    spider.style.top = '0px';
-  } else if (e.clientY >= (rectTop + borderWidth + wallWidth - spiderHalfWidth)
-    && e.clientY <= (rectTop + (2 * borderWidth) + wallWidth)) {
-    spider.style.top = wallWidth - (2 * spiderHalfWidth) + 'px';
-  } else {
-    spider.style.top = (e.clientY
+  let topY = (e.clientY
       - (rectTop + borderWidth))
-      - spiderHalfWidth + 'px';
+      - spiderHalfWidth;
+
+  let leftX = (e.clientX
+      - (rectLeft + borderWidth))
+      - spiderHalfWidth;
+
+  if (topY < 0) {
+    topY = 0;
   }
 
-  if (e.clientX >= rectLeft
-    && e.clientX <= (rectLeft + borderWidth + spiderHalfWidth)) {
-    spider.style.left = '0px';
-  } else if (e.clientX >= (rectLeft + borderWidth + wallWidth - spiderHalfWidth)
-    && e.clientX <= (rectLeft + (2 * borderWidth) + wallWidth)) {
-    spider.style.left = wallWidth - (2 * spiderHalfWidth) + 'px';
-  } else {
-    spider.style.left = (e.clientX
-      - (rectLeft + borderWidth))
-      - spiderHalfWidth + 'px';
+  if (leftX < 0) {
+    leftX = 0;
   }
+
+  if (topY > wallWidth - (2 * spiderHalfWidth)) {
+    topY = wallWidth - (2 * spiderHalfWidth);
+  }
+
+  if (leftX > wallWidth - (2 * spiderHalfWidth)) {
+    leftX = wallWidth - (2 * spiderHalfWidth);
+  }
+
+  spider.style.top = topY + 'px';
+  spider.style.left = leftX + 'px';
 });
