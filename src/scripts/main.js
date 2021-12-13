773b1eb582294742a25339b9e59e9c6d7bcecf3d
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-12-13 14:43:48 +0200

    Solution js_task-move-spider-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', e => {
+  const wallCoordsTop = wall.offsetTop + wall.clientTop;
+  const wallCoordsLeft = wall.offsetLeft + wall.clientLeft;
+
+  let spiderCoordsTop = e.clientY - wallCoordsTop - spider.clientHeight / 2;
+  let spiderCoordsLeft = e.clientX - wallCoordsLeft - spider.clientWidth / 2;
+
+  if (spiderCoordsTop < 0) {
+    spiderCoordsTop = 0;
+  };
+
+  if (spiderCoordsTop + spider.clientHeight > wall.clientHeight) {
+    spiderCoordsTop = wall.clientHeight - spider.clientHeight;
+  };
+
+  if (spiderCoordsLeft < 0) {
+    spiderCoordsLeft = 0;
+  };
+
+  if (spiderCoordsLeft + spider.clientWidth > wall.clientWidth) {
+    spiderCoordsLeft = wall.clientWidth - spider.clientWidth;
+  };
+
+  spider.style.left = `${spiderCoordsLeft}px`;
+  spider.style.top = `${spiderCoordsTop}px`;
 });
