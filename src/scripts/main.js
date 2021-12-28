747c2b3213cb05fff18b71b1368c994dbd0155f5
Vova Alputov <alputov1998@gmail.com>
2021-12-28 19:54:03 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const coordSp = spider.getBoundingClientRect();
+  const coordWl = wall.getBoundingClientRect();
+  const border =
+  parseInt(getComputedStyle(wall).getPropertyValue('border-top-width'));
+
+  const x = e.clientX - coordWl.left - border;
+  const y = e.clientY - coordWl.top - border;
+
+  const coordTop = y - coordSp.width / 2;
+  const coordLeft = x - coordSp.height / 2;
+
+  spider.style.top = coordTop + 'px';
+  spider.style.left = coordLeft + 'px';
 });
