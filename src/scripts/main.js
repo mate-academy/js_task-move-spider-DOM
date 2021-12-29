ad7a3a99b4874fa03a318e6163c3d77fa1dbb661
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2021-12-29 18:58:53 +0200

    last saved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', e => {
-  // write code here
+  const realX = spider.getBoundingClientRect().left - spider.width / 2;
+  const realY = spider.getBoundingClientRect().top - spider.height / 2;
+
+  let valueLeft = parseFloat(getComputedStyle(spider).left);
+  let valueTop = parseFloat(getComputedStyle(spider).top);
+
+  let moveLeft = valueLeft + (e.clientX - realX);
+  let moveTop = valueTop + (e.clientY - realY);
+
+  if (moveLeft < 0) {
+    moveLeft = spider.width;
+  } else if (moveLeft > wall.width) {
+    moveLeft = wall.width;
+  }
+
+  if (moveTop < 0) {
+    moveTop = spider.width;
+  } else if (moveTop > wall.height) {
+    moveTop = wall.height;
+  }
+
+  spider.style.left = `${moveLeft - spider.width}px`;
+  spider.style.top = `${moveTop - spider.height}px`;
 });
