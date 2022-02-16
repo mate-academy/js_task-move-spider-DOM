0c1f9e9438ae916da5d2ae8ef810eb793aabc5ea
Roman Kovalchuk <roma220702@gmail.com>
2022-02-16 19:02:23 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,39 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const x = e.clientX;
+  const y = e.clientY;
+  const spider = document.querySelector('.spider');
+  const plase = e.target;
+  const spiderWidth = spider.getBoundingClientRect().width;
+  const spiderHeight = spider.getBoundingClientRect().height;
+
+  if (!plase.classList.contains('wall')) {
+    return;
+  }
+
+  let movetop = y - plase.getBoundingClientRect().y - 10 - spiderHeight / 2;
+  let moveleft = x - plase.getBoundingClientRect().x - 10 - spiderWidth / 2;
+
+  if (movetop < 0) {
+    movetop = 0;
+  }
+
+  if (moveleft < 0) {
+    moveleft = 0;
+  }
+
+  if (y > plase.getBoundingClientRect().y
+  + plase.getBoundingClientRect().height - 10 - spiderHeight / 2) {
+    movetop = plase.getBoundingClientRect().height - 20 - spiderHeight;
+  }
+
+  if (x > plase.getBoundingClientRect().x
+  + plase.getBoundingClientRect().width - 10 - spiderWidth / 2) {
+    moveleft = plase.getBoundingClientRect().width - 20 - spiderWidth;
+  }
+  spider.style.top = `${movetop}px`;
+
+  spider.style.left = `${moveleft}px`;
 });
+
