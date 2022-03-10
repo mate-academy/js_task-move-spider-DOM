cf239bc543b3bdb44c172f1d353c073d318cde86
Misha Novikov <novikov1807@gmail.com>
2022-03-10 21:45:51 +0200

    Complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,33 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+  const styleSpider = getComputedStyle(spider);
+  const styleWall = getComputedStyle(wall);
+  let resultTop = e.offsetY - styleSpider.width.replace('px', '') / 2;
+  let resultLeft = e.offsetX - styleSpider.height.replace('px', '') / 2;
+  const trueLeftEnd = styleWall.width.replace('px', '')
+  - styleSpider.width.replace('px', '');
+  const trueTopEnd = styleWall.height.replace('px', '')
+  - styleSpider.height.replace('px', '');
+
+  if (e.offsetY < styleSpider.height.replace('px', '') / 2) {
+    resultTop = 0;
+  } else if (resultTop > trueTopEnd) {
+    resultTop = trueTopEnd;
+  }
+
+  if (e.offsetX < styleSpider.width.replace('px', '') / 2) {
+    resultLeft = 0;
+  } else if (resultLeft > trueLeftEnd) {
+    resultLeft = trueLeftEnd;
+  }
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  spider.style.left = `${resultLeft}px`;
+  spider.style.top = `${resultTop}px`;
 });
