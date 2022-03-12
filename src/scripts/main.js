56991910e59990f0257d628cbe8bdb6354bdbd35
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-03-12 10:22:26 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', e => {
-  // write code here
+  e.preventDefault();
+
+  if (e.target.closest('.wall')) {
+    const spiderW = spider.offsetHeight;
+    const spiderH = spider.offsetWidth;
+
+    const maxYcoord = wall.clientWidth - spiderW;
+    const maxXcoord = wall.clientHeight - spiderH;
+
+    let topY = e.y - (innerHeight - wall.clientHeight + spiderH) / 2;
+    let leftX = e.x - (innerWidth - wall.clientWidth + spiderW) / 2;
+
+    topY = topY < 0 ? 0 : topY > maxXcoord ? maxXcoord : topY;
+    leftX = leftX < 0 ? 0 : leftX > maxYcoord ? maxYcoord : leftX;
+
+    spider.style.top = topY + 'px';
+    spider.style.left = leftX + 'px';
+  }
 });
