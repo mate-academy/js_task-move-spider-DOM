4ecfd37fb03ef7da7d80b5c1ec6a826d29b51e32
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-03-15 14:24:54 +0200

    Tried to put the movement into the box

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,10 @@
 
 document.addEventListener('click', e => {
   const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const initialX = wall.offsetLeft;
+  const initialY = wall.offsetTop;
 
-  spider.style.left = e.clientX + 'px';
-  spider.style.top = e.clientY + 'px';
+  spider.style.left = e.clientX - initialX + 'px';
+  spider.style.top = e.clientY - initialY + 'px';
 });
