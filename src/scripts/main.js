a8b564c2b3266926b183c7cfd9c92173bd8bb5e8
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-03-15 20:46:16 +0200

    Test N7

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,27 +3,25 @@
 document.addEventListener('click', e => {
   const spider = document.querySelector('.spider');
   const wall = document.querySelector('.wall');
-  const initialX = wall.offsetRight;
-  const initialY = wall.offsetBottom;
+  const initialX = wall.offsetLeft;
+  const initialY = wall.offsetTop;
 
   if (e.target !== wall) {
     return false;
   }
 
   let movementX = (e.clientX - initialX
-  - parseFloat(spider.offsetHeight) / 2);
+  - parseFloat(spider.offsetWidth) / 2);
 
   let movementY = (e.clientY - initialY
   - parseFloat(spider.offsetHeight) / 2);
 
   if (movementX > (wall.offsetWidth - spider.offsetWidth)) {
-    movementX = wall.offsetWidth - wall.style.borderWidth / 2
-    - spider.offsetWidth;
+    movementX = wall.offsetWidth - spider.offsetWidth;
   }
 
   if (movementY > (wall.offsetHeight - spider.offsetHeight)) {
-    movementY = wall.offsetHeight - wall.style.borderWidth / 2
-    - spider.offsetHeight;
+    movementY = wall.offsetHeight - spider.offsetHeight;
   }
 
   spider.style.left = movementX + 'px';
