fc25bca3db041342b00cff4dc67ff6ec3061cea1
Dmytro Bychkivskyi <dbychkivskyi@cccis.com>
2022-03-24 17:30:37 +0200

    Finish hw.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  const wallBounds = wall.getBoundingClientRect();
+
+  let clientY = e.clientY - spider.getBoundingClientRect().height / 2;
+  let clientX = e.clientX - spider.getBoundingClientRect().width / 2;
+
+  if (clientY < wallBounds.top) {
+    clientY = wallBounds.top;
+  }
+
+  if (clientY > wallBounds.top + wall.clientHeight - spider.height) {
+    clientY = wallBounds.top + wall.clientHeight - spider.height;
+  }
+
+  if (clientX < wallBounds.left) {
+    clientX = wallBounds.left;
+  }
+
+  if (clientX > wallBounds.left + wall.clientWidth - spider.width) {
+    clientX = wallBounds.left + wall.clientWidth - spider.width;
+  }
+
+  spider.style.top = clientY - wallBounds.top + 'px';
+  spider.style.left = clientX - wallBounds.left + 'px';
 });
