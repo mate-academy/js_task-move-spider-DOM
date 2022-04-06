c4353e717649d858ffa100f74caf7a88ea523cd2
Yevgeniy Savchuk <savchukevgeniy1@gmail.com>
2022-04-06 16:11:20 +0300

    Solution without numbers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,20 +9,20 @@ document.addEventListener('click', e => {
     spider.style.top = `${e.offsetY - spider.clientHeight / 2}px`;
     spider.style.left = `${e.offsetX - spider.clientWidth / 2}px`;
 
-    if (e.offsetY < 25) {
+    if (e.offsetY < spider.clientHeight / 2) {
       spider.style.top = 0;
     }
 
-    if (e.offsetY > 375) {
-      spider.style.top = '350px';
+    if (e.offsetY > wall.clientHeight - spider.clientHeight / 2) {
+      spider.style.top = `${wall.clientHeight - spider.clientHeight}px`;
     }
 
-    if (e.offsetX < 25) {
+    if (e.offsetX < spider.clientWidth / 2) {
       spider.style.left = 0;
     }
 
-    if (e.offsetX > 375) {
-      spider.style.left = '350px';
+    if (e.offsetX > wall.clientWidth - spider.clientWidth / 2) {
+      spider.style.left = `${wall.clientWidth - spider.clientWidth}px`;
     }
   }
 });
