49325bf9c5809a8895f84cb31e88bdf721090a6b
Yevgeniy Savchuk <savchukevgeniy1@gmail.com>
2022-04-06 15:42:20 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,27 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target === wall) {
+    spider.style.top = `${e.offsetY - spider.clientHeight / 2}px`;
+    spider.style.left = `${e.offsetX - spider.clientWidth / 2}px`;
+
+    if (e.offsetY < 25) {
+      spider.style.top = 0;
+    }
+
+    if (e.offsetY > 375) {
+      spider.style.top = '350px';
+    }
+
+    if (e.offsetX < 25) {
+      spider.style.left = 0;
+    }
+
+    if (e.offsetX > 375) {
+      spider.style.left = '350px';
+    }
+  }
 });
