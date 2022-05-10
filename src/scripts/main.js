05327db4ca71eb1f9e5e4024adc124466ef1bfb0
Alex Nastychenko <nastychenko1993@gmail.com>
2022-05-10 19:21:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,50 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallStyle = getComputedStyle(wall);
+  const spiderrStyle = getComputedStyle(spider);
+
+  const spiderWidth = parseFloat(spiderrStyle.width);
+  const spiderHeight = parseFloat(spiderrStyle.height);
+
+  const minHorizontalPosition = parseFloat(wall.offsetLeft)
+    + parseFloat(wallStyle.borderWidth);
+  const minVerticalPosition = parseFloat(wall.offsetTop)
+    + parseFloat(wallStyle.borderWidth);
+
+  if (e.clientX < wall.offsetLeft || e.clientY < wall.offsetTop
+    || e.clientX > wall.offsetLeft + parseFloat(wallStyle.width)
+      + parseFloat(wallStyle.borderWidth) * 2
+    || e.clientY > wall.offsetTop + parseFloat(wallStyle.width)
+      + parseFloat(wallStyle.borderWidth) * 2) {
+    return;
+  }
+
+  spider.style.left = `${parseFloat(e.clientX) - spiderWidth / 2
+    - minHorizontalPosition}px`;
+
+  spider.style.top = `${parseFloat(e.clientY) - spiderHeight / 2
+    - minVerticalPosition}px`;
+
+  if (e.clientX < minHorizontalPosition + spiderWidth / 2) {
+    spider.style.left = 0;
+  }
+
+  if (e.clientX > minHorizontalPosition + parseFloat(wallStyle.width)
+    - spiderWidth / 2) {
+    spider.style.left = `${parseFloat(wallStyle.width)
+      - spiderWidth}px`;
+  }
+
+  if (e.clientY < minVerticalPosition + spiderHeight / 2) {
+    spider.style.top = 0;
+  }
+
+  if (e.clientY > minVerticalPosition + parseFloat(wallStyle.height)
+    - spiderHeight / 2) {
+    spider.style.top = `${parseFloat(wallStyle.height) - spiderHeight}px`;
+  }
 });
