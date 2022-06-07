5dc468883cd6151db6093161a41e898217ca3fa0
Kostiantyn <havryshkostya@gmail.com>
2022-06-07 13:19:19 +0400

    spidy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  let spiderX = e.offsetX - (spider.offsetWidth / 2);
+  let spiderY = e.offsetY;
+
+  const MaxX = wall.clientWidth - spider.clientWidth;
+  const MaxY = wall.clientHeight - spider.clientHeight;
+
+  if (spiderX > MaxX) {
+    spiderX = MaxX;
+  }
+
+  if (spiderY > MaxY) {
+    spiderY = MaxY;
+  }
+
+  spider.style.top = spiderY + 'px';
+  spider.style.left = spiderX + 'px';
 });
