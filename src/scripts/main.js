fddd0c747360d8d0d182d45b94ebaf2dba39b013
Artem Furdela <furdela.artem@gmail.com>
2022-06-18 21:32:24 +0300

    Move spider first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const calcDistance = (offset, prop) => {
+  const siderHalfSize = (parseFloat(getComputedStyle(spider)[prop]) / 2);
+  const wallSize = (parseFloat(getComputedStyle(wall)[prop]));
+
+  if (offset < siderHalfSize) {
+    return `0px`;
+  }
+
+  if (offset > wallSize - siderHalfSize) {
+    return `${wallSize - siderHalfSize * 2}px`;
+  }
+
+  return `${offset - siderHalfSize}px`;
+};
+
+wall.addEventListener('click', e => {
+  spider.style.top = calcDistance(e.offsetY, 'height');
+  spider.style.left = calcDistance(e.offsetX, 'width');
 });
