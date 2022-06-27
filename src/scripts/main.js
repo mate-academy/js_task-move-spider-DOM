889901757245288e815fb749116dbbc3777cec06
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-27 13:23:11 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,20 +5,20 @@ const wall = document.querySelector('.wall');
 const rect = wall.getBoundingClientRect();
 
 document.addEventListener('click', e => {
-  if (e.clientY > rect.bottom) {
+  if (e.clientY > rect.bottom - parseInt(getComputedStyle(wall).border)) {
     spider.style.top = `${parseInt(getComputedStyle(wall).height)
       - spider.height}px`;
-  } else if (e.clientY < rect.top) {
+  } else if (e.clientY < rect.top + parseInt(getComputedStyle(wall).border)) {
     spider.style.top = 0;
   } else {
     spider.style.top = `${e.clientY
       - wall.offsetTop - (spider.height / 2)}px`;
   }
 
-  if (e.clientX > rect.right) {
+  if (e.clientX > rect.right - parseInt(getComputedStyle(wall).border)) {
     spider.style.left = `${parseInt(getComputedStyle(wall).width)
       - spider.width}px`;
-  } else if (e.clientX < rect.left) {
+  } else if (e.clientX < rect.left + parseInt(getComputedStyle(wall).border)) {
     spider.style.left = 0;
   } else {
     spider.style.left = `${e.clientX
