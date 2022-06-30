e3386b3099b5765b503de4b3820eddbbaef9ee91
Tania Troshchuk <troschuk95@gmail.com>
2022-06-30 21:40:25 +0300

    Solition Task: Move the spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const maxCoordX = wall.clientWidth - spider.clientWidth;
+  const maxCoordY = wall.clientHeight - spider.clientHeight;
+  let x = e.offsetX - (spider.offsetWidth / 2);
+  let y = e.offsetY - (spider.offsetHeight / 2);
+
+  if (x < 0) {
+    x = 0;
+  };
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (x > maxCoordX) {
+    x = maxCoordX;
+  }
+
+  if (y > maxCoordY) {
+    y = maxCoordY;
+  }
+
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
 });
