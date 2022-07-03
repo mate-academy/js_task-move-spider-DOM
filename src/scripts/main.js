23149ca73c584d5685060298bcb7785d21bbddf6
Konstantin <ivanchenkomypost@gmail.com>
2022-07-03 17:31:50 +0300

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  let x = e.offsetX - spider.offsetWidth / 2;
+  let y = e.offsetY - spider.offsetHeight / 2;
+
+  [x, y] = [x, y].map(el => {
+    if (el > wall.clientWidth - spider.offsetWidth) {
+      return wall.clientWidth - spider.offsetWidth;
+    } else if (el < 0) {
+      return 0;
+    }
+
+    return el;
+  });
+
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
 });
