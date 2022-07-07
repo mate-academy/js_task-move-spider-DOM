f62699a3476d134d54435f4be7c332c3f11a2fbb
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-07-07 12:54:44 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  if (!e.target.classList.contains('wall')) {
+    return;
+  }
+
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const maxTop = wall.clientHeight - spider.clientHeight;
+  const maxLeft = wall.clientWidth - spider.clientWidth;
+
+  let topSp = e.offsetY - spider.clientHeight / 2;
+  let leftSp = e.offsetX - spider.clientWidth / 2;
+
+  topSp = topSp > 0 ? topSp : 0;
+  topSp = topSp < maxTop ? topSp : maxTop;
+  leftSp = leftSp > 0 ? leftSp : 0;
+  leftSp = leftSp < maxLeft ? leftSp : maxLeft;
+
+  spider.style.top = topSp + 'px';
+  spider.style.left = leftSp + 'px';
 });
