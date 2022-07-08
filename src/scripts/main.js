2f0a2592b7727508275d4634170c2ba57f0fb324
Zolkiewskyi Oleg <zolkiewskyioleg@gmail.com>
2022-07-08 15:06:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const position = e.target;
+
+  if (!position.classList.contains('wall')) {
+    return;
+  }
+
+  const positionX = e.offsetX - spider.offsetHeight / 2;
+  const positionY = e.offsetY - spider.offsetWidth / 2;
+
+  spider.style.left = positionX + 'px';
+  spider.style.top = positionY + 'px';
 });
