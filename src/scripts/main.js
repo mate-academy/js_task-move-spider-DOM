04548f845e45c4a2ed6a727bef0fbd8767fe9857
Max Shylenko <maxshylenko@gmail.com>
2022-07-13 00:26:58 +0200

    move spider DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = wall.querySelector('.spider');
+
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const coordTop = e.offsetY;
+  const coordLeft = e.offsetX;
+  const spiderHeight = spider.offsetHeight;
+  const spiderWidth = spider.offsetWidth;
+  const maxLeft = wallWidth - spiderWidth;
+  const maxHeight = wallHeight - spiderHeight;
+  const minCoord = '0px';
+
+  spider.style.top = (coordTop >= maxHeight)
+    ? `${maxHeight}px`
+    : (coordTop <= spiderHeight)
+      ? minCoord
+      : `${coordTop - (spiderHeight / 2)}px`;
+
+  spider.style.left = (coordLeft >= maxLeft)
+    ? `${maxLeft}px`
+    : (coordLeft <= spiderWidth)
+      ? minCoord
+      : `${coordLeft - (spiderWidth / 2)}px`;
 });
