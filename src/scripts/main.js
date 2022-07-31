01871b48608765fcb3e9d5b90defc04e71f55147
Olha Arama <arama_o_f@ukr.net>
2022-07-31 18:34:29 +0300

    Spider was moved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  let coordX = e.clientX - wall.offsetLeft - wall.clientLeft
+    - spider.offsetWidth / 2;
+  let coordY = e.clientY - wall.offsetTop - wall.clientTop
+    - spider.offsetHeight / 2;
+
+  if (!wall.contains(e.target)) {
+    return;
+  }
+
+  if (coordX < 0) {
+    coordX = 0;
+  }
+
+  if (coordY < 0) {
+    coordY = 0;
+  }
+
+  if (coordX + spider.clientWidth > wall.clientWidth) {
+    coordX = wall.clientWidth - spider.clientWidth;
+  }
+
+  if (coordY + spider.clientHeight > wall.clientHeight) {
+    coordY = wall.clientHeight - spider.clientHeight;
+  }
+
+  spider.style.left = coordX + 'px';
+  spider.style.top = coordY + 'px';
 });
