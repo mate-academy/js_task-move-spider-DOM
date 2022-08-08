03b5536f5e565bb9e8be307ade9bb2ee40676d50
Yura Atamaniuk <atam3905@gmail.com>
2022-08-08 17:11:23 +0300

    spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const wall = document.querySelector(`.wall`);
+const spider = document.querySelector(`.spider`);
+const halfSpiderHeight = spider.offsetHeight / 2;
+const halfSpiderWidth = spider.offsetWidth / 2;
+
 document.addEventListener('click', e => {
   // write code here
+  if (e.target.className === 'wall') {
+    const border = wall.clientTop;
+    let y = e.clientY - wall.offsetTop - halfSpiderHeight - border;
+    let x = e.clientX - wall.offsetLeft - halfSpiderWidth - border;
+
+    x = Math.max(0, x);
+    x = Math.min(wall.offsetWidth - border * 2 - spider.offsetWidth, x);
+
+    y = Math.max(0, y);
+    y = Math.min(wall.offsetHeight - border * 2 - spider.offsetWidth, y);
+
+    spider.style.top = `${y}px`;
+    spider.style.left = `${x}px`;
+  }
 });
