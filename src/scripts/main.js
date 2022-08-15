87e232df7cea2739c3b7836d5949e3c32e15cdee
Andrii Kuzmenko <andriikuzmenko35@gmail.com>
2022-08-15 19:44:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const playGround = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const borderWidth = parseFloat(getComputedStyle(playGround).borderWidth);
+  const spiderHalfSize = spider.clientHeight / 2;
+  const playGroundHeight = playGround.clientHeight;
+  const playGroundWidth = playGround.clientWidth;
+
+  if (document.elementFromPoint(e.clientX, e.clientY) !== playGround) {
+    return;
+  };
+
+  if (e.offsetY < borderWidth + spiderHalfSize) {
+    spider.style.top = `${spiderHalfSize}px`;
+  } else if (playGroundHeight - e.offsetY < borderWidth + spiderHalfSize) {
+    spider.style.top = `${playGroundHeight - spiderHalfSize}px`;
+  } else {
+    spider.style.top = `${e.offsetY}px`;
+  }
+
+  if (e.offsetX < borderWidth + spiderHalfSize) {
+    spider.style.left = `${spiderHalfSize}px`;
+  } else if (playGroundWidth - e.offsetX < borderWidth + spiderHalfSize) {
+    spider.style.left = `${playGroundWidth - spiderHalfSize}px`;
+  } else {
+    spider.style.left = `${e.offsetX}px`;
+  }
+
+  spider.style.transform = 'translate(-50%, -50%)';
 });
