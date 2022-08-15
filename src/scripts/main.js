61e14a95a97619b71b7bde06933ec216e3cba0a6
Kostya Berehoviy <kostyaber3@gmail.com>
2022-08-15 22:50:23 +0300

    JS Move Spider Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+document.addEventListener('click', (e) => {
+  let wall = document.querySelector('.wall');
+  let spider = document.querySelector('.spider');
+  
+  
+  if (e.target !== wall) {
+    return;
+  }
+
+  let x = e.offsetX - spider.clientWidth / 2;
+  let y = e.offsetY - spider.clientHeight / 2;
+
+  const min = 0;
+  const max = wall.clientWidth - spider.clientWidth;
+
+  if (x < min) {
+    x = min;
+  }
+
+  if (y < min) {
+    y = min;
+  }
+
+  if (x > max) {
+    x = max;
+  }
+
+  if (y > max) {
+    y = max;
+  }
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });
