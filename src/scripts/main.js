52c671a148983b5cb34432ac5c80a405b1c537df
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-16 16:13:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const x = e.offsetX;
+  const y = e.offsetY;
+
+  spider.style.left = x - spider.width / 2 + 'px';
+  spider.style.top = y - spider.height / 2 + 'px';
+
+  if (parseInt(spider.style.left) < 0) {
+    spider.style.left = 0 + 'px';
+  }
+
+  if (parseInt(spider.style.left) + spider.width > wall.clientWidth) {
+    spider.style.left = wall.clientWidth - spider.width + 'px';
+  }
+
+  if (parseInt(spider.style.top) < 0) {
+    spider.style.top = 0 + 'px';
+  }
+
+  if (parseInt(spider.style.top) + spider.height > wall.clientHeight) {
+    spider.style.top = wall.clientHeight - spider.height + 'px';
+  }
 });
