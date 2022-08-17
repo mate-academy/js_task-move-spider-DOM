4c38972dfa9744b0ae96f24be57e55be48732651
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-08-17 15:48:16 +0300

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+
+  const maxXCoord = wall.clientWidth - spider.clientWidth;
+  const maxYCoord = wall.clientHeight - spider.clientHeight;
+
+  let coordX = e.offsetX - spider.clientWidth / 2;
+  let coordY = e.offsetY - spider.clientHeight / 2;
+
+  coordX = coordX > maxXCoord ? maxXCoord : coordX;
+  coordX = coordX < 0 ? 0 : coordX;
+
+  coordY = coordY > maxYCoord ? maxXCoord : coordY;
+  coordY = coordY < 0 ? 0 : coordY;
+
+  spider.style.top = `${coordY}px`;
+  spider.style.left = `${coordX}px`;
 });
