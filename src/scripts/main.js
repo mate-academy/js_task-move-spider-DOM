c23c71c34ef7092f22e4b891064f29b513285d41
Taras Kozii <taras_kozii@ukr.net>
2022-08-21 15:04:31 +0300

    solution v01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const coordYmax = wall.clientHeight - spider.clientHeight;
+  const coordXmax = wall.clientWidth - spider.clientWidth;
+  const coordY = e.offsetY - (spider.clientHeight / 2);
+  const coordX = e.offsetX - (spider.clientWidth / 2);
+
+  coordX < 0 ? spider.style.left = 0 + 'px'
+    : spider.style.left = `${coordX}px`;
+
+  if (coordX > coordXmax) {
+    spider.style.left = `${coordXmax}px`;
+  }
+
+  coordY < 0 ? spider.style.top = '0px' : spider.style.top = `${coordY}px`;
+
+  if (coordY > coordYmax) {
+    spider.style.top = `${coordYmax}px`;
+  }
 });
