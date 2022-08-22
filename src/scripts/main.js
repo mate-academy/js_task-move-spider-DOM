7355d05559b99e913e3d8a74aadcfffde329dbde
Anton Chetveryk <chetverykantonma@gmail.com>
2022-08-22 13:37:04 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const spider = document.getElementsByClassName('spider')[0];
+const wall = document.getElementsByClassName('wall')[0];
+
 document.addEventListener('click', e => {
-  // write code here
+  const border = wall.clientLeft;
+  const halfWidth = spider.width / 2;
+
+  const indentX = e.clientX - wall.offsetLeft - halfWidth - border;
+  const indentY = e.clientY - wall.offsetTop - halfWidth - border;
+
+  if (e.target.closest('.wall')) {
+    spider.style.left = indentX + 'px';
+    spider.style.top = indentY + 'px';
+  };
+
+  if (indentX < 0) {
+    spider.style.left = 0 + 'px';
+  }
+
+  if (indentX > wall.clientWidth - spider.width) {
+    spider.style.left = wall.clientWidth - spider.width + 'px';
+  }
+
+  if (indentY < 0) {
+    spider.style.top = 0 + 'px';
+  }
+
+  if (indentY > wall.clientHeight - spider.width) {
+    spider.style.top = wall.clientHeight - spider.width + 'px';
+  }
+
 });
