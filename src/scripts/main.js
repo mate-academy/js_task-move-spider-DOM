4b69fcd86355ec23e26c685c8ad7e51457f96473
Mark Shapka <mark.shapka1@gmail.com>
2022-08-27 20:56:00 +0300

    deleted inner if-else

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,29 +6,19 @@ document.addEventListener('click', e => {
   const spider = document.querySelector('.spider');
   const spiderH = parseFloat(getComputedStyle(spider).height) / 2;
   const spiderW = parseFloat(getComputedStyle(spider).width) / 2;
-  const wallH = getComputedStyle(wall).height;
-  const wallW = getComputedStyle(wall).width;
-  const wallB = getComputedStyle(wall).border;
-  const cursW = e.offsetX;
-  const cursH = e.offsetY;
+  const wallH = parseFloat(getComputedStyle(wall).height);
+  const wallW = parseFloat(getComputedStyle(wall).width);
+  const wallB = parseFloat(getComputedStyle(wall).border);
+  const cursW = parseFloat(e.offsetX);
+  const cursH = parseFloat(e.offsetY);
 
   if (e.target.closest('.wall')) {
-    if (parseFloat(cursH) < (parseFloat(wallB) + (parseFloat(spiderH)))) {
-      spider.style.top = '0px';
-    } else if (parseFloat(cursH) > ((parseFloat(wallH)) - parseFloat(
-      spiderH))) {
-      spider.style.top = `${parseFloat(wallH) - parseFloat(spiderH) * 2}px`;
-    } else {
-      spider.style.top = `${parseFloat(cursH) - parseFloat(spiderH)}px`;
-    }
+    spider.style.top = cursH < (wallB + spiderH)
+      ? '0px' : cursH > (wallH - spiderH)
+        ? `${wallH - spiderH * 2}px` : `${cursH - spiderH}px`;
 
-    if (parseFloat(cursW) < (parseFloat(wallB) + (parseFloat(spiderW)))) {
-      spider.style.left = '0px';
-    } else if (parseFloat(cursW) > ((parseFloat(wallW)) - parseFloat(
-      spiderW))) {
-      spider.style.left = `${parseFloat(wallW) - parseFloat(spiderW) * 2}px`;
-    } else {
-      spider.style.left = `${parseFloat(cursW) - parseFloat(spiderW)}px`;
-    }
+    spider.style.left = cursW < (wallB + spiderW)
+      ? '0px' : cursW > (wallW - spiderW)
+        ? `${wallW - spiderW * 2 }px` : `${cursW - spiderW}px`;
   }
 });
