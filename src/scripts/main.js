30033315b5c45925c8c94293bb88e7b5e5e9d11e
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-13 10:14:53 +0300

    move_spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.getElementsByClassName('spider')[0];
+  const wall = document.getElementsByClassName('wall')[0];
+
+  if (e.target === wall) {
+    spider.style.top = cage(e.offsetY, spider, wall) + 'px';
+    spider.style.left = cage(e.offsetX, spider, wall) + 'px';
+    spider.style.transform = 'translate(-50%, -50%)';
+  }
 });
+
+function cage(coordProp, elem, fieldCage) {
+  if ((coordProp - elem.clientHeight / 2) <= 0) {
+    return elem.clientHeight / 2;
+  }
+
+  if ((coordProp - fieldCage.clientHeight) >= 0) {
+    return fieldCage.clientHeight - elem.clientHeight / 2;
+  }
+
+  return coordProp;
+};
