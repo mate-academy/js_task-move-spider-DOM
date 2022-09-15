f986f937300a05eb1a2e3ef925946890790ed09e
Inna <dragotaiv@gmail.com>
2022-09-15 22:14:45 +0300

    move_spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  const halfSpider = spider.width / 2;
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  let coordCenterSpiderX = e.offsetX - halfSpider;
+  let coordCenterSpiderY = e.offsetY - halfSpider;
+
+  if (e.offsetX < halfSpider) {
+    coordCenterSpiderX = 0;
+  }
+
+  if (e.offsetY < halfSpider) {
+    coordCenterSpiderY = 0;
+  }
+
+  if (e.offsetX > wall.clientWidth - halfSpider) {
+    coordCenterSpiderX = wall.clientWidth - spider.width;
+  }
+
+  if (e.offsetY > wall.clientHeight - halfSpider) {
+    coordCenterSpiderY = wall.clientHeight - spider.width;
+  }
+
+  spider.style = `left: ${coordCenterSpiderX}px ; top: ${coordCenterSpiderY}px`;
 });
