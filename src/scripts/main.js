fa7f88e9bf974fb7ca8084271b7f62f02b39dad7
Paul Voronin <voron.paul@gmail.com>
2022-09-20 20:49:39 +0300

    ver.1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallWidth = wall.clientWidth;
+const wallНeight = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderНeight = spider.clientHeight;
+
+wall.addEventListener('click', e => {
+  let spiderX = e.offsetX - spiderWidth / 2;
+  let spiderY = e.offsetY - spiderНeight / 2;
+
+  if (spiderX < 0) {
+    spiderX = 0;
+  }
+
+  if (spiderY < 0) {
+    spiderY = 0;
+  }
+
+  if (spiderX > wallWidth - spiderWidth) {
+    spiderX = wallWidth - spiderWidth;
+  }
+
+  if (spiderY > wallНeight - spiderНeight) {
+    spiderY = wallНeight - spiderНeight;
+  }
+
+  spider.style.top = `${spiderY}px`;
+  spider.style.left = `${spiderX}px`;
 });
