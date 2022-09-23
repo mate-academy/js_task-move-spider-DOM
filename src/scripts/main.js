b3f6a949f32c1924dfbaf20b40e93bd6b86483d2
Yevheniia Danylchuk <shenijad1@gmail.com>
2022-09-23 13:43:51 +0300

    moving spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  const wallClick = e.target;
+  let x = e.offsetX - (spider.height / 2);
+  let y = e.offsetY - (spider.height / 2);
+
+  const maxY = wall.clientHeight - spider.clientHeight;
+  const maxX = wall.clientWidth - spider.clientWidth;
+
+  if (x < 1) {
+    x = 0;
+  };
+
+  if (y < 1) {
+    y = 0;
+  };
+
+  if (x > maxX) {
+    x = maxX;
+  }
+
+  if (y > maxY) {
+    y = maxY;
+  }
+
+  if (wallClick.className === 'wall') {
+    spider.style.left = `${x}px`;
+    spider.style.top = `${y}px`;
+  }
 });
