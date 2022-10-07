8bac98f7bce9606687b7e34a3aba17492465722c
Danil <dasha8854567@gmail.com>
2022-10-07 14:54:52 +0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+
+document.querySelector('.wall').addEventListener('click', e => {
+  let topp = e.offsetY;
+  let left = e.offsetX;
+
+  if (e.target.classList.contains('spider')) {
+    spider.style.top = `${parseFloat(spider.style.top) + topp - 25}px`;
+    spider.style.left = `${parseFloat(spider.style.left) + left - 25}px`;
+
+    return;
+  }
+
+  if (topp < 25) {
+    topp = 25;
+  }
+
+  if (left < 25) {
+    left = 25;
+  }
+
+  if (topp > 375) {
+    topp = 375;
+  }
+
+  if (left > 375) {
+    left = 375;
+  }
+
+  spider.style.top = `${topp}px`;
+  spider.style.left = `${left}px`;
+  spider.style.transform = 'translate(-50%, -50%)';
 });
