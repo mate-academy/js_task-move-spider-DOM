715f0c94bccded28a9bb73e80be0f413b788b71c
Oleksandr Naumchak <naumchak@ukr.net>
2022-10-10 18:24:03 +0300

    JS task move spider DOM (first solution)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  const pos = e.target.closest('.wall');
+
+  if (!pos) {
+    return;
+  }
+
+  const clickX = e.offsetX;
+  const clickY = e.offsetY;
+  const spiderCenterHeight = spider.height / 2;
+  const spiderCenterWidth = spider.width / 2;
+
+  let spiderCoordsTop = (clickY - spiderCenterHeight);
+  let spiderCoordsLeft = (clickX - spiderCenterWidth);
+
+  if (spiderCoordsTop < 0) {
+    spiderCoordsTop = 0;
+  }
+
+  if (spiderCoordsLeft < 0) {
+    spiderCoordsLeft = 0;
+  }
+
+  if (e.offsetX + spiderCenterWidth > pos.clientWidth) {
+    spiderCoordsLeft = pos.clientWidth - spider.width;
+  }
+
+  if (e.offsetY + spiderCenterHeight > pos.clientHeight) {
+    spiderCoordsTop = pos.clientHeight - spider.height;
+  }
+
+  spider.style.top = spiderCoordsTop + 'px';
+  spider.style.left = spiderCoordsLeft + 'px';
 });
