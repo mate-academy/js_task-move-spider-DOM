eefdc973baf9d3faffaa6eaac256517f28b4d8dc
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-10-13 15:26:35 +0300

    move the spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,28 +7,36 @@ document.addEventListener('click', e => {
   const wallCoord = wall.getBoundingClientRect();
   const spiderCoord = spider.getBoundingClientRect();
 
-  if (e.clientX - e.width * 16 / 2 - parseFloat(getComputedStyle(spider).width)
-    / 2 < wallCoord.x) {
-    spider.style.left = 0;
-  } else if (e.clientX + e.width * 16
-    / 2 + parseFloat(getComputedStyle(spider).width)
-    / 2 > wallCoord.x + wallCoord.width) {
-    spider.style.left = parseFloat(getComputedStyle(wall).width)
-    - parseFloat(getComputedStyle(spider).width) + 'px';
-  } else {
-    spider.style.left = e.clientX - wallCoord.left - spiderCoord.width
-    / 2 - e.width * 16 / 2 + 'px';
-  }
+  if (e.clientX >= wallCoord.x
+    && e.clientX <= wallCoord.x + wallCoord.width
+    && e.clientY >= wallCoord.y
+    && e.clientY <= wallCoord.y + wallCoord.height) {
+    if (e.clientX - wallCoord.x - spiderCoord.width
+      / 2 - e.width * 16 / 2 < 0) {
+      spider.style.left = 0;
+    } else if (e.clientX - wallCoord.x + spiderCoord.width
+      / 2 - e.width * 16 / 2 > wallCoord.width) {
+      spider.style.left = wallCoord.width - spiderCoord.width
+      - parseFloat(getComputedStyle(wall).borderWidth) * 2 + 'px';
+    } else if (e.clientX - wallCoord.x + spiderCoord.width
+      / 2 - e.width * 16 / 2 < wallCoord.width
+      - parseFloat(getComputedStyle(wall).borderWidth) * 2) {
+      spider.style.left = e.clientX - wallCoord.x - spiderCoord.width
+      / 2 - e.width * 16 / 2 + 'px';
+    }
 
-  if (e.clientY - parseFloat(getComputedStyle(spider).height)
-    / 2 < wallCoord.y) {
-    spider.style.top = 0;
-  } else if (e.clientY + parseFloat(getComputedStyle(spider).width)
-    / 2 > wallCoord.y + wallCoord.height) {
-    spider.style.top = parseFloat(getComputedStyle(wall).height)
-    - parseFloat(getComputedStyle(spider).height) + 'px';
-  } else {
-    spider.style.top = e.clientY - wallCoord.top - spiderCoord.height
-    / 2 - e.width * 16 / 2 + 'px';
+    if (e.clientY - wallCoord.y - spiderCoord.height
+      / 2 - e.height * 16 / 2 < 0) {
+      spider.style.top = 0 + 'px';
+    } else if (e.clientY - wallCoord.y + spiderCoord.height
+      / 2 - e.height * 16 / 2 > wallCoord.height) {
+      spider.style.top = wallCoord.height - spiderCoord.height
+      - parseFloat(getComputedStyle(wall).borderWidth) * 2 + 'px';
+    } else if (e.clientY - wallCoord.y + spiderCoord.height
+      / 2 - e.height * 16 / 2 < wallCoord.height
+      - parseFloat(getComputedStyle(wall).borderWidth) * 2) {
+      spider.style.top = e.clientY - wallCoord.y - spiderCoord.height
+      / 2 - e.height * 16 / 2 + 'px';
+    }
   }
 });
