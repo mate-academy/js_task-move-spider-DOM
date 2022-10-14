5995766ed1a28f0c1b199be226e9eea5cd3b0dfc
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-10-14 12:25:25 +0300

    solution with offsetX/Y

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,39 +6,37 @@ const wall = document.querySelector('.wall');
 document.addEventListener('click', e => {
   const wallCoord = wall.getBoundingClientRect();
   const spiderCoord = spider.getBoundingClientRect();
-  const pointerHalfWidth = e.width * 16 / 2;
-  const pointerHalfHeight = e.height * 16 / 2;
-  const wallBorderWidth = parseFloat(getComputedStyle(wall).borderWidth) * 2;
-  const wallWidth = wallCoord.width;
-  const wallHeight = wallCoord.height;
-  const spiderWidth = spiderCoord.width;
-  const spiderHeight = spiderCoord.height;
-  const innerCoordX = e.clientX - wallCoord.x;
-  const innerCoordY = e.clientY - wallCoord.y;
+  const spiderHalfHeight = spider.clientHeight / 2;
 
-  if (e.clientX >= wallCoord.x
-      && e.clientX <= wallCoord.x + wallWidth
-      && e.clientY >= wallCoord.y
-      && e.clientY <= wallCoord.y + wallHeight) {
-    if (innerCoordX - spiderWidth / 2 - pointerHalfWidth < 0) {
+  if (e.clientX > wallCoord.left && e.clientX < wallCoord.right) {
+    if (e.target === wall && e.offsetX - spider.clientWidth / 2 < 0) {
       spider.style.left = 0;
-    } else if (innerCoordX + spiderWidth / 2 - pointerHalfWidth > wallWidth) {
-      spider.style.left = wallWidth - spiderWidth - wallBorderWidth + 'px';
-    } else if (innerCoordX + spiderWidth
-      / 2 - pointerHalfWidth < wallWidth - wallBorderWidth) {
-      spider.style.left = innerCoordX - spiderWidth
-      / 2 - pointerHalfWidth + 'px';
     }
 
-    if (innerCoordY - spiderHeight / 2 - pointerHalfHeight < 0) {
-      spider.style.top = 0 + 'px';
-    } else if (innerCoordY + spiderHeight
-      / 2 - pointerHalfHeight > wallHeight) {
-      spider.style.top = wallHeight - spiderHeight - wallBorderWidth + 'px';
-    } else if (innerCoordY + spiderHeight
-      / 2 - pointerHalfHeight < wallHeight - wallBorderWidth) {
-      spider.style.top = innerCoordY - spiderHeight
-      / 2 - pointerHalfHeight + 'px';
+    if (e.target === wall
+      && e.offsetX + spider.clientWidth / 2 > wall.clientWidth) {
+      spider.style.left = wall.clientWidth - spider.clientWidth + 'px';
+    }
+
+    if (e.target === wall && e.offsetX - spider.clientWidth
+      / 2 > 0 && e.offsetX - spiderCoord.width
+      / 2 < wall.clientWidth - spider.clientWidth) {
+      spider.style.left = e.offsetX - spider.clientWidth / 2 + 'px';
+    }
+  }
+
+  if (e.clientY > wallCoord.top && e.clientY < wallCoord.bottom) {
+    if (e.target === wall && e.offsetY - spiderHalfHeight < 0) {
+      spider.style.top = 0;
+    }
+
+    if (e.target === wall && e.offsetY + spiderHalfHeight > wall.clientHeight) {
+      spider.style.top = wall.clientHeight - spider.clientHeight + 'px';
+    }
+
+    if (e.target === wall && e.offsetY - spiderHalfHeight > 0
+      && e.offsetY + spiderHalfHeight < wall.clientHeight) {
+      spider.style.top = e.offsetY - spiderHalfHeight + 'px';
     }
   }
 });
