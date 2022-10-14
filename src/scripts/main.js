dc31ce2a972ffc72b821fdee6e630212517df45f
Oleksiy Uchaev <corob.dir@gmail.com>
2022-10-14 16:33:07 +0300

    Solution for spry spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderSize = spider.offsetWidth;
+
+const limitAxis = {
+  x: wall.clientWidth - spiderSize,
+  y: wall.clientHeight - spiderSize,
+};
+
 document.addEventListener('click', e => {
-  // write code here
+  let [x, y] = [ e.offsetX, e.offsetY ]
+    .map(i => i - spiderSize / 2);
+
+  if (!e.target.closest('.wall')) {
+    return;
+  };
+
+  if (x > limitAxis.x) {
+    x = limitAxis.x;
+  } else if (x < 0) {
+    x = 0;
+  };
+
+  if (y > limitAxis.y) {
+    y = limitAxis.y;
+  } else if (y < 0) {
+    y = 0;
+  };
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });
+
+spider.style.pointerEvents = 'none';
