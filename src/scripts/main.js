65852fc188de7d7e88477270bded97cb0e5fdded
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-10-14 12:31:47 +0300

    solution with clientX/Y

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,36 +7,39 @@ document.addEventListener('click', e => {
   const wallCoord = wall.getBoundingClientRect();
   const spiderCoord = spider.getBoundingClientRect();
   const spiderHalfHeight = spider.clientHeight / 2;
+  const borderSize = parseFloat(getComputedStyle(wall).borderWidth);
+  const leftCoord = e.clientX - wallCoord.left - borderSize;
+  const topCoord = e.clientY - wallCoord.top - borderSize;
 
   if (e.clientX > wallCoord.left && e.clientX < wallCoord.right) {
-    if (e.target === wall && e.offsetX - spider.clientWidth / 2 < 0) {
+    if (e.target === wall && leftCoord - spider.clientWidth / 2 < 0) {
       spider.style.left = 0;
     }
 
     if (e.target === wall
-      && e.offsetX + spider.clientWidth / 2 > wall.clientWidth) {
+      && leftCoord + spider.clientWidth / 2 > wall.clientWidth) {
       spider.style.left = wall.clientWidth - spider.clientWidth + 'px';
     }
 
-    if (e.target === wall && e.offsetX - spider.clientWidth
-      / 2 > 0 && e.offsetX - spiderCoord.width
+    if (e.target === wall && leftCoord - spider.clientWidth
+      / 2 > 0 && leftCoord - spiderCoord.width
       / 2 < wall.clientWidth - spider.clientWidth) {
-      spider.style.left = e.offsetX - spider.clientWidth / 2 + 'px';
+      spider.style.left = leftCoord - spider.clientWidth / 2 + 'px';
     }
   }
 
   if (e.clientY > wallCoord.top && e.clientY < wallCoord.bottom) {
-    if (e.target === wall && e.offsetY - spiderHalfHeight < 0) {
+    if (e.target === wall && topCoord - spiderHalfHeight < 0) {
       spider.style.top = 0;
     }
 
-    if (e.target === wall && e.offsetY + spiderHalfHeight > wall.clientHeight) {
+    if (e.target === wall && topCoord + spiderHalfHeight > wall.clientHeight) {
       spider.style.top = wall.clientHeight - spider.clientHeight + 'px';
     }
 
-    if (e.target === wall && e.offsetY - spiderHalfHeight > 0
-      && e.offsetY + spiderHalfHeight < wall.clientHeight) {
-      spider.style.top = e.offsetY - spiderHalfHeight + 'px';
+    if (e.target === wall && topCoord - spiderHalfHeight > 0
+      && topCoord + spiderHalfHeight < wall.clientHeight) {
+      spider.style.top = topCoord - spiderHalfHeight + 'px';
     }
   }
 });
