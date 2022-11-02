b2cf7e5c57e3d86ec35e2a5faa950859b318946b
Kolya Movchan <kmovchan261@gmail.com>
2022-11-02 12:05:59 +0200

    js_task-move-spider-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,43 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const wallBorderTop = parseFloat(getComputedStyle(wall).borderTop);
+  const wallBorderLeft = parseFloat(getComputedStyle(wall).borderLeft);
+
+  const centeredY = spider.clientHeight / 2;
+  const centeredX = spider.clientWidth / 2;
+
+  let y = e.clientY - wall.offsetTop - wallBorderTop - centeredY;
+  let x = e.clientX - wall.offsetLeft - wallBorderLeft - centeredX;
+
+  const underTop = y < -centeredY;
+  const overTop = y >= wall.clientHeight - centeredY;
+  const underLeft = x < -centeredX;
+  const overRight = x >= wall.clientWidth - centeredX;
+
+  if (underTop || overTop || underLeft || overRight) {
+    return;
+  };
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (x < 0) {
+    x = 0;
+  };
+
+  if (y >= wall.clientHeight - spider.clientHeight) {
+    y = wall.clientHeight - spider.clientHeight;
+  }
+
+  if (x >= wall.clientWidth - spider.clientWidth) {
+    x = wall.clientWidth - spider.clientWidth;
+  }
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });
