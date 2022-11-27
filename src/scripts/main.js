3b84077c496e81d60e2ec225cb09e2caaf5ecc91
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-11-27 09:29:32 +0100

    ### Task: Move the spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
-
 document.addEventListener('click', e => {
-  // write code here
+  if (!e.target.closest('.wall')) {
+    return;
+  };
+
+  const wall = document.querySelector('.wall');
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+
+  const spider = document.querySelector('.spider');
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  let x = (e.offsetY - spiderHeight / 2);
+  let y = (e.offsetX - spiderWidth / 2);
+
+  spider.style.top = x + 'px';
+  spider.style.left = y + 'px';
+
+  if (x < 0) {
+    spider.style.top = '0px';
+  };
+
+  if (y < 0) {
+    spider.style.left = '0px';
+  };
+
+  if (x > (wallHeight - spiderHeight)) {
+    spider.style.top = wallHeight - spiderHeight + 'px';
+  };
+
+  if (y > (wallWidth - spiderWidth)) {
+    spider.style.left = wallWidth - spiderWidth + 'px';
+  };
 });
