4fe13440e9bb413e10caffd41cca679d2b8e6edb
Yurii Teslia <teslya.yuriy@gmail.com>
2022-11-30 23:46:42 +0200

    updated script to pass tests

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,9 +16,11 @@ const addHorizontalMove = wall.offsetLeft + spider.width - leftBorder * 1.5;
 const addVerticalMove = wall.offsetTop + spider.height - topBorder * 1.5;
 
 document.addEventListener('click', e => {
-  const currentRightCoord = Math.max(e.clientX - addHorizontalMove, 0);
-  const currentBottomCoord = Math.max(e.clientY - addVerticalMove, 0);
+  if (e.target === wall) {
+    const currentRightCoord = Math.max(e.clientX - addHorizontalMove, 0);
+    const currentBottomCoord = Math.max(e.clientY - addVerticalMove, 0);
 
-  spider.style.left = `${Math.min(currentRightCoord, maxRightCoord)}px`;
-  spider.style.top = `${Math.min(currentBottomCoord, maxBottomCoord)}px`;
+    spider.style.left = `${Math.min(currentRightCoord, maxRightCoord)}px`;
+    spider.style.top = `${Math.min(currentBottomCoord, maxBottomCoord)}px`;
+  }
 });
