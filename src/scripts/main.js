4c747a3288deb4c9e35c7cd4bfca4eec53a7fd6b
Yurii Teslia <teslya.yuriy@gmail.com>
2022-11-30 07:26:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const getStyle = (elem, styleName) =>
+  getComputedStyle(elem).getPropertyValue(styleName);
+
+const leftBorder = parseInt(getStyle(wall, 'border-bottom-width'));
+const topBorder = parseInt(getStyle(wall, 'border-top-width'));
+
+const maxRightCoord = parseInt(getStyle(wall, 'width')) - spider.width;
+const maxBottomCoord = parseInt(getStyle(wall, 'height')) - spider.height;
+
+const addHorizontalMove = wall.offsetLeft + spider.width - leftBorder;
+const addVerticalMove = wall.offsetTop + spider.height - topBorder;
+
 document.addEventListener('click', e => {
-  // write code here
+  spider.style.left = `${Math.min(Math.max(e.clientX - addHorizontalMove, 0), maxRightCoord)}px`;
+  spider.style.top = `${Math.min(Math.max(e.clientY - addVerticalMove, 0), maxBottomCoord)}px`;
 });
