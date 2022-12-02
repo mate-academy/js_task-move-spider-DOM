8914c8c86cf6e582c6fa58f2439f0b75a6e8bb98
Panianchuk M <panianchuk@gmail.com>
2022-12-02 02:45:53 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  let coordsX = e.offsetX - spider.clientWidth / 2;
+  let coordsY = e.offsetY - spider.clientHeight / 2;
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+
+  if (coordsX > maxX) {
+    coordsX = maxX;
+  }
+
+  if (coordsY > maxY) {
+    coordsY = maxY;
+  }
+
+  if (coordsX < 0) {
+    coordsX = 0;
+  }
+
+  if (coordsY < 0) {
+    coordsY = 0;
+  }
+
+  spider.style.left = coordsX + 'px';
+  spider.style.top = coordsY + 'px';
 });
