18e1d5b2219c2d05c0ae807aa509e77ec4d75683
Maria Bramnik <mariabramnik@gmail.com>
2022-12-05 22:12:26 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall')
+const coordsWall = wall.getBoundingClientRect();
+   
+    const wallX = coordsWall['x'];
+    const wallY = coordsWall['y'];
+
+    spider.style.top = wallY + 'px';
+    spider.style.left = wallX + 'px';
+    spider.style.position = 'fixed';
+
 document.addEventListener('click', e => {
-  // write code here
-});
+    let item = e.target;
+    const itemParent = item.closest('.wall');
+
+    if (itemParent === null) {
+      return;
+    }
+ 
+    let x = e.clientX;
+    let y = e.clientY;  
+    spider.style.left = x - (spider.offsetWidth/2)  +'px' ;
+    spider.style.top = y - (spider.offsetHeight / 2) + 'px';
+  });
