db5f000d0b827b85a901d0b18bf5ca64b896ddf3
Maria Bramnik <mariabramnik@gmail.com>
2022-12-07 00:01:26 +0200

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,45 @@
 'use strict';
 
 const spider = document.querySelector('.spider');
-const wall = document.querySelector('.wall')
+const wall = document.querySelector('.wall');
 const coordsWall = wall.getBoundingClientRect();
-   
-    const wallX = coordsWall['x'];
-    const wallY = coordsWall['y'];
 
-    spider.style.top = wallY + 'px';
-    spider.style.left = wallX + 'px';
-    spider.style.position = 'fixed';
+const borderWidth = parseInt(getComputedStyle(wall)
+.getPropertyValue('border-width'));
+
+const wallX = coordsWall['x'] + borderWidth;
+const wallY = coordsWall['y'] + borderWidth;
+const wallW = coordsWall['width'] - 2 * borderWidth;
+const wallH = coordsWall['height'] - 2 * borderWidth;
+
+spider.style.top = wallY + 'px';
+spider.style.left = wallX + 'px';
+spider.style.position = 'fixed';
 
 document.addEventListener('click', e => {
-    let item = e.target;
-    const itemParent = item.closest('.wall');
+  const item = e.target;
+  const itemParent = item.closest('.wall');
 
-    if (itemParent === null) {
-      return;
-    }
- 
-    let x = e.clientX;
-    let y = e.clientY;  
-    spider.style.left = x - (spider.offsetWidth/2)  +'px' ;
+  if (itemParent === null) {
+    return;
+  }
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  if (x < (wallX + spider.offsetWidth)) {
+    spider.style.left = wallX + 'px';
+  } else if (x > (wallX + wallW - spider.offsetWidth / 2)) {
+    spider.style.left = wallX + wallW - spider.offsetWidth + 'px';
+  } else {
+    spider.style.left = x - (spider.offsetWidth / 2) + 'px';
+  }
+
+  if (y < (wallY + spider.offsetHeight)) {
+    spider.style.top = wallY + 'px';
+  } else if (y > (wallY + wallH - spider.clientHeight / 2)) {
+    spider.style.top = wallY + wallH - spider.clientHeight + 'px';
+  } else {
     spider.style.top = y - (spider.offsetHeight / 2) + 'px';
-  });
+  }
+});
