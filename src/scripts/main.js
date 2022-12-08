5276a57d0ff97fde4c867fc16ebb7367399dd791
anton-sosonskyi <anton.sosonskyi@gmail.com>
2022-12-08 12:28:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,30 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const checkOutOfBounds = (coord) => {
+    const corectCoord = coord < 0
+      ? 0
+      : coord > wall.clientHeight - spider.clientHeight
+        ? wall.clientHeight - spider.clientHeight
+        : coord;
+
+    return corectCoord;
+  };
+
+  const getCoordinates = () => {
+    const coordX = e.clientX - wall.offsetLeft - spider.clientWidth / 2;
+    const coordY = e.clientY - wall.offsetTop - spider.clientWidth / 2;
+
+    return {
+      coordX, coordY,
+    };
+  };
+
+  if (e.target.closest('.wall')) {
+    spider.style.left = checkOutOfBounds(getCoordinates().coordX) + 'px';
+    spider.style.top = checkOutOfBounds(getCoordinates().coordY) + 'px';
+  }
 });
