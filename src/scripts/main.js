bfc333bc43f74d49811fbd4113697579e7b9eea5
Aleks <offsset@gmail.com>
2022-12-15 19:47:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,43 +4,45 @@ document.addEventListener('click', e => {
   const spider = document.querySelector('.spider');
   const windEl = document.querySelector('.wall');
   const wind = e.target.closest('.wall');
-  const borderWindEl = (windEl.offsetWidth - windEl.clientWidth) / 2;
 
   if (!wind) {
     return;
   }
 
   if (
-    (e.clientX - windEl.offsetLeft - (spider.offsetWidth / 2 + borderWindEl))
-      > (windEl.offsetWidth - (spider.offsetWidth + borderWindEl * 2))
+    (e.clientX - windEl.offsetLeft
+      - (spider.offsetWidth / 2 + windEl.clientLeft))
+      > (windEl.offsetWidth - (spider.offsetWidth + windEl.clientLeft * 2))
   ) {
     spider.style.left = (windEl.offsetWidth - (
-      spider.offsetWidth + borderWindEl * 2
+      spider.offsetWidth + windEl.clientLeft * 2
     )) + 'px';
   } else if (
-    (e.clientX - windEl.offsetLeft) < (spider.offsetWidth / 2 + borderWindEl)
+    (e.clientX - windEl.offsetLeft)
+     < (spider.offsetWidth / 2 + windEl.clientLeft)
   ) {
     spider.style.left = 0 + 'px';
   } else {
     spider.style.left = (
-      e.clientX - windEl.offsetLeft - (spider.offsetWidth / 2 + borderWindEl)
+      e.clientX - windEl.offsetLeft
+       - (spider.offsetWidth / 2 + windEl.clientLeft)
     ) + 'px';
   }
 
   if (
-    (e.clientY - windEl.offsetTop - (spider.offsetWidth / 2 + borderWindEl))
-     > (windEl.offsetHeight - (spider.offsetWidth + borderWindEl * 2))
+    (e.clientY - windEl.offsetTop - (spider.offsetWidth / 2 + windEl.clientTop))
+     > (windEl.offsetHeight - (spider.offsetWidth + windEl.clientTop * 2))
   ) {
     spider.style.top = (
-      windEl.offsetHeight - (spider.offsetWidth + borderWindEl * 2)
+      windEl.offsetHeight - (spider.offsetWidth + windEl.clientTop * 2)
     ) + 'px';
   } else if (
-    (e.clientY - windEl.offsetTop) < (spider.offsetWidth / 2 + borderWindEl)
+    (e.clientY - windEl.offsetTop) < (spider.offsetWidth / 2 + windEl.clientTop)
   ) {
     spider.style.top = 0 + 'px';
   } else {
     spider.style.top = (
-      e.clientY - windEl.offsetTop - (spider.offsetWidth / 2 + borderWindEl)
+      e.clientY - windEl.offsetTop - (spider.offsetWidth / 2 + windEl.clientTop)
     ) + 'px';
   }
 });
