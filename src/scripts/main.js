b27317b256f3196470223278ee737d16a233b380
Aleks <offsset@gmail.com>
2022-12-15 16:36:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,46 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const windEl = document.querySelector('.wall');
+  const wind = e.target.closest('.wall');
+  const borderWindEl = (windEl.offsetWidth - windEl.clientWidth) / 2;
+
+  if (!wind) {
+    return;
+  }
+
+  if (
+    (e.clientX - windEl.offsetLeft - (spider.offsetWidth / 2 + borderWindEl))
+      > (windEl.offsetWidth - (spider.offsetWidth + borderWindEl * 2))
+  ) {
+    spider.style.left = (windEl.offsetWidth - (
+      spider.offsetWidth + borderWindEl * 2
+    )) + 'px';
+  } else if (
+    (e.clientX - windEl.offsetLeft) < (spider.offsetWidth / 2 + borderWindEl)
+  ) {
+    spider.style.left = 0 + 'px';
+  } else {
+    spider.style.left = (
+      e.clientX - windEl.offsetLeft - (spider.offsetWidth / 2 + borderWindEl)
+    ) + 'px';
+  }
+
+  if (
+    (e.clientY - windEl.offsetTop - (spider.offsetWidth / 2 + borderWindEl))
+     > (windEl.offsetHeight - (spider.offsetWidth + borderWindEl * 2))
+  ) {
+    spider.style.top = (
+      windEl.offsetHeight - (spider.offsetWidth + borderWindEl * 2)
+    ) + 'px';
+  } else if (
+    (e.clientY - windEl.offsetTop) < (spider.offsetWidth / 2 + borderWindEl)
+  ) {
+    spider.style.top = 0 + 'px';
+  } else {
+    spider.style.top = (
+      e.clientY - windEl.offsetTop - (spider.offsetWidth / 2 + borderWindEl)
+    ) + 'px';
+  }
 });
