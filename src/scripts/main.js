aac58692a7c4e3caa80787e1123666d7e0ac14d3
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-12-18 20:05:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,20 @@
 
 document.addEventListener('click', e => {
   // write code here
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
-  const spiderSize = getComputedStyle(spider).height;
-  const wallSize = getComputedStyle(wall).height;
-  const min = 0;
-  const max = parseFloat(wallSize) - parseFloat(spiderSize);
 
-  const coordinateX = e.layerX - spider.clientWidth / 2;
-  const coordinateY = e.layerY - spider.clientHeight / 2;
+  const coordinateX = e.offsetX - spider.clientWidth / 2;
+  const coordinateY = e.offsetY - spider.clientHeight / 2;
+  const wallSize = parseFloat(wall.clientWidth) - spider.clientWidth;
 
-  spider.style.left = coordinateX < min
-    ? 0 : coordinateX > max ? max + 'px' : coordinateX + 'px';
+  spider.style.left = coordinateX < 0
+    ? 0 : coordinateX > wallSize ? `${wallSize}px` : `${coordinateX}px`;
 
-  spider.style.top = coordinateY < min
-    ? 0 : coordinateY > max ? max + 'px' : coordinateY + 'px';
+  spider.style.top = coordinateY < 0
+    ? 0 : coordinateY > wallSize ? `${wallSize}px` : `${coordinateY}px`;
 });
