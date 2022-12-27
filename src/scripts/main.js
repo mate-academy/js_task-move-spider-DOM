c9c92148f0c94d7acae895da41e7c3d033d4baac
Rafael Mamedov <rafmamedov@gmail.com>
2022-12-27 12:51:41 +0200

    Fixed not crossing wall boundaries

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,24 @@ document.addEventListener('click', e => {
   const wall = document.querySelector('.wall');
 
   if (e.target === wall) {
-    spider.style.top = `${e.offsetY}px`;
     spider.style.left = `${e.offsetX}px`;
+    spider.style.top = `${e.offsetY}px`;
     spider.style.transform = 'translate(-50%, -50%)';
-  }
+
+    if (e.offsetX < 25) {
+      spider.style.left = '25px';
+    }
+
+    if (e.offsetX > 375) {
+      spider.style.left = '375px';
+    }
+
+    if (e.offsetY < 25) {
+      spider.style.top = '25px';
+    }
+
+    if (e.offsetY > 375) {
+      spider.style.top = '375px';
+    }
+  };
 });
