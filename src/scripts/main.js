f1822210d0b49e97c33d98746756d4ed7c37d671
Rafael Mamedov <rafmamedov@gmail.com>
2022-12-27 12:21:03 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (e.target === wall) {
+    spider.style.top = `${e.offsetY}px`;
+    spider.style.left = `${e.offsetX}px`;
+    spider.style.transform = 'translate(-50%, -50%)';
+  }
 });
