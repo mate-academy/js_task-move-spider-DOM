914c6b72122c6084150e5825e238bf73b5e1034c
Rafael Mamedov <rafmamedov@gmail.com>
2022-12-27 15:18:33 +0200

    Removed hardcoded sizes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,26 +3,28 @@
 document.addEventListener('click', e => {
   const spider = document.querySelector('.spider');
   const wall = document.querySelector('.wall');
+  const halfSpiderWidth = +getComputedStyle(spider).width.slice(0, -2) / 2;
+  const wallWidth = +getComputedStyle(wall).width.slice(0, -2);
 
   if (e.target === wall) {
     spider.style.left = `${e.offsetX}px`;
     spider.style.top = `${e.offsetY}px`;
     spider.style.transform = 'translate(-50%, -50%)';
 
-    if (e.offsetX < 25) {
-      spider.style.left = '25px';
+    if (e.offsetX < halfSpiderWidth) {
+      spider.style.left = `${halfSpiderWidth}px`;
     }
 
-    if (e.offsetX > 375) {
-      spider.style.left = '375px';
+    if (e.offsetX > wallWidth - halfSpiderWidth) {
+      spider.style.left = `${wallWidth - halfSpiderWidth}px`;
     }
 
-    if (e.offsetY < 25) {
-      spider.style.top = '25px';
+    if (e.offsetY < halfSpiderWidth / 2) {
+      spider.style.top = `${halfSpiderWidth}px`;
     }
 
-    if (e.offsetY > 375) {
-      spider.style.top = '375px';
+    if (e.offsetY > wallWidth - halfSpiderWidth) {
+      spider.style.top = `${wallWidth - halfSpiderWidth}px`;
     }
   };
 });
