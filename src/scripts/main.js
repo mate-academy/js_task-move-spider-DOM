28e9995a170765c1fc0dd625f38e6cdf660b855e
Aleks <offsset@gmail.com>
2023-01-04 22:02:57 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,54 +1,53 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const wallClW = wall.clientWidth;
+const wallClL = wall.clientLeft;
+
+const wallClH = wall.clientHeight;
+const wallClT = wall.clientTop;
+
+const spider = document.querySelector('.spider');
+const spiderOfW = spider.offsetWidth;
+const spiderOfH = spider.offsetHeight;
+
+const minCoordX = wall.getBoundingClientRect().x + wallClL;
+const maxCoordX = minCoordX + wallClW - wallClL - spiderOfW;
+
+const minCoordY = wall.getBoundingClientRect().y + wallClT;
+const maxCoordY = minCoordY + wallClH - wallClT - spiderOfH;
+
 document.addEventListener('click', e => {
-  const spider = document.querySelector('.spider');
-  const spiderOfW = spider.offsetWidth;
-  const windEl = document.querySelector('.wall');
   const wind = e.target.closest('.wall');
-  const windElBoLe = windEl.clientLeft;
-  const windElBorderTop = windEl.clientTop;
-  const windElOfW = windEl.offsetWidth;
-  const windElOfL = windEl.offsetLeft;
-  const windElOfT = windEl.offsetTop;
-  const windElOfH = windEl.offsetHeight;
 
   if (!wind) {
     return;
   }
 
-  if (
-    (
-      e.clientX - windElOfL - (spiderOfW / 2 + windElBoLe)
-    ) > (
-      windElOfW - (spiderOfW + windElBoLe * 2)
-    )
-  ) {
-    spider.style.left = (
-      windElOfW - (spiderOfW + windElBoLe * 2)
-    ) + 'px';
-  } else if ((e.clientX - windElOfL) < (spiderOfW / 2 + windElBoLe)) {
-    spider.style.left = 0 + 'px';
-  } else {
-    spider.style.left = (
-      e.clientX - windElOfL - (spiderOfW / 2 + windElBoLe)
-    ) + 'px';
+  const x = e.clientX;
+  const y = e.clientY;
+
+  switch (true) {
+    case x < minCoordX:
+      spider.style.left = 0 + 'px';
+      break;
+    case x > maxCoordX:
+      spider.style.left = (wallClW - spiderOfW) + 'px';
+      break;
+    default:
+      spider.style.left = (x - minCoordX) + 'px';
+      break;
   }
 
-  if (
-    (
-      e.clientY - windElOfT - (spiderOfW / 2 + windElBorderTop)
-    ) > (
-      windElOfH - (spiderOfW + windElBorderTop * 2)
-    )
-  ) {
-    spider.style.top = (
-      windElOfH - (spiderOfW + windElBorderTop * 2)
-    ) + 'px';
-  } else if ((e.clientY - windElOfT) < (spiderOfW / 2 + windElBorderTop)) {
-    spider.style.top = 0 + 'px';
-  } else {
-    spider.style.top = (
-      e.clientY - windElOfT - (spiderOfW / 2 + windElBorderTop)
-    ) + 'px';
+  switch (true) {
+    case y < minCoordY:
+      spider.style.top = 0 + 'px';
+      break;
+    case y > maxCoordY:
+      spider.style.top = (wallClH - spiderOfW) + 'px';
+      break;
+    default:
+      spider.style.top = (y - minCoordY) + 'px';
+      break;
   }
 });
