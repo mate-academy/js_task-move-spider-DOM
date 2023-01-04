564971ab74fc3a0a66d9c48b8fafd86f35a2afe5
Nick Busarhin <busarginkolia@gmail.com>
2023-01-04 22:44:15 +0200

    moving spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,41 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const wallData = wall.getBoundingClientRect();
+
+  const wallPositionX = wallData.left + wall.clientLeft;
+  const wallPositionY = wallData.top + wall.clientTop;
+
+  let spiderX = e.clientX - wallPositionX - spider.offsetWidth / 2;
+  let spiderY = e.clientY - wallPositionY - spider.offsetHeight / 2;
+
+  const horisontalLimit = wall.clientWidth - spider.offsetWidth;
+  const verticalLimit = wall.clientHeight - spider.offsetHeight;
+
+  if (spiderX < 0) {
+    spiderX = 0;
+  }
+
+  if (spiderX > horisontalLimit) {
+    spiderX = horisontalLimit;
+  }
+
+  if (spiderY < 0) {
+    spiderY = 0;
+  }
+
+  if (spiderY > verticalLimit) {
+    spiderY = verticalLimit;
+  }
+
+  if (e.target.className !== 'wall') {
+    return false;
+  }
+
+  spider.style.cssText
+    = `margin-top: ${spiderY}px;
+    margin-left: ${spiderX}px`;
 });
