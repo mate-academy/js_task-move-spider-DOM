36d9a15434df7d61e3fdaa39abcbf229c7733652
Yaroslav <yaroslavfoorgala@gmail.com>
2023-01-09 10:42:40 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,44 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const border = wall.clientLeft;
+
 document.addEventListener('click', e => {
-  // write code here
+  const coordsForSpider = spider.getBoundingClientRect();
+  const coordsForWall = wall.getBoundingClientRect();
+
+  const maxVertical
+    = coordsForWall.height - border * 2 - coordsForSpider.height;
+  const maxGorizont
+    = coordsForWall.width - border * 2 - coordsForSpider.width;
+
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  let left = e.clientX - coordsForWall.left
+    - wall.clientLeft - coordsForSpider.width / 2;
+
+  let topO = e.clientY - coordsForWall.top
+    - wall.clientTop - coordsForSpider.height / 2;
+
+  if (left < 0) {
+    left = 0;
+  }
+
+  if (left > maxGorizont) {
+    left = maxGorizont;
+  }
+
+  if (topO < 0) {
+    topO = 0;
+  }
+
+  if (topO > maxVertical) {
+    topO = maxVertical;
+  }
+
+  spider.style.left = left + 'px';
+  spider.style.top = topO + 'px';
 });
