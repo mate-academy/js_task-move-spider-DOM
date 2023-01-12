76f11d36cf997ca15572e0888078b0e24212cffa
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-01-12 10:16:05 +0000

    move spider task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  const wallXCorrection = wall.getBoundingClientRect().left;
+  const wallYCorrection = wall.getBoundingClientRect().top;
+  let xPos = e.clientX - (spider.clientWidth / 2)
+  - wallXCorrection;
+  let yPos = e.clientY - (spider.clientHeight / 2)
+  - wallYCorrection;
+
+  if (xPos < 0) {
+    xPos = 0;
+  } else if (xPos > wall.clientWidth - spider.clientWidth) {
+    xPos = wall.clientWidth - spider.clientWidth;
+  }
+
+  if (yPos < 0) {
+    yPos = 0;
+  } else if (yPos > wall.clientWidth - spider.clientWidth) {
+    yPos = wall.clientHeight - spider.clientWidth;
+  }
+
+  spider.style.left = xPos + 'px';
+
+  spider.style.top = yPos + 'px';
 });
