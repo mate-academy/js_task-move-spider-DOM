3a9f6ca4b5ae791839613c6bf07e326aae3ce783
EhorSaprykin <astralik1993@gmail.com>
2023-01-12 10:05:27 +0200

    Now spider can move

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
+  const wallCoords = wall.getBoundingClientRect();
+
+  const spiderXBorder = e.clientX - wallCoords.x - 35;
+  const spiderYBorder = e.clientY - wallCoords.y - 35;
+
+  spider.style.left = spiderXBorder + 'px';
+  spider.style.top = spiderYBorder + 'px';
+
+  if (spiderXBorder < 0) {
+    spider.style.left = '0px';
+  };
+
+  if (spiderYBorder < 0) {
+    spider.style.top = '0px';
+  }
+
+  if (spiderXBorder > 350) {
+    spider.style.left = '350px';
+  };
+
+  if (spiderYBorder > 350) {
+    spider.style.top = '350px';
+  }
 });
