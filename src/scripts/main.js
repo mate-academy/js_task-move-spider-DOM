2a527b82a091bb108f5a8b4ef33bece589612fed
EhorSaprykin <astralik1993@gmail.com>
2023-01-15 11:20:24 +0200

    Add some var's

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,33 +6,32 @@ const wall = document.querySelector('.wall');
 wall.addEventListener('click', (e) => {
   const wallCoords = wall.getBoundingClientRect();
   const spiderCoords = spider.getBoundingClientRect();
+  const borderSize = (wall.offsetWidth - wall.clientWidth) / 2;
   const min = 0;
   const maxX = 350;
   const maxY = 350;
 
   const spiderXBorder
-    = e.clientX - wallCoords.x - (spiderCoords.width / 2)
-      - (wall.offsetWidth - wall.clientWidth) / 2;
+    = e.clientX - wallCoords.x - (spiderCoords.width / 2) - borderSize;
   const spiderYBorder
-    = e.clientY - wallCoords.y - (spiderCoords.height / 2)
-      - (wall.offsetWidth - wall.clientWidth) / 2;
+    = e.clientY - wallCoords.y - (spiderCoords.height / 2) - borderSize;
 
   spider.style.left = spiderXBorder + 'px';
   spider.style.top = spiderYBorder + 'px';
 
   if (spiderXBorder < min) {
-    spider.style.left = '0px';
+    spider.style.left = min + 'px';
   };
 
   if (spiderYBorder < min) {
-    spider.style.top = '0px';
+    spider.style.top = min + 'px';
   }
 
   if (spiderXBorder > maxX) {
-    spider.style.left = '350px';
+    spider.style.left = maxX + 'px';
   };
 
   if (spiderYBorder > maxY) {
-    spider.style.top = '350px';
-  }
+    spider.style.top = maxY + 'px';
+  };
 });
