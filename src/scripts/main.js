d0fff607ef4c05fc53a94844176de19bcc53238f
Kate <ekaterina2002aleks@gmail.com>
2023-01-19 14:26:01 +0200

    Move spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (!e.target.matches('.wall')) {
+    return;
+  }
+
+  const x = e.clientX;
+  const y = e.clientY;
+  const wallCoords = wall.getBoundingClientRect();
+  let spiderTop = y - wallCoords.top;
+  const spiderLeft = x - wallCoords.left;
+
+  if (spiderTop < wallCoords.top + wall.clientTop) {
+    spiderTop = wallCoords.top;
+  }
+
+  spider.style.top = spiderTop + 'px';
+  spider.style.left = spiderLeft + 'px';
+  spider.style.transform = 'translate(-50%, -50%)';
+
+  // const wallPositionX = wallCoords.left + wall.clientLeft;
+
+  // console.log(wall.clientLeft);
 });
