5f915a582b06f545932612b1cb3f8d5a27a1a934
Anton Vasyliev <magisterol963@gmail.com>
2023-01-20 14:32:36 +0200

    solution; git commit -am solution git commit -am solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,42 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  const item = e.target;
+
+  if (item !== wall) {
+    return;
+  }
+
+  const wallStyle = getComputedStyle(wall);
+  const border = parseFloat(wallStyle.borderBlockEndWidth);
+  const wallWidth = parseFloat(wallStyle.width);
+  const wallHeight = parseFloat(wallStyle.height);
+
+  const spiderHalf = spider.width / 2;
+
+  let topCoord = e.clientY - wall.offsetTop - border;
+  let leftCoord = e.clientX - wall.offsetLeft - border;
+
+  if (leftCoord >= wallWidth - spiderHalf) {
+    leftCoord = wallWidth - spiderHalf;
+  }
+
+  if (leftCoord <= spiderHalf) {
+    leftCoord = spiderHalf;
+  }
+
+  if (topCoord >= wallHeight - spiderHalf) {
+    topCoord = wallHeight - spiderHalf;
+  }
+
+  if (topCoord <= spiderHalf) {
+    topCoord = spiderHalf;
+  }
+
+  spider.style.transform = 'translate(-50%, -50%)';
+  spider.style.top = `${topCoord}px`;
+  spider.style.left = `${leftCoord}px`;
 });
