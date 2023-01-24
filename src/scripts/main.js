ee18a72051f34fb8c7d2ae0c4b291a0ed950a24e
Vadym Prydatok <vadymprydatok@gmail.com>
2023-01-24 19:01:52 +0200

    move_spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', e => {
+  let positionTop = e.offsetY;
+  let positionLeft = e.offsetX;
+  const halfSpiderWidth = spider.clientWidth / 2;
+  const halfSpiderHeight = spider.clientHeight / 2;
+
+  if (positionTop + spider.clientHeight > wall.clientHeight) {
+    positionTop = wall.clientHeight - halfSpiderHeight;
+  }
+
+  if (positionLeft + spider.clientWidth > wall.clientWidth) {
+    positionLeft = wall.clientWidth - halfSpiderWidth;
+  }
+
+  if (positionLeft - halfSpiderWidth < 0) {
+    positionLeft = halfSpiderWidth;
+  }
+
+  if (positionTop - halfSpiderHeight < 0) {
+    positionTop = halfSpiderHeight;
+  }
+
+  spider.style.top = `${positionTop - halfSpiderHeight}px`;
+  spider.style.left = `${positionLeft - halfSpiderWidth}px`;
 });
