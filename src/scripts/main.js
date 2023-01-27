28814e9f5856ecb7d237afc0b877f3d64cb60616
Olena Hapon <olenagapon90gmail.com>
2023-01-27 12:12:05 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (!e.target.matches('.wall')) {
+    return;
+  };
+
+  const maxWidth = wall.clientWidth - spider.offsetWidth;
+  const maxHeight = wall.clientHeight - spider.offsetWidth;
+  let spiderPositionX = e.offsetX - (spider.offsetWidth / 2);
+
+  let spiderPositionY = e.offsetY - (spider.offsetHeight / 2);
+
+  if (spiderPositionX < 0) {
+    spiderPositionX = 0;
+  }
+
+  if (spiderPositionX > maxWidth) {
+    spiderPositionX = maxWidth;
+  }
+
+  if (spiderPositionY < 0) {
+    spiderPositionY = 0;
+  }
+
+  if (spiderPositionY > maxHeight) {
+    spiderPositionY = maxHeight;
+  }
+
+  spider.style.top = spiderPositionY + 'px';
+  spider.style.left = spiderPositionX + 'px';
 });
