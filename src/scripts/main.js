cb3bf4c1644c10c50e4b28f1d4afc19d14a1151a
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-28 22:56:34 +0200

    spider move

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const wallHeightLimit = wall.clientHeight - spider.offsetHeight;
+  const wallWidthLimit = wall.clientWidth - spider.offsetWidth;
+
+  wall.addEventListener('click', k => {
+    let positionLeft = k.offsetX - (spider.offsetWidth / 2);
+    let positionTop = k.offsetY - (spider.offsetHeight / 2);
+
+    if (positionLeft < 0) {
+      positionLeft = 0;
+    }
+
+    if (positionLeft > wallWidthLimit) {
+      positionLeft = wallWidthLimit;
+    }
+
+    if (positionTop < 0) {
+      positionTop = 0;
+    }
+
+    if (positionTop > wallHeightLimit) {
+      positionTop = wallHeightLimit;
+    }
+
+    spider.style.top = `${positionTop}px`;
+    spider.style.left = `${positionLeft}px`;
+  });
 });
