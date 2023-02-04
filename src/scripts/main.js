b2513858908f84ba0b9110683adb76f76176173e
Aleksey-Aleksey <l0734062646@gmail.com>
2023-02-04 20:23:03 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  let x = e.offsetX - spider.offsetWidth / 2;
+  let y = e.offsetY - spider.offsetHeight / 2;
+
+  const wallWidth = wall.clientWidth - spider.offsetWidth;
+  const wallHeight = wall.clientHeight - spider.offsetHeight;
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (x > wallWidth) {
+    x = wallWidth;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (y > wallHeight) {
+    y = wallHeight;
+  }
+
+  spider.style.top = y + 'px';
+  spider.style.left = x + 'px';
 });
