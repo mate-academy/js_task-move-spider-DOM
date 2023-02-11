af6c64edc6aea80d2bb10c577c82eda93e37e317
haduigon <haduigon@gmail.com>
2023-02-11 18:10:44 +0200

    second spider commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,37 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
 
-let wall = document.querySelector('.wall')
+const spider = document.querySelector('.spider');
 
-let spider = document.querySelector('.spider')
-
-let spiderStyle = window.getComputedStyle(spider)
-
-document.addEventListener('click', (event) => {
-
+document.addEventListener('click', (e) => {
   const coords = {
     x: event.offsetX - spider.clientWidth / 2,
-    y: event.offsetY - spider.clientHeight / 2
+    y: event.offsetY - spider.clientHeight / 2,
   };
 
   if (event.target !== wall) {
-    return
+    return;
   }
 
   function getCorrds(coord, wallSize, spiderSize) {
-    if ( coord < 0) {
+
+    const wallArea = wallSize - spiderSize
+
+    if (coord < 0) {
       return 0;
     }
-    if (coord > wallSize - spiderSize){
-      return wallSize - spiderSize;
+
+    if (coord > wallArea) {
+      return wallArea;
     }
 
     return coord;
   }
 
-  coords.x = getCorrds(coords.x, wall.clientWidth, spider.clientWidth)
-  coords.y = getCorrds(coords.y, wall.clientHeight, spider.clientHeight)
+  coords.x = getCorrds(coords.x, wall.clientWidth, spider.clientWidth);
+  coords.y = getCorrds(coords.y, wall.clientHeight, spider.clientHeight);
 
   spider.style.left = `${coords.x}px`;
   spider.style.top = `${coords.y}px`;
-
-})
-
+});
