9c75060e0f1edf49cdf7819d64843718dfa860ac
haduigon <haduigon@gmail.com>
2023-02-11 12:52:42 +0200

    first spider commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,39 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+
+let wall = document.querySelector('.wall')
+
+let spider = document.querySelector('.spider')
+
+let spiderStyle = window.getComputedStyle(spider)
+
+document.addEventListener('click', (event) => {
+
+  const coords = {
+    x: event.offsetX - spider.clientWidth / 2,
+    y: event.offsetY - spider.clientHeight / 2
+  };
+
+  if (event.target !== wall) {
+    return
+  }
+
+  function getCorrds(coord, wallSize, spiderSize) {
+    if ( coord < 0) {
+      return 0;
+    }
+    if (coord > wallSize - spiderSize){
+      return wallSize - spiderSize;
+    }
+
+    return coord;
+  }
+
+  coords.x = getCorrds(coords.x, wall.clientWidth, spider.clientWidth)
+  coords.y = getCorrds(coords.y, wall.clientHeight, spider.clientHeight)
+
+  spider.style.left = `${coords.x}px`;
+  spider.style.top = `${coords.y}px`;
+
+})
+
