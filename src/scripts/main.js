b0d0b28a542bb13079fbb9ec50803580081ae9bb
Kate <novak5683@gmail.com>
2023-02-12 15:29:33 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+document.getElementsByClassName('wall')[0].addEventListener('click', e => {
+  const spider = document.getElementsByClassName('spider')[0];
+  const wall = document.getElementsByClassName('wall')[0];
+  const borderWidth = parseInt(window.getComputedStyle(wall)
+    .getPropertyValue('border-width'));
+
+  if (wall) {
+    const x = e.clientX - wall.offsetLeft - spider.offsetWidth
+      / 2 - borderWidth;
+    const y = e.clientY - wall.offsetTop - spider.offsetHeight
+      / 2 - borderWidth;
+
+    const maxX = wall.clientWidth - spider.offsetWidth;
+    const resX = Math.min(Math.max(x, 0), maxX);
+
+    const maxY = wall.clientHeight - spider.offsetHeight;
+    const resY = Math.min(Math.max(y, 0), maxY);
+
+    spider.style
+      = `
+      top: ${resY}px; left: ${resX}px;
+    `;
+  }
 });
