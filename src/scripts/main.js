272c9b10021d271b5287b6f95d42f8c9b10a6a82
Jaxxxon78 <jaxxxon78@ukr.net>
2023-02-12 21:39:54 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallCoords = wall.getBoundingClientRect();
+
+  if (e.clientY < wallCoords.top || e.clientY > wallCoords.bottom) {
+    return;
+  }
+
+  if (e.clientX < wallCoords.left || e.clientX > wallCoords.right) {
+    return;
+  }
+
+  let spiderTop = (e.clientY - wallCoords.top - spider.clientHeight / 2
+    - wall.clientTop);
+
+  spiderTop = spiderTop < 0 ? 0 : spiderTop;
+
+  if (spiderTop > wall.clientHeight - spider.clientHeight) {
+    spiderTop = wall.clientHeight - spider.clientHeight;
+  }
+
+  let spiderLeft = (e.clientX - wallCoords.left - spider.clientWidth / 2
+  - wall.clientLeft);
+
+  if (spiderLeft > wall.clientWidth - spider.clientWidth) {
+    spiderLeft = wall.clientWidth - spider.clientWidth;
+  }
+
+  spiderLeft = spiderLeft < 0 ? 0 : spiderLeft;
+
+  spider.style.top = spiderTop + 'px';
+  spider.style.left = spiderLeft + 'px';
 });
