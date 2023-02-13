875de4cb7da1fbeeb313a4a6bbd915894100ac68
Vasyl Pavlenko <genlus@ukr.net>
2023-02-13 15:10:11 +0200

    Move for spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const field = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const fieldCoords = field.getBoundingClientRect();
+
+  const spiderCoords = {
+    top: e.clientY - fieldCoords.top
+      - field.clientTop - spider.clientHeight / 2,
+    left: e.clientX - fieldCoords.left
+      - field.clientLeft - spider.clientWidth / 2,
+  };
+
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  if (spiderCoords.top < 0) {
+    spiderCoords.top = 0;
+  }
+
+  if (spiderCoords.left < 0) {
+    spiderCoords.left = 0;
+  }
+
+  if (spiderCoords.left + spider.clientWidth > field.clientWidth) {
+    spiderCoords.left = field.clientWidth - spider.clientWidth;
+  }
+
+  if (spiderCoords.top + spider.clientHeight > field.clientHeight) {
+    spiderCoords.top = field.clientHeight - spider.clientHeight;
+  }
+
+  spider.style.left = spiderCoords.left + 'px';
+  spider.style.top = spiderCoords.top + 'px';
 });
