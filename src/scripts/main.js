b09bf7daaa13061be2bf0089c25b50b006845190
Oleksandr <98667890az@gmail.com>
2023-02-16 18:09:55 +0200

    move-spider-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', e => {
+  const workSpaceY = wall.clientHeight - spider.clientHeight;
+  const workSpaceX = wall.clientWidth - spider.clientWidth;
+
+  let normalizedY = e.offsetY;
+  let normalizedX = e.offsetX;
+
+  if (e.offsetY < 0) {
+    normalizedY = spider.clientHeight;
+  }
+
+  if (e.offsetY > workSpaceY) {
+    normalizedY = workSpaceY;
+  }
+
+  if (e.offsetX < 0) {
+    normalizedX = spider.clientWidth;
+  }
+
+  if (e.offsetX > workSpaceX) {
+    normalizedX = workSpaceX;
+  }
+
+  spider.style.left = `${normalizedX}px`;
+  spider.style.top = `${normalizedY}px`;
 });
