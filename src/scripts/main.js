cf5b498100cbb42fe76db731fb83aeef45778e03
Nigga <yomzfk@gmail.com>
2023-02-16 14:17:23 +0200

    Move spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (!e.target.closest('.wall')) {
+    return;
+  };
+
+  const maxWidth = wall.clientWidth - spider.clientWidth;
+  const maxHeigth = wall.clientHeight - spider.clientHeight;
+  let x = e.offsetX - spider.clientWidth / 2;
+  let y = e.offsetY - spider.clientHeight / 2;
+
+  if (x < 1) {
+    x = 0;
+  }
+
+  if (y < 1) {
+    y = 0;
+  }
+
+  if (maxWidth < x) {
+    x = maxWidth;
+  }
+
+  if (maxHeigth < y) {
+    y = maxHeigth;
+  }
+
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
 });
