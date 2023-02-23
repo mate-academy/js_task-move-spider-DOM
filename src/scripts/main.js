90c6da687a4cad4754baf7537af67aeec0ee25fa
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2023-02-23 22:47:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,39 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const fieldRef = document.querySelector('.wall');
+  const spiderRef = document.querySelector('.spider');
+
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  const widthLimit = fieldRef.clientWidth - spiderRef.clientWidth;
+  const heigthLimit = fieldRef.clientHeight - spiderRef.clientHeight;
+  const fieldCoords = fieldRef.getBoundingClientRect();
+
+  let calculatedX = e.clientX - fieldCoords.left - fieldRef.clientLeft
+    - spiderRef.clientWidth / 2;
+
+  let calculatedY = e.clientY - fieldCoords.top - fieldRef.clientTop
+    - spiderRef.clientHeight / 2;
+
+  if (calculatedX < 0) {
+    calculatedX = 0;
+  }
+
+  if (calculatedY < 0) {
+    calculatedY = 0;
+  }
+
+  if (calculatedX > widthLimit) {
+    calculatedX = widthLimit;
+  }
+
+  if (calculatedY > heigthLimit) {
+    calculatedY = heigthLimit;
+  }
+
+  spiderRef.style.left = calculatedX + 'px';
+  spiderRef.style.top = calculatedY + 'px';
 });
