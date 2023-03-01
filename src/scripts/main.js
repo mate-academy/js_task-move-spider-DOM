422f7b6f5308548885730bbf4310092cb18d222f
Mykola Huryn <mukola197022@gmail.com>
2023-03-01 13:43:51 +0100

    js_task-move-spider-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const maxCoordY = wall.clientHeight - spider.clientHeight;
+const maxCoordX = wall.clientWidth - spider.clientWidth;
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  let spiderCoordY = e.offsetY - (spider.clientHeight / 2);
+  let spiderCoordX = e.offsetX - (spider.clientWidth / 2);
+
+  if (spiderCoordX > maxCoordX) {
+    spiderCoordX = maxCoordX;
+  }
+
+  if (spiderCoordY > maxCoordY) {
+    spiderCoordY = maxCoordY;
+  }
+
+  if (spiderCoordX < 0) {
+    spiderCoordX = 0;
+  }
+
+  if (spiderCoordY < 0) {
+    spiderCoordY = 0;
+  }
+  spider.style.top = `${spiderCoordY}px`;
+  spider.style.left = `${spiderCoordX}px`;
 });
