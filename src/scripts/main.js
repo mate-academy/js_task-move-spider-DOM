4fa934fdce814318e9b4b040548449282e95d4b4
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-03-02 12:20:40 +0200

    Solution-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,32 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const coordinateMouseX = e.offsetX;
+  const coordinateMouseY = e.offsetY;
+
+  const widthWall = e.clientX;
+  const hightWall = e.clientY;
+
+  wall.style.position = 'reletive';
+  spider.style.position = 'absolute';
+
+  console.log(e);
+
+  const widthSpider = getComputedStyle(spider);
+  const halfWidthSpider = parseFloat(widthSpider.width) / 2;
+
+  if (coordinateMouseX >= widthWall) {
+    spider.style.left = widthWall;
+  } else {
+    spider.style.left = `${coordinateMouseX - halfWidthSpider}px`;
+  }
+
+  if (coordinateMouseY >= hightWall) {
+    spider.style.hight = hightWall;
+  } else {
+    spider.style.top = `${coordinateMouseY - halfWidthSpider}px`;
+  }
 });
