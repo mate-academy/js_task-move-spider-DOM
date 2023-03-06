b06127063033b8780200efdd4734d73efaa5910c
Yelisieiev Kim <rbv271@outlook.com>
2023-03-06 16:51:16 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,37 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const maxX = wall.clientWidth - spider.clientWidth;
+const maxY = wall.clientHeight - spider.clientWidth;
+const indentLeft = wall.offsetLeft + wall.clientLeft;
+const indentTop = wall.offsetTop + wall.clientTop;
+
 document.addEventListener('click', e => {
-  // write code here
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  let coordX = e.clientX - indentLeft - spider.clientWidth / 2;
+  let coordY = e.clientY - indentTop - spider.clientHeight / 2;
+
+  if (coordX > maxX) {
+    coordX = maxX;
+  }
+
+  if (coordY > maxY) {
+    coordY = maxY;
+  }
+
+  if (coordX < 0) {
+    coordX = 0;
+  }
+
+  if (coordY < 0) {
+    coordY = 0;
+  }
+
+  spider.style.top = `${coordY}px`;
+  spider.style.left = `${coordX}px`;
 });
