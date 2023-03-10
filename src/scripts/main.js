f8e684df4d307c17799677bfcc1b1ec4231d903d
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-03-10 13:33:54 +0200

    Task move spider 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,16 +10,8 @@ document.addEventListener('click', e => {
 
   const limitWidth = wall.clientWidth - spider.clientWidth;
   const limitHeight = wall.clientHeight - spider.clientHeight;
-  let x = e.offsetX - spider.clientWidth / 2;
-  let y = e.offsetY - spider.clientHeight / 2;
-
-  if (x < 1) {
-    x = 0;
-  }
-
-  if (y < 1) {
-    y = 0;
-  }
+  let x = Math.max(0, e.offsetX - spider.clientWidth / 2);
+  let y = Math.max(0, e.offsetY - spider.clientHeight / 2);
 
   if (x > limitWidth) {
     x = limitWidth;
