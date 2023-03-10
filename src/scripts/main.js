77c9b57b01f4fb2bd1e430a9d37d70d6fe56adbb
Andrew Litkovets <andriylao@gmail.com>
2023-03-10 17:26:13 +0200

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', evnt => {
+  let spiderCoordsTop = evnt.offsetY - spider.height / 2;
+  let spiderCoordsLeft = evnt.offsetX - spider.width / 2;
+
+  if (spiderCoordsTop < 0) {
+    spiderCoordsTop = 0;
+  }
+
+  if (spiderCoordsLeft < 0) {
+    spiderCoordsLeft = 0;
+  }
+
+  if (evnt.offsetY > wall.clientHeight) {
+    spiderCoordsTop = wall.clientHeight - spider.height;
+  }
+
+  if (evnt.offsetX > wall.clientWidth) {
+    spiderCoordsLeft = wall.clientWidth - spider.width;
+  }
+
+  spider.style.top = spiderCoordsTop + 'px';
+  spider.style.left = spiderCoordsLeft + 'px';
 });
