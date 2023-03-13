f1a88f116d271951e04f5a00e922400edbbded0c
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-13 15:18:53 +0200

    Make element follow mouse by click with restricted movement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const spiderWall = document.querySelector('.wall');
+  const wallData = spiderWall.getBoundingClientRect();
+  const clickableArea = e.target.closest('.wall');
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  const xArea = spiderWall.clientWidth - spider.offsetWidth;
+  const yArea = spiderWall.clientHeight - spider.offsetHeight;
+  const supposedXForSpider = mouseX - wallData.x - spider.offsetWidth / 2;
+  const supposedYForSpider = mouseY - wallData.y - spider.offsetHeight / 2;
+  const newXForSpider = Math.min(Math.max(0, supposedXForSpider), xArea);
+  const newYForSpider = Math.min(Math.max(0, supposedYForSpider), yArea);
+
+  if (!clickableArea) {
+    return;
+  }
+
+  spider.style.top = `${newYForSpider}px`;
+  spider.style.left = `${newXForSpider}px`;
 });
