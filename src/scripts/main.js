db9360e77e0c8bd734b0366167902eaafdd65693
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-03-23 16:32:50 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const { offsetX, offsetY } = e;
+
+  const spiderHalf = parseFloat(spider.width) / 2;
+  const maxWidth = wall.clientWidth - spider.offsetWidth;
+  const maxHeight = wall.clientHeight - spider.offsetHeight;
+
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  spider.style.left = offsetX - spiderHalf <= maxWidth
+    ? `${offsetX - spiderHalf}px`
+    : `${wall.clientWidth - spider.clientWidth}px`;
+
+  if (offsetX < spider.clientHeight) {
+    spider.style.left = '0px';
+  }
+
+  spider.style.top = offsetY - spiderHalf <= maxHeight
+    ? `${offsetY - spiderHalf}px`
+    : `${wall.clientHeight - spider.clientHeight}px`;
+
+  if (offsetY < spider.clientHeight) {
+    spider.style.top = '0px';
+  }
 });
