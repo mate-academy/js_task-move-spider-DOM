af6cf525b14e1e438a32b40ed2fe9516b7703c8c
Oleksandr Plias <pls777@ukr.net>
2023-03-31 14:00:00 +0300

    Fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,33 +3,31 @@
 document.addEventListener('click', e => {
   const wall = document.querySelector('.wall');
 
-  if (e.target.className !== 'wall') {
+  if (!(e.target.closest('.wall'))) {
     return;
   }
 
   const spider = document.querySelector('.spider');
 
-  const wallCoords = wall.getBoundingClientRect();
+  let x = e.offsetX - spider.clientWidth / 2;
+  let y = e.offsetY - spider.clientHeight / 2;
+  const maxWidth = wall.clientWidth - spider.clientWidth;
+  const maxHeight = wall.clientHeight - spider.clientHeight;
 
-  let y = e.clientY - wallCoords.top
-    - wall.clientTop - spider.clientHeight / 2;
-  let x = e.clientX - wallCoords.left
-    - wall.clientLeft - spider.clientWidth / 2;
-
-  if (y <= 0) {
-    y = 0;
-  };
-
-  if (x <= 0) {
+  if (x < 0) {
     x = 0;
-  };
-
-  if (y + spider.clientHeight > wall.clientHeight) {
-    y = wall.clientHeight - spider.clientHeight;
   }
 
-  if (x + spider.clientWidth > wall.clientWidth) {
-    x = wall.clientWidth - spider.clientWidth;
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (x > maxWidth) {
+    x = maxWidth;
+  }
+
+  if (y > maxHeight) {
+    y = maxHeight;
   }
 
   spider.style.top = `${y}px`;
