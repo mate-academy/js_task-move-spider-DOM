c7f1f1c9d20e760e47a4c9aed345fdb0181c2219
Andrii Kovalskyi <andrii.kovalskyi02@gmail.com>
2023-03-31 16:05:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+function constrain(value, min, max) {
+  return Math.min(Math.max(value, min), max);
+}
+
 document.addEventListener('click', e => {
-  // write code here
+  if (!(e.target.closest('.wall'))) {
+    return;
+  }
+
+  const wallRect = wall.getBoundingClientRect();
+  const maxWidth = wall.clientWidth - spider.offsetWidth;
+  const maxHeight = wall.clientHeight - spider.offsetHeight;
+
+  const xCoords = constrain(e.clientX - wallRect.left - spider.offsetWidth / 2, 0, maxWidth);
+  const yCoords = constrain(e.clientY - wallRect.top - spider.offsetHeight / 2, 0, maxHeight);
+
+  spider.style.left = xCoords + 'px';
+  spider.style.top = yCoords + 'px';
 });
