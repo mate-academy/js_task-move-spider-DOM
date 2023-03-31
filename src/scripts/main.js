0234f2029cd4a919c97e261e81c1e6b10efa4e62
Andrii Kovalskyi <andrii.kovalskyi02@gmail.com>
2023-03-31 16:05:51 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,8 +16,10 @@ document.addEventListener('click', e => {
   const maxWidth = wall.clientWidth - spider.offsetWidth;
   const maxHeight = wall.clientHeight - spider.offsetHeight;
 
-  const xCoords = constrain(e.clientX - wallRect.left - spider.offsetWidth / 2, 0, maxWidth);
-  const yCoords = constrain(e.clientY - wallRect.top - spider.offsetHeight / 2, 0, maxHeight);
+  const xCoords = constrain(e.clientX - wallRect.left - spider.offsetWidth / 2,
+    0, maxWidth);
+  const yCoords = constrain(e.clientY - wallRect.top - spider.offsetHeight / 2,
+    0, maxHeight);
 
   spider.style.left = xCoords + 'px';
   spider.style.top = yCoords + 'px';
