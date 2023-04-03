e70b84f71c400e124e8e3eb7dc6a5b79aa43b806
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-04-04 00:18:15 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const width = wall.clientWidth - spider.clientWidth;
+const height = wall.clientHeight - spider.clientHeight;
+
 document.addEventListener('click', e => {
-  // write code here
+  const target = e.target;
+
+  if (!target.classList.contains('wall')) {
+    return;
+  }
+
+  let spiderCoordX = e.offsetX - spider.clientWidth / 2;
+  let spiderCoordY = e.offsetY - spider.clientHeight / 2;
+
+  if (spiderCoordX > width) {
+    spiderCoordX = width;
+  }
+
+  if (spiderCoordX < 0) {
+    spiderCoordX = 0;
+  }
+
+  if (spiderCoordY > height) {
+    spiderCoordY = height;
+  }
+
+  if (spiderCoordY < 0) {
+    spiderCoordY = 0;
+  }
+
+  spider.style.left = `${spiderCoordX}px`;
+  spider.style.top = `${spiderCoordY}px`;
 });
