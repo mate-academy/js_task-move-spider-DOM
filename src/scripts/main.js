7c91993bafcf0f354c2b9133c094ec9b6a29ad07
DmytroHorshunov <cnimipignak@icloud.com>
2023-04-03 13:12:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  e.preventDefault();
+
+  if (!e.target.classList.contains('wall')) {
+    return false;
+  }
+
+  const spiderWidth = spider.clientWidth;
+  const maxTop = wall.clientHeight - spiderWidth;
+  const maxLeft = wall.clientWidth - spiderWidth;
+
+  let spiderTop = e.offsetY - spiderWidth / 2;
+  let spiderLeft = e.offsetX - spiderWidth / 2;
+
+  if (spiderTop > maxTop) {
+    spiderTop = maxTop;
+  }
+
+  if (spiderLeft > maxLeft) {
+    spiderLeft = maxLeft;
+  }
+
+  if (spiderLeft < 0) {
+    spiderLeft = 0;
+  }
+
+  if (spiderTop < 0) {
+    spiderTop = 0;
+  }
+
+  spider.style.top = spiderTop + 'px';
+  spider.style.left = spiderLeft + 'px';
 });
