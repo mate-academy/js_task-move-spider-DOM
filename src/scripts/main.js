16ace398a913ec9634422299b03abf0adb257740
Daryna Hnidash <melissa998877@gmail.com>
2023-04-06 14:29:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+  const spiderCentr = spider.clientWidth / 2;
+  const width = wall.clientWidth - spider.clientWidth;
+
+  spider.style.top = e.offsetY - spiderCentr + 'px';
+  spider.style.left = e.offsetX - spiderCentr + 'px';
+
+  if (e.offsetY - spiderCentr > width) {
+    spider.style.top = width + 'px';
+  }
+
+  if (e.offsetY - spiderCentr < 0) {
+    spider.style.top = '0px';
+  };
+
+  if (e.offsetX - spiderCentr > width) {
+    spider.style.left = width + 'px';
+  }
+
+  if (e.offsetX - spiderCentr < 0) {
+    spider.style.left = '0px';
+  };
 });
