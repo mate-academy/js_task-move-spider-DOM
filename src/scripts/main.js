d3221a55fbf1a92689b93020ee1571d32c1fc265
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-11 20:55:00 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider')
 
-document.addEventListener('click', e => {
-  // write code here
+wall.addEventListener('click', event => {
+  const positionY = event.clientY - wall.offsetTop;
+  const positionX = event.clientX - wall.offsetLeft;
+  const maxy = wall.clientHeight - spider.offsetWidth;
+  const maxh = wall.clientWidth - spider.offsetHeight;
+
+  spider.style.top = `${Math.min(positionY, maxy)}px`;
+  spider.style.left =`${Math.min(positionX, maxh)}px`;
 });
