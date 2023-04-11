a291a21802073053499c3b4bace9f4decc724e2f
Anna Grechana <grechanaanna@gmial.com>
2023-04-12 01:48:14 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const spiderH = spider.clientHeight;
+const wall = document.querySelector('.wall');
+
+const border = (wall.offsetHeight - wall.clientHeight) / 2;
+
 document.addEventListener('click', e => {
-  // write code here
+
+  if (e.clientY
+    > (wall.offsetTop + wall.offsetHeight - border - (spiderH / 2))) {
+    return;
+  };
+
+  if (e.clientY < wall.offsetTop + border + (spiderH / 2)) {
+
+    return;
+  };
+
+  if (e.clientX
+    > (wall.offsetLeft + wall.offsetWidth - border - (spiderH / 2))) {
+    return;
+  }
+
+  if (e.clientX < wall.offsetLeft + border + (spiderH / 2)) {
+    return;
+  }
+  spider.style.top = e.clientY - wall.offsetTop - spiderH / 2 - border + 'px';
+  spider.style.left = e.clientX - wall.offsetLeft - spiderH / 2 - border + 'px';
+
 });
