608717084a1033f041616c0d3d34a2d3ddd70ff9
Mykyta Zaloha <nikitajesting@gmail.com>
2023-04-11 22:00:37 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+  const spiderCenter = spider.clientWidth / 2;
+  const width = wall.clientWidth - spider.clientWidth;
+
+  spider.style.top = e.offsetY - spiderCenter + 'px';
+  spider.style.left = e.offsetX - spiderCenter + 'px';
+
+  if (e.offsetY - spiderCenter > width) {
+    spider.style.top = width + 'px';
+  }
+
+  if (e.offsetY - spiderCenter < 0) {
+    spider.style.top = '0px';
+  }
+
+  if (e.offsetX - spiderCenter > width) {
+    spider.style.left = width + 'px';
+  }
+
+  if (e.offsetX - spiderCenter < 0) {
+    spider.style.left = '0px';
+  };
 });
