fbcb00dfd5283487a2a0478883b605173c23e50a
Anna Grechana <grechanaanna@gmial.com>
2023-04-12 23:11:03 +0300

    2nd version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,28 +5,31 @@ const spiderH = spider.clientHeight;
 const wall = document.querySelector('.wall');
 
 const border = (wall.offsetHeight - wall.clientHeight) / 2;
+// console.log(spiderH)
 
 document.addEventListener('click', e => {
+  const clicin = e.target;
 
-  if (e.clientY
-    > (wall.offsetTop + wall.offsetHeight - border - (spiderH / 2))) {
-    return;
-  };
-
-  if (e.clientY < wall.offsetTop + border + (spiderH / 2)) {
-
-    return;
-  };
-
-  if (e.clientX
-    > (wall.offsetLeft + wall.offsetWidth - border - (spiderH / 2))) {
+  if (!wall.contains(clicin)) {
     return;
   }
 
-  if (e.clientX < wall.offsetLeft + border + (spiderH / 2)) {
-    return;
-  }
   spider.style.top = e.clientY - wall.offsetTop - spiderH / 2 - border + 'px';
   spider.style.left = e.clientX - wall.offsetLeft - spiderH / 2 - border + 'px';
 
+  if (e.clientY > wall.offsetTop + wall.offsetHeight - spiderH / 2) {
+    spider.style.top = wall.clientHeight - spiderH + 'px';
+  }
+
+  if (e.clientY < wall.offsetTop + spiderH / 2) {
+    spider.style.top = 0 + 'px';
+  }
+
+  if (e.clientX < wall.offsetLeft + spiderH / 2) {
+    spider.style.left = 0 + 'px';
+  }
+
+  if (e.clientX > wall.offsetLeft + wall.offsetWidth - spiderH / 2) {
+    spider.style.left = wall.clientWidth - spiderH + 'px';
+  }
 });
