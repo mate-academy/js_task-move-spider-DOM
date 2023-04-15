d06b60955339ec1781730b5882ffa18256049556
Dmytro Somik <dmytro.somik@mccann.com>
2023-04-15 16:29:31 +0100

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
   // write code here
+  const element = document.elementFromPoint(e.clientX, e.clientY);
+
+  if (!element.classList.contains('wall')) {
+    return;
+  }
+
+  spider.style.top = `${e.clientY - element.offsetTop}px`;
+  spider.style.left = `${e.clientX - element.offsetLeft}px`;
 });
