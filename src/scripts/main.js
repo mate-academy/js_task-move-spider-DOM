04248e874719a2781a81efb9cf7ebbd14732784e
popeye88 <aleksei.taran@gmail.com>
2023-04-17 19:06:22 +0300

    in prgrs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+let mousePos = {
+  x: undefined, y: undefined,
+};
+
 document.addEventListener('click', e => {
-  // write code here
+  const wall = e.target;
+
+  if (wall.classList.contains('wall')) {
+    const spider = document.querySelector('.spider');
+    const spiderSize = getComputedStyle(spider).width;
+
+    mousePos = {
+      x: e.layerX, y: e.layerY,
+    };
+
+    spider.style.transform = `translateY(
+      ${mousePos.y - parseInt(spiderSize) - 10}px)
+      translateX(${mousePos.x - parseInt(spiderSize) - 10}px)`;
+
+    console.log(`translateY(
+      ${mousePos.y - parseInt(spiderSize) / 2}px)
+      translateX(${mousePos.x - parseInt(spiderSize) / 2}px)`);
+  }
 });
