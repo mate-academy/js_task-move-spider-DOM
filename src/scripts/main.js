abf311ce650e1bf6523db0244ac615102223ca00
Kateryna Stemblivska <ekaterina_09@ukr.net>
2023-04-18 13:31:41 +0300

    Is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,17 +8,17 @@ document.addEventListener('click', e => {
   const maxLeft = wall.clientWidth - spider.clientWidth;
   const minLeft = wallCoords.left + wall.clientLeft + (spider.clientWidth / 2);
   const maxTop = wall.clientHeight - spider.clientHeight;
-  const minTop = wallCoords.top + wall.clientLeft + (spider.clientWidth / 2);
+  const minTop = wallCoords.top + wall.clientTop + (spider.clientWidth / 2);
 
   if (e.target !== wall) {
     return;
   }
 
   spider.style.left = (e.clientX < minLeft)
-    ? 0 + 'px'
+    ? '0px'
     : Math.min(e.clientX - minLeft, maxLeft) + 'px';
 
   spider.style.top = (e.clientY < minTop)
-    ? 0 + 'px'
+    ? '0px'
     : Math.min(e.clientY - minTop, maxTop) + 'px';
 });
