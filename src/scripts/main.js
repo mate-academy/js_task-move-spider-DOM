9074faf4f1768459de6ab0fb3f56ef4f6a9980a9
Mikołaj <ostermi@wp.pl>
2023-04-21 13:58:55 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+
+document.addEventListener('click', clickEvent => {
+  if (clickEvent.target.classList.contains('wall')) {
+    const styles = window.getComputedStyle(clickEvent.target);
+    const wallBorderWidth = parseInt(styles.getPropertyValue('border-width'));
+
+    const moveVertically = clickEvent.offsetY - spider.clientHeight
+    - wallBorderWidth;
+    const moveHorizontally = clickEvent.offsetX - spider.clientWidth
+    - wallBorderWidth;
+
+    spider.style.position = 'relative';
+    spider.style.left = `${moveHorizontally < 0 ? 0 : moveHorizontally}px`;
+
+    spider.style.top = `${moveVertically < 0 ? 0 : moveVertically}px`;
+  }
 });
