bba6279334adab96f84b1b33024c18e7098fc86c
Victor Buldenko <gookidoo@gmail.com>
2023-04-26 17:29:26 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+document.addEventListener('click', (e) => {
+  if (e.target.closest('.wall')) {
+    const mouseX = e.pageX - wall.offsetLeft - wall.clientLeft;
+    const mouseY = e.pageY - wall.offsetTop - wall.clientTop;
+
+    if (mouseX < spider.clientWidth / 2) {
+      spider.style.left = 0 + 'px';
+    } else if (mouseX > (wall.clientWidth - spider.clientWidth / 2)) {
+      spider.style.left = wall.clientWidth - spider.clientWidth + 'px';
+    } else {
+      spider.style.left = mouseX - spider.clientWidth / 2 + 'px';
+    }
+
+    if (mouseY < spider.clientHeight / 2) {
+      spider.style.top = 0 + 'px';
+    } else if (mouseY > (wall.clientHeight - spider.clientHeight / 2)) {
+      spider.style.top = wall.clientHeight - spider.clientHeight + 'px';
+    } else {
+      spider.style.top = mouseY - spider.clientHeight / 2 + 'px';
+    }
+  }
 });
