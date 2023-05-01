55be6f64d2ada9090d7910ee607eb34c91faa845
Anna Peretiatko <tobossanna@gmail.com>
2023-05-01 14:25:00 +0200

    Move spider solution v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderRect = spider.getBoundingClientRect();
+const spiderWidth = spiderRect.width;
+const spiderHeight = spiderRect.height;
+
+wall.addEventListener('click', e => {
+  if (e.target === wall) {
+    const wallRect = wall.getBoundingClientRect();
+    const clickX = e.clientX - wallRect.left;
+    const clickY = e.clientY - wallRect.top;
+
+    const spiderCenterX = clickX - spiderWidth + 15;
+    const spiderCenterY = clickY - spiderHeight + 10;
+
+    const maxX = wallRect.width - (spiderWidth * 1.4);
+    const maxY = wallRect.height - (spiderHeight * 1.4);
+    const newX = Math.min(Math.max(spiderCenterX, 0), maxX);
+    const newY = Math.min(Math.max(spiderCenterY, 0), maxY);
+
+    spider.style.left = newX + 'px';
+    spider.style.top = newY + 'px';
+  }
 });
