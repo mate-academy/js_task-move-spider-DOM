c038751321e0f60786b2917272471c81edb71235
Nick <tortickk8475@gmail.com>
2023-05-02 12:28:12 +0300

    fix: change hardcoded values

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,12 @@
 const body = document.body;
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
+const wallStyles = getComputedStyle(wall);
+const wallBorderWidth = parseFloat(
+  wallStyles.getPropertyValue('border-width'),
+);
+const toCenterSpiderWidth = spider.clientWidth / 2 + wallBorderWidth;
+const toCenterSpiderHeight = spider.clientHeight / 2 + wallBorderWidth;
 
 document.addEventListener('click', (e) => {
   const positionZero = 0 + 'px';
@@ -11,22 +17,22 @@ document.addEventListener('click', (e) => {
     return;
   }
 
-  spider.style.top = e.clientY - wall.offsetTop - 35 + 'px';
-  spider.style.left = e.clientX - wall.offsetLeft - 35 + 'px';
+  spider.style.top = e.clientY - wall.offsetTop - toCenterSpiderHeight + 'px';
+  spider.style.left = e.clientX - wall.offsetLeft - toCenterSpiderWidth + 'px';
 
-  if (e.clientY < wall.offsetTop + 35) {
+  if (e.clientY < wall.offsetTop + toCenterSpiderHeight) {
     spider.style.top = positionZero;
   }
 
-  if (e.clientY > wall.offsetTop - 15 + wall.clientHeight) {
+  if (e.clientY > wall.offsetTop - toCenterSpiderHeight + wall.clientHeight) {
     spider.style.top = 350 + 'px';
   }
 
-  if (e.clientX < wall.offsetLeft + 35) {
+  if (e.clientX < wall.offsetLeft + toCenterSpiderHeight) {
     spider.style.left = positionZero;
   }
 
-  if (e.clientX > wall.offsetLeft - 15 + wall.clientWidth) {
+  if (e.clientX > wall.offsetLeft - toCenterSpiderWidth + wall.clientWidth) {
     spider.style.left = 350 + 'px';
   }
 });
