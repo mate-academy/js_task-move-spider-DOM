c4e53003377765019fcaf230663669e19673156e
Antonio S <soi4.toxa@ukr.net>
2023-05-04 17:56:53 +0300

    Move-spider-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,52 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const target = e.target;
+
+  if (!target.classList.contains('wall')) {
+    return;
+  }
+
+  const spiderImg = document.querySelector('img.spider');
+  const styleWall = getComputedStyle(target);
+  const styleSpider = getComputedStyle(spiderImg);
+
+  const widthWall = parseFloat(styleWall.width);
+  const heightWall = parseFloat(styleWall.height);
+  const borderWall = parseFloat(styleWall.borderWidth);
+  const halfWidthSpider = parseFloat(styleSpider.width) / 2;
+  const halfHeightSpider = parseFloat(styleSpider.height) / 2;
+
+  const wallX = target.getBoundingClientRect().left + window.pageXOffset;
+  const wallY = target.getBoundingClientRect().top + window.pageYOffset;
+
+  const startXInsideWall = wallX + borderWall + halfWidthSpider;
+  const startYInsideWall = wallY + borderWall + halfHeightSpider;
+  const endXInsideWall = wallX + borderWall + widthWall - halfWidthSpider;
+  const endYInsideWall = wallY + borderWall + heightWall - halfHeightSpider;
+
+  let axisX = e.clientX;
+  let axisY = e.clientY;
+
+  if (axisX < startXInsideWall) {
+    axisX = startXInsideWall;
+  }
+
+  if (axisX > endXInsideWall) {
+    axisX = endXInsideWall;
+  }
+
+  if (axisY < startYInsideWall) {
+    axisY = startYInsideWall;
+  }
+
+  if (axisY > endYInsideWall) {
+    axisY = endYInsideWall;
+  }
+
+  axisX -= wallX + borderWall;
+  axisY -= wallY + borderWall;
+
+  spiderImg.style.top = `${axisY - halfWidthSpider}px`;
+  spiderImg.style.left = `${axisX - halfHeightSpider}px`;
 });
