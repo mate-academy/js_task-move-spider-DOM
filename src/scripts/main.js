bf21a04a12240a40cff631feffd9313b185444d4
Roman Moroz <65686226+morozit@users.noreply.github.com>
2023-05-09 00:42:01 +0300

    add task moroz v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  const maxWidth = wall.clientWidth - spider.clientWidth;
+  const maxHeight = wall.clientHeight - spider.clientHeight;
+
+  let coordSpiderX = e.offsetX - (spider.clientWidth / 2);
+  let coordSpiderY = e.offsetY - (spider.clientHeight / 2);
+
+  if (coordSpiderX > maxWidth) {
+    coordSpiderX = maxWidth;
+  }
+
+  if (coordSpiderY > maxHeight) {
+    coordSpiderY = maxHeight;
+  }
+
+  if (coordSpiderX < 0) {
+    coordSpiderX = 0;
+  }
+
+  if (coordSpiderY < 0) {
+    coordSpiderY = 0;
+  }
+
+  spider.style.left = `${coordSpiderX}px`;
+  spider.style.top = `${coordSpiderY}px`;
 });
