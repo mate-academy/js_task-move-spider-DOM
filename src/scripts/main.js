62b4edab672927de5c3e44efd908dafff7017608
OlegRak86 <rakolegit@gmail.com>
2023-05-10 16:46:05 +0300

    js_task-move-spider-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const pading = (wall.offsetWidth - wall.clientWidth) / 2;
+  const wallX = wall.getBoundingClientRect().x;
+  const wallY = wall.getBoundingClientRect().y;
+  const spider = document.querySelector('.spider');
+
+  if (!e.target.closest('.wall') || e.target.closest('.spider')) {
+    return;
+  }
+
+  if (e.clientX <= pading + wallX + spider.clientWidth / 2) {
+    spider.style.left = 0 + 'px';
+  } else if (e.clientX >= wallX + wall.clientWidth - spider.clientWidth / 2) {
+    spider.style.left = wall.clientWidth - spider.clientWidth + 'px';
+  } else {
+    spider.style.left = e.clientX - pading - wallX
+    - spider.clientWidth / 2 + 'px';
+  }
+
+  if (e.clientY <= pading + wallY
+  + spider.clientHeight / 2) {
+    spider.style.top = 0 + 'px';
+  } else if (e.clientY >= wallY + wall.clientHeight
+  - spider.clientHeight / 2) {
+    spider.style.top = wall.clientHeight - spider.clientHeight + 'px';
+  } else {
+    spider.style.top = e.clientY - pading - wallY
+    - spider.clientHeight / 2 + 'px';
+  }
 });
