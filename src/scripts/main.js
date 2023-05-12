4f914306a2e251a9e4cc3324bf2109b6885add3f
metin <metinbicaksiz@gmail.com>
2023-05-12 10:57:40 +0300

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,37 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
-});
+document.addEventListener('click', followMouse);
+
+function followMouse(e) {
+  if (!e.target.closest('.wall')) {
+    return false;
+  }
+
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  let y = e.offsetY;
+  let x = e.offsetX;
+
+  if (e.target.matches('.spider')) {
+    y += spider.offsetTop;
+    x += spider.offsetLeft;
+  }
+
+  let topSpider = y - spider.offsetHeight / 2;
+  let leftSpider = x - spider.offsetWidth / 2;
+
+  if (y <= spider.offsetHeight / 2) {
+    topSpider = 0;
+  } else if (y >= wall.clientHeight - spider.offsetHeight / 2) {
+    topSpider = wall.clientHeight - spider.offsetHeight;
+  }
+
+  if (x <= spider.offsetWidth / 2) {
+    leftSpider = 0;
+  } else if (x >= wall.clientWidth - spider.offsetWidth / 2) {
+    leftSpider = wall.clientWidth - spider.offsetWidth;
+  }
+
+  spider.style.left = leftSpider + 'px';
+  spider.style.top = topSpider + 'px';
+};
