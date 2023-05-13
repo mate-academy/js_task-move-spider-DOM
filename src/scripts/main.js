7eec14a601cfe5def319e2b5915249879259aa5d
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-05-13 15:28:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderSize = parseFloat(getComputedStyle(spider).width);
+const wallSize = parseFloat(getComputedStyle(wall).width);
+
+function getOffset(initialOffset) {
+  if (initialOffset < 0) {
+    return 0;
+  }
+
+  if (initialOffset > wallSize - spiderSize) {
+    return wallSize - spiderSize;
+  }
+
+  return initialOffset;
+};
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  spider.style.left = `${getOffset(e.offsetX - spiderSize / 2)}px`;
+  spider.style.top = `${getOffset(e.offsetY - spiderSize / 2)}px`;
 });
