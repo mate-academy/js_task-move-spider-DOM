d18cc9b184ff3581543f1001c040892fb7c65019
Ruslan Pashkov <nothingsfading@gmail.com>
2023-05-18 17:42:49 +0300

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
+  const spiderSize = spider.clientWidth;
+  const wallSize = wall.clientWidth;
+
+  const mouseX = e.offsetX - (spiderSize / 2);
+  const mouseY = e.offsetY - (spiderSize / 2);
+
+  const sizeDifference = wallSize - spiderSize;
+
+  spider.style.top = `${getNormalizedCoord(mouseY, sizeDifference)}px`;
+  spider.style.left = `${getNormalizedCoord(mouseX, sizeDifference)}px`;
 });
+
+function getNormalizedCoord(coord, max) {
+  if (coord < 0) {
+    return 0;
+  }
+
+  if (coord > max) {
+    return max;
+  }
+
+  return coord;
+}
