80276a903bc24aca8dbcf1747bb78f9a96421632
DefaultGitUser <usingforgit@gmail.com>
2023-05-18 16:06:03 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const spider = document.querySelector('.wall').firstElementChild;
+const wall = document.querySelector('.wall');
+const normalizeOffset = (offset) => {
+  if (offset < 0) {
+    return 0;
+  }
+
+  if (offset > wall.clientWidth - spider.clientWidth) {
+    return wall.clientWidth - spider.clientWidth;
+  }
+
+  return offset;
+};
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const x = normalizeOffset(e.offsetX - spider.clientWidth / 2);
+  const y = normalizeOffset(e.offsetY - spider.clientHeight / 2);
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
