6c3a13eeaaea16d145d9032aa30b59599c0af7c4
Sasha Green <aleksa146311@gmail.com>
2023-05-25 23:20:34 +0300

    add task move spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+
+  if (!wall || !wall.contains(e.target)) {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+
+  const offsetY = calcOffset(wall.clientHeight, spider.clientHeight, e.offsetY);
+  const offsetX = calcOffset(wall.clientWidth, spider.clientWidth, e.offsetX);
+
+  spider.style.top = `${offsetY}px`;
+  spider.style.left = `${offsetX}px`;
 });
+
+function calcOffset(wallSide, spiderSide, targetOffset) {
+  const offset = targetOffset - spiderSide / 2;
+
+  if (offset < 0) {
+    return 0;
+  }
+
+  const maxOffset = wallSide - spiderSide;
+
+  return offset > maxOffset ? maxOffset : offset;
+}
