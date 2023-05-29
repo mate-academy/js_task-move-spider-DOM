466f57f0a933fd7bb4731ce3b377347a8501a62b
Alessandra Nitti <alessandrannitti@gmail.com>
2023-05-29 19:07:11 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return 0;
+  }
+
+  const maxTop = wall.clientHeight - spider.clientHeight;
+  const maxLeft = wall.clientWidth - spider.clientWidth;
+
+  let spiderLeft = e.clientY - wall.offsetTop
+    - wall.clientTop - spider.clientHeight / 2;
+
+  let spiderTop = e.clientX - wall.offsetLeft
+    - wall.clientLeft - spider.clientWidth / 2;
+
+  // Restrict the spider position within the field boundaries
+  spiderLeft = Math.max(0, Math.min(spiderLeft, maxLeft));
+  spiderTop = Math.max(0, Math.min(spiderTop, maxTop));
+
+  // Set the spider's position
+  spider.style.top = `${spiderLeft}px`;
+  spider.style.left = `${spiderTop}px`;
 });
