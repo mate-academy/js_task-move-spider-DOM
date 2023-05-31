e60872dd45c88ffd432e826ea87135115e9b8ee1
AndriiYelieva <andrii.yelieva@gmail.com>
2023-05-31 11:31:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+document.addEventListener('click', (even) => {
+  const wall = even.target.closest('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (!wall) {
+    return;
+  }
+
+  const wallTop = wall.offsetTop;
+  const wallLeft = wall.offsetLeft;
+
+  const clickX = even.pageX - (wallLeft + spider.offsetWidth / 2);
+  const clickY = even.pageY - (wallTop + spider.offsetHeight / 2);
+
+  const widthBorder = 20;
+
+  const maxX = wall.offsetWidth - spider.offsetWidth - widthBorder;
+  const maxY = wall.offsetHeight - spider.offsetHeight - widthBorder;
+
+  const validX = Math.min(Math.max(clickX, 0), maxX);
+  const validY = Math.min(Math.max(clickY, 0), maxY);
+
+  spider.style.left = `${validX}px`;
+  spider.style.top = `${validY}px`;
 });
