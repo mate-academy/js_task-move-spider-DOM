69d4b6ca50fe53cffa6d2b05c9f9f7bd9ba5cb94
Oleksandr Kulibaba <alks.aesthetic@gmail.com>
2023-05-31 17:49:26 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const borderRight = wall.clientWidth - spider.clientWidth;
+  const borderBottom = wall.clientHeight - spider.clientHeight;
+
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  let mouseY = e.clientY - wall.offsetTop - wall.clientTop - spider.height / 2;
+  let mouseX = e.clientX - wall.offsetLeft - wall.clientLeft - spider.width / 2;
+
+  if (mouseX > borderRight) {
+    mouseX = borderRight;
+  }
+
+  if (mouseX < 0) {
+    mouseX = 0;
+  }
+
+  if (mouseY > borderBottom) {
+    mouseY = borderBottom;
+  }
+
+  if (mouseY < 0) {
+    mouseY = 0;
+  }
+
+  spider.style.left = `${mouseX}px`;
+  spider.style.top = `${mouseY}px`;
 });
