7ec07dd03174b8b3719dab19d75870b422df786b
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2023-06-07 20:37:02 +0200

    spider solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (wall.className !== 'wall') {
+    return;
+  }
+
+  const rectWall = wall.getBoundingClientRect();
+
+  const x = e.clientX - rectWall.left - spider.clientWidth / 2;
+  const y = e.clientY - rectWall.top - spider.clientHeight / 2;
+
+  const posX = Math.max(
+    0, Math.min(x, wall.clientWidth - spider.clientWidth)
+  );
+  const posY = Math.max(
+    0, Math.min(y, wall.clientHeight - spider.clientHeight)
+  );
+
+  spider.style.left = `${posX}px`;
+  spider.style.top = `${posY}px`;
 });
