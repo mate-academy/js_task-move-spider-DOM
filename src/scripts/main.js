724f75016227e0eea119470fa9a5a1a3b7e83320
Vasyl Kozak <sasdaiv@gmail.com>
2023-06-08 11:11:24 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,35 @@
 
 document.addEventListener('click', e => {
   // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  let spiderY = e.offsetY - spider.clientHeight / 2;
+  let spiderX = e.offsetX - spider.clientWidth / 2;
+
+  const maxY = wall.clientHeight - spider.clientHeight;
+  const maxX = wall.clientWidth - spider.clientWidth;
+
+  if (spiderY < 0) {
+    spiderY = 0;
+  }
+
+  if (maxY < spiderY) {
+    spiderY = maxY;
+  }
+
+  if (spiderX < 0) {
+    spiderX = 0;
+  }
+
+  if (maxX < spiderX) {
+    spiderX = maxX;
+  }
+
+  spider.style.top = `${spiderY}px`;
+  spider.style.left = `${spiderX}px`;
 });
