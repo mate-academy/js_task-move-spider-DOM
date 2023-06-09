d164331567e072c3ea80b443e7654f527b7c70e2
Hanna Balabukha <anna.aniskovets1992@ukr.net>
2023-06-09 15:13:02 +0300

    'done'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const place = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  let clientX = e.offsetX - spiderWidth / 2;
+  let clientY = e.offsetY - spiderHeight / 2;
+
+  const maxX = place.clientWidth - spiderWidth;
+  const maxY = place.clientHeight - spiderHeight;
+  const minX = 0;
+  const minY = 0;
+
+  if (clientX > maxX) {
+    clientX = maxX;
+  }
+
+  if (clientY > maxY) {
+    clientY = maxY;
+  }
+
+  if (clientX < minX) {
+    clientX = minX;
+  }
+
+  if (clientY < minY) {
+    clientY = minY;
+  }
+
+  spider.style.left = `${clientX}px`;
+  spider.style.top = `${clientY}px`;
 });
