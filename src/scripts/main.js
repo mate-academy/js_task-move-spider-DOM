3cc49f17042f4abd25cd9dd4b0bb2d74a1eb565b
domkab <dominykas.bobinas@gmail.com>
2023-06-10 21:07:09 +0300

    1.0.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,48 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const spiderParent = spider.parentElement;
+
+spiderParent.addEventListener('click', function(e) {
+  const parentRect = spiderParent.getBoundingClientRect();
+
+  const style = getComputedStyle(spiderParent);
+  const borderLeftWidth = parseFloat(style.borderLeftWidth);
+  const borderTopWidth = parseFloat(style.borderTopWidth);
+  const borderRightWidth = parseFloat(style.borderRightWidth);
+  const borderBottomWidth = parseFloat(style.borderBottomWidth);
+
+  const maxLeft
+    = parentRect.width
+      - spider.offsetWidth
+      - borderRightWidth
+      - borderLeftWidth;
+
+  const maxTop
+  = parentRect.height
+    - spider.offsetHeight
+    - borderBottomWidth
+    - borderTopWidth;
+
+  let left = e.clientX - parentRect.left - borderLeftWidth;
+  let spiderTop = e.clientY - parentRect.top - borderTopWidth;
+
+  if (left < 0) {
+    left = 0;
+  };
+
+  if (spiderTop < 0) {
+    spiderTop = 0;
+  };
+
+  if (left > maxLeft) {
+    left = maxLeft;
+  };
+
+  if (spiderTop > maxTop) {
+    spiderTop = maxTop;
+  };
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${spiderTop}px`;
 });
