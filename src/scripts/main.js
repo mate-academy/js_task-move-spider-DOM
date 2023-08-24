708d303191f9a0517d0933a930c791d093a5bb70
Dmytro Loboda <dmytro.loboda@gmail.com>
2023-08-25 03:55:17 +0800

    moving spider solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const topBorder = wall.clientHeight - spider.clientHeight;
+  const leftBorder = wall.clientWidth - spider.clientWidth;
+
+  let spiderX
+    = e.clientX - wall.offsetLeft - wall.clientLeft - spider.clientWidth / 2;
+
+  let spiderY
+    = e.clientY - wall.offsetTop - wall.clientTop - spider.clientHeight / 2;
+
+  spiderX = Math.max(0, Math.min(leftBorder, spiderX));
+  spiderY = Math.max(0, Math.min(topBorder, spiderY));
+
+  spider.style.left = `${spiderX}px`;
+  spider.style.top = `${spiderY}px`;
 });
