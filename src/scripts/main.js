c9c1d88e10892120e2606286be4214e7b550e831
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-09-17 21:58:56 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  let clientX = e.offsetX - spider.clientWidth / 2;
+  let clientY = e.offsetY - spider.clientHeight / 2;
+
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+  const minX = 0;
+  const minY = 0;
+
+  clientX = (clientX > maxX) ? maxX : (clientX < minX) ? minX : clientX;
+  clientY = (clientY > maxY) ? maxY : (clientY < minY) ? minY : clientY;
+
+  spider.style.left = `${clientX}px`;
+  spider.style.top = `${clientY}px`;
 });
