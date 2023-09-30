cc448b85bbd435faf4296fd848f927d104add482
meryam wazeen <maryamwazeen2014@gmail.com>
2023-09-30 13:39:38 +0300

    my spider not moving as requested

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderWidth = spider.clientWidth / 2;
+const spiderHeight = spider.clientHeight / 2;
+
+wall.addEventListener('click', (e) => {
+  const x = Math.max(0, e.clientX - wall
+    .getBoundingClientRect().left - spiderWidth);
+  const y = Math.max(0, e.clientY - wall
+    .getBoundingClientRect().top - spiderHeight);
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
+
+window.addEventListener('resize', () => location.reload());
