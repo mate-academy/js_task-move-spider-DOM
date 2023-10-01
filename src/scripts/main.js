e57236f2d0fe7b5fc33d89322388c22015f47211
meryam wazeen <maryamwazeen2014@gmail.com>
2023-10-01 12:25:41 +0300

    fixed my code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,17 +2,26 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-const spiderWidth = spider.clientWidth / 2;
-const spiderHeight = spider.clientHeight / 2;
 
 wall.addEventListener('click', (e) => {
-  const x = Math.max(0, e.clientX - wall
-    .getBoundingClientRect().left - spiderWidth);
-  const y = Math.max(0, e.clientY - wall
-    .getBoundingClientRect().top - spiderHeight);
+  // Calculate the position relative to the wall's top-left corner
+  const x = e.clientX - wall.getBoundingClientRect().left;
+  const y = e.clientY - wall.getBoundingClientRect().top;
 
-  spider.style.left = `${x}px`;
-  spider.style.top = `${y}px`;
+  // Calculate the spider's position to center it under the pointer
+  const spiderX = x - spider.clientWidth / 2;
+  const spiderY = y - spider.clientHeight / 2;
+
+  // Ensure the spider stays within the boundaries of the wall
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+
+  const clampedX = Math.max(0, Math.min(spiderX, maxX));
+  const clampedY = Math.max(0, Math.min(spiderY, maxY));
+
+  // Set the spider's position
+  spider.style.left = `${clampedX}px`;
+  spider.style.top = `${clampedY}px`;
 });
 
 window.addEventListener('resize', () => location.reload());
