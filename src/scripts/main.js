32b037b7b24b5d54f21a83de25e905784aa383ca
AndruhaMan <andreysut12@gmail.com>
2023-10-02 23:33:03 +0300

    The task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+
+const maxPositionX = wall.clientWidth - spiderWidth;
+const maxPositionY = wall.clientHeight - spiderHeight;
+
+document.addEventListener('click', event => {
+  if (event.target !== wall) {
+    return;
+  }
+
+  const newPositionX = event.offsetX - spiderWidth / 2;
+  const newPositionY = event.offsetY - spiderHeight / 2;
+
+  spider.style.top = Math.max(0, Math.min(maxPositionY, newPositionY)) + 'px';
+  spider.style.left = Math.max(0, Math.min(maxPositionX, newPositionX)) + 'px';
 });
