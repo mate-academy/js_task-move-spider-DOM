a26215140e564cf7a10a9c6d8bb84e0ea6324975
Volodymyr <psn22782@gmail.com>
2023-10-08 20:58:25 +0300

    solutin

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  spider.style.top = `${e.offsetY - spider.clientHeight / 2}px`;
+  spider.style.left = `${e.offsetX - spider.clientWidth / 2}px`;
 });
