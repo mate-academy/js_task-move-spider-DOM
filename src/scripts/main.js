035f140da93eaf7f55e63fb1a28f07c251722611
david.shamiiev <david.shamiiev@rad4m.com>
2023-10-11 16:25:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const spidy = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  let clientX = e.offsetX - spidy.clientWidth / 2;
+  let clientY = e.offsetY - spidy.clientHeight / 2;
+
+  const maxX = wall.clientWidth - spidy.clientWidth;
+  const maxY = wall.clientHeight - spidy.clientHeight;
+  const minX = 0;
+  const minY = 0;
+
+  clientX = Math.max(minX, Math.min(maxX, clientX));
+  clientY = Math.max(minY, Math.min(maxY, clientY));
+
+  spidy.style.left = `${clientX}px`;
+  spidy.style.top = `${clientY}px`;
 });
