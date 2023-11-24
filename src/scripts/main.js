ceb1e4cf67535e6abff1e321d6b37b1859efe838
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-24 23:02:20 +0100

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const body = document.body;
+
+wall.addEventListener('click', e => {
+  const spiderBodyX = (body.clientWidth - wall.clientWidth) / 2;
+  const spiderBodyY = (body.clientHeight - wall.clientHeight) / 2;
+
+  let positionX = e.clientX - spiderBodyX;
+  let positionY = e.clientY - spiderBodyY;
+
+  if (positionX < spider.offsetWidth / 2) {
+    positionX = 0;
+  } else if ((positionX + spider.offsetWidth / 2) > wall.clientWidth) {
+    positionX = wall.clientWidth - spider.offsetWidth;
+  } else {
+    positionX -= spider.offsetWidth / 2;
+  }
+
+  if (positionY < spider.offsetHeight / 2) {
+    positionY = 0;
+  } else if ((positionY + spider.offsetHeight / 2) > wall.clientHeight) {
+    positionY = wall.clientHeight - spider.offsetHeight;
+  } else {
+    positionY -= spider.offsetHeight / 2;
+  }
+
+  spider.style.left = positionX + 'px';
+  spider.style.top = positionY + 'px';
 });
