b925418accb6af124b646778a3e1c892f4c76de1
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-25 00:35:46 +0100

    add task soulution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target.classList.contains('wall')) {
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    if (x > 375) {
+      x = 375;
+    } else if (x < 25) {
+      x = 25;
+    }
+
+    if (y > 375) {
+      y = 375;
+    } else if (y < 25) {
+      y = 25;
+    }
+
+    spider.style.marginLeft = (x - 25) + 'px';
+    spider.style.marginTop = (y - 25) + 'px';
+  }
 });
