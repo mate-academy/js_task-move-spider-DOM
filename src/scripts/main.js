e999141d8a7c1c1ac162f527f36e8adbb973dafe
Akilover <119785153+liza-strykharchuk@users.noreply.github.com>
2023-11-30 15:30:32 +0200

    code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target === wall) {
+    const {
+      offsetX,
+      offsetY,
+    } = e;
+
+    const maxX = wall.clientWidth - spider.clientWidth;
+    const maxY = wall.clientHeight - spider.clientHeight;
+
+    const x = Math.min(Math.max(
+      offsetX - spider.clientWidth / 2, 0,
+    ), maxX);
+
+    const y = Math.min(Math.max(
+      offsetY - spider.clientHeight / 2, 0,
+    ), maxY);
+
+    spider.style.top = `${y}px`;
+    spider.style.left = `${x}px`;
+  }
 });
