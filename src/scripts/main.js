f5c7529b9a09da8297c86a74751d7352b9b45226
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-13 17:54:05 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
-'use strict';
+"use strict";
 
-document.addEventListener('click', e => {
-  // write code here
+document.addEventListener("click", (e) => {
+  const spider = document.querySelector(".spider");
+  const wall = document.querySelector(".wall");
+
+  const clickX = e.clientX;
+  const clickY = e.clientY;
+  const wallRect = wall.getBoundingClientRect();
+
+  const spiderX = Math.max(
+    wallRect.left,
+    Math.min(
+      wallRect.right - spider.clientWidth,
+      clickX - spider.clientWidth / 2
+    )
+  );
+  const spiderY = Math.max(
+    wallRect.top,
+    Math.min(
+      wallRect.bottom - spider.clientHeight,
+      clickY - spider.clientHeight / 2
+    )
+  );
+
+  spider.style.left = spiderX + "px";
+  spider.style.top = spiderY + "px";
 });
