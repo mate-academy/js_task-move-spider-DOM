5926abff9a48875b5a0367f2b6179c0fc8728fe2
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-20 17:34:55 +0200

    Revert "Moving spider"
    This reverts commit 08752623131029f5b1f612d880b3df4a4d683ece.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,5 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  const wall = document.querySelector('.wall');
-  const spider = document.querySelector('.spider');
-
-  if (e.target !== wall) {
-    return;
-  }
-
-  const halfSpiderW = spider.clientWidth / 2;
-  const halfSpiderH = spider.clientHeight / 2;
-
-  let coordsSpiderX = e.offsetX - halfSpiderW;
-  let coordsSpiderY = e.offsetY - halfSpiderH;
-
-  if (e.offsetX < halfSpiderW) {
-    coordsSpiderX = 0;
-  }
-
-  if (e.offsetY < halfSpiderH) {
-    coordsSpiderY = 0;
-  }
-
-  if (e.offsetX > wall.clientWidth - halfSpiderW) {
-    coordsSpiderX = wall.clientWidth - spider.clientWidth;
-  }
-
-  if (e.offsetY > wall.clientHeight - halfSpiderH) {
-    coordsSpiderY = wall.clientHeight - spider.clientHeight;
-  }
-
-  spider.style.cssText = `
-    top: ${coordsSpiderY}px;
-    left: ${coordsSpiderX}px;
-  `;
+  // write code here
 });
