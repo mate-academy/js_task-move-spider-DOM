a70316f2fadfa752bdfeffd9dd468e4e118a7ddc
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-20 23:47:14 +0200

    spider dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const spiderHalfWidth = spider.clientWidth / 2;
+  const spiderHalfHeight = spider.clientHeight / 2;
+
+  let spiderLeft = e.offsetX - spiderHalfWidth;
+  let spiderTop = e.offsetY - spiderHalfHeight;
+
+  if (e.offsetX < spiderHalfWidth) {
+    spiderLeft = 0;
+  }
+
+  if (e.offsetY < spiderHalfHeight) {
+    spiderTop = 0;
+  }
+
+  if (e.offsetX > wall.clientWidth - spiderHalfWidth) {
+    spiderLeft = wall.clientWidth - spider.clientWidth;
+  }
+
+  if (e.offsetY > wall.clientHeight - spiderHalfHeight) {
+    spiderTop = wall.clientHeight - spider.clientHeight;
+  }
+
+  spider.style.left = spiderLeft + 'px';
+  spider.style.top = spiderTop + 'px';
 });
