a2a4f8fedd5f581e6ed94e6954c0cf82fdc51ba2
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-09 12:59:22 +0200

    add task soluition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,39 @@
+/* eslint-disable no-console */
 'use strict';
+
+const correctionСoordinate = (coordinate, maxValue) => (
+  coordinate < 0
+    ? 0
+    : (coordinate > maxValue
+      ? maxValue : coordinate)
+);
 
 document.addEventListener('click', e => {
   // write code here
+  const wall = e.target.closest('.wall');
+
+  if (wall) {
+    const spider = document.querySelector('.spider');
+
+    const heightWall = parseFloat(
+      window.getComputedStyle(wall).getPropertyValue('height'),
+    );
+    const widthWall = parseFloat(
+      window.getComputedStyle(wall).getPropertyValue('width'),
+    );
+
+    const heightWindow = window.innerHeight;
+    const widtWindow = window.innerWidth;
+
+    const xCenter
+      = (e.clientY - (heightWindow - heightWall) / 2 - spider.height / 2);
+    const yCenter
+      = (e.clientX - (widtWindow - widthWall) / 2 - spider.width / 2);
+
+    spider.style.top
+      = `${correctionСoordinate(xCenter, heightWall - spider.height)}px`;
+
+    spider.style.left
+      = `${correctionСoordinate(yCenter, widthWall - spider.width)}px`;
+  }
 });
