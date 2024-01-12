4287b93060761c7acafc554462eb1f928b2de482
Sergey Arnautov <sirius9312@ukr.net>
2024-01-12 16:49:54 +0200

    add task solution-spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,51 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', e => {
+  const coordXOnWall = e.clientX - wall.offsetLeft - wall.clientLeft;
+  const coordYOnWall = e.clientY - wall.offsetTop - wall.clientTop;
+
+  // spider moves on the top side of the wall
+  if (coordYOnWall < spider.offsetHeight / 2) {
+    spider.style.top = '0';
+
+    // on top right corner
+    if (coordXOnWall > wall.clientWidth - spider.offsetWidth / 2) {
+      spider.style.left = `${wall.clientWidth - spider.offsetWidth}px`;
+    } else if (coordXOnWall < spider.offsetWidth / 2) {
+      // on top left corner
+      spider.style.left = '0';
+    } else { // moving
+      spider.style.left = `${coordXOnWall - spider.offsetWidth / 2}px`;
+    }
+  } else if (coordXOnWall < spider.offsetWidth / 2) {
+    // spider moves on the left side of the wall
+    spider.style.left = '0';
+
+    if (coordYOnWall > wall.clientHeight - spider.offsetHeight / 2) {
+      // on bottom left corner
+      spider.style.top = `${wall.clientHeight - spider.offsetHeight}px`;
+    } else { // moving
+      spider.style.top = `${coordYOnWall - spider.offsetHeight / 2}px`;
+    }
+  } else if (coordXOnWall > wall.clientWidth - spider.offsetWidth / 2) {
+    // spider moves on the right side of the wall
+    spider.style.left = `${wall.clientWidth - spider.offsetWidth}px`;
+
+    // on bottom right corner
+    if (coordYOnWall > wall.clientHeight - spider.offsetHeight / 2) {
+      spider.style.top = `${wall.clientHeight - spider.offsetHeight}px`;
+    } else { // moving
+      spider.style.top = `${coordYOnWall - spider.offsetHeight / 2}px`;
+    }
+  } else if (coordYOnWall > wall.clientHeight - spider.offsetHeight / 2) {
+    // spider moves on the bottom side of the wall
+    spider.style.top = `${wall.clientHeight - spider.offsetHeight}px`;
+    spider.style.left = `${coordXOnWall - spider.offsetWidth / 2}px`;
+  } else { // spider moves on the wall
+    spider.style.left = `${coordXOnWall - spider.offsetWidth / 2}px`;
+    spider.style.top = `${coordYOnWall - spider.offsetHeight / 2}px`;
+  }
 });
