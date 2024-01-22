cf2a72009a3ecc705f635445877e3ff4dc6c6dcf
LabPetro <lamppetro30@outlook.com>
2024-01-22 22:58:59 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
+/* eslint-disable max-len */
 'use strict';
 
 document.addEventListener('click', e => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const x = e.clientX - spider.clientWidth / 2 - wall.getBoundingClientRect().left;
+  const y = e.clientY - spider.clientHeight / 2 - wall.getBoundingClientRect().top;
+
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+
+  spider.style.left = Math.max(0, Math.min(x, maxX)) + 'px';
+  spider.style.top = Math.max(0, Math.min(y, maxY)) + 'px';
 });
