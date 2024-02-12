79fdc7389e5e8034f4d3d9e618ec77b6c57d8d9a
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-02-12 12:12:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const borderSize = parseInt(getComputedStyle(wall)
+  .getPropertyValue('border-width'));
+const wallSize = parseInt(getComputedStyle(wall)
+  .getPropertyValue('height'));
+const spiderHalfOfSize = spider.height / 2;
+const wallRect = wall.getBoundingClientRect();
+
+function getPosition(cursorPosition, distanceToEdge) {
+  return (cursorPosition < borderSize + spiderHalfOfSize + distanceToEdge
+    ? spiderHalfOfSize
+    : cursorPosition > wallSize - spiderHalfOfSize + distanceToEdge
+      ? wallSize - spiderHalfOfSize
+      : cursorPosition - distanceToEdge - borderSize) - spiderHalfOfSize;
+}
+
+wall.addEventListener('click', e => {
+  spider.style.top = `${getPosition(e.clientY, wallRect.y)}px`;
+  spider.style.left = `${getPosition(e.clientX, wallRect.x)}px`;
 });
