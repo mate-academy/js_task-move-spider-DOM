14dc9c3a1760a0b8fff065bce52d8928132d8cee
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-02-12 20:00:40 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target === wall) {
+    const maxLeft = wall.clientHeight - spider.clientHeight;
+    const maxTop = wall.clientWidth - spider.clientWidth;
+
+    const minLeftTop = 0;
+
+    const pozLeft = e.offsetX - spider.clientHeight / 2;
+    const pozTop = e.offsetY - spider.clientWidth / 2;
+
+    if (pozLeft > maxLeft) {
+      spider.style.left = maxLeft + 'px';
+    } else if (pozLeft < minLeftTop) {
+      spider.style.left = minLeftTop + 'px';
+    } else {
+      spider.style.left = pozLeft + 'px';
+    }
+
+    if (pozTop > maxTop) {
+      spider.style.top = maxTop + 'px';
+    } else if (pozTop < minLeftTop) {
+      spider.style.top = minLeftTop + 'px';
+    } else {
+      spider.style.top = pozTop + 'px';
+    }
+  }
 });
