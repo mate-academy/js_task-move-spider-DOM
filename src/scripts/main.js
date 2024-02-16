2b42b216bae87b2a65ed74f687c04254e916019b
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-16 12:33:05 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-document.addEventListener('click', e => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+document.addEventListener('click', (e) => {
+  if (e.target !== wall) {
+    return;
+  }
+
+  const maxLeft = wall.clientHeight - spider.clientHeight;
+  const maxTop = wall.clientWidth - spider.clientWidth;
+  const minLeftTop = 0;
+
+  const newLeft = Math.min(Math.max(
+    e.offsetX - spider.clientHeight / 2, minLeftTop), maxLeft);
+  const newTop = Math.min(Math.max(
+    e.offsetY - spider.clientWidth / 2, minLeftTop), maxTop);
+
+  spider.style.left = `${newLeft}px`;
+  spider.style.top = `${newTop}px`;
 });
