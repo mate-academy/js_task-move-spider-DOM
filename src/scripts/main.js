d9466ee54bf1ef5061a999faf0f7d110052564bb
Horizon-git <apbreloaded947@gmail.com>
2024-02-22 18:24:14 +0200

    completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', e => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const maxLeft = wall.clientWidth - spider.clientWidth;
+  const maxTop = wall.clientHeight - spider.clientHeight;
+
+  const topSpiderPosition = Math.min(Math.max(
+    e.offsetY - spider.clientHeight / 2, 0,
+  ), maxTop);
+  const leftSpiderPosition = Math.min(Math.max(
+    e.offsetX - spider.clientWidth / 2, 0,
+  ), maxLeft);
+
+
+  spider.style.top = `${topSpiderPosition}px`;
+  spider.style.left = `${leftSpiderPosition}px`;
 });
