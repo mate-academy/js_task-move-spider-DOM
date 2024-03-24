b3da1b35a445448bad049a6371da75884150e5cc
Hishchak Tetiana <tanaiosered@ukr.net>
2024-03-24 21:20:37 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+  const x = e.clientX;
+  const y = e.clientY;
+  const startWallX = wall.offsetLeft;
+  const startWallY = wall.offsetTop;
+  let left = x - startWallX - spider.clientWidth / 2;
+  let topY = y - startWallY - spider.clientHeight / 2;
+
+  if (left > maxX) {
+    left = maxX;
+  }
+
+  if (left < 0) {
+    left = 0;
+  }
+
+  if (topY > maxY) {
+    topY = maxY;
+  }
+
+  if (topY < 0) {
+    topY = 0;
+  }
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${topY}px`;
 });
