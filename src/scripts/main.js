751d7e766893a2d18b907003a4d118be53cddf2d
Semen Sokolovskyi <anori.fjord@gmail.com>
2024-03-27 15:56:43 +0200

    controlled spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target.className !== 'wall') {
+    return;
+  }
+
+  const possibleWidth = wall.clientWidth - spider.clientWidth;
+  const possibleHeight = wall.clientHeight - spider.clientHeight;
+
+  const positionX = e.offsetX - spider.clientWidth / 2;
+  const positionY = e.offsetY - spider.clientHeight / 2;
+
+  spider.style.top = `${Math.max(0, Math.min(possibleHeight, positionY))}px`;
+  spider.style.left = `${Math.max(0, Math.min(possibleWidth, positionX))}px`;
 });
