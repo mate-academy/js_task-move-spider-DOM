a6c9fb5209124d3fba8f2bce18dc94103ec7a429
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-11 20:36:27 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const offsetX = Math.min(
+    Math.max(e.offsetX, spider.clientWidth / 2),
+    wall.clientWidth,
+  );
+  const offsetY = Math.min(
+    Math.max(e.offsetY, spider.clientHeight / 2),
+    wall.clientHeight,
+  );
+
+  spider.style.top = `${offsetY - spider.clientHeight / 2}px`;
+  spider.style.left = `${offsetX - spider.clientWidth / 2}px`;
 });
