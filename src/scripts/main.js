3ad60264415a5739951b854c76491fad36da6b74
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2024-04-12 16:55:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const spiderContainer = document.querySelector('div');
+const spider = document.querySelector('img');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target.tagName === 'DIV') {
+    const cursorX = e.clientX;
+    const cursorY = e.clientY;
+
+    const containerRect = spiderContainer.getBoundingClientRect();
+
+    const spiderX = cursorX - containerRect.left - spider.clientWidth / 2;
+    const spiderY = cursorY - containerRect.top - spider.clientHeight / 2;
+
+    const maxX = spiderContainer.clientWidth - spider.clientWidth;
+    const maxY = spiderContainer.clientHeight - spider.clientHeight;
+
+    spider.style.left = `${Math.min(Math.max(spiderX, 0), maxX)}px`;
+    spider.style.top = `${Math.min(Math.max(spiderY, 0), maxY)}px`;
+  }
 });
