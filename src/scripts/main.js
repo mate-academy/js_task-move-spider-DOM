36961f3272dcf26a7d4a47708e11894eae5b0f0d
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-04-24 10:37:12 +0300

    added task move spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+document.addEventListener('click', function (event) {
+  const clickX = event.clientX - wall.offsetLeft;
+  const clickY = event.clientY - wall.offsetTop;
+
+  const spiderX = clickX - spider.offsetWidth / 2;
+  const spiderY = clickY - spider.offsetHeight / 2;
+
+  if (
+    spiderX >= 0 &&
+    spiderY >= 0 &&
+    spiderX + spider.offsetWidth <= wall.offsetWidth &&
+    spiderY + spider.offsetHeight <= wall.offsetHeight
+  ) {
+    spider.style.left = spiderX + 'px';
+    spider.style.top = spiderY + 'px';
+  }
 });
