0bc65fafc3a5cf1c9377b20367920cf25ed5539f
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-05-28 12:29:58 +0100

    Added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,41 @@
-'use strict';
+/* eslint-disable no-console */
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const clickX = e.clientX;
+  const clickY = e.clientY;
+
+  if (!wall.contains(e.target)) {
+    return;
+  }
+
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  const coords = {
+    top: clickY - wallRect.top - spiderRect.height / 2 - wall.clientTop,
+    left: clickX - wallRect.left - spiderRect.width / 2 - wall.clientLeft,
+  };
+
+  coords.top = Math.max(
+    0,
+    Math.min(
+      wallRect.height - spiderRect.height - 2 * wall.clientTop,
+      coords.top,
+    ),
+  );
+
+  coords.left = Math.max(
+    0,
+    Math.min(
+      wallRect.width - spiderRect.width - 2 * wall.clientLeft,
+      coords.left,
+    ),
+  );
+
+  spider.style = `
+    top: ${coords.top}px;
+    left: ${coords.left}px
+  `;
 });
