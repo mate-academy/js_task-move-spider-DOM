587030b891221994337fdb8e4f1c82b4c036262f
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-06-04 17:01:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,44 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = e.target.closest('.wall');
+
+  if (wall) {
+    const { width: spiderWidth, height: spiderHeight } =
+      spider.getBoundingClientRect();
+
+    const { width: wallWidth, height: wallHeight } =
+      wall.getBoundingClientRect();
+    const { borderWidth } = getComputedStyle(wall);
+
+    const floatBorderWidth = parseFloat(borderWidth);
+
+    let getTopPosition = e.offsetY - spiderHeight / 2;
+    let getSidePosition = e.offsetX - spiderWidth / 2;
+
+    const maxTopPosition = wallHeight - spiderHeight - floatBorderWidth * 2;
+
+    const maxSidePosition = wallWidth - spiderWidth - floatBorderWidth * 2;
+
+    if (getSidePosition < 0) {
+      getSidePosition = 0;
+    }
+
+    if (getTopPosition > maxTopPosition) {
+      getTopPosition = maxTopPosition;
+    }
+
+    if (getSidePosition > maxSidePosition) {
+      getSidePosition = maxSidePosition;
+    }
+
+    if (getTopPosition < 0) {
+      getTopPosition = 0;
+    }
+
+    spider.style.top = getTopPosition + 'px';
+    spider.style.left = getSidePosition + 'px';
+  }
 });
