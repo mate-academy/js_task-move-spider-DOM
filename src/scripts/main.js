819930c194cff51d81ab4871a6f77c22b02defcf
Viktoriya Riabets <vikamalik30@gmail.com>
2024-06-11 20:43:26 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  let newLeft = e.clientX - wallRect.left - spiderRect.width / 2;
+  let newTop = e.clientY - wallRect.top - spiderRect.height / 2;
+
+  // Перевірка на межі стінки
+  if (newLeft < 0) {
+    newLeft = 0;
+  } else if (newLeft + spiderRect.width > wallRect.width) {
+    newLeft = wallRect.width - spiderRect.width;
+  }
+
+  if (newTop < 0) {
+    newTop = 0;
+  } else if (newTop + spiderRect.height > wallRect.height) {
+    newTop = wallRect.height - spiderRect.height;
+  }
+
+  spider.style.left = `${newLeft}px`;
+  spider.style.top = `${newTop}px`;
 });
