46fbdc5cdf40bf97106c92bbce0ab0e363918970
getchards <kazansasha02@gmail.com>
2024-06-18 17:45:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
-'use strict';
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
 
-document.addEventListener('click', (e) => {
-  // write code here
+spider.style.position = 'absolute';
+
+wall.addEventListener('click', (e) => {
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  const wallRect = wall.getBoundingClientRect();
+
+  let marginLeft = e.clientX - wallRect.left;
+  let marginTop = e.clientY - wallRect.top;
+
+  marginLeft -= spiderWidth / 2;
+  marginTop -= spiderHeight / 2;
+
+  if (marginLeft < 0) {
+    marginLeft = 0;
+  } else if (marginLeft + spiderWidth > wallRect.width) {
+    marginLeft = wallRect.width - spiderWidth;
+  }
+
+  if (marginTop < 0) {
+    marginTop = 0;
+  } else if (marginTop + spiderHeight > wallRect.height) {
+    marginTop = wallRect.height - spiderHeight;
+  }
+
+  spider.style.left = marginLeft + 'px';
+  spider.style.top = marginTop + 'px';
 });
