b1f186f3805890b0341da1ef71f67c9bf6f76d60
SerjDSA <serjdsa7@gmail.com>
2024-06-18 06:43:44 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
-'use strict';
+function moveSpider(e) {
+  e.preventDefault();
 
-document.addEventListener('click', (e) => {
-  // write code here
-});
+  const container = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+  const containerParams = container.getBoundingClientRect();
+  const spiderParams = spider.getBoundingClientRect();
+
+  if (e.target.closest('.wall')) {
+    let coordY =
+      e.clientY -
+      containerParams.top -
+      container.clientTop -
+      spiderParams.height / 2;
+    let coordX =
+      e.clientX -
+      containerParams.left -
+      container.clientLeft -
+      spiderParams.width / 2;
+
+    coordY = Math.max(
+      0,
+      Math.min(coordY, container.clientHeight - spider.clientHeight),
+    );
+
+    coordX = Math.max(
+      0,
+      Math.min(coordX, container.clientWidth - spider.clientWidth),
+    );
+    spider.style.top = `${coordY}px`;
+    spider.style.left = `${coordX}px`;
+  }
+}
+document.addEventListener('click', moveSpider);
