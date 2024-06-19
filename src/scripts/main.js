74e736dc2027c6980383e7d936d57e968ae5530c
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-19 19:23:58 +0300

    [Refactor] moveSpider func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,55 +1,37 @@
 'use strict';
 
-const BORDER_WIDTH = 10;
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-function calculatePostion(current, min, max) {
-  if (current < min) {
-    return min;
-  }
+function moveSpider({ target, offsetX, offsetY }) {
+  if (target.classList.contains('wall')) {
+    const { width: spiderWidth, height: spiderHeight } =
+      spider.getBoundingClientRect();
 
-  if (current > max) {
-    return max;
-  }
+    const wallWidth = wall.clientWidth;
+    const wallHeight = wall.clientHeight;
 
-  return current;
+    const minPosX = 0;
+    const maxPosX = wallWidth - spiderWidth;
+
+    const minPosY = 0;
+    const maxPosY = wallHeight - spiderHeight;
+
+    const currentPosX = offsetX - spiderWidth / 2;
+    const currentPosY = offsetY - spiderHeight / 2;
+
+    const posX =
+      currentPosX <= wallWidth / 2
+        ? Math.max(minPosX, currentPosX)
+        : Math.min(maxPosX, currentPosX);
+
+    const posY =
+      currentPosY <= wallHeight / 2
+        ? Math.max(minPosY, currentPosY)
+        : Math.min(maxPosY, currentPosY);
+
+    spider.style.cssText = `left: ${posX}px; top: ${posY}px`;
+  }
 }
 
-function checkIsInRange(clientX, clientY, surfaceRect) {
-  const isInXRange =
-    clientX > surfaceRect.x && clientX < surfaceRect.x + surfaceRect.width;
-
-  const isInYRange =
-    clientY > surfaceRect.y && clientY < surfaceRect.y + surfaceRect.height;
-
-  return isInXRange && isInYRange;
-}
-
-document.addEventListener('click', (e) => {
-  const { clientX, clientY } = e;
-
-  const wallRect = wall.getBoundingClientRect();
-
-  const isInRange = checkIsInRange(clientX, clientY, wallRect);
-
-  if (isInRange) {
-    // We are already have this props in the event,
-    // but the task says use clientX/clientY
-    const offsetX = Math.round(clientX - BORDER_WIDTH - wallRect.x);
-    const offsetY = Math.round(clientY - BORDER_WIDTH - wallRect.y);
-
-    const xCoord = offsetX - spider.width / 2;
-    const yCoord = offsetY - spider.height / 2;
-
-    const minLeft = 0;
-    const minTop = 0;
-    const maxLeft = wallRect.width - spider.width - BORDER_WIDTH * 2;
-    const maxTop = wallRect.height - spider.height - BORDER_WIDTH * 2;
-
-    const leftPos = calculatePostion(xCoord, minLeft, maxLeft);
-    const topPos = calculatePostion(yCoord, minTop, maxTop);
-
-    spider.style.cssText = `left: ${leftPos}px; top: ${topPos}px`;
-  }
-});
+document.addEventListener('click', moveSpider);
