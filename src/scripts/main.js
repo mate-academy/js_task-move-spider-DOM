91453213334a607bc8c1cda996c6a7f4d3fbdfaf
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-20 14:55:26 +0200

    center spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const wallSizes = wall.getBoundingClientRect();
+
+  spider.style.top = `${e.clientY - wallSizes.top - spider.clientHeight / 1.5}px`;
+  spider.style.left = `${e.clientX - wallSizes.left - spider.clientWidth / 1.5}px`;
 });
