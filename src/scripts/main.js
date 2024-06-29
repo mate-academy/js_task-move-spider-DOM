e74f895639b1604e4032403ddb4ef8003497d94b
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-06-29 12:05:00 +0300

    add test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.getElementsByTagName('div')[0];
+const spider = document.getElementsByTagName('img')[0];
+
+wall.addEventListener('click', (e) => {
+  const x = e.clientX - wall.offsetLeft;
+  const y = e.clientY - wall.offsetTop;
+
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+
+  const constrainedX = Math.max(0, Math.min(x, maxX));
+  const constrainedY = Math.max(0, Math.min(y, maxY));
+
+  spider.style.left = `${constrainedX}px`;
+  spider.style.top = `${constrainedY}px`;
 });
