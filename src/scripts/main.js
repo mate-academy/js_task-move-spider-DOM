729418e9fa339d1b9b3552f6dca2bbe8edc5292a
luter <newgglutt@gmail.com>
2024-07-04 17:15:14 +0300

    newww

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const walls = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  document.addEventListener('click', (e) => {
+    const wallRect = walls.getBoundingClientRect();
+    const spiderRect = spider.getBoundingClientRect();
+
+    let newLeft = e.clientX - wallRect.left - spiderRect.width / 2;
+    let newTop = e.clientY - wallRect.top - spiderRect.height / 2;
+
+    if (newLeft < 0) {
+      newLeft = 0;
+    } else if (newLeft > wallRect.width - spiderRect.width) {
+      newLeft = wallRect.width - spiderRect.width;
+    }
+
+    if (newTop < 0) {
+      newTop = 0;
+    } else if (newTop > wallRect.height - spiderRect.height) {
+      newTop = wallRect.height - spiderRect.height;
+    }
+
+    spider.style.left = `${newLeft}px`;
+    spider.style.top = `${newTop}px`;
+  });
 });
