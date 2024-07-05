07e664dbb56bc82763f59d518fba92d19c58f88e
Anastasiia <vasilisandreeva00@gmail.com>
2024-07-05 19:01:45 +0300

    create task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (!wall.contains(e.target)) {
+    return;
+  }
+
+  let coordX = e.offsetX;
+  let coordY = e.offsetY;
+
+  const spiderWidth = spider.width;
+  const spiderHeight = spider.height;
+
+  coordX = Math.max(
+    spiderWidth / 2,
+    Math.min(coordX, wall.clientWidth - spiderWidth / 2),
+  );
+
+  coordY = Math.max(
+    spiderHeight / 2,
+    Math.min(coordY, wall.clientHeight - spiderHeight / 2),
+  );
+
+  spider.style.left = coordX - spiderWidth / 2 + 'px';
+  spider.style.top = coordY - spiderHeight / 2 + 'px';
 });
