79afe121b77c07f93fb744664171c28f2d841d82
under-a <mymail15@i.ua>
2024-07-05 16:32:33 +0300

    Renamed variables, also borders are accounted for in the calculations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,26 +9,26 @@ document.addEventListener('click', (e) => {
     const spiderRect = spider.getBoundingClientRect();
     const borderWidth = parseInt(window.getComputedStyle(wall).borderWidth);
 
-    let spiderTop = e.clientY - wallRect.y - spiderRect.height / 2;
-    let spiderLeft = e.clientX - wallRect.x - spiderRect.width / 2;
+    let spiderX = e.clientY - wallRect.y - spiderRect.height / 2 - borderWidth;
+    let spiderY = e.clientX - wallRect.x - spiderRect.width / 2 - borderWidth;
 
-    if (spiderTop < 0) {
-      spiderTop = 0;
+    if (spiderX < 0) {
+      spiderX = 0;
     }
 
-    if (spiderTop > wallRect.height - spiderRect.height) {
-      spiderTop = wallRect.height - spiderRect.height - borderWidth * 2;
+    if (spiderX > wallRect.height - spiderRect.height - borderWidth * 2) {
+      spiderX = wallRect.height - spiderRect.height - borderWidth * 2;
     }
 
-    if (spiderLeft < 0) {
-      spiderLeft = 0;
+    if (spiderY < 0) {
+      spiderY = 0;
     }
 
-    if (spiderLeft > wallRect.width - spiderRect.width) {
-      spiderLeft = wallRect.width - spiderRect.width - borderWidth * 2;
+    if (spiderY > wallRect.width - spiderRect.width - borderWidth * 2) {
+      spiderY = wallRect.width - spiderRect.width - borderWidth * 2;
     }
 
-    spider.style.top = `${spiderTop}px`;
-    spider.style.left = `${spiderLeft}px`;
+    spider.style.top = `${spiderX}px`;
+    spider.style.left = `${spiderY}px`;
   }
 });
