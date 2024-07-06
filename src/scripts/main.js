c47d0e55c2ccf21f492fc7142b0a44f2279229a6
HunchakAndrii <andreygunchak1@gmail.com>
2024-07-06 15:49:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,15 @@
 
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
-const HAVF_SIZE_SPIDER = 25;
 const BORDER = 10;
 
 wall.addEventListener('click', (e) => {
   const wallRect = wall.getBoundingClientRect();
-  let x = e.clientX - wallRect.left - HAVF_SIZE_SPIDER;
-  let y = e.clientY - wallRect.top - HAVF_SIZE_SPIDER;
+  const spiderRect = spider.getBoundingClientRect();
+  const centerOfSpider = Math.round((spiderRect.width * Math.sqrt(2)) / 2);
+
+  let x = e.clientX - wallRect.left - centerOfSpider;
+  let y = e.clientY - wallRect.top - centerOfSpider;
 
   if (x < 0) {
     x = 0;
@@ -18,12 +20,12 @@ wall.addEventListener('click', (e) => {
     y = 0;
   }
 
-  if (x > wallRect.width - HAVF_SIZE_SPIDER * 2 - BORDER) {
-    x = wallRect.width - HAVF_SIZE_SPIDER * 2 - BORDER * 2;
+  if (x > wallRect.width - spiderRect.width) {
+    x = wallRect.width - spiderRect.width - BORDER * 2;
   }
 
-  if (y > wallRect.height - HAVF_SIZE_SPIDER * 2 - BORDER) {
-    y = wallRect.height - HAVF_SIZE_SPIDER * 2 - BORDER * 2;
+  if (y > wallRect.height - spiderRect.height) {
+    y = wallRect.height - spiderRect.height - BORDER * 2;
   }
 
   if (e.target === wall) {
