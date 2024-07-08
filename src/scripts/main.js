d1ed575062d30f292da85918160468f436be84c4
Galers <galosaandrew@gmail.com>
2024-07-08 16:31:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const wallCoords = wall.getBoundingClientRect();
+  const spiderHalfHeight = spider.clientHeight / 2;
+  const spiderHalfWidth = spider.clientWidth / 2;
+
+  let top = e.clientY - wallCoords.top - wall.clientTop - spiderHalfHeight;
+  let left = e.clientX - wallCoords.left - wall.clientLeft - spiderHalfWidth;
+
+  top = Math.max(0, Math.min(top, wall.clientHeight - spider.clientHeight));
+  left = Math.max(0, Math.min(left, wall.clientWidth - spider.clientWidth));
+
+  spider.style.top = `${top}px`;
+  spider.style.left = `${left}px`;
 });
