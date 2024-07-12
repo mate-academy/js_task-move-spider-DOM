9749cf8f0179f37264ccdefd8bc537a81b3d05f5
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-07-12 10:50:45 +0100

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
   // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const maxY = wall.clientHeight - spider.clientHeight;
+  const maxX = wall.clientWidth - spider.clientWidth;
+
+  const yPos = e.offsetY - spider.clientHeight / 2;
+  const xPos = e.offsetX - spider.clientWidth / 2;
+
+  const y = Math.max(0, Math.min(maxY, yPos));
+  const x = Math.max(0, Math.min(maxX, xPos));
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });
