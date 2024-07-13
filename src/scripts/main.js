4cc4738c4cb46fa196f6b8769d926101a2f66847
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-07-13 19:35:17 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const coordsWall = wall.getBoundingClientRect();
+  const sqearWall = e.target.closest('.wall');
+
+  if (sqearWall) {
+    const topClick = e.clientY - coordsWall.y - spider.offsetHeight / 2;
+    const leftClick = e.clientX - coordsWall.x - spider.offsetWidth / 2;
+
+    const topSpider = Math.max(
+      0,
+      Math.min(wall.clientHeight - spider.clientHeight, topClick),
+    );
+    const leftSpider = Math.max(
+      0,
+      Math.min(wall.clientHeight - spider.clientHeight, leftClick),
+    );
+
+    spider.style.left = leftSpider + 'px';
+    spider.style.top = topSpider + 'px';
+  }
 });
