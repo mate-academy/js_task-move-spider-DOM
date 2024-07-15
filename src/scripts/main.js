7cd80284645be120d662b8972a0bceabc4d7e0c8
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2024-07-15 15:54:53 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,28 +3,46 @@
 document.addEventListener('click', (e) => {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('img');
-  const notWallX = (window.innerWidth - wall.offsetWidth) / 2;
-  const notWallY = (window.innerHeight - wall.offsetHeight) / 2;
+
+  const rect = wall.getBoundingClientRect();
+  const wallLeft = rect.left + scrollX;
+  const wallTop = rect.top + scrollY;
   const borderLeft = parseInt(getComputedStyle(wall).borderLeftWidth);
   const borderTop = parseInt(getComputedStyle(wall).borderTopWidth);
   const wallWidth = parseInt(getComputedStyle(wall).width);
   const wallHeight = parseInt(getComputedStyle(wall).height);
-  const NotAvailableXLeft = e.clientX < notWallX + borderLeft;
-  const NotAvailableXRight = e.clientX > notWallX + borderLeft + wallWidth;
-  const NotAvailableYTop = e.clientY < notWallY + borderTop;
-  const NotAvailableXBottom = e.clientY > notWallY + borderTop + wallHeight;
-
-  if (
+  const NotAvailableXLeft = e.clientX < wallLeft + borderLeft;
+  const NotAvailableXRight = e.clientX > wallLeft + borderLeft + wallWidth;
+  const NotAvailableYTop = e.clientY < wallTop + borderTop;
+  const NotAvailableYBottom = e.clientY > wallTop + borderTop + wallHeight;
+  const notAvailable =
     NotAvailableXLeft ||
     NotAvailableXRight ||
     NotAvailableYTop ||
-    NotAvailableXBottom
-  ) {
+    NotAvailableYBottom;
+
+  if (notAvailable) {
     return;
   }
 
-  const newLeft = e.clientX - notWallX - borderLeft - spider.offsetWidth / 2;
-  const newTop = e.clientY - notWallY - borderTop - spider.offsetHeight / 2;
+  let newLeft = e.clientX - wallLeft - borderLeft - spider.offsetWidth / 2;
+  let newTop = e.clientY - wallTop - borderTop - spider.offsetHeight / 2;
+
+  if (newLeft < 0) {
+    newLeft = 0;
+  }
+
+  if (newLeft > wallWidth - spider.offsetWidth) {
+    newLeft = wallWidth - spider.offsetWidth;
+  }
+
+  if (newTop < 0) {
+    newTop = 0;
+  }
+
+  if (newTop > wallHeight - spider.offsetHeight) {
+    newTop = wallHeight - spider.offsetHeight;
+  }
 
   spider.style.left = `${newLeft}px`;
   spider.style.top = `${newTop}px`;
