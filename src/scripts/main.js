d2513c1ca65555ef36947724cfc3f8c1bc2e533a
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2024-07-15 12:56:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('img');
+  const notWallX = (window.innerWidth - wall.offsetWidth) / 2;
+  const notWallY = (window.innerHeight - wall.offsetHeight) / 2;
+  const borderLeft = parseInt(getComputedStyle(wall).borderLeftWidth);
+  const borderTop = parseInt(getComputedStyle(wall).borderTopWidth);
+  const wallWidth = parseInt(getComputedStyle(wall).width);
+  const wallHeight = parseInt(getComputedStyle(wall).height);
+  let newLeft = e.clientX - notWallX - borderLeft - spider.offsetWidth / 2;
+  let newTop = e.clientY - notWallY - borderTop - spider.offsetHeight / 2;
+
+  if (newLeft < 0) {
+    newLeft = 0;
+  }
+
+  if (newLeft > wallWidth - spider.offsetWidth) {
+    newLeft = wallWidth - spider.offsetWidth;
+  }
+
+  if (newTop < 0) {
+    newTop = 0;
+  }
+
+  if (newTop > wallHeight - spider.offsetHeight) {
+    newTop = wallHeight - spider.offsetHeight;
+  }
+
+  spider.style.left = `${newLeft}px`;
+  spider.style.top = `${newTop}px`;
 });
