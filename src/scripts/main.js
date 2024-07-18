2ab14c6c69dc259df6b444a648022f5cce150f64
Melnyk Anna <anetamelnyk25@gmail.com>
2024-07-18 17:25:56 +0300

    move_spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const wallTop = wall.offsetTop;
+  const wallLeft = wall.offsetLeft;
+
+  const border = wall.clientTop;
+
+  let x = e.clientX - border - wallLeft - spider.width / 2;
+  let y = e.clientY - border - wallTop - spider.width / 2;
+
+  const maxX = wall.clientWidth - spider.offsetWidth;
+  const maxY = wall.clientHeight - spider.offsetHeight;
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (x > maxX) {
+    x = maxX;
+  }
+
+  if (y > maxY) {
+    y = maxY;
+  }
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
