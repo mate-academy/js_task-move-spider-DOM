fad952b18f3a7d37279b6d09820ac94187831c4d
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-20 08:06:59 +0300

    refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,31 +3,31 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 const wallBorderWidth = Number.parseFloat(getComputedStyle(wall).borderWidth);
-const defaultOffsetX =
+const offsetX =
   wall.getBoundingClientRect().left + wallBorderWidth + spider.width / 2;
-const defaultOffsetY =
-  wall.getBoundingClientRect().top + wallBorderWidth + spider.height / 2;
 const maxX = wall.clientWidth - spider.clientWidth;
+const offsetY =
+  wall.getBoundingClientRect().top + wallBorderWidth + spider.height / 2;
 const maxY = wall.clientHeight - spider.clientHeight;
 
 document.addEventListener('click', (e) => {
   if (!e.target.closest('.wall')) {
     return;
   }
-  spider.style.left = getPosition(e.clientX, defaultOffsetX, maxX) + 'px';
-  spider.style.top = getPosition(e.clientY, defaultOffsetY, maxY) + 'px';
+  spider.style.left = getCoordinate(e.clientX, offsetX, maxX) + 'px';
+  spider.style.top = getCoordinate(e.clientY, offsetY, maxY) + 'px';
 });
 
-function getPosition(currentPos, defOffset, maxPos) {
-  const position = currentPos - defOffset;
+function getCoordinate(eventCoordinate, offset, maxCoordinate) {
+  const coordinate = eventCoordinate - offset;
 
-  if (position < 0) {
+  if (coordinate < 0) {
     return 0;
   }
 
-  if (position > maxPos) {
-    return maxPos;
+  if (coordinate > maxCoordinate) {
+    return maxCoordinate;
   }
 
-  return position;
+  return coordinate;
 }
