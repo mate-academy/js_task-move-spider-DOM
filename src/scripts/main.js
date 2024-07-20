1c024401b5623dddad035a953bcfeb9ba001279f
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-20 07:57:40 +0300

    refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const wall = document.querySelector('.wall');
-const wallRect = wall.getBoundingClientRect();
-const wallBorderSize = Number.parseFloat(getComputedStyle(wall).borderWidth);
 const spider = document.querySelector('.spider');
-
-const defaultOffsetX = wallRect.left + wallBorderSize + spider.width / 2;
-const defaultOffsetY = wallRect.top + wallBorderSize + spider.height / 2;
+const wallBorderWidth = Number.parseFloat(getComputedStyle(wall).borderWidth);
+const defaultOffsetX =
+  wall.getBoundingClientRect().left + wallBorderWidth + spider.width / 2;
+const defaultOffsetY =
+  wall.getBoundingClientRect().top + wallBorderWidth + spider.height / 2;
 const maxX = wall.clientWidth - spider.clientWidth;
 const maxY = wall.clientHeight - spider.clientHeight;
 
@@ -14,22 +14,20 @@ document.addEventListener('click', (e) => {
   if (!e.target.closest('.wall')) {
     return;
   }
-
-  let x = e.clientX - defaultOffsetX;
-  let y = e.clientY - defaultOffsetY;
-
-  if (x < 0) {
-    x = 0;
-  } else if (x > maxX) {
-    x = maxX;
-  }
-
-  if (y < 0) {
-    y = 0;
-  } else if (y > maxY) {
-    y = maxY;
-  }
-
-  spider.style.left = x + 'px';
-  spider.style.top = y + 'px';
+  spider.style.left = getPosition(e.clientX, defaultOffsetX, maxX) + 'px';
+  spider.style.top = getPosition(e.clientY, defaultOffsetY, maxY) + 'px';
 });
+
+function getPosition(currentPos, defOffset, maxPos) {
+  const position = currentPos - defOffset;
+
+  if (position < 0) {
+    return 0;
+  }
+
+  if (position > maxPos) {
+    return maxPos;
+  }
+
+  return position;
+}
