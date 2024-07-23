ddfa4a62a1e569f11b4ff9461a13ed36a61e3174
Oleksandr Lytovchenko <jaster93him@gmail.com>
2024-07-23 22:37:37 +0300

    spider moves on click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  e.preventDefault();
+
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+  const target = e.target;
+
+  if (target.classList.contains('wall')) {
+    const wallRect = wall.getBoundingClientRect();
+
+    const wallLeft = wallRect.left + window.scrollX;
+    const wallTop = wallRect.top + window.scrollY;
+
+    const spiderWidth = spider.clientWidth;
+    const spiderHeight = spider.clientHeight;
+
+    let positionLeft = e.clientX - wallLeft - spiderWidth / 2 - wall.clientLeft;
+    let positionTop = e.clientY - wallTop - spiderHeight / 2 - wall.clientTop;
+
+    const maxX = wall.clientWidth - spiderWidth;
+    const maxY = wall.clientHeight - spiderHeight;
+
+    if (positionLeft < 0) {
+      positionLeft = 0;
+    } else if (positionLeft > maxX) {
+      positionLeft = maxX;
+    }
+
+    if (positionTop < 0) {
+      positionTop = 0;
+    } else if (positionTop > maxY) {
+      positionTop = maxY;
+    }
+
+    spider.style.left = `${positionLeft}px`;
+    spider.style.top = `${positionTop}px`;
+  }
 });
