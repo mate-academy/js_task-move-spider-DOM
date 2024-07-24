184451b3e607643317c62a4126f8a8f22836ec03
Katya <evil.666.buny@gmail.com>
2024-07-24 13:58:03 +0300

    script added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const wallSize = {
+  width: wall.clientWidth,
+  height: wall.clientHeight,
+};
+
+const spiderSize = {
+  width: spider.clientWidth,
+  height: spider.clientHeight,
+};
+
+const maxX = wallSize.width - spiderSize.width;
+const maxY = wallSize.height - spiderSize.height;
+
+wall.addEventListener('click', (e) => {
+  if (e.target !== wall) {
+    return;
+  }
+
+  const clickY = e.offsetY;
+  const clickX = e.offsetX;
+
+  const coordX = Math.max(0, Math.min(clickX - spiderSize.width / 2, maxX));
+  const coordY = Math.max(0, Math.min(clickY - spiderSize.height / 2, maxY));
+
+  spider.style.top = `${coordY}px`;
+  spider.style.left = `${coordX}px`;
 });
