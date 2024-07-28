479020669e28cfedcf63e98c2cca795068830bff
Maksym_enKO <finside41@gmail.com>
2024-07-28 22:26:30 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
-'use strict';
+function ready() {
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
 
-document.addEventListener('click', (e) => {
-  // write code here
-});
+  wall.addEventListener('click', (e) => {
+    const {
+      left: wallLeft,
+      top: wallTop,
+      width: wallWidth,
+      height: wallHeight,
+    } = wall.getBoundingClientRect();
+
+    const computedStyle = getComputedStyle(wall);
+
+    const borderWidth = parseFloat(computedStyle.borderWidth);
+
+    const { width: spiderWidth, height: spiderHeight } =
+      spider.getBoundingClientRect();
+
+    let newX = e.clientX - wallLeft - spiderWidth;
+    let newY = e.clientY - wallTop - spiderHeight;
+
+    newX = Math.max(
+      0,
+      Math.min(newX, wallWidth - spiderWidth - borderWidth * 2),
+    );
+
+    newY = Math.max(
+      0,
+      Math.min(newY, wallHeight - spiderHeight - borderWidth * 2),
+    );
+
+    spider.style.left = `${newX}px`;
+    spider.style.top = `${newY}px`;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', ready);
