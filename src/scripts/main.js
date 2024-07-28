420ed6459795bb31398b1d8a24984e129a1a02dd
Maksym_enKO <finside41@gmail.com>
2024-07-28 23:07:54 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,37 @@
-function ready() {
+'use strict';
+
+document.addEventListener('click', (e) => {
   const spider = document.querySelector('.spider');
   const wall = document.querySelector('.wall');
 
-  wall.addEventListener('click', (e) => {
-    const {
-      left: wallLeft,
-      top: wallTop,
-      width: wallWidth,
-      height: wallHeight,
-    } = wall.getBoundingClientRect();
+  const {
+    left: wallLeft,
+    top: wallTop,
+    width: wallWidth,
+    height: wallHeight,
+  } = wall.getBoundingClientRect();
 
-    const computedStyle = getComputedStyle(wall);
+  const computedStyle = getComputedStyle(wall);
 
-    const borderWidth = parseFloat(computedStyle.borderWidth);
+  const borderWidth = parseFloat(computedStyle.borderWidth);
 
-    const { width: spiderWidth, height: spiderHeight } =
-      spider.getBoundingClientRect();
+  const { width: spiderWidth, height: spiderHeight } =
+    spider.getBoundingClientRect();
 
-    let newX = e.clientX - wallLeft - spiderWidth;
-    let newY = e.clientY - wallTop - spiderHeight;
+  let newX = e.clientX - wallLeft - spiderWidth;
+  let newY = e.clientY - wallTop - spiderHeight;
 
-    newX = Math.max(
-      0,
-      Math.min(newX, wallWidth - spiderWidth - borderWidth * 2),
-    );
+  newX = Math.max(0, Math.min(newX, wallWidth - spiderWidth - borderWidth * 2));
 
-    newY = Math.max(
-      0,
-      Math.min(newY, wallHeight - spiderHeight - borderWidth * 2),
-    );
+  newY = Math.max(
+    0,
+    Math.min(newY, wallHeight - spiderHeight - borderWidth * 2),
+  );
 
-    spider.style.left = `${newX}px`;
-    spider.style.top = `${newY}px`;
-  });
-}
+  if (!wall.contains(e.target)) {
+    return;
+  }
 
-document.addEventListener('DOMContentLoaded', ready);
+  spider.style.left = `${newX}px`;
+  spider.style.top = `${newY}px`;
+});
