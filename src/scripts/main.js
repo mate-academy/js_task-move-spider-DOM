81735619b3dcba3a3d3a9691b0e9948e756e9ffb
Liudmyla Hnydyn <gnydynluda@gmail.com>
2024-07-28 18:08:47 -0400

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const topPos = wall.offsetTop;
+const leftPos = wall.offsetLeft;
+const widthSpider = spider.offsetWidth / 2;
+const heightSpider = spider.offsetHeight / 2;
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target.closest('.wall')) {
+    const x = e.clientX - leftPos - heightSpider;
+    const y = e.clientY - topPos - widthSpider;
+
+    spider.style.cssText = `
+    top: ${y}px;
+    left: ${x}px;
+    `;
+  }
 });
