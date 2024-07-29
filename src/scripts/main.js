858a2a679188b35fcd2328b59d1a5235c318fe9b
Mykhailo Shvets <mikhailshvets48@gmail.com>
2024-07-29 13:12:16 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
+  const spider = document.querySelector('.spider');
+
+  const maxHeight = wall.clientHeight - spider.clientHeight;
+  const maxWidth = wall.clientWidth - spider.clientWidth;
+
+  let spiderY =
+    e.clientY - wall.getBoundingClientRect().top - spider.offsetHeight / 2 - 10;
+
+  let spiderX =
+    e.clientX - wall.getBoundingClientRect().left - spider.offsetWidth / 2 - 10;
+
+  if (spiderY < 0) {
+    spiderY = 0;
+  }
+
+  if (spiderY > maxHeight) {
+    spiderY = maxHeight;
+  }
+
+  if (spiderX < 0) {
+    spiderX = 0;
+  }
+
+  if (spiderX > maxWidth) {
+    spiderX = maxWidth;
+  }
+
+  spider.style.top = `${spiderY}px`;
+  spider.style.left = `${spiderX}px`;
 });
