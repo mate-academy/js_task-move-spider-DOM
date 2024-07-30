cba9cf5a7fbad48b8d68952ac1137485d0610e4a
Your Name <ruslankurman13@gmail.com>
2024-07-31 00:05:31 +0300

    reset

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,44 @@
 'use strict';
 
-document.addEventListener('click', function (e) {
-  const spider = document.querySelector('.spider');
+document.addEventListener('click', (e) => {
+  // Write code here
   const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-  const wallY = wall.clientHeight;
-  const wallX = wall.clientWidth;
-  const spiderY = spider.clientHeight;
-  const spiderX = spider.clientWidth;
-  const clickY = e.offsetY;
-  const clickX = e.offsetX;
-  const maxX = wallX - spiderX;
-  const maxY = wallY - spiderY;
-  const goToX = Math.max(0, Math.min(clickX - spiderX / 2, maxX));
-  const goToY = Math.max(0, Math.min(clickY - spiderY / 2, maxY));
+  // Calculate the bounding rectangle of the wall
+  const wallCoords = wall.getBoundingClientRect();
 
-  spider.style.top = `${goToY}px`;
-  spider.style.left = `${goToX}px`;
+  // Check if the click is within the wall's boundaries
+  if (
+    e.clientX >= wallCoords.left &&
+    e.clientX <= wallCoords.right &&
+    e.clientY >= wallCoords.top &&
+    e.clientY <= wallCoords.bottom
+  ) {
+    const spiderCoords = {
+      top:
+        e.clientY - wallCoords.top - wall.clientTop - spider.clientHeight / 2,
+      left:
+        e.clientX - wallCoords.left - wall.clientLeft - spider.clientWidth / 2,
+    };
+
+    if (spiderCoords.top < 0) {
+      spiderCoords.top = 0;
+    }
+
+    if (spiderCoords.left < 0) {
+      spiderCoords.left = 0;
+    }
+
+    if (spiderCoords.left + spider.clientWidth > wall.clientWidth) {
+      spiderCoords.left = wall.clientWidth - spider.clientWidth;
+    }
+
+    if (spiderCoords.top + spider.clientHeight > wall.clientHeight) {
+      spiderCoords.top = wall.clientHeight - spider.clientHeight;
+    }
+
+    spider.style.left = spiderCoords.left + 'px';
+    spider.style.top = spiderCoords.top + 'px';
+  }
 });
