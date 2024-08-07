f917a0f6597e77e326768b5b489e530864551930
Ostap Hedz <lordanvil18@gmail.com>
2024-08-07 10:52:14 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  if (e.target !== wall) {
+    return;
+  }
+
+  const wallBounding = wall.getBoundingClientRect();
+  const spiderBounding = spider.getBoundingClientRect();
+  const border = wall.clientTop;
+
+  const spiderHeight = spiderBounding.height / 2;
+  const spiderWidth = spiderBounding.width / 2;
+
+  const spiderX = e.clientX - wallBounding.left - spiderWidth - border;
+  const spiderY = e.clientY - wallBounding.top - spiderHeight - border;
+
+  const clickX = Math.max(
+    0,
+    Math.min(spiderX, wall.clientWidth - spider.clientWidth),
+  );
+  const clickY = Math.max(
+    0,
+    Math.min(spiderY, wall.clientHeight - spider.clientHeight),
+  );
+
+  spider.style.left = `${clickX}px`;
+  spider.style.top = `${clickY}px`;
 });
