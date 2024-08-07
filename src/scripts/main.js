d4bacb957552884d3df7ce903006622638f2536b
LadyLiza <elizabet132010@gmail.com>
2024-08-07 18:21:49 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = wall.querySelector('.spider');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  let x = e.offsetX - spiderWidth / 2;
+  let y = e.offsetY - spiderHeight / 2;
+
+  x = Math.max(0, Math.min(x, wallWidth - spiderWidth));
+  y = Math.max(0, Math.min(y, wallHeight - spiderHeight));
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });
