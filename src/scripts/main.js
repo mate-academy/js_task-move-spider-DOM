7c36d60da2b34d69f9e6c037bc79bff3862f1048
Mainaiev <farid.mainaiev@proton.me>
2024-08-11 22:35:41 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  e.preventDefault();
+
+  if (e.target.className === 'wall') {
+    const wallRect = wall.getBoundingClientRect();
+    const spiderHeight = spider.offsetHeight;
+    const spiderWidth = spider.offsetWidth;
+
+    let positionX = e.clientX - wallRect.left - spider.offsetWidth / 2;
+    let positionY = e.clientY - wallRect.top - spiderHeight / 2;
+
+    if (positionY < 0) {
+      positionY = 0;
+    } else if (positionY + spiderHeight > wallRect.height) {
+      positionY = wallRect.height - spiderHeight;
+    }
+
+    if (positionX < 0) {
+      positionY = 0;
+    } else if (positionX + spiderWidth > wallRect.height) {
+      positionX = wallRect.height - spiderWidth;
+    }
+
+    spider.style.top = `${positionY}px`;
+    spider.style.left = `${positionX}px`;
+  }
 });
