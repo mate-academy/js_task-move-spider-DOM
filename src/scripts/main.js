17e93a3d14316a2699d94912c1ad0a3f5944e700
Oleksii Symonets <alex.simonets@gmail.com>
2024-08-14 22:16:47 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,45 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
-});
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+document.addEventListener('click', callback);
+
+function callback(e) {
+  if (e.target !== wall) {
+    return;
+  }
+
+  const x = e.x;
+  const y = e.y;
+
+  const leftBorder = wall.offsetLeft + wall.clientLeft;
+  const topBorder = wall.offsetTop + wall.clientTop;
+  const totalWidth = wall.clientWidth;
+  const totalHeight = wall.clientHeight;
+
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+
+  let newLeft = x - leftBorder - spiderWidth / 2;
+  let newTop = y - topBorder - spiderHeight / 2;
+
+  if (newLeft < 0) {
+    newLeft = 0;
+  }
+
+  if (newLeft > totalWidth - spiderWidth) {
+    newLeft = totalWidth - spiderWidth;
+  }
+
+  if (newTop < 0) {
+    newTop = 0;
+  }
+
+  if (newTop > totalHeight - spiderHeight) {
+    newTop = totalHeight - spiderHeight;
+  }
+
+  spider.style.top = newTop + 'px';
+  spider.style.left = newLeft + 'px';
+}
