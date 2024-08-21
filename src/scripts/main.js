ab56a1f7bf83e1f9ad86f737d7902916332aaf78
Kateryna <moskkat40@gmail.com>
2024-08-21 14:36:48 +0200

    add spider move task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const posX = wall.clientWidth - spider.clientWidth;
+  const posY = wall.clientHeight - spider.clientHeight;
+  let x = e.offsetX - spider.clientWidth / 2;
+  let y = e.offsetY - spider.clientHeight / 2;
+
+  if (x > posX) {
+    x = posX;
+  }
+
+  if (y > posY) {
+    y = posY;
+  }
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
