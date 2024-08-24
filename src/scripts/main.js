397810ae8aa80c5901a547ed8a49d451a4720b2d
Yehor Gordiienko <egordienko319@gmail.com>
2024-08-24 18:55:33 +0300

    did a task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  let newLeft = x - wallRect.left - spiderRect.width / 2;
+  let newTop = y - wallRect.top - spiderRect.height / 2;
+
+  newLeft = Math.max(0, Math.min(wallRect.width - spiderRect.width, newLeft));
+  newTop = Math.max(0, Math.min(wallRect.height - spiderRect.height, newTop));
+
+  spider.style.left = `${newLeft}px`;
+  spider.style.top = `${newTop}px`;
 });
