5f515b0d00156ba401c8bb5f6e777b4167119e17
Maksym Mohyla <greenparksquad@gmail.com>
2024-09-01 15:02:02 +0300

    moving spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallRect = wall.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+
+wall.addEventListener('click', (e) => {
+  let x = e.clientX - wallRect.left - wall.clientLeft - spiderRect.width / 2;
+  let y = e.clientY - wallRect.top - wall.clientTop - spiderRect.height / 2;
+
+  x = Math.max(0, Math.min(x, wall.clientWidth - spider.clientWidth));
+  y = Math.max(0, Math.min(y, wall.clientHeight - spider.clientHeight));
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
