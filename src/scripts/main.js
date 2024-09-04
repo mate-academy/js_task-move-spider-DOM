2932e785f8b30d3dcb7a426963a0a94582de66be
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-04 16:15:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+wall.addEventListener('click', (e) => {
+  const spider = document.querySelector('.spider');
+  const halfSpiderWidth = spider.clientWidth / 2;
+  const halfSpiderHeight = spider.clientHeight / 2;
+
+  const cursorX = e.clientX - wall.offsetLeft - wall.clientLeft;
+  const cursorY = e.clientY - wall.offsetTop - wall.clientTop;
+
+  spider.style.top = `
+    clamp(
+      ${halfSpiderHeight}px,
+      ${cursorY}px,
+      ${wallHeight - halfSpiderHeight}px
+    )
+  `;
+
+  spider.style.left = `
+    clamp(
+      ${halfSpiderWidth}px,
+      ${cursorX}px,
+      ${wallWidth - halfSpiderWidth}px
+    )
+  `;
+
+  spider.style.transform = 'translate(-50%, -50%)';
 });
