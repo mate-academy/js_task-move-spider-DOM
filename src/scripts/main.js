3a4c0e5cbf43fff57d48f74543db9e8b9c67374b
Yehor Gordiienko <egordienko319@gmail.com>
2024-09-06 18:47:44 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,21 @@
-'use strict';
-
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
 wall.addEventListener('click', (e) => {
-  const wallBorder = parseInt(getComputedStyle(wall).border);
+  const wallStyle = getComputedStyle(wall);
+  const wallBorder = parseInt(wallStyle.borderWidth, 10);
+
   const wallRect = wall.getBoundingClientRect();
 
-  let spiderLeft = e.clientX - wallRect.x - wallBorder - spider.offsetWidth / 2;
-  let spiderTop = e.clientY - wallRect.y - wallBorder - spider.offsetHeight / 2;
+  let spiderLeft =
+    e.clientX - wallRect.left - wallBorder - spider.offsetWidth / 2;
+  let spiderTop =
+    e.clientY - wallRect.top - wallBorder - spider.offsetHeight / 2;
 
   const maxLeft = wall.offsetWidth - spider.offsetWidth - wallBorder * 2;
   const maxTop = wall.offsetHeight - spider.offsetHeight - wallBorder * 2;
 
   spiderLeft = Math.min(Math.max(spiderLeft, 0), maxLeft);
-
   spiderTop = Math.min(Math.max(spiderTop, 0), maxTop);
 
   spider.style.left = `${spiderLeft}px`;
