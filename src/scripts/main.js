760535d90091c3fe875e7804a16b421d238f1036
Serhii-Khobotov <hobbitura@gmail.com>
2024-09-10 19:33:03 +0300

    all tests is passed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-let marginLeft = (body.clientWidth - wall.clientWidth) / 2;
-let marginTop = (body.clientHeight - wall.clientHeight) / 2;
+const marginLeft = (body.clientWidth - wall.clientWidth) / 2;
+const marginTop = (body.clientHeight - wall.clientHeight) / 2;
 
 wall.addEventListener('click', (e) => {
   spider.style.top = `${e.clientY - marginTop - spider.clientHeight / 2}px`;
@@ -14,16 +14,15 @@ wall.addEventListener('click', (e) => {
     spider.style.top = 0;
   }
 
-  if (parseInt(spider.style.top) > (wall.clientHeight - spider.clientHeight)) {
-    spider.style.top = `${(wall.clientHeight - spider.clientHeight)}px`;
+  if (parseInt(spider.style.top) > wall.clientHeight - spider.clientHeight) {
+    spider.style.top = `${wall.clientHeight - spider.clientHeight}px`;
   }
 
   if (parseInt(spider.style.left) < 0) {
     spider.style.left = 0;
   }
 
-  if (parseInt(spider.style.left) > (wall.clientWidth - spider.clientWidth)) {
+  if (parseInt(spider.style.left) > wall.clientWidth - spider.clientWidth) {
     spider.style.left = `${wall.clientWidth - spider.clientWidth}px`;
   }
 });
-
