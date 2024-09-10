67416ceef451adbd39c99308d38cc6d102957efe
Iryna Zahorodnia <irishas25@ukr.net>
2024-09-10 14:33:39 +0300

    move spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallPosition = wall.getBoundingClientRect();
+
+wall.addEventListener('click', (e) => {
+  const clickX = e.x;
+  const clickY = e.y;
+  const spiderWidth = spider.clientWidth;
+  const spiderHeight = spider.clientHeight;
+  const maxLeft = wall.clientWidth - spiderWidth;
+  const maxTop = wall.clientHeight - spiderHeight;
+
+  const spiderCoords = {
+    left: clickX - wallPosition.x - wall.clientLeft - spiderWidth / 2,
+    top: clickY - wallPosition.y - wall.clientTop - spiderHeight / 2,
+  };
+
+  if (spiderCoords.left < 0) {
+    spiderCoords.left = 0;
+  } else if (spiderCoords.left > maxLeft) {
+    spiderCoords.left = maxLeft;
+  }
+
+  if (spiderCoords.top < 0) {
+    spiderCoords.top = 0;
+  } else if (spiderCoords.top > maxTop) {
+    spiderCoords.top = maxTop;
+  }
+
+  spider.style.left = `${spiderCoords.left}px`;
+  spider.style.top = `${spiderCoords.top}px`;
 });
