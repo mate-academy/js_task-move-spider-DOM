6c08b8030c8694365d1c003bcd667d3a92b2dfab
Serhii-Khobotov <hobbitura@gmail.com>
2024-09-10 18:29:41 +0300

    The spider move correctly

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const body = document.querySelector('body')
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+let marginLeft = (body.clientWidth - wall.clientWidth) / 2;
+let marginTop = (body.clientHeight - wall.clientHeight) / 2;
+
+wall.addEventListener('click', (e) => {
+  spider.style.top = `${e.clientY - marginTop - spider.clientHeight / 2}px`;
+  spider.style.left = `${e.clientX - marginLeft - spider.clientWidth / 2}px`;
+
+  if (parseInt(spider.style.top) < 0) {
+    spider.style.top = 0;
+  }
+
+  if (parseInt(spider.style.top) > (wall.clientHeight - spider.clientHeight)) {
+    spider.style.top = `${(wall.clientHeight - spider.clientHeight)}px`;
+  }
+
+  if (parseInt(spider.style.left) < 0) {
+    spider.style.left = 0;
+  }
+
+  if (parseInt(spider.style.left) > (wall.clientWidth - spider.clientWidth)) {
+    spider.style.left = `${wall.clientWidth - spider.clientWidth}px`;
+  }
 });
+
