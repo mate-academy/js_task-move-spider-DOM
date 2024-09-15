46f7e799436999c0f299e20d9eb192061436808a
Yaroslav Halynskyi <125606535+Yaroslav65@users.noreply.github.com>
2024-09-15 22:15:59 +0200

    move-spider-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall ');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const rectWall = wall.getBoundingClientRect();
+  const borderSize = 10;
+  const wallWidth = rectWall.width - 2 * borderSize;
+  const wallHeight = rectWall.height - 2 * borderSize;
+
+  const x = e.offsetX - (spider.offsetWidth / 2);
+  const y = e.offsetY - (spider.offsetHeight / 2);
+
+  const newX = Math.max(0, Math.min(x, wallWidth - spider.offsetWidth));
+  const newY = Math.max(0, Math.min(y, wallHeight - spider.offsetHeight));
+
+  spider.style.left = `${newX}px`;
+  spider.style.top = `${newY}px`;
 });
