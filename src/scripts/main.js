6d702c1acdacd2cc064f5836ee777aa9c28642d0
missk <rostock.vasiljev@gmail.com>
2024-10-03 16:00:17 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  const computedStyle = getComputedStyle(wall);
+  const borderWidth = parseInt(computedStyle.borderLeftWidth) * 2;
+
+  let x = e.clientX - wallRect.left - wall.clientLeft - spiderRect.width / 2;
+  let y = e.clientY - wallRect.top - wall.clientTop - spiderRect.height / 2;
+
+  if (x < 0) {
+    x = 0;
+  } else if (x > wallRect.width - spiderRect.width - borderWidth) {
+    x = wallRect.width - spiderRect.width - borderWidth;
+  }
+
+  if (y < 0) {
+    y = 0;
+  } else if (y > wallRect.height - spiderRect.height - borderWidth) {
+    y = wallRect.height - spiderRect.height - borderWidth;
+  }
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
