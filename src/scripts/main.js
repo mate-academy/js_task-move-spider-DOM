39625d8209fc72fb6c91dbdd40c5de6d3af06e6a
Kseniia Karpenko <ksushakarpenko635@gmail.com>
2024-10-05 19:10:55 +0200

    test`s check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wallSize = wall.getBoundingClientRect();
+  const spiderSize = spider.getBoundingClientRect();
+
+  const positionY =
+    e.clientY - wallSize.top - wall.clientTop - spiderSize.height / 2;
+  const positionX =
+    e.clientX - wallSize.left - wall.clientLeft - spiderSize.width / 2;
+
+  if (
+    !(
+      positionY < 0 ||
+      positionX < 0 ||
+      positionY > wall.clientHeight - spider.clientHeight ||
+      positionX > wall.clientWidth - spider.clientWidth
+    )
+  ) {
+    spider.style.top = positionY + 'px';
+    spider.style.left = positionX + 'px';
+  }
 });
