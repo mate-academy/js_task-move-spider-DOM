c70051a2ab1a77f3cfe0630e1b458ab268b7f435
associate2coder <olatsko@gmail.com>
2024-10-05 16:25:24 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const { offsetX: x, offsetY: y } = e;
+
+  const newLeft = Math.max(
+    0,
+    Math.min(x - spider.clientWidth / 2, wall.clientWidth - spider.clientWidth),
+  );
+
+  const newTop = Math.max(
+    0,
+    Math.min(
+      y - spider.clientHeight / 2,
+      wall.clientHeight - spider.clientHeight,
+    ),
+  );
+
+  spider.style.left = `${newLeft}px`;
+  spider.style.top = `${newTop}px`;
 });
