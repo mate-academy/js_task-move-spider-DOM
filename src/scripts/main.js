19f0d55f1dd11b7e83533791c1fa95720372edb7
Ivan Solopenko <solopenko16@gmail.com>
2025-01-08 18:59:51 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (!wall || !spider) {
+    return;
+  }
+
+  const spiderRect = spider.getBoundingClientRect();
+  const wallRect = wall.getBoundingClientRect();
+
+  const spiderHalfWidth = spiderRect.width / 2;
+  const spiderHalfHeight = spiderRect.height / 2;
+
+  const borderSize = (wallRect.height - wall.clientHeight) / 2;
+
+  const clickX = e.clientX;
+  const clickY = e.clientY;
+
+  if (clickX < wallRect.left || clickX > wallRect.left + wall.offsetWidth) {
+    return;
+  }
+
+  if (clickY < wallRect.top || clickY > wallRect.top + wall.offsetHeight) {
+    return;
+  }
+
+  let posTop = clickY - wallRect.top - spiderHalfHeight - borderSize;
+  let posLeft = clickX - wallRect.left - spiderHalfWidth - borderSize;
+
+  posTop = Math.max(0, Math.min(posTop, wall.clientHeight - spiderRect.height));
+  posLeft = Math.max(0, Math.min(posLeft, wall.clientWidth - spiderRect.width));
+
+  spider.style.top = `${posTop}px`;
+  spider.style.left = `${posLeft}px`;
 });
