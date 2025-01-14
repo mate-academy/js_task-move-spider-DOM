2a0687820017a2662554fbaa07ce554ffe114092
Sasha Popovych <sashapopovych98@gmail.com>
2025-01-14 15:03:51 +0100

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (wall.contains(e.target)) {
+    spider.style.top = `${
+      e.offsetY < 25 ? 0 : e.offsetY > 350 ? 350 : e.offsetY - 25
+    }px`;
+
+    spider.style.left = `${
+      e.offsetX < 25 ? 0 : e.offsetX > 350 ? 350 : e.offsetX - 25
+    }px`;
+  }
 });
