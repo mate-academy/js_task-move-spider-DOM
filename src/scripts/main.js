f60780e10aa5c35fce0682c42df6fb21a93a0ede
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2025-01-15 15:23:53 +0200

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+  const spiderSizeX = spider.clientWidth;
+  const spiderSizeY = spider.clientHeight;
+
+  const startX = wall.offsetLeft + wall.clientLeft;
+  const startY = wall.offsetTop + wall.clientTop;
+  const currentX = e.clientX - startX;
+  const currentY = e.clientY - startY;
+
+  if (
+    currentX >= spiderSizeX / 2 &&
+    currentY >= spiderSizeY / 2 &&
+    currentX <= wall.clientWidth - spiderSizeX / 2 &&
+    currentY <= wall.clientHeight - spiderSizeY / 2
+  ) {
+    spider.style.top = `${currentY - spiderSizeY / 2}px`;
+    spider.style.left = `${currentX - spiderSizeX / 2}px`;
+  }
 });
