821a8bc3bc7847eb451842284205af5fdefe9192
Nikolay <diedaemon@gmail.com>
2025-01-16 20:23:24 +0100

    Task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallSize = wall.getBoundingClientRect();
+
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  const spiderX =
+    e.clientX - wallSize.left - wall.clientLeft - spider.width / 2;
+  const spiderY = e.clientY - wallSize.top - wall.clientTop - spider.height / 2;
+
+  const maxLeft = wall.clientWidth - spider.offsetWidth;
+  const maxTop = wall.clientHeight - spider.offsetHeight;
+
+  spider.style.left = `${Math.max(0, Math.min(spiderX, maxLeft))}px`;
+  spider.style.top = `${Math.max(0, Math.min(spiderY, maxTop))}px`;
 });
