68ba841343df2d9a0942de945d63a4f56fdf9a3e
Kateryna Berdnikova <berdekaterinasp@gmail.com>
2025-01-16 14:19:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const wallTop = wall.offsetTop;
+  const wallLeft = wall.offsetLeft;
+  const border = parseInt(getComputedStyle(wall).borderWidth);
+
+  const spider = document.querySelector('.spider');
+  const spiderWidth = parseInt(spider.width) / 2;
+  const spiderHeight = parseInt(spider.height) / 2;
+
+  if (
+    e.clientX < wallLeft ||
+    e.clientY < wallTop ||
+    e.clientX > wallLeft + wall.offsetWidth ||
+    e.clientY > wallTop + wall.offsetHeigth
+  ) {
+    return;
+  }
+
+  spider.style.left = e.clientX - wallLeft - spiderWidth - border + 'px';
+  spider.style.top = e.clientY - wallTop - spiderHeight - border + 'px';
+
+  if (e.clientX < wallLeft + border + spiderWidth) {
+    spider.style.left = 0 + 'px';
+  }
+
+  if (e.clientY < wallTop + border + spiderHeight) {
+    spider.style.top = 0 + 'px';
+  }
+
+  if (e.clientX > wallLeft + border + wall.clientWidth - spiderWidth) {
+    spider.style.left = wall.clientWidth - spider.width + 'px';
+  }
+
+  if (e.clientY > wallTop + border + wall.clientHeight - spiderHeight) {
+    spider.style.top = wall.clientHeight - spider.height + 'px';
+  }
 });
