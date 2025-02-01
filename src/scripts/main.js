c74e46b75d23d491c111336eabe6f28743d21963
Ivan Lavruk <ilavr8725@gmail.com>
2025-02-01 18:52:29 +0100

    move-spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallReact = wall.getBoundingClientRect();
+  const wallStyle = getComputedStyle(wall);
+
+  const borderLeft = parseInt(wallStyle.borderLeftWidth) || 0;
+  const borderTop = parseInt(wallStyle.borderTopWidth) || 0;
+
+  const x = e.clientX - wallReact.left - spider.width / 2 - borderLeft;
+  const y = e.clientY - wallReact.top - spider.height / 2 - borderTop;
+
+  spider.style.position = 'absolute';
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
