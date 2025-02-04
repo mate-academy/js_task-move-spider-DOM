2432c48a0fd705e20c4f1e115c89611883196e45
Uusuff <90629840+Uusuff@users.noreply.github.com>
2025-02-04 17:20:51 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+  let newX = e.clientX - wallRect.left - spiderRect.width / 2;
+  let newY = e.clientY - wallRect.top - spiderRect.height / 2;
+
+  newX = Math.max(0, Math.min(newX, wallRect.width - spiderRect.width));
+  newY = Math.max(0, Math.min(newY, wallRect.height - spiderRect.height));
+
+  spider.style.left = `${newX}px`;
+  spider.style.top = `${newY}px`;
 });
