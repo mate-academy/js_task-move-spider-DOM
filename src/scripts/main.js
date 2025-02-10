837236dd1c01d4b4aadf0fa179d30b2598c00309
Nadiia Bulmak <nadgaval@gmail.com>
2025-02-10 20:13:00 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
+// event.clientX/event.clientY
 
-document.addEventListener('click', (e) => {
-  // write code here
-});
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+document.addEventListener('click', moveSpider);
+
+function moveSpider(e) {
+  const wallInfo = wall.getBoundingClientRect();
+  const spiderInfo = spider.getBoundingClientRect();
+  const spiderWidthCenter = spiderInfo.width / 2;
+  const spiderHeightCenter = spiderInfo.height / 2;
+
+  const wallPosition = e.target.closest('.wall');
+
+  if (wallPosition) {
+    let newLeft = e.clientX - wallInfo.left - spiderWidthCenter;
+    let newTop = e.clientY - wallInfo.top - spiderHeightCenter;
+
+    if (newLeft < 0) {
+      newLeft = 0;
+    } else if (newLeft > wallInfo.width - spiderInfo.width) {
+      newLeft = wallInfo.width - spiderInfo.width;
+    }
+
+    if (newTop < 0) {
+      newTop = 0;
+    } else if (newTop > wallInfo.height - spiderInfo.height) {
+      newTop = wallInfo.height - spiderInfo.height;
+    }
+
+    spider.style.left = `${newLeft}px`;
+    spider.style.top = `${newTop}px`;
+  }
+}
