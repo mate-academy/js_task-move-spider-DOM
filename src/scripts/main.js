317ee455a1bb7a93fe5266670c3214aa57d5a628
Kateryna Shepel <MariyaKaterina@gmail.com>
2025-02-13 18:27:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = e.target.closest('.wall');
+
+  if (wall) {
+    const wallOffsetLeft = wall.offsetLeft + wall.clientLeft;
+    const wallOffsetTop = wall.offsetTop + wall.clientTop;
+    const coorX = e.clientX - wallOffsetLeft - spider.width / 2;
+    const coorY = e.clientY - wallOffsetTop - spider.height / 2;
+    const maxX = wall.clientWidth - spider.clientWidth;
+    const maxY = wall.clientHeight - spider.clientHeight;
+
+    const x = getCoor(coorX, maxX, 0);
+    const y = getCoor(coorY, maxY, 0);
+
+    spider.style.left = x + 'px';
+    spider.style.top = y + 'px';
+  }
 });
+
+function getCoor(coor, max, min) {
+  const result = Math.min(Math.max(min, coor), max);
+
+  return result;
+}
