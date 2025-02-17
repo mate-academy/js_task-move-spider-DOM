7b844f3de924a1d7a9b1171ff2904a3791955176
troleksii <aliosha123418@gmail.com>
2025-02-17 20:32:19 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const clickableZone = document.querySelector('.wall');
+
+clickableZone.addEventListener('click', (e) => {
+  const rect = clickableZone.getBoundingClientRect();
+
+  let x = e.clientX - rect.left - 10;
+  let y = e.clientY - rect.top - 10;
+
+  x -= spider.offsetWidth / 2;
+  y -= spider.offsetHeight / 2;
+
+  spider.style.transform = `translate(${x}px, ${y}px)`;
 });
