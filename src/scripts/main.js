6bc4d37310438a712b3fc2d7ed5fdc9d66949e8b
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-02-17 13:43:35 -0600

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const spiderEl = document.querySelector('.spider');
+const wallEl = document.querySelector('.wall');
+const halfSpider = spiderEl.clientHeight / 2;
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target.closest('.wall')) {
+    let positTop = e.offsetY;
+    let positLeft = e.offsetX;
+
+    positTop = Math.max(
+      halfSpider,
+      Math.min(positTop, wallEl.clientHeight - halfSpider),
+    );
+
+    positLeft = Math.max(
+      halfSpider,
+      Math.min(positLeft, wallEl.clientWidth - halfSpider),
+    );
+
+    spiderEl.style.top = `${positTop}px`;
+    spiderEl.style.left = `${positLeft}px`;
+    spiderEl.style.transform = 'translate(-50%, -50%)';
+  }
 });
