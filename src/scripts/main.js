579c2deb468012ae4adfd92fc3899bec9cbc535a
Leh-02 <ivanlyash51@gmail.com>
2025-03-05 22:03:56 +0200

    task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
 
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const coordX = e.clientX;
+  const coordY = e.clientY;
+
+  const spiderSize = parseInt(getComputedStyle(spider).width, 10);
+  const wallBorder = parseInt(getComputedStyle(wall).borderWidth, 10);
+
+  const wallRect = wall.getBoundingClientRect();
+
+  let vertShift = coordY - wallRect.top - wallBorder - spiderSize / 2;
+  let horizShift = coordX - wallRect.left - wallBorder - spiderSize / 2;
+
+  spider.style.top = `${vertShift}px`;
+  spider.style.left = `${horizShift}px`;
 });
+
