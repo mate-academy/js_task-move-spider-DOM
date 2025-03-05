8d0accc4ff48b68c5ac0855d7106630e6968b374
Samarash Eugene <samarasha95@gmail.com>
2025-03-05 09:45:34 +0200

    four out of 6 tasks are implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+  const spiderWidth = spiderRect.width;
+  const spiderHeight = spiderRect.height;
+
+  const wallBorder = wall.clientTop * 2;
+
+  let x = e.clientX - wallRect.left - spiderWidth / 2 - wallBorder / 2;
+  let y = e.clientY - wallRect.top - spiderHeight / 2 - wallBorder / 2;
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (x > wallRect.width - spiderWidth) {
+    x = wallRect.width - spiderHeight - wallBorder;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (y > wallRect.height - spiderHeight) {
+    y = wallRect.height - spiderHeight - wallBorder;
+  }
+
+  spider.style.top = y + 'px';
+  spider.style.left = x + 'px';
 });
