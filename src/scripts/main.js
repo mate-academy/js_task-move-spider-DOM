551eca67ac1ed751326cf228c7d47ea878ffddf4
Dima Komisarov <komissarov854@gmail.com>
2025-03-10 21:25:46 +0200

    solutin

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+  const wallRect = wall.getBoundingClientRect();
+
+  const coordX = e.clientX - wallRect.left - spiderWidth / 2;
+  const coordY = e.clientY - wallRect.top - spiderHeight / 2;
+
+  if (
+    coordX > 0 &&
+    coordX < wall.clientHeight - spiderWidth &&
+    coordY > 0 &&
+    coordY < wall.clientHeight - spiderHeight
+  ) {
+    spider.style.transform = `translate(${coordX}px, ${coordY}px)`;
+  }
 });
