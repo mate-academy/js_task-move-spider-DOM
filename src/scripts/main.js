c45aed28b2015db99c35006db81866b4146acd13
inikiforov <ievgen.nikiforov@tedata.de>
2025-03-12 17:10:38 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+
+const wallTop = wall.offsetTop;
+const wallLeft = wall.offsetLeft;
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const x = e.clientX;
+  const y = e.clientY;
+
+  let spiderX = x - wallLeft - spiderWidth / 2;
+  let spiderY = y - wallTop - spiderHeight / 2;
+
+  spiderX = Math.max(0, Math.min(spiderX, wallWidth - spiderWidth));
+  spiderY = Math.max(0, Math.min(spiderY, wallHeight - spiderHeight));
+
+  spider.style.left = `${spiderX}px`;
+  spider.style.top = `${spiderY}px`;
 });
