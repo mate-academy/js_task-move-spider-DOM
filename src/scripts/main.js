d91da0ca5107aedf35e99b379e6b9bfca7a106c6
Andrii <andreyfil1337@gmail.com>
2025-03-23 23:09:49 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-shadow */
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.pointerEvents = 'none';
+
+wall.addEventListener('click', (event) => {
+  if(event.target.matches('.spider')) {
+    return;
+  }
+
+
+  const x = Math.min(Math.max(spider.offsetWidth / 2,
+    event.offsetX), wall.clientWidth - spider.offsetWidth / 2);
+  const y = Math.min(Math.max(spider.offsetHeight / 2,
+    event.offsetY), wall.clientHeight - spider.offsetHeight / 2);
+
+  spider.style.translate = `calc(${x}px - 50%) calc(${y}px - 50%)`;
 });
