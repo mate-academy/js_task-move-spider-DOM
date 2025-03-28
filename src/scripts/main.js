fcf353c2aa79dcf8f4aee6d46c36eb8c6c8442b2
Solomiia Palka <solyaqw@gmail.com>
2025-03-28 14:08:13 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
-});
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+function moveSpider(evenT) {
+  const wallRect = wall.getBoundingClientRect();
+
+  const spiderRect = spider.getBoundingClientRect();
+  const spiderWidth = spiderRect.width;
+  const spiderHeight = spiderRect.height;
+
+  const centerX = evenT.clientX - wallRect.left - spiderWidth / 2;
+  const centerY = evenT.clientY - wallRect.top - spiderHeight / 2;
+
+  const constrainedX = Math.max(
+    0,
+    Math.min(centerX, wallRect.width - spiderWidth),
+  );
+
+  const constrainedY = Math.max(
+    0,
+    Math.min(centerY, wallRect.height - spiderHeight),
+  );
+
+  spider.style.transform = `translate(${constrainedX}px, ${constrainedY}px)`;
+}
+
+wall.addEventListener('click', moveSpider);
