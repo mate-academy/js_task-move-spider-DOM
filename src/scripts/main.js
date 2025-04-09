53095e13861444f17649bc2bee95d7b7da896f57
gabai62 <gabai62it@gmail.com>
2025-04-09 13:29:51 +0300

    spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const target = e.target;
+
+  if (target.tagName === 'DIV') {
+    const clickX = e.clientX;
+    const clickY = e.clientY;
+    const wallCoords = wall.getBoundingClientRect();
+    const spiderCoords = spider.getBoundingClientRect();
+    const borderWidth = parseFloat(window.getComputedStyle(wall).borderWidth);
+
+    if (
+      clickX >= wallCoords.left &&
+      clickX <= wallCoords.right &&
+      clickY >= wallCoords.top &&
+      clickY <= wallCoords.bottom
+    ) {
+      let newX =
+        clickX - wallCoords.left - spiderCoords.width / 2 - borderWidth;
+      let newY =
+        clickY - wallCoords.top - spiderCoords.height / 2 - borderWidth;
+
+      const minValue = 0;
+      const maxX = wallCoords.width - spiderCoords.width - borderWidth * 2;
+      const maxY = wallCoords.height - spiderCoords.height - borderWidth * 2;
+
+      newX = Math.max(minValue, Math.min(newX, maxX));
+      newY = Math.max(minValue, Math.min(newY, maxY));
+
+      spider.style.top = newY + 'px';
+      spider.style.left = newX + 'px';
+    }
+  }
 });
