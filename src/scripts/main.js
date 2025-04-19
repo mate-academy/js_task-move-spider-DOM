e96c94c730e93d0baefce41cb2159847a0ce1a08
apr1ori <vladgolenok4@gmail.com>
2025-04-19 19:25:08 +0200

    task 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const htmlHeight = document.documentElement.clientHeight;
+const htmlWidth = document.documentElement.clientWidth;
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (
+    !(e.clientY < (htmlHeight - wall.clientHeight) / 2) &&
+    e.clientY < (htmlHeight + wall.clientHeight) / 2 &&
+    !(e.clientX < (htmlWidth - wall.clientWidth) / 2) &&
+    e.clientX < (htmlWidth + wall.clientWidth) / 2
+  ) {
+    spider.style.top = `${e.clientY - (htmlHeight - wall.clientHeight) / 2 - spider.clientHeight / 2}px`;
+    spider.style.left = `${e.clientX - (htmlWidth - wall.clientWidth) / 2 - spider.clientWidth / 2}px`;
+  }
 });
