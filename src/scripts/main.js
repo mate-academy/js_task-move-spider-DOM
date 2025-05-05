6c96d878b094287d31efe4f6f98cae04f44e7d9d
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-05-05 11:32:37 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const clickZone = e.target.closest('.wall');
+
+  if (!clickZone) {
+    return;
+  }
+
+  const spiderHalfWidth = spider.offsetWidth / 2;
+  const spiderHalfHeight = spider.offsetHeight / 2;
+
+  let x = e.offsetX - spiderHalfWidth;
+  let y = e.offsetY - spiderHalfHeight;
+
+  x = Math.max(0, Math.min(x, wall.clientWidth - spider.offsetWidth));
+  y = Math.max(0, Math.min(y, wall.clientHeight - spider.offsetHeight));
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
