0ca5eeb2cc8762ca9ebe1eb432be81f4df7860a5
Alexey Kravets <kravecoleksiy@gmail.com>
2025-05-08 21:40:06 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const spiderElement = document.querySelector('.spider');
+const wallElement = document.querySelector('.wall');
+
+spiderElement.style.position = 'absolute';
+wallElement.style.position = 'relative';
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (!wallElement.contains(e.target)) {
+    return;
+  }
+
+  const spiderRect = spiderElement.getBoundingClientRect();
+  const wallRect = wallElement.getBoundingClientRect();
+  const wallBorder = parseInt(getComputedStyle(wallElement).borderWidth);
+
+  const x = e.clientX - wallRect.left - spiderRect.width / 2 - wallBorder;
+  const y = e.clientY - wallRect.top - spiderRect.height / 2 - wallBorder;
+
+  const finalX = Math.min(
+    Math.max(x, 0),
+    wallRect.width - spiderRect.width - 2 * wallBorder,
+  );
+  const finalY = Math.min(
+    Math.max(y, 0),
+    wallRect.height - spiderRect.height - 2 * wallBorder,
+  );
+
+  spiderElement.style.left = `${finalX}px`;
+  spiderElement.style.top = `${finalY}px`;
 });
