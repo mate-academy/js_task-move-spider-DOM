2da5bd3f89d10dbda15ae904100c4882d5bafdec
z3rt <z3rt@MacBook-Pro-Aleksandr.local>
2025-05-27 00:02:08 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
+  const wallEdges = wall.getBoundingClientRect();
+
+  const wallX = wallEdges.left;
+  const wallY = wallEdges.top;
+  const clientX = e.clientX;
+  const clientY = e.clientY;
+
+  let spiderX = clientX - wallX - spider.offsetWidth / 2;
+  let spiderY = clientY - wallY - spider.offsetHeight / 2;
+
+  spiderX = Math.max(
+    0,
+    Math.min(spiderX, wall.clientWidth - spider.offsetWidth),
+  );
+
+  spiderY = Math.max(
+    0,
+    Math.min(spiderY, wall.clientHeight - spider.offsetHeight),
+  );
+
+  spider.style.left = spiderX + 'px';
+  spider.style.top = spiderY + 'px';
 });
