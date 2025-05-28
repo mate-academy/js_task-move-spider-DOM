9e915215318cd32815171d1fdf8520297988f87b
combine143 <combine142@gmail.com>
2025-05-29 01:49:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,11 +7,22 @@ wall.addEventListener('click', (e) => {
   const rectWall = wall.getBoundingClientRect();
   const rectSpider = spider.getBoundingClientRect();
 
-  let x = e.clientX - rectWall.x - rectSpider.width / 2 - 10;
-  let y = e.clientY - rectWall.y - rectSpider.height / 2 - 10;
+  const wallStyles = getComputedStyle(wall);
+  const borderLeft = parseFloat(wallStyles.borderLeftWidth);
+  const borderTop = parseFloat(wallStyles.borderTopWidth);
 
-  x = Math.min(Math.max(x, 0), rectWall.width - rectSpider.width - 20);
-  y = Math.min(Math.max(y, 0), rectWall.height - rectSpider.height - 20);
+  let x = e.clientX - rectWall.x - rectSpider.width / 2 - borderLeft;
+  let y = e.clientY - rectWall.y - rectSpider.height / 2 - borderTop;
+
+  x = Math.min(
+    Math.max(x, 0),
+    rectWall.width - rectSpider.width - borderLeft * 2,
+  );
+
+  y = Math.min(
+    Math.max(y, 0),
+    rectWall.height - rectSpider.height - borderTop * 2,
+  );
 
   spider.style.left = `${x}px`;
   spider.style.top = `${y}px`;
