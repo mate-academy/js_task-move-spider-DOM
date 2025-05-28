1a5b6a00b4661a0e50c898390bed39d192f3f33e
combine143 <combine142@gmail.com>
2025-05-29 01:36:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
+  const spider = document.querySelector('.spider');
+  const rectWall = wall.getBoundingClientRect();
+  const rectSpider = spider.getBoundingClientRect();
+
+  let x = e.clientX - rectWall.x - rectSpider.width / 2 - 10;
+  let y = e.clientY - rectWall.y - rectSpider.height / 2 - 10;
+
+  x = Math.min(Math.max(x, 0), rectWall.width - rectSpider.width - 20);
+  y = Math.min(Math.max(y, 0), rectWall.height - rectSpider.height - 20);
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
