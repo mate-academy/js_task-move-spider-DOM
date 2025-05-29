ca0d4839595894952c42987153832a22aebf4565
Taras Holotiuk <taras.holotiuk@gmail.com>
2025-05-29 15:30:11 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallX = wall.offsetLeft;
+  const wallY = wall.offsetTop;
+  const wallBorderWidth = parseInt(getComputedStyle(wall).borderWidth, 10);
+  const clickX = e.pageX;
+  const clickY = e.pageY;
+
+  spider.style.position = 'absolute';
+  spider.style.left = `${clickX - wallX - wallBorderWidth - spider.width / 2}px`;
+  spider.style.top = `${clickY - wallY - wallBorderWidth - spider.height / 2}px`;
 });
