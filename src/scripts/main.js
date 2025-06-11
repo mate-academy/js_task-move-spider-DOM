e7aac769bb0b8402489cda28ec259d9c25fba9d3
Liudmyla Panchenko <liudmyla.panchenko29@gmail.com>
2025-06-11 18:03:10 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (!wall.contains(e.target)) {
+    return;
+  }
+
+  const x = e.offsetX - spider.offsetWidth / 2;
+  const y = e.offsetY - spider.offsetHeight / 2;
+
+  const left = Math.max(0, Math.min(wall.clientWidth - spider.offsetWidth, x));
+  const t = Math.max(0, Math.min(wall.clientHeight - spider.offsetHeight, y));
+
+  spider.style.left = left + 'px';
+  spider.style.top = t + 'px';
 });
