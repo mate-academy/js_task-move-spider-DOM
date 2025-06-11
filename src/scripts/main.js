e9203cb9b817ccca1cee401057ed69ba40d79f42
Sofiia13 <sophiyastanish@gmail.com>
2025-06-11 12:29:32 +0300

    fix some bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,17 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', ()  => {
-  const wall= document.querySelector('.wall');
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
 
   wall.style.position = 'relative';
   spider.style.position = 'absolute';
-  
+
   wall.addEventListener('click', (e) => {
     const wallMeasurements = wall.getBoundingClientRect();
 
-    if (e.clientX < wallMeasurements.left ||
+    if (
+      e.clientX < wallMeasurements.left ||
       e.clientX > wallMeasurements.right ||
       e.clientY < wallMeasurements.top ||
       e.clientY > wallMeasurements.bottom
@@ -21,22 +22,19 @@ document.addEventListener('DOMContentLoaded', ()  => {
     const spiderWidth = spider.offsetWidth;
     const spiderHeight = spider.offsetHeight;
 
-    let coordXRelativeToWall = e.clientX - wallMeasurements.left;
-    let coordYRelativeToWall = e.clientY - wallMeasurements.top;
+    const coordXRelativeToWall = e.clientX - wallMeasurements.left;
+    const coordYRelativeToWall = e.clientY - wallMeasurements.top;
 
-    let desiredTop = coordYRelativeToWall - spiderHeight / 2;
-    let desiredLeft = coordXRelativeToWall - spiderWidth / 2;
+    const desiredTop = coordYRelativeToWall - spiderHeight / 2;
+    const desiredLeft = coordXRelativeToWall - spiderWidth / 2;
 
-    const maxTop = wallMeasurements.height - spiderHeight;
-    const maxLeft = wallMeasurements.width - spiderWidth;
+    const maxTop = wall.clientHeight - spiderHeight;
+    const maxLeft = wall.clientWidth - spiderWidth;
 
     const finalTop = Math.max(0, Math.min(desiredTop, maxTop));
     const finalLeft = Math.max(0, Math.min(desiredLeft, maxLeft));
 
     spider.style.top = finalTop + 'px';
     spider.style.left = finalLeft + 'px';
-  })
-
-  
-
+  });
 });
