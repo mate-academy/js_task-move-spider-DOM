34e3cbd33b518d59517769cd2241db9ccb07a18a
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-06-19 23:45:47 +0300

    fixed main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,36 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  const wall = document.querySelector('.wall');
-  const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
 
-  const wallRect = wall.getBoundingClientRect();
-  const spiderRect = spider.getBoundingClientRect();
+if (wall && spider) {
+  wall.addEventListener('click', (e) => {
+    const wallRect = wall.getBoundingClientRect();
+    const spiderRect = spider.getBoundingClientRect();
 
-  const clickX = e.clientX;
-  const clickY = e.clientY;
+    const clickX = e.clientX;
+    const clickY = e.clientY;
 
-  let leftPos = clickX - wallRect.left - spiderRect.width / 2;
-  let topPos = clickY - wallRect.top - spiderRect.height / 2;
+    let leftPos = clickX - wallRect.left - spiderRect.width / 2;
+    let topPos = clickY - wallRect.top - spiderRect.height / 2;
 
-  if (leftPos < 0) {
-    leftPos = 0;
-  }
+    if (leftPos < 0) {
+      leftPos = 0;
+    }
 
-  if (topPos < 0) {
-    topPos = 0;
-  }
+    if (topPos < 0) {
+      topPos = 0;
+    }
 
-  if (leftPos + spiderRect.width > wallRect.width) {
-    leftPos = wallRect.width - spiderRect.width;
-  }
+    if (leftPos + spiderRect.width > wallRect.width) {
+      leftPos = wallRect.width - spiderRect.width;
+    }
 
-  if (topPos + spiderRect.height > wallRect.height) {
-    topPos = wallRect.height - spiderRect.height;
-  }
+    if (topPos + spiderRect.height > wallRect.height) {
+      topPos = wallRect.height - spiderRect.height;
+    }
 
-  spider.style.left = `${leftPos}px`;
-  spider.style.top = `${topPos}px`;
-});
+    spider.style.left = `${leftPos}px`;
+    spider.style.top = `${topPos}px`;
+  });
+}
