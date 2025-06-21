1369eeb0bf72ea4ca2c674a3475935e0079743bd
YuliaSabitova <yuliasab2017@mail.com>
2025-06-21 19:25:57 +0300

    add corrected task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,39 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  const wall = document.querySelector('.wall');
-  const spider = document.querySelector('.spider');
+  const wallSize = wall.getBoundingClientRect();
+  const spiderSize = spider.getBoundingClientRect();
+  const clickInWall = e.target.closest('.wall');
 
-  const halfSpiderWidth = spider.offsetWidth / 2;
-  const halfSpiderHeight = spider.offsetHeight / 2;
-
-  spider.style.transform = 'translate(-50%, -50%)';
-  spider.style.left = `${halfSpiderWidth}px`;
-  spider.style.top = `${halfSpiderHeight}px`;
-
-  if (wall.contains(e.target)) {
-    const wallRect = wall.getBoundingClientRect();
-    const clickXRelative = e.clientX - wallRect.left - wall.clientLeft;
-    const clickYRelative = e.clientY - wallRect.top - wall.clientTop;
-    const wallContentWidth = wall.clientWidth;
-    const wallContentHeight = wall.clientHeight;
-    const limitedX = Math.max(
-      halfSpiderWidth,
-      Math.min(clickXRelative, wallContentWidth - halfSpiderWidth),
-    );
-    const limitedY = Math.max(
-      halfSpiderHeight,
-      Math.min(clickYRelative, wallContentHeight - halfSpiderHeight),
-    );
-
-    spider.style.left = `${limitedX}px`;
-    spider.style.top = `${limitedY}px`;
+  if (!clickInWall) {
+    return;
   }
+
+  const border = wall.clientTop;
+  const halfSpiderWidth = spiderSize.width / 2;
+  const halfSpiderHeight = spiderSize.height / 2;
+  let positionX = e.clientX - wallSize.left - halfSpiderWidth - border;
+  let positionY = e.clientY - wallSize.top - halfSpiderHeight - border;
+
+  if (positionX < 0) {
+    positionX = 0;
+  }
+
+  if (positionY < 0) {
+    positionY = 0;
+  }
+
+  if (positionX + spiderSize.width >= wallSize.width) {
+    positionX = wallSize.width - spiderSize.width - border * 2;
+  }
+
+  if (positionY + spiderSize.height >= wallSize.height) {
+    positionY = wallSize.height - spiderSize.height - border * 2;
+  }
+
+  spider.style.left = `${positionX}px`;
+  spider.style.top = `${positionY}px`;
 });
