5f3e176866ee60a06cefe80b3c7d2d4e6d44b841
Diana Berezan <berezandiana24@gmail.com>
2025-06-28 11:49:56 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,14 +16,13 @@ document.addEventListener('click', (e) => {
     return;
   }
 
-  let newLeft =
-    e.clientX - wallRect.left - wall.clientLeft - spiderRect.width / 2;
-  let newTop =
-    e.clientY - wallRect.top - wall.clientTop - spiderRect.height / 2;
+  let newLeft = e.clientX - wallRect.left - spiderRect.width / 2;
+  let newTop = e.clientY - wallRect.top - spiderRect.height / 2;
 
   newLeft = Math.max(0, Math.min(newLeft, wall.clientWidth - spiderRect.width));
   newTop = Math.max(0, Math.min(newTop, wall.clientHeight - spiderRect.height));
 
+  spider.style.position = 'absolute';
   spider.style.left = `${newLeft}px`;
   spider.style.top = `${newTop}px`;
 });
