615807aa8a13e013c659c115844bdf7a2cd6480f
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-07-31 23:08:42 +0300

    spider on wall

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('img');
+const wall = document.querySelector('.wall');
+
+let move = false;
+
+function moveSpider(x, y) {
+  const wallRect = wall.getBoundingClientRect();
+  const spiderW = spider.offsetWidth + 20;
+  const spiderH = spider.offsetHeight + 20;
+
+  let spiderX = x - wallRect.left - 35;
+  let spiderY = y - wallRect.top - 35;
+
+  spiderX = Math.max(0, Math.min(spiderX, wallRect.width - spiderW));
+  spiderY = Math.max(0, Math.min(spiderY, wallRect.height - spiderH));
+
+  spider.style.left = `${spiderX}px`;
+  spider.style.top = `${spiderY}px`;
+}
+
+wall.addEventListener('click', (e) => {
+  moveSpider(e.clientX, e.clientY);
+
+  if (move) {
+    return;
+  }
+  move = true;
+
+  wall.addEventListener('mousemove', (el) => {
+    moveSpider(el.clientX, el.clientY);
+  });
 });
