db84b9a78fc717ea7dcb2ef1649e7ef4d89f574b
tvk777 <klimenko.tv7@gmail.com>
2025-08-14 18:10:14 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,35 @@
 /* eslint no-console: ["error", { allow: ["warn", "log"] }] */
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  const wallCoords = wall.getBoundingClientRect();
+  const spiderCoords = {
+    x: e.clientX - wallCoords.left - wall.clientLeft - spider.offsetWidth / 2,
+    y: e.clientY - wallCoords.top - wall.clientTop - spider.offsetHeight / 2,
+  };
+
+  if (spiderCoords.x < 0) {
+    spiderCoords.x = 0;
+  }
+
+  if (spiderCoords.y < 0) {
+    spiderCoords.y = 0;
+  }
+
+  if (spiderCoords.x + spider.clientWidth > wall.clientWidth) {
+    spiderCoords.x = wall.clientWidth - spider.clientWidth;
+  }
+
+  if (spiderCoords.y + spider.clientHeight > wall.clientHeight) {
+    spiderCoords.y = wall.clientHeight - spider.clientHeight;
+  }
+
+  spider.style.left = spiderCoords.x + 'px';
+  spider.style.top = spiderCoords.y + 'px';
 });
