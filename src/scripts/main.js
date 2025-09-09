3e03cbf334ef4595304cfdd5978ae6c10e3c8ab0
Vasyliev Vadym <protocol222333@gmail.com>
2025-09-09 21:33:00 +0300

    temp: bypass failing tests for deployment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const spiderSize = parseInt(getComputedStyle(spider).height, 10);
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target === wall) {
+    const wallRect = wall.getBoundingClientRect();
+    const x = e.clientX - wallRect.left - spiderSize / 2;
+    const y = e.clientY - wallRect.top - spiderSize / 2;
+
+    const limitedX = Math.min(
+      Math.max(x, 0),
+      wall.clientWidth - spider.clientWidth,
+    );
+
+    const limitedY = Math.min(
+      Math.max(y, 0),
+      wall.clientHeight - spider.clientHeight,
+    );
+
+    spider.style.left = limitedX + 'px';
+    spider.style.top = limitedY + 'px';
+  }
 });
