bef874bdcd1f9315d67e7e6aada0ba5d67f26398
Vasyliev Vadym <protocol222333@gmail.com>
2025-09-11 11:42:29 +0300

    fix: some fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,26 +2,25 @@
 
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
-const spiderSize = parseInt(getComputedStyle(spider).height, 10);
-const borderSize = parseInt(getComputedStyle(wall).border, 10);
 
-document.addEventListener('click', (e) => {
-  if (e.target === wall) {
-    const wallRect = wall.getBoundingClientRect();
-    const x = e.clientX - wallRect.left - spiderSize / 2 - borderSize;
-    const y = e.clientY - wallRect.top - spiderSize / 2 - borderSize;
+wall.addEventListener('click', (e) => {
+  const wallRect = wall.getBoundingClientRect();
+  const spiderSize = spider.getBoundingClientRect().height;
+  const borderSize = (wallRect.width - wall.clientWidth) / 2;
 
-    const limitedX = Math.min(
-      Math.max(x, 0),
-      wall.clientWidth - spider.clientWidth,
-    );
+  const x = e.clientX - wallRect.left - spiderSize / 2 - borderSize;
+  const y = e.clientY - wallRect.top - spiderSize / 2 - borderSize;
 
-    const limitedY = Math.min(
-      Math.max(y, 0),
-      wall.clientHeight - spider.clientHeight,
-    );
+  const limitedX = Math.min(
+    Math.max(x, 0),
+    wall.clientWidth - spider.clientWidth,
+  );
 
-    spider.style.left = limitedX + 'px';
-    spider.style.top = limitedY + 'px';
-  }
+  const limitedY = Math.min(
+    Math.max(y, 0),
+    wall.clientHeight - spider.clientHeight,
+  );
+
+  spider.style.left = limitedX + 'px';
+  spider.style.top = limitedY + 'px';
 });
