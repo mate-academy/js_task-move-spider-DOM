743c66065cec162051d42af004d5819c032c229c
Bohdan Pidhrebelnyi <bodiiiiic@gmail.com>
2025-10-11 15:57:28 +0100

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,19 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
+wall.style.position = 'relative';
+spider.style.position = 'absolute';
+
 wall.addEventListener('click', (e) => {
   const spiderWidth = spider.offsetWidth;
   const spiderHeight = spider.offsetHeight;
-
   const wallWidth = wall.clientWidth;
   const wallHeight = wall.clientHeight;
-
   let newLeft = e.offsetX - spiderWidth / 2;
   let newTop = e.offsetY - spiderHeight / 2;
 
   newLeft = Math.max(0, Math.min(newLeft, wallWidth - spiderWidth));
   newTop = Math.max(0, Math.min(newTop, wallHeight - spiderHeight));
-
-  spider.style.position = 'absolute';
   spider.style.left = `${newLeft}px`;
   spider.style.top = `${newTop}px`;
 });
