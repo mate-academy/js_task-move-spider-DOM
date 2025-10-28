bdf12bce8286d7eb084943fbba1ae379d5857455
wzfxc <maksohrimenko1@gmail.com>
2025-10-28 15:18:22 +0200

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
+  const x = Math.max(
+    0,
+    Math.min(
+      e.offsetX - spider.offsetWidth / 2,
+      wall.clientWidth - spider.offsetWidth,
+    ),
+  );
+
+  const y = Math.max(
+    0,
+    Math.min(
+      e.offsetY - spider.offsetHeight / 2,
+      wall.clientHeight - spider.offsetHeight,
+    ),
+  );
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
