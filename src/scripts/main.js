ea4e9068c3d480aad0707cb98ff06d3b162f5fed
Sofiia <sofiiamuz@gmail.com>
2025-11-14 23:14:14 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,49 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('div.wall');
+  const spider = document.querySelector('img.spider');
+
+  const details = wall.getBoundingClientRect();
+
+  const clickX = e.clientX;
+  const clickY = e.clientY;
+
+  const isOutside =
+    clickX < details.left ||
+    clickX > details.right ||
+    clickY < details.top ||
+    clickY > details.bottom;
+
+  if (isOutside) {
+    return;
+  }
+
+  let x = e.clientX - details.left - wall.clientLeft;
+  let y = e.clientY - details.top - wall.clientTop;
+
+  x = x - spider.offsetWidth / 2;
+  y = y - spider.offsetHeight / 2;
+
+  const maxX = wall.clientWidth - spider.offsetWidth;
+  const maxY = wall.clientHeight - spider.offsetHeight;
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (x > maxX) {
+    x = maxX;
+  }
+
+  if (y > maxY) {
+    y = maxY;
+  }
+
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
 });
