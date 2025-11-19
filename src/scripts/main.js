1b36b69c93b519869e29b24bc68a5f58c37d8b84
Andrey <inokatre08@gmail.com>
2025-11-19 20:57:55 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.position = 'relative';
+
+wall.addEventListener('click', (e) => {
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+  const clickX = e.clientX - wall.getBoundingClientRect().left;
+  const clickY = e.clientY - wall.getBoundingClientRect().top;
+
+  let left = clickX - spiderWidth / 2;
+  let Mytop = clickY - spiderHeight / 2;
+
+  if (left < 0) {
+    left = 0;
+  }
+
+  if (Mytop < 0) {
+    Mytop = 0;
+  }
+
+  if (left > wallWidth - spiderWidth) {
+    left = wallWidth - spiderWidth;
+  }
+
+  if (Mytop > wallHeight - spiderHeight) {
+    Mytop = wallHeight - spiderHeight;
+  }
+
+  spider.style.position = 'absolute';
+  spider.style.left = `${left}px`;
+  spider.style.top = `${Mytop}px`;
 });
