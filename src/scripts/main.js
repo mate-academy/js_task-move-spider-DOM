9f783f20e477eef4036a363e906e6a309cb0d0f9
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-29 23:11:12 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const rect = wall.getBoundingClientRect();
+const borderTop = parseFloat(getComputedStyle(wall).borderTopWidth);
+const borderLeft = parseFloat(getComputedStyle(wall).borderLeftWidth);
+
 document.addEventListener('click', (e) => {
   // write code here
+
+  const spiderObj = spider.getBoundingClientRect();
+  let topCoords = e.clientY - rect.top - borderTop - spiderObj.height / 2;
+  let leftCoords = e.clientX - rect.left - borderLeft - spiderObj.width / 2;
+
+  const maxTop = wall.clientHeight - spiderObj.height;
+  const maxLeft = wall.clientWidth - spiderObj.width;
+
+  topCoords = Math.max(0, Math.min(topCoords, maxTop));
+  leftCoords = Math.max(0, Math.min(leftCoords, maxLeft));
+
+  spider.style.top = topCoords + 'px';
+  spider.style.left = leftCoords + 'px';
 });
