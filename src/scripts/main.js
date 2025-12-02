40950ced04b48a15fe9de85faeb71e7f69779647
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2025-12-02 16:01:26 +0200

    Add tsk solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
   // write code here
+  const spiderRec = spider.getBoundingClientRect();
+  const wallRec = wall.getBoundingClientRect();
+  const clientX = e.clientX;
+  const clientY = e.clientY;
+
+  const computedLeft = clientX - wallRec.left - spiderRec.width / 2;
+  const computedTop = clientY - wallRec.top - spiderRec.height / 2;
+
+  const minLocalX = 0;
+  const maxLocalX = wall.clientWidth - spiderRec.width;
+  const clampedX = Math.min(Math.max(computedLeft, minLocalX), maxLocalX);
+
+  const minLocalY = 0;
+  const maxLocalY = wall.clientHeight - spiderRec.height;
+  const clampedY = Math.min(Math.max(computedTop, minLocalY), maxLocalY);
+
+  spider.style.left = `${clampedX}px`;
+  spider.style.top = `${clampedY}px`;
 });
