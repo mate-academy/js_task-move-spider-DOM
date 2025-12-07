e79fa4526e7096f3cf529d17dc7dbd624281bd8e
Alex <1inboxna@gmail.com>
2025-12-07 16:40:39 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,40 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const rect = wall.getBoundingClientRect();
+  const coordinatesTop = e.clientY - rect.top;
+  const coordinatesLeft = e.clientX - rect.left;
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  const half = spiderWidth / 2;
+
+  const wallWidth = wall.offsetWidth;
+  const wallHeight = wall.offsetHeight;
+
+  let newTop = coordinatesTop - half;
+  let newLeft = coordinatesLeft - half;
+
+  if (newTop < 0) {
+    newTop = 0;
+  }
+
+  if (newTop > wallHeight - spiderHeight) {
+    newTop = wallHeight - spiderHeight;
+  }
+
+  if (newLeft < 0) {
+    newLeft = 0;
+  }
+
+  if (newLeft > wallWidth - spiderWidth) {
+    newLeft = wallWidth - spiderWidth;
+  }
+
+  spider.style.top = newTop + 'px';
+  spider.style.left = newLeft + 'px';
 });
