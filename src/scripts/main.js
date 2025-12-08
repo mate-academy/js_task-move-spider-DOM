edf4090b32a635e2dfbebf469a1475501c6172c9
Maksym Halushko <gallikillag@gmail.com>
2025-12-08 16:51:11 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,31 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  const wallRect = wall.getBoundingClientRect();
+  let spiderLeft = x - wallRect.left - wall.clientLeft - spider.offsetWidth / 2;
+  let spiderTop = y - wallRect.top - wall.clientTop - spider.offsetHeight / 2;
+
+  if (spiderLeft < 0) {
+    spiderLeft = 0;
+  }
+
+  if (spiderTop < 0) {
+    spiderTop = 0;
+  }
+
+  if (spiderLeft + spider.clientWidth > wall.clientWidth) {
+    spiderLeft = wall.clientWidth - spider.clientWidth;
+  }
+
+  if (spiderTop + spider.clientHeight > wall.clientHeight) {
+    spiderTop = wall.clientHeight - spider.clientHeight;
+  }
+
+  spider.style.left = spiderLeft + 'px';
+  spider.style.top = spiderTop + 'px';
 });
