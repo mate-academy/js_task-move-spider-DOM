6180cc079b8935174281682ee86623c0e632b202
SageUA <panchyshyn33@gmail.com>
2025-12-10 15:51:01 +0200

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,16 @@ document.addEventListener('click', (e) => {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
   const wallRect = wall.getBoundingClientRect();
+
   const spiderHalfWidth = spider.offsetWidth / 2;
   const spiderHalfHeight = spider.offsetHeight / 2;
-  const spiderCenterWidth = e.offsetX - spiderHalfWidth;
-  const spiderCenterHeight = e.offsetY - spiderHalfHeight;
+
+  const clickXinsideWall = e.clientX - wallRect.left - wall.clientLeft;
+  const clickYinsideWall = e.clientY - wallRect.top - wall.clientTop;
+
+  const spiderCenterWidth = clickXinsideWall - spiderHalfWidth;
+  const spiderCenterHeight = clickYinsideWall - spiderHalfHeight;
+
   const limitWallWidth = wall.clientWidth - spider.offsetWidth;
   const limitWallHeight = wall.clientHeight - spider.offsetHeight;
 
@@ -22,5 +28,5 @@ document.addEventListener('click', (e) => {
   const finalX = Math.max(0, Math.min(spiderCenterWidth, limitWallWidth));
   const finalY = Math.max(0, Math.min(spiderCenterHeight, limitWallHeight));
 
-  spider.style.transform = `translateX(${finalX}px) translateY(${finalY}px)`;
+  spider.style.transform = `translate(${finalX}px, ${finalY}px)`;
 });
