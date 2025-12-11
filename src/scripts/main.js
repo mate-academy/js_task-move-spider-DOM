69e997b5460637aef65c62b075e817b6747c0df9
Inna Haichenia <gajchenja2424@ukr.net>
2025-12-11 23:40:52 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', function (e) {
+  const wallRect = wall.getBoundingClientRect();
+  const BORDER_WIDTH = 10;
+  const FULL_COMPENSATION = 20;
+
+  const desiredX = e.clientX - wallRect.left - BORDER_WIDTH;
+  const desiredY = e.clientY - wallRect.top - BORDER_WIDTH;
+
+  const spiderRect = spider.getBoundingClientRect();
+  const spiderWidth = spiderRect.width;
+  const spiderHeight = spiderRect.height;
+
+  const minX = spiderWidth / 2;
+  const maxX = wallRect.width - FULL_COMPENSATION - spiderWidth / 2;
+
+  const minY = spiderHeight / 2;
+  const maxY = wallRect.height - FULL_COMPENSATION - spiderHeight / 2;
+
+  const finalX = Math.max(minX, Math.min(maxX, desiredX));
+  const finalY = Math.max(minY, Math.min(maxY, desiredY));
+
+  const cssLeft = finalX - spiderWidth / 2;
+  const cssTop = finalY - spiderHeight / 2;
+
+  spider.style.position = 'absolute';
+  spider.style.left = cssLeft + 'px';
+  spider.style.top = cssTop + 'px';
 });
