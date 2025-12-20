46f89fd39fe1ced16b5a814984b23060b416bb59
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2025-12-20 13:35:58 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,19 @@ document.addEventListener('click', (e) => {
     return;
   }
 
+  const wallRect = wall.getBoundingClientRect();
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  let y = e.clientY - spiderHeight / 2;
+  let x = e.clientX - spiderWidth / 2;
+
+  y = Math.max(wallRect.top, Math.min(y, wallRect.bottom - spiderHeight));
+
+  x = Math.max(wallRect.left, Math.min(x, wallRect.right - spiderWidth));
+
   spider.style.position = 'fixed';
-  spider.style.top = `${e.clientY - spider.offsetHeight / 2}px`;
-  spider.style.left = `${e.clientX - spider.offsetWidth / 2}px`;
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });
