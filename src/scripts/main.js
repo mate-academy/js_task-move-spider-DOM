9d367ccf4ec04afdb59097776c3613d9eea22acc
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2025-12-20 12:36:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = e.target.closest('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (!wall) {
+    return;
+  }
+
+  spider.style.position = 'fixed';
+  spider.style.top = `${e.clientY - spider.offsetHeight / 2}px`;
+  spider.style.left = `${e.clientX - spider.offsetWidth / 2}px`;
 });
