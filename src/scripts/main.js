9ba055e500642c80c7d73fe867f0ad026927700a
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2025-12-21 19:42:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const target = e.target.closest('.wall');
+  const spider = document.querySelector('.spider');
+  const border = 10;
+
+  if (target) {
+    const correction = target.getBoundingClientRect();
+    let y = e.clientY - correction.top - border - spider.offsetHeight / 2;
+    let x = e.clientX - correction.left - border - spider.offsetWidth / 2;
+
+    y = Math.max(0, Math.min(y, target.clientHeight - spider.clientHeight));
+    x = Math.max(0, Math.min(x, target.clientWidth - spider.clientWidth));
+
+    spider.style.top = y + 'px';
+    spider.style.left = x + 'px';
+  }
 });
