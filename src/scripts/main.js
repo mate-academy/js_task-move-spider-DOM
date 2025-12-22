8415f3da41e0f8b9d31f76291e393387d61f9b24
Dmytro <dimusgamer2.0@gmail.com>
2025-12-22 15:20:59 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = e.target.closest('.wall');
+
+  if (!wall) {
+    return;
+  }
+
+  const styles = window.getComputedStyle(wall);
+  const wallBounds = wall.getBoundingClientRect();
+
+  const borderLeft = parseInt(styles.borderLeftWidth);
+  const borderTop = parseInt(styles.borderTopWidth);
+
+  let y = e.clientY - wallBounds.top - borderTop - spider.offsetHeight / 2;
+  let x = e.clientX - wallBounds.left - borderLeft - spider.offsetWidth / 2;
+
+  y = Math.max(0, Math.min(y, wall.clientHeight - spider.clientHeight));
+  x = Math.max(0, Math.min(x, wall.clientWidth - spider.clientWidth));
+
+  spider.style.top = y + 'px';
+  spider.style.left = x + 'px';
 });
