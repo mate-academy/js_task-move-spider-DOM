95c8c25629695a4428194f1ba49b8ec90c17f8e5
Dima Sidenko <sidenkodima2202@gmail.com>
2026-01-06 17:01:41 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (e) => {
+  let distanceToLeft =
+    e.clientX - wall.offsetLeft - wall.clientLeft - spider.offsetWidth / 2;
+  let distanceToTop =
+    e.clientY - wall.offsetTop - wall.clientTop - spider.offsetWidth / 2;
+
+  if (distanceToLeft < 0) {
+    distanceToLeft = 0;
+  }
+
+  if (distanceToLeft > wall.clientWidth - spider.offsetWidth) {
+    distanceToLeft = wall.clientWidth - spider.offsetWidth;
+  }
+
+  if (distanceToTop < 0) {
+    distanceToTop = 0;
+  }
+
+  if (distanceToTop > wall.clientWidth - spider.offsetWidth) {
+    distanceToTop = wall.clientWidth - spider.offsetWidth;
+  }
+
+  spider.style.left = distanceToLeft + 'px';
+  spider.style.top = distanceToTop + 'px';
 });
