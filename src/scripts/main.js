9320eb227d991e127884c83613149f69ef450a94
Vlad Chudin <chudintttks@gmail.com>
2026-01-26 11:43:51 +0100

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const spiderRect = spider.getBoundingClientRect();
+  const wallRect = wall.getBoundingClientRect();
+  const xRelative = e.pageX - (wallRect.left + window.scrollX);
+  const yRelative = e.pageY - (wallRect.top + window.scrollY);
+  let x = xRelative - spiderRect.width / 2;
+  let y = yRelative - spiderRect.height / 2;
+
+  x = Math.max(0, Math.min(x, wallRect.width - spiderRect.width));
+  y = Math.max(0, Math.min(y, wallRect.height - spiderRect.height));
+
+  spider.style.top = y + 'px';
+  spider.style.left = x + 'px';
 });
