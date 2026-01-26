310c0bc6f0f5dd75f1a449faeb5d45d8afb0f181
Andriy <andriit35@gmail.com>
2026-01-26 18:03:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const width = wall.clientWidth - spider.offsetWidth;
+const height = wall.clientHeight - spider.offsetHeight;
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target.classList.contains('wall')) {
+    spider.style.left = `${Math.max(0, Math.min(width, e.offsetX - spider.offsetWidth / 2))}px`;
+    spider.style.top = `${Math.max(0, Math.min(height, e.offsetY - spider.offsetHeight / 2))}px`;
+  }
 });
