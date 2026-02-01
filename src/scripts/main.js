8147722bea0d3aac66a2961e98eebb90353aba99
Adrian Holka <goholka1997@gmail.com>
2026-02-01 16:40:06 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  const wallRect = wall.getBoundingClientRect();
+
+  // klik poza ścianą → nic nie rób
+  if (
+    e.clientX < wallRect.left ||
+    e.clientX > wallRect.right ||
+    e.clientY < wallRect.top ||
+    e.clientY > wallRect.bottom
+  ) {
+    return;
+  }
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  let left = e.clientX - wallRect.left - spiderWidth / 2;
+  let topPosition = e.clientY - wallRect.top - spiderHeight / 2;
+
+  left = Math.max(0, Math.min(left, wall.offsetWidth - spiderWidth));
+
+  topPosition = Math.max(
+    0,
+    Math.min(topPosition, wall.offsetHeight - spiderHeight),
+  );
+
+  spider.style.left = `${left}px`;
+  spider.style.top = `${topPosition}px`;
 });
