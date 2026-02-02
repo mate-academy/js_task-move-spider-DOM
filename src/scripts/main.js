46c6efb4dfc58a33fc0afa3882e2c5c0ca67727c
Oleksandr Ratseburzhynskyi <ratseburzhynskyi@gmail.com>
2026-02-02 17:35:13 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.position = 'relative';
+spider.style.position = 'absolute';
+
+wall.addEventListener('click', (e) => {
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  let leftPos = e.offsetX - spiderWidth / 2;
+  let topPos = e.offsetY - spiderHeight / 2;
+
+  leftPos = Math.max(0, Math.min(leftPos, wall.clientWidth - spiderWidth));
+  topPos = Math.max(0, Math.min(topPos, wall.clientHeight - spiderHeight));
+
+  spider.style.left = leftPos + 'px';
+  spider.style.top = topPos + 'px';
 });
