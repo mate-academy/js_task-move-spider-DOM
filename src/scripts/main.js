560c26ba638ebb03b8a8005f4a67da7d09eb4ffc
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-02-14 20:11:22 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,22 @@
 'use strict';
 
-const spider = document.querySelector('.spider');
-const wall = document.querySelector('.wall');
-
 document.addEventListener('click', (e) => {
-  if (!e.target.closest('.wall')) {
+  const currentWall = e.target.closest('.wall')
+
+  if (!currentWall) {
     return;
   }
 
-  const wallRect = wall.getBoundingClientRect();
+  const spider = currentWall.querySelector('.spider');
+
+  const wallRect = currentWall.getBoundingClientRect();
   const spiderRect = spider.getBoundingClientRect();
 
-  const spiderWidht = spiderRect.width;
+  const spiderWidth = spiderRect.width;
   const spiderHeight = spiderRect.height;
 
-  const requiredTop =
-    e.clientY - wallRect.top - spiderHeight / 2;
-  const requiredLeft =
-    e.clientX - wallRect.left - spiderWidht / 2;
+  const requiredTop = e.clientY - wallRect.top - spiderHeight / 2;
+  const requiredLeft = e.clientX - wallRect.left - spiderWidth / 2;
 
   const finalTop = Math.max(
     0,
@@ -25,7 +24,7 @@ document.addEventListener('click', (e) => {
   );
   const finalLeft = Math.max(
     0,
-    Math.min(wallRect.width - spiderWidht, requiredLeft),
+    Math.min(wallRect.width - spiderWidth, requiredLeft),
   );
 
   spider.style.top = `${finalTop}px`;
