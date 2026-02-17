eccb7d5524eda3eb6cb035602c268d5ccbbde15c
freelinex <freelinex7@gmail.com>
2026-02-17 23:00:32 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,44 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = wall.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wallPos = wall.getBoundingClientRect();
+
+  if (
+    e.clientX < wallPos.left ||
+    e.clientX > wallPos.right ||
+    e.clientY < wallPos.top ||
+    e.clientY > wallPos.bottom
+  ) {
+    return;
+  }
+
+  const borderLeft = wall.clientLeft;
+  const borderTop = wall.clientTop;
+
+  let left = e.clientX - wallPos.left - spider.offsetWidth / 2 - borderLeft;
+  let topPos = e.clientY - wallPos.top - spider.offsetHeight / 2 - borderTop;
+
+  const maxLeft = wall.clientWidth - spider.clientWidth;
+  const maxTop = wall.clientHeight - spider.clientHeight;
+
+  if (left < 0) {
+    left = 0;
+  }
+
+  if (left > maxLeft) {
+    left = maxLeft;
+  }
+
+  if (topPos < 0) {
+    topPos = 0;
+  }
+
+  if (topPos > maxTop) {
+    topPos = maxTop;
+  }
+  spider.style.left = left + 'px';
+  spider.style.top = topPos + 'px';
 });
