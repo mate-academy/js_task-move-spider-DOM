8c2f51e1c153d5aabf47b0723c5f31076eccc385
Anastasiia Tkachenko <nastya240509@gmail.com>
2026-02-19 23:32:10 +0200

    final solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,48 +1,80 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
-
 document.addEventListener('click', (e) => {
-  if (!wall || !spider) {
-    return;
-  }
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-  const wallPos = wall.getBoundingClientRect();
+  const wallRect = wall.getBoundingClientRect();
 
   if (
-    e.clientX < wallPos.left ||
-    e.clientX > wallPos.right ||
-    e.clientY < wallPos.top ||
-    e.clientY > wallPos.bottom
+    e.clientX < wallRect.left ||
+    e.clientX > wallRect.right ||
+    e.clientY < wallRect.top ||
+    e.clientY > wallRect.bottom
   ) {
     return;
   }
 
-  const borderLeft = wall.clientLeft;
-  const borderTop = wall.clientTop;
+  let x = e.clientX - wallRect.left - wall.clientLeft - spider.clientWidth / 2;
 
-  let left = e.clientX - wallPos.left - spider.offsetWidth / 2 - borderLeft;
-  let topPos = e.clientY - wallPos.top - spider.offsetHeight / 2 - borderTop;
+  let y = e.clientY - wallRect.top - wall.clientTop - spider.clientHeight / 2;
 
-  const maxLeft = wall.clientWidth - spider.offsetWidth;
-  const maxTop = wall.clientHeight - spider.offsetHeight;
-
-  if (left < 0) {
-    left = 0;
+  if (x < 0) {
+    x = 0;
   }
 
-  if (left > maxLeft) {
-    left = maxLeft;
+  if (y < 0) {
+    y = 0;
   }
 
-  if (topPos < 0) {
-    topPos = 0;
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const maxY = wall.clientHeight - spider.clientHeight;
+
+  if (x > maxX) {
+    x = maxX;
   }
 
-  if (topPos > maxTop) {
-    topPos = maxTop;
+  if (y > maxY) {
+    y = maxY;
   }
-  spider.style.left = left + 'px';
-  spider.style.top = topPos + 'px';
+
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
 });
+
+// const wallPos = wall.getBoundingClientRect();
+
+// if (
+//   e.clientX < wallPos.left ||
+//   e.clientX > wallPos.right ||
+//   e.clientY < wallPos.top ||
+//   e.clientY > wallPos.bottom
+// ) {
+//   return;
+
+// const borderLeft = wall.clientLeft;
+// const borderTop = wall.clientTop;
+
+// let left = e.clientX - wallPos.left - spider.offsetWidth / 2 - borderLeft;
+// let topPos = e.clientY - wallPos.top - spider.offsetHeight / 2 - borderTop;
+
+// const maxLeft = wall.offsetWidth - spider.offsetWidth;
+// const maxTop = wall.offsetHeight - spider.offsetHeight;
+
+// if (left < 0) {
+//   left = 0;
+// }
+
+// if (left > maxLeft) {
+//   left = maxLeft;
+// }
+
+// if (topPos < 0) {
+//   topPos = 0;
+// }
+
+// if (topPos > maxTop) {
+//   topPos = maxTop;
+// }
+// spider.style.left = left + 'px';
+// spider.style.top = topPos + 'px';
