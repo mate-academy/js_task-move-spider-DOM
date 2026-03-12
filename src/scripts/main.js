10e273d26a319111b8c49f0767d8b4037d98fa6b
Yevhen Serdiuk <yevhen.srdk@gmail.com>
2026-03-12 17:54:23 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = e.target.closest('.wall');
+
+  if (!wall) {
+    return;
+  }
+
+  const spider = wall.querySelector('.spider');
+  const rect = wall.getBoundingClientRect();
+  let x = e.clientX - rect.left - wall.clientLeft;
+  let y = e.clientY - rect.top - wall.clientTop;
+
+  const halfSpider = spider.offsetWidth / 2;
+  const wallWidth = wall.clientWidth;
+  const wallHeight = wall.clientHeight;
+
+  x = Math.max(halfSpider, Math.min(x, wallWidth - halfSpider));
+  y = Math.max(halfSpider, Math.min(y, wallHeight - halfSpider));
+
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
+
+  spider.style.transform = 'translate(-50%, -50%)';
 });
