6faebcc11e4c5801e2f6d9327bb9cb3660eaa676
kennyprog <overbafer08@gmail.com>
2026-04-08 13:39:30 +0300

    solved 4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,17 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-const spiderSize = 50;
-
 wall.addEventListener('click', (e) => {
   const wallRect = wall.getBoundingClientRect();
 
-  let x = e.clientX - wallRect.left - spiderSize / 2;
-  let y = e.clientY - wallRect.top - spiderSize / 2;
+  const sWidth = spider.offsetWidth;
+  const sHeight = spider.offsetHeight;
 
-  x = Math.max(0, Math.min(x, wallRect.width - spiderSize));
-  y = Math.max(0, Math.min(y, wallRect.height - spiderSize));
+  let x = e.clientX - wallRect.left - sWidth / 2;
+  let y = e.clientY - wallRect.top - sHeight / 2;
+
+  x = Math.max(0, Math.min(x, wallRect.width - sWidth));
+  y = Math.max(0, Math.min(y, wallRect.height - sHeight));
 
   spider.style.transform = `translate(${x}px, ${y}px)`;
 });
