b44fae81ab6e48845ba691ac09d4b449db2e779c
kennyprog <overbafer08@gmail.com>
2026-04-08 15:02:08 +0300

    solved 6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,17 +6,11 @@ const wall = document.querySelector('.wall');
 wall.addEventListener('click', (e) => {
   const wallRect = wall.getBoundingClientRect();
 
-  const sWidth = spider.offsetWidth;
-  const sHeight = spider.offsetHeight;
+  const clickXInsideWall = e.clientX - wallRect.left;
+  const clickYInsideWall = e.clientY - wallRect.top;
 
-  let x = e.clientX - wallRect.left - sWidth / 2;
-  let y = e.clientY - wallRect.top - sHeight / 2;
+  const finalX = clickXInsideWall - spider.offsetWidth / 2;
+  const finalY = clickYInsideWall - spider.offsetHeight / 2;
 
-  const maxX = wallRect.width - sWidth;
-  const maxY = wallRect.height - sHeight;
-
-  x = Math.max(0, Math.min(x, maxX));
-  y = Math.max(0, Math.min(y, maxY));
-
-  spider.style.transform = `translate(${x}px, ${y}px)`;
+  spider.style.transform = `translate(${finalX}px, ${finalY}px)`;
 });
