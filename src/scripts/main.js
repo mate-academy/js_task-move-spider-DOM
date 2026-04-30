3c27337095bbfa5666c612af57216315d6e0471b
Maksym <zubchenko.2001@gmail.com>
2026-04-30 16:35:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+
+wall.addEventListener('click', (ev) => {
+  const rect = wall.getBoundingClientRect();
+
+  let left =
+    event.clientX - rect.left - wall.clientLeft - spider.offsetWidth / 2;
+  let topp =
+    event.clientY - rect.top - wall.clientTop - spider.offsetHeight / 2;
+
+  const maxLeft = wall.clientWidth - spider.offsetWidth;
+  const maxTop = wall.clientHeight - spider.offsetHeight;
+
+  left = Math.max(0, Math.min(left, maxLeft));
+  topp = Math.max(0, Math.min(topp, maxTop));
+
+  spider.style.left = left + 'px';
+  spider.style.top = topp + 'px';
 });
