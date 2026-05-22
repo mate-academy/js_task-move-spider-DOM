4b5e1df38272127d661862a5e7a0b9b43baf308f
Vita Martsekhovska <marcehovskavita@gmail.com>
2026-05-22 14:16:34 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const ourWall = document.querySelector('.wall');
+const ourSpider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  const coorsWall = ourWall.getBoundingClientRect();
+  const clickX = e.clientX - coorsWall.left - ourWall.clientLeft;
+  const clickY = e.clientY - coorsWall.top - ourWall.clientTop;
+  const rawLeft = clickX - ourSpider.clientWidth / 2;
+  const rawTop = clickY - ourSpider.clientHeight / 2;
+  const maxLeft = ourWall.clientWidth - ourSpider.clientWidth;
+  const maxTop = ourWall.clientHeight - ourSpider.clientHeight;
+  const spiderLeft = Math.min(maxLeft, Math.max(0, rawLeft));
+  const spiderTop = Math.min(maxTop, Math.max(0, rawTop));
+
+  ourSpider.style.left = `${spiderLeft}px`;
+  ourSpider.style.top = `${spiderTop}px`;
 });
