72d2bfcc493154b8a09c2fa5d489215a6741162a
Serhii Senkiv <sergei.senkiv6@gmail.com>
2026-06-14 19:27:19 +0300

    impl

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const clamp = (value, min, max) => Math.max(min, Math.min(value, max));
+
+wall.addEventListener('click', (e) => {
+  const rect = wall.getBoundingClientRect();
+
+  const x = e.clientX - rect.left - wall.clientLeft;
+  const y = e.clientY - rect.top - wall.clientTop;
+
+  let l = x - spider.offsetWidth / 2;
+  let t = y - spider.offsetHeight / 2;
+
+  l = clamp(l, 0, wall.clientWidth - spider.offsetWidth);
+  t = clamp(t, 0, wall.clientHeight - spider.offsetHeight);
+
+  spider.style.left = `${l}px`;
+  spider.style.top = `${t}px`;
 });
