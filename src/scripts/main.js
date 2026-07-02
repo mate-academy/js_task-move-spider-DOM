4a70670f99c3fbf4edfd66d3454abcc8593b7d25
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-07-02 11:42:58 +0100

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+
+  if (e.target !== wall) {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  const localX =
+    e.clientX - spiderRect.width / 2 - wallRect.left - wall.clientLeft;
+  const localY =
+    e.clientY - spiderRect.height / 2 - wallRect.top - wall.clientTop;
+  const minTop = 0;
+  const maxTop = wall.clientHeight - spiderRect.height;
+  const minLeft = 0;
+  const maxLeft = wall.clientWidth - spiderRect.width;
+  const topPos = Math.max(minTop, Math.min(localY, maxTop));
+  const leftPos = Math.max(minLeft, Math.min(localX, maxLeft));
+
+  spider.style.top = `${topPos}px`;
+  spider.style.left = `${leftPos}px`;
 });
