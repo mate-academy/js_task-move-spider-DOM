8baa9bdbce5c0ff899cc3fe746a3820d26fc3e63
Mykyta <f1xggofficial@gmail.com>
2026-07-16 19:58:30 +0200

    mentors help

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,23 @@
-'use strict';
-
 document.addEventListener('click', (e) => {
   const wall = document.querySelector('.wall');
 
-  if (e.target.className !== 'wall') {
+  if (e.target !== wall) {
     return;
   }
 
   const spider = document.querySelector('.spider');
-  const posibleWidth = wall.clientWidth - spider.clientWidth;
-  const posibleHeight = wall.clientHeight - spider.clientHeight;
 
-  const positionX = e.offsetX - spider.clientWidth / 2;
-  const positionY = e.offsetY - spider.clientHeight / 2;
+  const possibleWidth = wall.clientWidth - spider.clientWidth;
+  const possibleHeight = wall.clientHeight - spider.clientHeight;
 
-  const spiderX = Math.max(Math.min(positionX, posibleWidth), 0);
-  const spiderY = Math.max(Math.min(positionY, posibleHeight), 0);
+  const wallPosition = wall.getBoundingClientRect();
+  const positionX =
+    e.clientX - wallPosition.left - wall.clientLeft - spider.clientWidth / 2;
+  const positionY =
+    e.clientY - wallPosition.top - wall.clientTop - spider.clientHeight / 2;
+
+  const spiderX = Math.max(0, Math.min(positionX, possibleWidth));
+  const spiderY = Math.max(0, Math.min(positionY, possibleHeight));
 
   spider.style.left = `${spiderX}px`;
   spider.style.top = `${spiderY}px`;
