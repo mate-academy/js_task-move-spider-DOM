ee1da03aa7e6f542e395caf18d2752b77d7c2c7c
Mariana <zh.yu.st22@gmail.com>
2026-07-20 22:39:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallPosition = wall.getBoundingClientRect();
+  const clickXInsideWall = e.clientX - wallPosition.left;
+  const clickYInsideWall = e.clientY - wallPosition.top;
+  const spiderX = clickXInsideWall - spider.offsetWidth / 2;
+  const spiderY = clickYInsideWall - spider.offsetHeight / 2;
+
+  const clampedX = Math.max(
+    0,
+    Math.min(spiderX, wall.offsetWidth - spider.offsetWidth),
+  );
+  const clampedY = Math.max(
+    0,
+    Math.min(spiderY, wall.offsetHeight - spider.offsetHeight),
+  );
+
+  spider.style.left = clampedX + 'px';
+  spider.style.top = clampedY + 'px';
 });
