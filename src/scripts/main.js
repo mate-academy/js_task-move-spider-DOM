cdda6854ec24b7a4a39c0d152c952ccd140c606c
darvexon <darvexon.w@proton.me>
2026-07-25 19:13:46 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (e.target !== wall) {
+    return;
+  }
+
+  const maxY = wall.clientHeight - spider.clientHeight;
+  const maxX = wall.clientWidth - spider.clientWidth;
+  const rect = wall.getBoundingClientRect();
+
+  let positionY =
+    e.clientY - wall.clientTop - rect.top - spider.clientHeight / 2;
+  let positionX =
+    e.clientX - wall.clientLeft - rect.left - spider.clientWidth / 2;
+
+  if (positionY < 0) {
+    positionY = 0;
+  } else if (positionY > maxY) {
+    positionY = maxY;
+  }
+
+  if (positionX < 0) {
+    positionX = 0;
+  } else if (positionX > maxX) {
+    positionX = maxX;
+  }
+
+  spider.style.top = `${positionY}px`;
+  spider.style.left = `${positionX}px`;
 });
