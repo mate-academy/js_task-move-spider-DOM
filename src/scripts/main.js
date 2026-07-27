0218fcc7cac3a2a8c9861eeff12ed7fb61f85328
Yehor Mishchenko <130080322+myehor@users.noreply.github.com>
2026-07-27 18:20:53 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,36 @@
 
 document.addEventListener('click', (e) => {
   // write code here
+  if (!e.target.closest('.wall')) {
+    return;
+  }
+
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+  const wallRect = wall.getBoundingClientRect();
+  const leftPosition = e.clientX - wallRect.left - wall.clientLeft;
+  const topPosition = e.clientY - wallRect.top - wall.clientTop;
+
+  spider.style.left =
+    getCenteredPosition(leftPosition, spiderWidth, wall.clientWidth) + 'px';
+
+  spider.style.top =
+    getCenteredPosition(topPosition, spiderHeight, wall.clientHeight) + 'px';
 });
+
+const getCenteredPosition = function (position, value, wallSize) {
+  const maxPosition = wallSize - value;
+  const centeredPosition = position - value / 2;
+
+  if (centeredPosition < 0) {
+    return 0;
+  }
+
+  if (centeredPosition > maxPosition) {
+    return maxPosition;
+  }
+
+  return centeredPosition;
+};
