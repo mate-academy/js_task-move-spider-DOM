aa3c61c77fe01ad3b45bb4bb5b9a67d39b07af48
Andrew <a.tupko98>
2026-08-05 08:36:26 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,30 +3,30 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-wall.addEventListener('click', (event) => {
+wall.addEventListener('click', (e) => {
   const rect = wall.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
 
   let left = x - spider.offsetWidth / 2;
-  let top = y - spider.offsetHeight / 2;
+  let topp = y - spider.offsetHeight / 2;
 
   if (left < 0) {
     left = 0;
   }
 
-  if (top < 0) {
-    top = 0;
+  if (topp < 0) {
+    topp = 0;
   }
 
   if (left > wall.offsetWidth - spider.offsetWidth) {
     left = wall.offsetWidth - spider.offsetWidth;
   }
 
-  if (top > wall.offsetHeight - spider.offsetHeight) {
-    top = wall.offsetHeight - spider.offsetHeight;
+  if (topp > wall.offsetHeight - spider.offsetHeight) {
+    topp = wall.offsetHeight - spider.offsetHeight;
   }
 
   spider.style.left = left + 'px';
-  spider.style.top = top + 'px';
+  spider.style.top = topp + 'px';
 });
