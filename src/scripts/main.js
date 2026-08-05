678580316732172cb3cfa69fb27c10658cf869f7
Nik200309 <nikitapidlypnij@gmail.com>
2026-08-05 16:21:02 +0300

    First attmpt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
   // write code here
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  let x = e.clientX - wallRect.left - wall.clientLeft - spiderRect.width / 2;
+  let y = e.clientY - wallRect.top - wall.clientTop - spiderRect.height / 2;
+
+  x = Math.max(0, Math.min(x, wallRect.width - spiderRect.width));
+  y = Math.max(0, Math.min(y, wallRect.height - spiderRect.height));
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
+
 });
