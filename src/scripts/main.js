3f5efe7c4e6a13697a5cd33168b75ee2db54192f
Olya <fialkaolya08@gmail.com>
2026-08-20 20:11:51 +0200

    Removed comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,33 +2,14 @@
 
 const wall = document.querySelector('.wall');
 
-// const borderWallTop= wall.offsetTop + wall.clientTop;
-// const borderWallLeft = wall.offsetLeft + wall.clientLeft;
-// const borderWallRight = wall.clientWidth;
-// const borderWallBottom = wall.offsetTop + wall.clientTop + wall.clientHeight;
-
-// //console.log('borderWallTop', borderWallTop);
-// //console.log('borderWallLeft', borderWallLeft);
-// console.log('borderWallRight', borderWallRight);
-// console.log('borderWallBottom', borderWallBottom);
-
 const spider = document.querySelector('.spider');
-
-// console.log('wall.offset',wall.offsetLeft, wall.offsetTop);
-// console.log(window.scrollX, window.scrollY);
-// console.log('wall.client', wall.clientLeft, wall.clientTop);
-// console.log('wall.clientWH', wall.clientWidth, wall.clientHeight);
-
-// console.log('wall', wall.getBoundingClientRect());
 
 wall.addEventListener('click', (e) => {
   const wallRect = wall.getBoundingClientRect();
 
-  let leftForSpider =
-    e.clientX - wall.clientLeft - spider.clientWidth / 2 - wallRect.left;
+  let leftForSpider = e.clientX - spider.clientWidth / 2 - wallRect.left;
 
-  let topForSpider =
-    e.clientY - wall.clientTop - spider.clientHeight / 2 - wallRect.top;
+  let topForSpider = e.clientY - spider.clientHeight / 2 - wallRect.top;
 
   const rightForSpider = leftForSpider + spider.clientWidth;
   const bottomForSpider = topForSpider + spider.clientHeight;
