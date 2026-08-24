33d6218c0a1e36631b3b04e73176aa63e83b0133
Gregory Lyon <ggylyon@gmail.com>
2026-08-24 21:43:38 +0300

    Added the solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,50 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  e.preventDefault();
+
+  const field = e.target.closest('.wall');
+
+  if (!field) {
+    return;
+  }
+
+  const spider = document.querySelector('.spider');
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  const spiderOffsetX = spiderWidth / 2;
+  const spiderOffsetY = spiderHeight / 2;
+
+  const fieldWidth = field.clientWidth;
+  const fieldHeight = field.clientHeight;
+
+  const clickX = e.clientX - field.offsetLeft - field.clientLeft;
+  const clickY = e.clientY - field.offsetTop - field.clientTop;
+
+  spider.style.top = `${clickY - spiderOffsetY}px`;
+  spider.style.left = `${clickX - spiderOffsetX}px`;
+
+  if (Number(spider.style.left.replaceAll('px', '')) < 0) {
+    spider.style.left = `0px`;
+  }
+
+  if (
+    Number(spider.style.left.replaceAll('px', '')) + spiderWidth >
+    fieldWidth
+  ) {
+    spider.style.left = `${fieldWidth - spiderWidth}px`;
+  }
+
+  if (Number(spider.style.top.replaceAll('px', '')) < 0) {
+    spider.style.top = `0px`;
+  }
+
+  if (
+    Number(spider.style.top.replaceAll('px', '')) + spiderHeight >
+    fieldHeight
+  ) {
+    spider.style.top = `${fieldHeight - spiderHeight}px`;
+  }
 });
