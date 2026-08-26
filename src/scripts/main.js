2e377e25fe06d56be9b40d0532b58ee75ad17b64
ALex <sasha.hivuk@gmail.com>
2026-08-26 11:39:31 +0300

    spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,36 @@
 
 document.addEventListener('click', (e) => {
   // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (!wall.contains(e.target)) {
+    return;
+  }
+
+  const clickX =
+    e.target === spider ? e.offsetX + spider.offsetLeft : e.offsetX;
+  const clickY = e.target === spider ? e.offsetY + spider.offsetTop : e.offsetY;
+
+  let coordX = clickX - spider.clientWidth / 2;
+  let coordY = clickY - spider.clientHeight / 2;
+
+  if (coordX < 0) {
+    coordX = 0;
+  }
+
+  if (coordX > wall.clientWidth - spider.clientWidth) {
+    coordX = wall.clientWidth - spider.clientWidth;
+  }
+
+  if (coordY < 0) {
+    coordY = 0;
+  }
+
+  if (coordY > wall.clientHeight - spider.clientHeight) {
+    coordY = wall.clientHeight - spider.clientHeight;
+  }
+
+  spider.style.left = `${coordX}px`;
+  spider.style.top = `${coordY}px`;
 });
