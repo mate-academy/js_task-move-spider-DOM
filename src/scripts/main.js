5b70ff7f18b27073515f556d6a5894b27fff68a4
Your Name <you@example.com>
2026-09-11 21:38:17 +0300

    Solution_v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const wallRect = wall.getBoundingClientRect();
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const spiderSize = spider.clientHeight / 2;
+  const border = (wall.offsetHeight - wall.clientHeight) / 2;
+
+  const verticalMove = e.clientY - wallRect.top - border - spiderSize;
+  const horizontalMove = e.clientX - wallRect.left - border - spiderSize;
+
+  spider.style.left = `${horizontalMove}px`;
+  spider.style.top = `${verticalMove}px`;
 });
